ee6c514659556d96196dbf1249cd98a6e43bcdb3
afl1 <afl2001@gmail.com>
2018-01-06 15:26:10 +0000

    kodi: reintroduce amlogic specific patches

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -35,6 +35,10 @@ get_graphicdrivers
 
 PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dbus"
 
+if [ "$PROJECT" = "Amlogic" ]; then
+  PKG_PATCH_DIRS="amlogic"
+fi
+
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libX11 libXext libdrm libXrandr"
   KODI_XORG="-DCORE_PLATFORM_NAME=x11"
