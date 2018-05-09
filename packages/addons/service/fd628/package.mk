8192b8d4b71efea708449a66de02eaa1223c61af
Arthur Liberman <arthur_liberman@hotmail.com>
2018-05-09 22:33:39 +0300

    fd628: Rework addon build setup Update copyrights.

--- packages/addons/service/fd628/package.mk
+++ packages/addons/service/fd628/package.mk
@@ -1,7 +1,6 @@
 ################################################################################
 #      This file is part of CoreELEC - http://coreelec.org
-#      Copyright (C) 2018-present Arthur Liberman (arthur_liberman@hotmail.com)
-#      Copyright (C) 2018-present Team CoreELEC
+#      Copyright (C) 2018 Arthur Liberman (arthur_liberman (at) hotmail.com)
 #
 #  CoreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,29 +17,30 @@
 ################################################################################
 
 PKG_NAME="fd628"
-PKG_VERSION="1.2"
-PKG_REV="100"
+PKG_VERSION="1.0"
+PKG_REV="120"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://coreelec.org"
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
-PKG_SHORTDESC="fd628: Kodi service to light up additional icons on devices with FD628 display"
-PKG_LONGDESC="fd628: Kodi service to light up additional icons on devices with FD628 display"
+PKG_SHORTDESC="Service for controlling FD628 VFD display icons"
+PKG_LONGDESC="Service for controlling FD628 VFD display icons, e.g. Ethernet/WiFi connection status and Time"
 PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="service.fd628"
+PKG_ADDON_NAME="FD628 Service"
 PKG_ADDON_PROJECTS="S905 S912"
 PKG_ADDON_TYPE="xbmc.service"
-
-make_target() {
-  $SED -e "s|@PKG_VERSION@|$PKG_VERSION|g" \
-       -i addon.xml
-}
+PKG_MAINTAINER="Arthur Liberman (arthur_liberman@hotmail.com)"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
-  cp -R $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID
+
+  cp $PKG_DIR/addon.xml $ADDON_BUILD/$PKG_ADDON_ID
+
+  # set only version (revision will be added by buildsystem)
+  $SED -e "s|@ADDON_VERSION@|$ADDON_VERSION|g" \
+       -i $ADDON_BUILD/$PKG_ADDON_ID/addon.xml
 }
