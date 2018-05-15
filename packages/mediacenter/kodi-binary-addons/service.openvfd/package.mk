21a1103bccfa8be1c6909f39c17d3169e89d8d00
Adam Green <greena88@gmail.com>
2018-05-15 02:05:52 +0100

    service.openvfd: introduce package

---
+++ packages/mediacenter/kodi-binary-addons/service.openvfd/package.mk
@@ -0,0 +1,37 @@
+################################################################################
+#      This file is part of CoreELEC - http://coreelec.org
+#      Copyright (C) 2018 Arthur Liberman (arthur_liberman (at) hotmail.com)
+#
+#  CoreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  CoreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with CoreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="service.openvfd"
+PKG_VERSION="db5671b"
+PKG_SHA256="6cfc55840248e5d36808413082fb4b7c6a6c332cbdfa0cc273df0dc389d15f4e"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/arthur-liberman/service.openvfd"
+PKG_URL="https://github.com/arthur-liberman/service.openvfd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="service"
+PKG_SHORTDESC="service.openvfd"
+PKG_LONGDESC="service.openvfd"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.service"
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
+  cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID
+}
