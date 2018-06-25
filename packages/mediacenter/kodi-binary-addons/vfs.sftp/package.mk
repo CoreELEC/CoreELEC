d8afae500a57f26c5c39d0fc76be56c87800418a
Adam Green <greena88@gmail.com>
2018-06-25 01:08:11 +0100

    vfs.sftp: introduce sftp vfs addon package

---
+++ packages/mediacenter/kodi-binary-addons/vfs.sftp/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of CoreELEC - https://coreelec.org
+#      Copyright (C) 2018-present CoreELEC (team (at) coreelec.org)
+#
+#  CoreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  CoreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with CoreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="vfs.sftp"
+PKG_VERSION="0f4caa1"
+PKG_SHA256="73f7cf011fe2c5524ac66a0f031b11e1daa7a174e4c52d95602d65ca7d4f46d0"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_URL="https://github.com/notspiff/vfs.sftp/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="vfs.sftp"
+PKG_LONGDESC="vfs.sftp"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.vfs"
