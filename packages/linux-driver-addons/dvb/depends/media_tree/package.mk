de3a0189d8d69d8ace01c3c96c3d36e3342527bc
afl1 <afl2001@gmail.com>
2018-10-24 01:12:27 +0200

    media_tree: revert to 2018-09-11

--- packages/linux-driver-addons/dvb/depends/media_tree/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="media_tree"
-PKG_VERSION="2018-08-08-8179de98cd35"
-PKG_SHA256="e4a2c0cddb306c22ccbd0ea997bd020bf2b9c6946b1cc9467ecffbbbafde7f54"
+PKG_VERSION="2018-09-11-985cdcb08a04"
+PKG_SHA256="309230df9ce4e72f0dcc0e4f63c69bc28c3a622c2e502f7844eaf10d7edd2843"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.linuxtv.org/media_tree.git"
 PKG_URL="http://linuxtv.org/downloads/drivers/linux-media-${PKG_VERSION}.tar.bz2"
