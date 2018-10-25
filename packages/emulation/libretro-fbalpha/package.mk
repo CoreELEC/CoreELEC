afb76dffd87ad3d0ccb111c48ecd45a1f4c941a6
Radostan Riedel <raybuntu@googlemail.com>
2018-10-25 14:45:48 +0200

    libretro-fbalpha: update to d013e28
    Fix Neon

--- packages/emulation/libretro-fbalpha/package.mk
+++ packages/emulation/libretro-fbalpha/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-fbalpha"
-PKG_VERSION="20daa807957d7fce64371620271b502811318163"
-PKG_SHA256="73bb074e85327848a05d2a6e64b2b7c6786c3db35e84eb8999d72bf663ef4f7d"
+PKG_VERSION="d013e285571a9ec7e3e0482ef4bc184de1a0c919"
+PKG_SHA256="3720f53f352fa609a34c4312651d8b254df7ce5c232a5ee6528c2289d7048c98"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/fbalpha"
 PKG_URL="https://github.com/libretro/fbalpha/archive/$PKG_VERSION.tar.gz"
@@ -17,7 +17,11 @@ PKG_LIBPATH="$PKG_LIBNAME"
 PKG_LIBVAR="FBALPHA_LIB"
 
 make_target() {
-  make -f makefile.libretro
+  if target_has_feature neon; then
+    make -f makefile.libretro HAVE_NEON=1
+  else
+    make -f makefile.libretro
+  fi
 }
 
 makeinstall_target() {
