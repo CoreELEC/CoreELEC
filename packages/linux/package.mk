c2c1d53d647e93a6fcadb96b83b05fd90228c4ca
cdu13a <cdu13a@gmail.com>
2018-10-30 14:26:54 -0400

    CoreELEC-settings: bump to 2346a04

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -24,8 +24,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-3.14)
-    PKG_VERSION="8d23b949e0d11bfdff118ac5aaac7fa5aba58ece"
-    PKG_SHA256="f2edb8a1a8bb4f61f302866ab36a409eaae389995f50a58c0d00d6b2a66b7c46"
+    PKG_VERSION="2346a04a8b2edda0bc2ec904670857aa73c36f67"
+    PKG_SHA256="f8d85b771af39284f3ec1c75fd9ef0fede273c3c92f8ee12bb1bd0123dd3399b"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
