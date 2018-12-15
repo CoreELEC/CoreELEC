b88d83f9de5c5dba23d7a89f404a7aa3195cb4e9
afl1 <afl2001@gmail.com>
2018-12-15 21:45:45 +0100

    media_tree_aml: fix build

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_aml"
-PKG_VERSION="490788fa4bd25a0b5c6b214b6141ad35404f5a05"
-PKG_SHA256="aae2e6aff7eeb6ae26b80b3e4aac1661e6ede32ea5b9ee9cd38c4cdbfb392307"
+PKG_VERSION="c0b4444da6c354bb0224f2c9af083aa231a951a8"
+PKG_SHA256="a800520ffde0de70382adfb54985b87f820d0db6edc7f9700f842fde4fef1a19"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
