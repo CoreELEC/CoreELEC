fb780057303614f52b875fdc1145ea568b2aa0f2
adamg <adamg88@users.noreply.github.com>
2018-12-30 04:01:35 +0000

    aml-vnc: cleanup package

--- packages/addons/service/aml-vnc/package.mk
+++ packages/addons/service/aml-vnc/package.mk
@@ -2,26 +2,21 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="aml-vnc"
-PKG_VERSION="0.1"
-PKG_REV="3"
+PKG_VERSION="1.0"
+PKG_SHA256=""
+PKG_REV="100"
 PKG_ARCH="arm aarch64"
-PKG_LICENSE="OSS"
+PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/kszaq/aml-vnc"
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain libvncserver"
-PKG_PRIORITY="optional"
-PKG_SECTION="service/system"
-PKG_SHORTDESC="VNC Server for Amlogic devices"
-PKG_LONGDESC="VNC Server for Amlogic devices"
-PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in libreelec forum / irc channel"
+PKG_SECTION="service"
+PKG_SHORTDESC="Amlogic VNC server"
+PKG_LONGDESC="Amlogic VNC server ($PKG_VERSION) is a Virtual Network Computing (VNC) server for Amlogic devices"
 
 PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Amlogic VNC"
 PKG_ADDON_TYPE="xbmc.service"
-PKG_ADDON_PROVIDES=""
-
-PKG_AUTORECONF="no"
-
-PKG_MAINTAINER="kszaq (kszaquitto at gmail.com)"
 
 makeinstall_target() {
   : # nop
@@ -29,6 +24,5 @@ makeinstall_target() {
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -P $PKG_BUILD/aml-vnc $ADDON_BUILD/$PKG_ADDON_ID/bin
-  debug_strip $ADDON_BUILD/$PKG_ADDON_ID/
+    cp -P $PKG_BUILD/aml-vnc $ADDON_BUILD/$PKG_ADDON_ID/bin
 }
