2fdcef2a75a9a6246fc69e368edc2c49f6096bfb
afl1 <afl2001@gmail.com>
2019-01-11 22:37:29 +0100

    media_tree_aml: align asyncfifo to *188 size

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_aml"
-PKG_VERSION="e64ee9b314759b70c5f4ee762a6e2ff2768d29a3"
-PKG_SHA256="91b17c00b2b15cb0ffe70f2a4b81beb263fc5d1653c3b06c12984a6a0c45caa3"
+PKG_VERSION="a33945a9786cdabe6230fc20ae1e9bd7ade3b541"
+PKG_SHA256="37254cd73f6ef7c9f08249754d94bd2701bfbac100d94e24d7a61facad7ae3a8"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
