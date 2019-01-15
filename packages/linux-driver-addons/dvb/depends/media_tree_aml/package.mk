c558a0f74f989d318c87ccfc4b7325cd62fdc869
afl1 <afl2001@gmail.com>
2019-01-15 11:18:40 +0100

    media_tree_aml: fix wetek dvb-t detection

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_aml"
-PKG_VERSION="45837d3e4a873b5fa384aff39cf22d9f0a8c62c6"
-PKG_SHA256="3081934d48e3e9c3983a9ee29096b3b49d01303cee86f664195d17fd60fcdd6b"
+PKG_VERSION="b9114d73bfddaed5ae8cbd9f2bc9f89a427ab999"
+PKG_SHA256="4fbe7fac032638e09811f45872f1b35b876298e01c430bb40c75f3aa48df3e8f"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
