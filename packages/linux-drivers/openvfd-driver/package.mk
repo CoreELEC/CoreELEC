32e3205cc97ca783ae35f93e1024c90ad028b8de
adamg <adamg88@users.noreply.github.com>
2019-01-15 02:05:29 +0000

    openvfd: rename package to openvfd-driver

--- packages/linux-drivers/openvfd-driver/package.mk
+++ packages/linux-drivers/openvfd-driver/package.mk
@@ -1,20 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2018 Arthur Liberman (arthur_liberman@hotmail.com)
+# Copyright (C) 2018-present Arthur Liberman (arthur_liberman@hotmail.com)
 
-PKG_NAME="openvfd"
-PKG_VERSION="d5af7690fc5148f6103964b950d1dc237ef08513"
-PKG_SHA256="3a09c8a61d3a106b8038433788c6c948abb9f4a2198e2934527984f72db395bc"
+PKG_NAME="openvfd-driver"
+PKG_VERSION="d753dd21ae9345b310111c5de43de3db4927c871"
+PKG_SHA256="ef3a0077aa5a83093d48980d998d6abf0f07cb16c95879ee7c5d80de0b40b588"
 PKG_ARCH="arm aarch64"
-PKG_LICENSE="GPL"
+PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/arthur-liberman/linux_openvfd"
 PKG_URL="https://github.com/arthur-liberman/linux_openvfd/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="linux_openvfd-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_SECTION="driver"
-PKG_SHORTDESC="openvfd: Driver for VFD displays"
-PKG_LONGDESC="openvfd: Driver for VFD displays"
-
+PKG_LONGDESC="openvfd-driver: an open source Linux driver for VFD displays"
 PKG_TOOLCHAIN="manual"
 
 pre_make_target() {
@@ -22,7 +19,9 @@ pre_make_target() {
 }
 
 make_target() {
-  kernel_make -C "$(kernel_path)" M="$PKG_BUILD/driver"
+  make ARCH=$TARGET_KERNEL_ARCH \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       -C "$(kernel_path)" M="$PKG_BUILD/driver"
 
   make OpenVFDService
 }
