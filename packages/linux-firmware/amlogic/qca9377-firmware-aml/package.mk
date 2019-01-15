eaab9ed047e811fd9c85e6696a4b84f30d2edfda
adamg <adamg88@users.noreply.github.com>
2019-01-15 02:57:26 +0000

    qca9377-firmware-aml: cleanup copied files and permissions

--- packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
+++ packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
@@ -5,14 +5,18 @@ PKG_NAME="qca9377-firmware-aml"
 PKG_VERSION="5e4b71211ecbb79e7693d2ee07361847f5a0cb40"
 PKG_SHA256="366bd14df08b4c31a653ce3e0d586854ba1e510dcab0487454e0d30bdc6ca56f"
 PKG_ARCH="arm aarch64"
-PKG_LICENSE="BSD-3c"
+PKG_LICENSE="Public Domain"
 PKG_SITE="https://github.com/boundarydevices/qca-firmware"
-PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/boundarydevices/qca-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="qca9377 Linux firmware"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
+  find $PKG_BUILD/ -type f -not -path '*/\.*' -exec chmod 644 {} \;
+
   mkdir -p $INSTALL/$(get_full_firmware_dir)
-    cp -a * $INSTALL/$(get_full_firmware_dir)
+    cp -PR qca wlan *.bin $INSTALL/$(get_full_firmware_dir)
+    cp -P LICENSE.qca_firmware $INSTALL/$(get_full_firmware_dir)
+    cp -P notice.txt $INSTALL/$(get_full_firmware_dir)/NOTICE.qca_firmware
 }
