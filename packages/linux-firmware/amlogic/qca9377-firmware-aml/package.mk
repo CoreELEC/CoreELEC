f2eee0d8b8bccaaa3eb01dac44f1a4f1950904c1
adamg88 <adamg88@users.noreply.github.com>
2019-01-24 11:44:05 +0000

    qca9377-firmware-aml: bump to d1c3403

--- packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
+++ packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
@@ -1,22 +1,19 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="qca9377-firmware-aml"
-PKG_VERSION="5e4b71211ecbb79e7693d2ee07361847f5a0cb40"
-PKG_SHA256="366bd14df08b4c31a653ce3e0d586854ba1e510dcab0487454e0d30bdc6ca56f"
+PKG_VERSION="d1c340302f986b06c8164b3064f1fb5722b90c6b"
+PKG_SHA256="6b4014885b7b8477e9c037aceac22dbe18fe0e587cde69c00f0fd6eb44d92df0"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="Public Domain"
-PKG_SITE="https://github.com/boundarydevices/qca-firmware"
-PKG_URL="https://github.com/boundarydevices/qca-firmware/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/qca-firmware"
+PKG_URL="https://github.com/CoreELEC/qca-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="qca9377 Linux firmware"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  find $PKG_BUILD/ -type f -not -path '*/\.*' -exec chmod 644 {} \;
-
   mkdir -p $INSTALL/$(get_full_firmware_dir)
-    cp -PR qca wlan *.bin $INSTALL/$(get_full_firmware_dir)
-    cp -P LICENSE.qca_firmware $INSTALL/$(get_full_firmware_dir)
-    cp -P notice.txt $INSTALL/$(get_full_firmware_dir)/NOTICE.qca_firmware
+    cp -PR * $INSTALL/$(get_full_firmware_dir)
 }
