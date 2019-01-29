ef7879b9810c01579529e41e12f54d30d62105a3
adamg88 <adamg88@users.noreply.github.com>
2019-01-29 06:43:17 +0000

    media_tree_aml: rebuild package if kernel is changed

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -4,13 +4,14 @@
 PKG_NAME="media_tree_aml"
 PKG_VERSION="2892daf9a3aedb63c3e4e80720185b74fdee5f8a"
 PKG_SHA256="1c981c2742af8ed438f9f976e80b1bf5998d6c0f921cab21b01ecbe43f42d4ed"
-PKG_ARCH="any"
 PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/CoreELEC"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="driver"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_LONGDESC="Source of Linux Kernel amlogic drivers to build with media_build."
 PKG_TOOLCHAIN="manual"
+
 
 unpack() {
   mkdir -p $PKG_BUILD/
