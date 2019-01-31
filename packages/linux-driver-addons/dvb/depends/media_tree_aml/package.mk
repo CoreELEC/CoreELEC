9b608879af124a52f5b330a0dfd1bd3d82459cd0
afl1 <afl2001@gmail.com>
2019-01-31 23:35:13 +0100

    media_tree_aml: fix demux reset

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_aml"
-PKG_VERSION="2892daf9a3aedb63c3e4e80720185b74fdee5f8a"
-PKG_SHA256="1c981c2742af8ed438f9f976e80b1bf5998d6c0f921cab21b01ecbe43f42d4ed"
+PKG_VERSION="10bbdcd4a7064037d7162609adbc59d728257994"
+PKG_SHA256="3d027b3ab59d81c2d559c86140af9bb64aba03f5327cbc7282180c0005056de8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
