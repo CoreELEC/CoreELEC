ff85f65219eae416dbb9c0a41e4abc7a58f8563a
afl1 <afl2001@gmail.com>
2019-02-09 21:34:25 +0100

    media_tree_aml: reset only single async fifo

--- packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_aml"
-PKG_VERSION="10bbdcd4a7064037d7162609adbc59d728257994"
-PKG_SHA256="3d027b3ab59d81c2d559c86140af9bb64aba03f5327cbc7282180c0005056de8"
+PKG_VERSION="999725858c21ca5cb3859613c8cc41cb12eb78f6"
+PKG_SHA256="aba88d6f661b87c47587799ddd50079305e74c2cbcc3acb59671294a06b2e580"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC"
 PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
