811707de6eb4878b3655aa89194458acd5f6d018
kszaq <kszaquitto@gmail.com>
2019-02-11 07:43:14 +0100

    u-boot_firmware: remove unused case, we build unified now

--- projects/Amlogic/packages/u-boot_firmware/package.mk
+++ projects/Amlogic/packages/u-boot_firmware/package.mk
@@ -11,13 +11,9 @@ PKG_SHORTDESC="u-boot_firmware: Required firmware Files for U-Boot"
 PKG_LONGDESC=""
 PKG_TOOLCHAIN=manual
 
-case "$DEVICE" in
-  "S905")
-    PKG_VERSION="2986172ef237858fb95bd5e4fa41ee89a660153e"
-    PKG_URL="https://github.com/CoreELEC/u-boot_firmware/archive/$PKG_VERSION.tar.gz"
-    PKG_SHA256="f22f472f6cff1707789e055a4552b6bcb57671ae7d89044a66fdc4a5803f5a08"
-    ;;
-esac
+PKG_VERSION="2986172ef237858fb95bd5e4fa41ee89a660153e"
+PKG_URL="https://github.com/CoreELEC/u-boot_firmware/archive/$PKG_VERSION.tar.gz"
+PKG_SHA256="f22f472f6cff1707789e055a4552b6bcb57671ae7d89044a66fdc4a5803f5a08"
 
 makeinstall_target() {
   :
