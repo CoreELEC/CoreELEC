ebbdd8ff99aec26fd8462b7e99ef9447b8b2a03b
Ray <1458889+Raybuntu@users.noreply.github.com>
2019-02-14 14:53:17 +0100

    opengl-meson: Add more symlinks for EGL and GLES

--- packages/graphics/opengl-meson/package.mk
+++ packages/graphics/opengl-meson/package.mk
@@ -6,8 +6,8 @@ PKG_NAME="opengl-meson"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/filesystem/"
-PKG_VERSION="8-r5p1-01rel0-armhf"
-PKG_SHA256="b2ad356f0f8c06c8bca077fe2dd5568b83e1879d32bea20c551ab1bf72402c29"
+PKG_VERSION="8-r7p0-01rel0-armhf"
+PKG_SHA256="d79526d849c2cc0c513335b7f78b9ea568c04d241af3a8c4636298a3ae4c9211"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="OpenGL ES pre-compiled libraries for Mali GPUs found in Amlogic Meson SoCs."
@@ -19,12 +19,16 @@ makeinstall_target() {
 
    ln -sf /var/lib/libEGL.so $INSTALL/usr/lib/libEGL.so
    ln -sf /usr/lib/libEGL.so $INSTALL/usr/lib/libEGL.so.1
+   ln -sf /usr/lib/libEGL.so $INSTALL/usr/lib/libEGL.so.1.0.0
    ln -sf /var/lib/libGLESv1_CM.so $INSTALL/usr/lib/libGLESv1_CM.so
    ln -sf /usr/lib/libGLESv1_CM.so $INSTALL/usr/lib/libGLESv1_CM.so.1
+   ln -sf /usr/lib/libGLESv1_CM.so $INSTALL/usr/lib/libGLESv1_CM.so.1.0.1
    ln -sf /var/lib/libGLESv2.so $INSTALL/usr/lib/libGLESv2.so
    ln -sf /usr/lib/libGLESv2.so $INSTALL/usr/lib/libGLESv2.so.2
+   ln -sf /usr/lib/libGLESv2.so $INSTALL/usr/lib/libGLESv2.so.2.0.0
    ln -sf /var/lib/libGLESv3.so $INSTALL/usr/lib/libGLESv3.so
    ln -sf /usr/lib/libGLESv3.so $INSTALL/usr/lib/libGLESv3.so.3
+   ln -sf /usr/lib/libGLESv3.so $INSTALL/usr/lib/libGLESv3.so.3.0.0
 
   mkdir -p $INSTALL/usr/sbin
     cp $PKG_DIR/scripts/libmali-overlay-setup $INSTALL/usr/sbin
