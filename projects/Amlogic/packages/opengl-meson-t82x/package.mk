b80a0db4ed8adc9ad3f5cf22186b2a9ca0707658
Dmitry_L <freebox_2002@mail.ru>
2019-03-06 13:40:38 +0300

    opengl-meson-t82x: bump package to d3e7cfe
    update to v3.2 Android Open GLES binaries

--- projects/Amlogic/packages/opengl-meson-t82x/package.mk
+++ projects/Amlogic/packages/opengl-meson-t82x/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2017-2019 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="opengl-meson-t82x"
-PKG_VERSION="915cb48"
-PKG_SHA256="9b5f65afa21250b67578c250da030a5829e69131ce91b2f167b01b1ed30be781"
+PKG_VERSION="d3e7cfed0c2b92640d874bb8c8c16d7ed832fdd4"
+PKG_SHA256="b4c9b2319da9b1d4aec4e401ee2bf7853303d75df4a192da7bd1e18353ec1334"
 PKG_LICENSE="nonfree"
-PKG_SITE="https://github.com/kszaq/opengl-meson-t82x"
-PKG_URL="https://github.com/kszaq/opengl-meson-t82x/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/opengl-meson-t82x"
+PKG_URL="https://github.com/CoreELEC/opengl-meson-t82x/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libhybris opengl-meson"
 PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
 PKG_LONGDESC="OpenGL ES pre-compiled libraries for Mali GPUs. The libraries were extracted from Khadas VIM2 Android firmware."
