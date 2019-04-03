353082c7691d28b47133caa2d18be6d9c24150d7
adamg <adamg88@users.noreply.github.com>
2019-04-03 03:04:57 +0100

    CoreELEC-Debug-Scripts: bump package to 7dc3073

--- projects/Amlogic/packages/CoreELEC-Debug-Scripts/package.mk
+++ projects/Amlogic/packages/CoreELEC-Debug-Scripts/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="CoreELEC-Debug-Scripts"
-PKG_VERSION="f4b513411e2b7570fb5b2a3d73f4b1afa7874ebb"
-PKG_SHA256="104c58309ead5e4ebf68f6144593a5bf0a0dc6b95ff26b1ec7840cc291578492"
+PKG_VERSION="7dc30732b8fbfd9685f8f340c04665d57ecb0711"
+PKG_SHA256="db9eb1269a9aa10127ad26bd62e16cc10147aabb0a100554c65f28e5cf7b816c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/cdu13a/CoreELEC-Debug-Scripts"
 PKG_URL="https://github.com/cdu13a/CoreELEC-Debug-Scripts/archive/$PKG_VERSION.tar.gz"
