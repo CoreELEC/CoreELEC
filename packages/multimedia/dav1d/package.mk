c88409e57ab9cec06064aa7daa9a3ada4e3446eb
cdu13a <cdu13a@gmail.com>
2019-05-01 13:46:20 -0400

    dav1d: bump to 0.3.0

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="0e3c21979b5e2e6e7e49968b1377e38220c58774"
-PKG_SHA256="3662184809fa29af39c920877cedec3724dc5c3cf71d94bec424f2a7ee4825cf"
+PKG_VERSION="0.3.0"
+PKG_SHA256="22946ddd8c5122d3490b250933328c3278da21b0659db9b7fa8f875e4735c2e6"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.jbkempf.com/blog/post/2018/Introducing-dav1d"
 PKG_URL="https://code.videolan.org/videolan/dav1d/-/archive/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.bz2"
