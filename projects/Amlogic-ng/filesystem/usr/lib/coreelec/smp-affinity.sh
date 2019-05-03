78b9889414a12dde26365c8caba19b82f74dcc62
Ray <1458889+rayelec@users.noreply.github.com>
2019-05-03 19:00:30 +0200

    Amlogic-ng: Add script to set smp-affinity on boot

---
+++ projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar
+#  14 rue de Plaisance, 75014 Paris, France
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+echo 2 /proc/irq/70/smp_affinity
+echo 4 /proc/irq/22/smp_affinity
+echo 8 /proc/irq/41/smp_affinity
+echo 10 /proc/irq/21/smp_affinity
+
+exit 0
