68f5fe57d5d5bdcf9cc7e370d1a86e31fe7d663c
Ray <1458889+rayelec@users.noreply.github.com>
2019-05-05 15:48:55 +0200

    smp-affinity: Forgot the ">"
    Doooh

--- projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
+++ projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -14,9 +14,9 @@
 #
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
-echo 2 /proc/irq/70/smp_affinity
-echo 4 /proc/irq/22/smp_affinity
-echo 8 /proc/irq/41/smp_affinity
-echo 10 /proc/irq/21/smp_affinity
+echo 2 > /proc/irq/70/smp_affinity
+echo 4 > /proc/irq/22/smp_affinity
+echo 8 > /proc/irq/41/smp_affinity
+echo 10 > /proc/irq/21/smp_affinity
 
 exit 0
