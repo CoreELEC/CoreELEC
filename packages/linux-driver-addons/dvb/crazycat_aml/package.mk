7feec74f34077269bf2bfe88e73b730b3dc0d37b
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:54 +0100

    linux-driver-addons: introduce CoreELEC changes

--- packages/linux-driver-addons/dvb/crazycat_aml/package.mk
+++ packages/linux-driver-addons/dvb/crazycat_aml/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-2018 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="crazycat_aml"
 PKG_VERSION="835dc72da3ee63df7f4057bd0507887454c005d1"
@@ -8,17 +9,26 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://bitbucket.org/CrazyCat/media_build"
 PKG_URL="https://bitbucket.org/CrazyCat/media_build/get/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux media_tree_cc_aml"
-PKG_NEED_UNPACK="$LINUX_DEPENDS media_tree_cc_aml"
+PKG_DEPENDS_TARGET="toolchain linux media_tree_cc_aml media_tree_aml"
+PKG_NEED_UNPACK="$LINUX_DEPENDS media_tree_cc_aml media_tree_aml"
 PKG_SECTION="driver.dvb"
-PKG_LONGDESC="DVB drivers from the latest kernel"
+PKG_LONGDESC="DVB driver for TBS cards with CrazyCats additions"
 
 PKG_IS_ADDON="embedded"
 PKG_IS_KERNEL_PKG="yes"
 PKG_ADDON_IS_STANDALONE="yes"
-PKG_ADDON_NAME="DVB drivers from the latest kernel"
+PKG_ADDON_NAME="DVB drivers for TBS"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
+
+case "$LINUX" in
+  amlogic-3.14)
+    PKG_PATCH_DIRS="amlogic-3.14"
+    ;;
+  amlogic-4.9)
+    PKG_PATCH_DIRS="amlogic-4.9"
+    ;;
+esac
 
 pre_make_target() {
   export KERNEL_VER=$(get_module_dir)
@@ -27,10 +37,15 @@ pre_make_target() {
 
 make_target() {
   cp -RP $(get_build_dir media_tree_cc_aml)/* $PKG_BUILD/linux
+  rm  -rf $PKG_BUILD/linux/drivers/media/platform/meson/wetek
+  rm  -rf $PKG_BUILD/linux/drivers/media/platform/meson/dvb-avl
+  cp -Lr $(get_build_dir media_tree_aml)/* $PKG_BUILD/linux
 
   # compile modules
   echo "obj-y += video_dev/" >> "$PKG_BUILD/linux/drivers/media/platform/meson/Makefile"
-  echo "obj-y += wetek/" >> "$PKG_BUILD/linux/drivers/media/platform/meson/Makefile"
+  echo "obj-y += dvb/" >> "$PKG_BUILD/linux/drivers/media/platform/meson/Makefile"
+  echo 'source "drivers/media/platform/meson/dvb/Kconfig"' >>  "$PKG_BUILD/linux/drivers/media/platform/Kconfig"
+  sed -e 's/ && RC_CORE//g' -i $PKG_BUILD/linux/drivers/media/usb/dvb-usb/Kconfig
 
   # make config all
   kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path) allyesconfig
@@ -39,9 +54,14 @@ make_target() {
   sed '/CONFIG_VIDEO_S5C73M3=m/d' -i $PKG_BUILD/v4l/.config
 
   # enable AML drivers
-  echo "CONFIG_IR_MESON=m" >> $PKG_BUILD/v4l/.config
   echo "CONFIG_V4L_AMLOGIC_VIDEO=m" >> $PKG_BUILD/v4l/.config
   echo "CONFIG_VIDEOBUF_RESOURCE=m" >> $PKG_BUILD/v4l/.config
+
+  # hack to workaround media_build bug
+  sed -e 's/CONFIG_VIDEO_SAA7146_VV=m/# CONFIG_VIDEO_SAA7146_VV is not set/g' -i $PKG_BUILD/v4l/.config
+  sed -e 's/CONFIG_VIDEO_S5K5BAF=m/# CONFIG_VIDEO_S5K5BAF is not set/g' -i $PKG_BUILD/v4l/.config
+  sed -e 's/CONFIG_VIDEO_TVP514X=m/# CONFIG_VIDEO_TVP514X is not set/g' -i $PKG_BUILD/v4l/.config
+  sed -e 's/CONFIG_VIDEO_TVP7002=m/# CONFIG_VIDEO_TVP7002 is not set/g' -i $PKG_BUILD/v4l/.config
 
   kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path)
 }
--- packages/linux-driver-addons/dvb/depends/media_tree_cc_aml/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_cc_aml/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2017-2018 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_tree_cc_aml"
 PKG_VERSION="2018-09-23"
@@ -7,20 +8,29 @@ PKG_SHA256="3b0cf3699317c04d9184b7e25056065fd374b20b851ee86a63ea2c70e219ee9e"
 PKG_LICENSE="GPL"
 PKG_SITE="https://bitbucket.org/CrazyCat/media_build/downloads/"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_DEPENDS_TARGET="toolchain media_tree_aml"
+PKG_NEED_UNPACK="$LINUX_DEPENDS media_tree_aml"
 PKG_LONGDESC="Source of Linux Kernel media_tree subsystem to build with media_build."
 PKG_TOOLCHAIN="manual"
 
 unpack() {
   mkdir -p $PKG_BUILD/
   tar -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz -C $PKG_BUILD/
+}
 
+post_unpack() {
   # hack/workaround for borked upstream kernel/media_build
-  # without removing atomisp there a lot additional includes that 
+  # without removing atomisp there a lot additional includes that
   # slowdown build process after modpost from 3min to 6min
   # even if atomisp is disabled via kernel.conf
   rm -rf $PKG_BUILD/drivers/staging/media/atomisp
   sed -i 's|^.*drivers/staging/media/atomisp.*$||' \
     $PKG_BUILD/drivers/staging/media/Kconfig
+}
+
+configure() {
+  rm -rf $PKG_BUILD/drivers/media/platform/meson/dvb
+  cp -Lr $(get_build_dir media_tree_aml)/* $PKG_BUILD/
+  echo 'source "drivers/media/platform/meson/dvb/Kconfig"' >>  "$PKG_BUILD/drivers/media/platform/Kconfig"
+  echo 'source "drivers/media/platform/meson/video_dev/Kconfig"' >>  "$PKG_BUILD/drivers/media/platform/Kconfig"
 }
