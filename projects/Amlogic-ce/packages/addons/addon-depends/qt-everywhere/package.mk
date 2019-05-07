e9d4989d900173c9ed151ad54a955e42cf551ff9
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:49 +0100

    addons: introduce CoreELEC changes

---
+++ projects/Amlogic-ce/packages/addons/addon-depends/qt-everywhere/package.mk
@@ -0,0 +1,118 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="qt-everywhere"
+PKG_VERSION="5.6.3"
+PKG_SHA256="2fa0cf2e5e8841b29a4be62062c1a65c4f6f2cf1beaf61a5fd661f520cd776d0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://qt-project.org"
+PKG_URL="http://download.qt.io/archive/qt/5.6/$PKG_VERSION/single/$PKG_NAME-opensource-src-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="pcre zlib"
+PKG_SOURCE_DIR="$PKG_NAME-opensource-src-$PKG_VERSION"
+PKG_LONGDESC="A cross-platform application and UI framework"
+
+PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
+                           -sysroot "${SYSROOT_PREFIX}"
+                           -hostprefix "${TOOLCHAIN}"
+                           -device linux-libreelec-g++
+                           -opensource -confirm-license
+                           -release
+                           -static
+                           -make libs
+                           -force-pkg-config
+                           -no-accessibility
+                           -no-sql-sqlite
+                           -no-sql-mysql
+                           -no-qml-debug
+                           -system-zlib
+                           -no-mtdev
+                           -no-gif
+                           -no-libpng
+                           -no-libjpeg
+                           -no-harfbuzz
+                           -no-libproxy
+                           -system-pcre
+                           -no-glib
+                           -silent
+                           -no-cups
+                           -no-iconv
+                           -no-evdev
+                           -no-tslib
+                           -no-icu
+                           -no-strip
+                           -no-fontconfig
+                           -no-dbus
+                           -no-opengl
+                           -no-libudev
+                           -no-libinput
+                           -no-eglfs
+                           -skip qt3d
+                           -skip qtactiveqt
+                           -skip qtandroidextras
+                           -skip qtcanvas3d
+                           -skip qtconnectivity
+                           -skip qtdeclarative
+                           -skip qtdoc
+                           -skip qtgraphicaleffects
+                           -skip qtimageformats
+                           -skip qtlocation
+                           -skip qtmacextras
+                           -skip qtmultimedia
+                           -skip qtquickcontrols
+                           -skip qtquickcontrols2
+                           -skip qtscript
+                           -skip qtsensors
+                           -skip qtserialbus
+                           -skip qtsvg
+                           -skip qttranslations
+                           -skip qtwayland
+                           -skip qtwebchannel
+                           -skip qtwebengine
+                           -skip qtwebsockets
+                           -skip qtwebview
+                           -skip qtwinextras
+                           -skip qtx11extras
+                           -skip qtxmlpatterns"
+
+configure_target() {
+  QMAKE_CONF_DIR="qtbase/mkspecs/devices/linux-libreelec-g++"
+
+  cd ..
+  mkdir -p ${QMAKE_CONF_DIR}
+
+  cat >"${QMAKE_CONF_DIR}/qmake.conf" <<EOF
+MAKEFILE_GENERATOR      = UNIX
+CONFIG                 += incremental
+QMAKE_INCREMENTAL_STYLE = sublib
+include(../../common/linux.conf)
+include(../../common/gcc-base-unix.conf)
+include(../../common/g++-unix.conf)
+load(device_config)
+QMAKE_CC         = ${CC}
+QMAKE_CXX        = ${CXX}
+QMAKE_LINK       = ${CXX}
+QMAKE_LINK_SHLIB = ${CXX}
+QMAKE_AR         = ${AR} cqs
+QMAKE_OBJCOPY    = ${OBJCOPY}
+QMAKE_NM         = ${NM} -P
+QMAKE_STRIP      = ${STRIP}
+QMAKE_CFLAGS     = ${CFLAGS}
+QMAKE_CXXFLAGS   = ${CXXFLAGS}
+QMAKE_LFLAGS     = ${LDFLAGS}
+load(qt_config)
+EOF
+
+  cat >"${QMAKE_CONF_DIR}/qplatformdefs.h" <<EOF
+#include "../../linux-g++/qplatformdefs.h"
+EOF
+
+  unset CC CXX LD RANLIB AR AS CPPFLAGS CFLAGS LDFLAGS CXXFLAGS
+  ./configure ${PKG_CONFIGURE_OPTS_TARGET}
+}
+
+post_makeinstall_target() {
+  # Qt installs directly to $SYSROOT_PREFIX so don't rely on scripts/build fixing this up
+  # PKG_ORIG_SYSROOT_PREFIX will be undefined when performing a legacy build
+  sed -e "s:\(['= ]\)/usr:\\1${PKG_ORIG_SYSROOT_PREFIX:-${SYSROOT_PREFIX}}/usr:g" -i "${PKG_ORIG_SYSROOT_PREFIX:-${SYSROOT_PREFIX}}/usr/lib"/libQt*.la
+}
