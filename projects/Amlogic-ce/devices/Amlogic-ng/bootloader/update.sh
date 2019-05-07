8fa4030c6dc0a8b62dbdf5a1f61ad8024c4c9324
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:44 +0100

    Amlogic-ng: introduce new project

---
+++ projects/Amlogic-ce/devices/Amlogic-ng/bootloader/update.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+[ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
+[ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
+[ -z "$BOOT_PART" ] && BOOT_PART=$(df "$BOOT_ROOT" | tail -1 | awk {' print $1 '})
+if [ -z "$BOOT_DISK" ]; then
+  case $BOOT_PART in
+    /dev/sd[a-z][0-9]*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,[0-9]*,,g")
+      ;;
+    /dev/mmcblk*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,p[0-9]*,,g")
+      ;;
+  esac
+fi
+
+mount -o rw,remount $BOOT_ROOT
+
+SUBDEVICE=""
+
+for arg in $(cat /proc/cmdline); do
+  case $arg in
+    boot=*)
+      boot="${arg#*=}"
+      case $boot in
+        /dev/mmc*)
+          BOOT_UUID="$(blkid $boot | sed 's/.* UUID="//;s/".*//g')"
+          ;;
+        UUID=*|LABEL=*)
+          BOOT_UUID="$(blkid | sed 's/"//g' | grep -m 1 -i " $boot " | sed 's/.* UUID=//;s/ .*//g')"
+          ;;
+      esac
+
+      if [ -f "/proc/device-tree/coreelec-dt-id" ]; then
+        DT_ID=$(cat /proc/device-tree/coreelec-dt-id)
+      fi
+
+      if [ -n "$DT_ID" ]; then
+        case $DT_ID in
+          *odroid_n2)
+            SUBDEVICE="Odroid_N2"
+            ;;
+        esac
+      fi
+
+      if [ -n "$DT_ID" -a -f "$SYSTEM_ROOT/usr/share/bootloader/device_trees/$DT_ID.dtb" ]; then
+        UPDATE_DTB_SOURCE="$SYSTEM_ROOT/usr/share/bootloader/device_trees/$DT_ID.dtb"
+      fi
+
+      if [ -f "$UPDATE_DTB_SOURCE" ]; then
+        echo "Updating device tree from $UPDATE_DTB_SOURCE..."
+        case $boot in
+          /dev/system)
+            dd if=/dev/zero of=/dev/dtb bs=256k count=1 status=none
+            dd if="$UPDATE_DTB_SOURCE" of=/dev/dtb bs=256k status=none
+            ;;
+          /dev/mmc*|LABEL=*|UUID=*)
+            cp -f "$UPDATE_DTB_SOURCE" "$BOOT_ROOT/dtb.img"
+            ;;
+        esac
+      fi
+
+      for all_dtb in /flash/*.dtb ; do
+        if [ -f $all_dtb ]; then
+          dtb=$(basename $all_dtb)
+          if [ -f $SYSTEM_ROOT/usr/share/bootloader/$dtb ]; then
+            echo "Updating $dtb..."
+            cp -p $SYSTEM_ROOT/usr/share/bootloader/$dtb $BOOT_ROOT
+          fi
+        fi
+      done
+      ;;
+
+    disk=*)
+      disk="${arg#*=}"
+      case $disk in
+        /dev/mmc*)
+          DISK_UUID="$(blkid $disk | sed 's/.* UUID="//;s/".*//g')"
+          ;;
+        UUID=*|LABEL=*)
+          DISK_UUID="$(blkid | sed 's/"//g' | grep -m 1 -i " $disk " | sed 's/.* UUID=//;s/ .*//g')"
+          ;;
+      esac
+      ;;
+  esac
+done
+
+if [ -d $BOOT_ROOT/device_trees ]; then
+  rm $BOOT_ROOT/device_trees/*.dtb
+  cp -p $SYSTEM_ROOT/usr/share/bootloader/device_trees/*.dtb $BOOT_ROOT/device_trees/
+fi
+
+if [ -f $SYSTEM_ROOT/usr/share/bootloader/${SUBDEVICE}_boot.ini ]; then
+  echo "Updating boot.ini..."
+  cp -p $SYSTEM_ROOT/usr/share/bootloader/${SUBDEVICE}_boot.ini $BOOT_ROOT/boot.ini
+  sed -e "s/@BOOT_UUID@/$BOOT_UUID/" \
+      -e "s/@DISK_UUID@/$DISK_UUID/" \
+      -i $BOOT_ROOT/boot.ini
+
+  if [ -f $SYSTEM_ROOT/usr/share/bootloader/config.ini ]; then
+    if [ ! -f $BOOT_ROOT/config.ini ]; then
+      echo "Creating config.ini..."
+      cp -p $SYSTEM_ROOT/usr/share/bootloader/config.ini $BOOT_ROOT/config.ini
+    fi
+  fi
+fi
+
+if [ "${SUBDEVICE}" == "Odroid_N2" ]; then
+  if [ -f $SYSTEM_ROOT/usr/share/bootloader/boot-logo-1080.bmp.gz ]; then
+    echo "Updating boot logos..."
+    cp -p $SYSTEM_ROOT/usr/share/bootloader/boot-logo-1080.bmp.gz $BOOT_ROOT
+  fi
+  if [ -f $SYSTEM_ROOT/usr/share/bootloader/timeout-logo-1080.bmp.gz ]; then
+    cp -p $SYSTEM_ROOT/usr/share/bootloader/timeout-logo-1080.bmp.gz $BOOT_ROOT
+  fi
+fi
+
+if [ -f $SYSTEM_ROOT/usr/share/bootloader/${SUBDEVICE}_u-boot -a ! -e /dev/system -a ! -e /dev/boot ]; then
+  echo "Updating u-boot on: $BOOT_DISK..."
+  dd if=$SYSTEM_ROOT/usr/share/bootloader/${SUBDEVICE}_u-boot of=$BOOT_DISK conv=fsync bs=1 count=112 status=none
+  dd if=$SYSTEM_ROOT/usr/share/bootloader/${SUBDEVICE}_u-boot of=$BOOT_DISK conv=fsync bs=512 skip=1 seek=1 status=none
+fi
+
+mount -o ro,remount $BOOT_ROOT
+
+if [ -e "/proc/device-tree/meson-remote/compatible" ]; then
+  echo "Executing remote-toggle..."
+  $SYSTEM_ROOT/usr/lib/coreelec/remote-toggle
+fi
---
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/opengl-meson/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="opengl-meson"
+PKG_VERSION="7bddce621a0c1e0cc12cfc8b707e93eb37fc0f82"
+PKG_SHA256="15400e78b918b15743b815c195be472899d4243143e405a7b50d5be1cd07ffd1"
+PKG_LICENSE="nonfree"
+PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/filesystem/"
+PKG_URL="https://github.com/CoreELEC/opengl-meson/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="OpenGL ES pre-compiled libraries for Mali GPUs found in Amlogic Meson SoCs."
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+    cp -p lib/eabihf/gondul/r12p0/fbdev/libMali.so $INSTALL/usr/lib
+
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libmali.so
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libmali.so.0
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libEGL.so
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libEGL.so.1
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libEGL.so.1.0.0
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLES_CM.so.1
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv1_CM.so
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv1_CM.so.1
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv1_CM.so.1.0.1
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv1_CM.so.1.1
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv2.so
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv2.so.2
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv2.so.2.0
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv2.so.2.0.0
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv3.so
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv3.so.3
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv3.so.3.0
+   ln -sf /usr/lib/libMali.so $INSTALL/usr/lib/libGLESv3.so.3.0.0
+}
