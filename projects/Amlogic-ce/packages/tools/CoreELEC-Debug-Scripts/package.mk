6010b24b48477f49c0b199054810762c16bc8983
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:58 +0100

    tools: introduce CoreELEC changes

---
+++ projects/Amlogic-ce/packages/tools/CoreELEC-Debug-Scripts/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="CoreELEC-Debug-Scripts"
+PKG_VERSION="7fce9b9492d5c12bbf129917b167574c7a113a18"
+PKG_SHA256="95ba4cfd4895844f574a28c664f45fd11528081724086e197f5fb385cdf4a4a4"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/cdu13a/CoreELEC-Debug-Scripts"
+PKG_URL="https://github.com/cdu13a/CoreELEC-Debug-Scripts/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_NAME="$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_LONGDESC="A set of scripts to help debug user issues with CoreELEC"
+PKG_TOOLCHAIN="manual"
+
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+    install -m 0755 dispinfo.sh $INSTALL/usr/bin/dispinfo
+    install -m 0755 remoteinfo.sh $INSTALL/usr/bin/remoteinfo
+    install -m 0755 audinfo.sh $INSTALL/usr/bin/audinfo
+    install -m 0755 ce-debug.sh $INSTALL/usr/bin/ce-debug
+}
---
+++ projects/Amlogic-ce/packages/tools/entware/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="entware"
+PKG_VERSION=""
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/Entware/Entware"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_LONGDESC="entware: A software repository that offers various software programs that can be installed on your device"
+PKG_TOOLCHAIN="manual"
+
+post_install() {
+  mkdir -p $INSTALL/usr/sbin
+    cp -P $PKG_DIR/scripts/installentware $INSTALL/usr/sbin
+
+    # Replace Entware Arch
+    sed -e "s/@ENTWARE_ARCH@/$ENTWARE_ARCH/g" \
+        -i $INSTALL/usr/sbin/installentware
+
+  enable_service entware.service
+}
