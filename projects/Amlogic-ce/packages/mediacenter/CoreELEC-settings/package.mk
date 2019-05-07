90a40c11c687938e632900cc3c5a76365774d6dc
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:48 +0100

    mediacenter: introduce CoreELEC changes

---
+++ projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="CoreELEC-settings"
+PKG_VERSION="5f7b66d01ad5e63b23dcc77f981f4c6ee09d85ad"
+PKG_SHA256="6190efa2433360426da7e1b8bc32069a8aa07f84a5fef91aeefde490c9955bbb"
+PKG_LICENSE="GPL"
+PKG_SITE="https://coreelec.org"
+PKG_URL="https://github.com/CoreELEC/service.coreelec.settings/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python2 connman pygobject dbus-python"
+PKG_LONGDESC="CoreELEC-settings: is a settings dialog for CoreELEC"
+
+PKG_MAKE_OPTS_TARGET="DISTRONAME=$DISTRONAME ROOT_PASSWORD=$ROOT_PASSWORD"
+
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET setxkbmap"
+else
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bkeymaps"
+fi
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/coreelec
+    cp $PKG_DIR/scripts/* $INSTALL/usr/lib/coreelec
+
+  ADDON_INSTALL_DIR=$INSTALL/usr/share/kodi/addons/service.coreelec.settings
+
+  $TOOLCHAIN/bin/python -Wi -t -B $TOOLCHAIN/lib/$PKG_PYTHON_VERSION/compileall.py $ADDON_INSTALL_DIR/resources/lib/ -f
+  rm -rf $(find $ADDON_INSTALL_DIR/resources/lib/ -name "*.py")
+
+  $TOOLCHAIN/bin/python -Wi -t -B $TOOLCHAIN/lib/$PKG_PYTHON_VERSION/compileall.py $ADDON_INSTALL_DIR/oe.py -f
+  rm -rf $ADDON_INSTALL_DIR/oe.py
+}
+
+post_install() {
+  enable_service backup-restore.service
+  enable_service factory-reset.service
+}
