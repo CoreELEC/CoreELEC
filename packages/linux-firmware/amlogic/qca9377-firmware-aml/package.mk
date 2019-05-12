dd51c3e0809464aa9ad81cd8e49df3c660761f26
adamg <adamg88@users.noreply.github.com>
2019-05-12 01:44:38 +0100

    qca9377-firmware-aml: revert package changes as some older devices have been unable to shutdown since the recent changes, this revert comes at the expense of bluetooth support for some newer devices

--- packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
+++ packages/linux-firmware/amlogic/qca9377-firmware-aml/package.mk
@@ -1,19 +1,18 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
-# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="qca9377-firmware-aml"
-PKG_VERSION="d1c340302f986b06c8164b3064f1fb5722b90c6b"
-PKG_SHA256="6b4014885b7b8477e9c037aceac22dbe18fe0e587cde69c00f0fd6eb44d92df0"
+PKG_VERSION="1.0.0-3"
+PKG_SHA256="9a9f214943e77e89ce8fc8c0dc5b41bc253478a9d92383a76590993df861f36d"
 PKG_ARCH="arm aarch64"
-PKG_LICENSE="Public Domain"
-PKG_SITE="https://github.com/CoreELEC/qca-firmware"
-PKG_URL="https://github.com/CoreELEC/qca-firmware/archive/$PKG_VERSION.tar.gz"
+PKG_LICENSE="BSD-3c"
+PKG_SITE="http://linode.boundarydevices.com/repos/apt/ubuntu-relx/pool/main/q/qca-firmware/"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="qca9377 Linux firmware"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p $INSTALL/$(get_full_firmware_dir)
-    cp -PR * $INSTALL/$(get_full_firmware_dir)
+    cp -a * $INSTALL/$(get_full_firmware_dir)
 }
