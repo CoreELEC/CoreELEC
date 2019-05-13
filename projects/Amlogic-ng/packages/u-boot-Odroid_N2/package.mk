1512aacb8f76aab5f5e5600f8d7111a3b3e5caa4
Portisch <hugo.portisch@yahoo.de>
2019-05-13 09:54:56 +0000

    u-boot-Odroid_N2: update to 9cc9a47

--- projects/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
+++ projects/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Odroid_N2"
-PKG_VERSION="2df26d5892116060be9739f3b2bdb16fcfee81a5"
-PKG_SHA256="83c24b74514c4e76e074b7766500570850da8e68b74eb274a901eb990a3212bb"
+PKG_VERSION="9cc9a47af1f138790c8ed32e7688d5653ea552e5"
+PKG_SHA256="e4b6275fec206d13565147dc3d56f34df9ff0e9c23dc7d2ace804b330408103c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
