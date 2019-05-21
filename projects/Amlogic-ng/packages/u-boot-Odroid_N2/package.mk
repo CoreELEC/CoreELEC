6288684d23c14a825e13a1b86bfdb743c2bcd3d0
Portisch <hugo.portisch@yahoo.de>
2019-05-21 13:21:10 +0000

    u-boot-Odroid_N2: bump to 2687e367

--- projects/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
+++ projects/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Odroid_N2"
-PKG_VERSION="7d2b63a1d503a234e99346b02156545dfb897193"
-PKG_SHA256="b3a9dd36f1bbbee50cb9266505abda7e3f35ab78a72aac91a89185d7fa943d56"
+PKG_VERSION="2687e367fa992a3eb116a307408d00afb62bc4fd"
+PKG_SHA256="b96e34633cb0983789c8c8f8afbebfec1356904b507107ab5063efb5d15d2419"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
