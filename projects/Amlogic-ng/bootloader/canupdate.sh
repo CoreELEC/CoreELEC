87489cb5dea18196bfbdc2025cfed6ede5b210c7
adamg <adamg88@users.noreply.github.com>
2019-05-29 04:28:23 +0100

    Amlogic-ng: properly detect and deny attempted upgrades from 3.14 and allow upgrading between arm and aarch64 builds

--- projects/Amlogic-ng/bootloader/canupdate.sh
+++ projects/Amlogic-ng/bootloader/canupdate.sh
@@ -1,11 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.tv)
 
-case $1 in
-   @PROJECT@)
-       exit 0
-    ;;
-   *)
-       exit 1
-    ;;
-esac
+# detect legacy kernel installs and abort to prevent upgrades
+if [ "$(uname -r)" = "3.14.29" ]; then
+  echo "Update from 3.14 is not supported!"
+  sleep 10
+  exit 1
+fi
+
+# allow upgrades between aarch64 and arm images
+PROJECT=$("$1" | cut -d. -f1)
+if [ "$1" = "${PROJECT}.aarch64" -o "$1" = "${PROJECT}.arm" ]; then
+  exit 0
+else
+  exit 1
+fi
