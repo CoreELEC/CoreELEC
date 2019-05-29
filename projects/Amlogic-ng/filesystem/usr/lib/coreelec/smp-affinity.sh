ec82bb59dfc35e0075651decb8afa133d6da9db9
Ray <1458889+rayelec@users.noreply.github.com>
2019-05-29 15:08:40 +0200

    Amlogic-ng: Fix smp-affinity.sh
    Find irq's by name

--- projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
+++ projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -3,9 +3,27 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
-echo 2 > /proc/irq/70/smp_affinity
-echo 4 > /proc/irq/22/smp_affinity
-echo 8 > /proc/irq/41/smp_affinity
-echo 10 > /proc/irq/21/smp_affinity
+SMP="../smp_affinity"
+MESONIR="`find /proc/irq/ -name ir-meson`"
+ETH0="`find /proc/irq/ -name eth0`"
+AOCECB="`find /proc/irq/ -name hdmi_aocecb`"
+USB3="`find /proc/irq/ -name xhci-hcd:usb1`"
+IRQ="$AOCECB $ETH0 $USB3 $MESONIR"
+
+cpu=0
+aff=1
+for i in $IRQ; do
+    if [ -f "$i/$SMP" ];then
+	cpu=$((cpu + 1))
+	if [ -d "/sys/devices/system/cpu/cpu$cpu" ];then
+	    aff=`printf '%x\n' $((aff * 2))`
+	else
+	    aff=2
+	    cpu=0
+	fi
+        echo "echo $aff > $i/$SMP"
+        echo $aff > $i/$SMP
+    fi
+done
 
 exit 0
