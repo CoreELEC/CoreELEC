d6d13cfcfd24fee1a35425754c58ae5de7b04463
afl1 <afl2001@gmail.com>
2019-05-29 22:03:20 +0200

    smp-affinity: fix hex calculation

--- projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
+++ projects/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -6,23 +6,29 @@
 SMP="../smp_affinity"
 MESONIR="`find /proc/irq/ -name ir-meson`"
 ETH0="`find /proc/irq/ -name eth0`"
+VDEC0="`find /proc/irq/ -name vdec-0`"
+VDEC1="`find /proc/irq/ -name vdec-1`"
+PREDI="`find /proc/irq/ -name pre_di`"
+AFIFO0="`find /proc/irq/ -name afifo0`"
 AOCECB="`find /proc/irq/ -name hdmi_aocecb`"
 USB3="`find /proc/irq/ -name xhci-hcd:usb1`"
-IRQ="$AOCECB $ETH0 $USB3 $MESONIR"
+IRQ="$AOCECB $ETH0 $USB3 $VDEC0 $VDEC1 $AFIFO0 $MESONIR $PREDI"
 
 cpu=0
 aff=1
 for i in $IRQ; do
     if [ -f "$i/$SMP" ];then
-	cpu=$((cpu + 1))
-	if [ -d "/sys/devices/system/cpu/cpu$cpu" ];then
-	    aff=`printf '%x\n' $((aff * 2))`
-	else
-	    aff=2
-	    cpu=0
-	fi
-        echo "echo $aff > $i/$SMP"
-        echo $aff > $i/$SMP
+        cpu=$((cpu + 1))
+        if [ -d "/sys/devices/system/cpu/cpu$cpu" ];then
+            aff=$((aff * 2))
+            haff=`printf '%x\n' $aff`
+        else
+            aff=2
+            haff=2
+            cpu=0
+        fi
+        echo "echo $haff > $i/$SMP"
+        echo $haff > $i/$SMP
     fi
 done
 
