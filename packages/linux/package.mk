91311ec7bd54d58e46fa8d2c2ed3acbef5c063e9
Dmitry_L <freebox_2002@mail.ru>
2019-05-31 23:57:51 +0300

    linux: bump amlogic-4.9 to 448cf9c

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -33,8 +33,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-4.9)
-    PKG_VERSION="f3707f33a6d4a295c7fc9758d6f50079647937e0"
-    PKG_SHA256="92414c3e53cf9136639385734dd8ae6962fc15f382d34f1313562b72512daa2d"
+    PKG_VERSION="448cf9c59eda46a07aa5c3c7d584941c70f46168"
+    PKG_SHA256="d514a2ca1c5668a20ddafc2aa4841617b565a93a23b09eee565f7a795e62db03"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
