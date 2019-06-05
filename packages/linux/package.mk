ad2361e0bd986924c9beb4bc6e3971312b8b008c
Portisch <hugo.portisch@yahoo.de>
2019-06-05 11:13:22 +0000

    linux: update amlogic-3.14 to f1bb0082

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -25,8 +25,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-3.14)
-    PKG_VERSION="98b18c05a5c1c878bd01ecbc6c9d1b33dde622bb"
-    PKG_SHA256="3badd5fff943fa66e86c75cb7709dfe2dc609f73d7d08a92c0f634eb9696878d"
+    PKG_VERSION="f1bb00821950b0f5cab1b8a820c697ba9fbd3183"
+    PKG_SHA256="5e5b32c9d7b00a299dd9131f51dd4457732b47772e5c773cec7c10adac593daa"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
