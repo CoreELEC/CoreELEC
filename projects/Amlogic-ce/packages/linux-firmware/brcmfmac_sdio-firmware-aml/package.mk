24abd54182b30744373493336fee713eeca84a61
adamg <adamg88@users.noreply.github.com>
2019-07-07 14:07:27 +0100

    brcmfmac_sdio-firmware-aml: update package for Amlogic-ng

--- projects/Amlogic-ce/packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="brcmfmac_sdio-firmware-aml"
-PKG_VERSION="0.1"
-PKG_SHA256="37e19eb005882793d26d0def8704417b21beb1c24d2b4489715e4ed7e7818776"
+PKG_VERSION="2bc4ef3797d87cad16e76cb8cff920a3c749392b"
+PKG_SHA256="355ac6b4b8d4545713928994131fa58af182af1ee4986fde2eab927663d7ae91"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/LibreELEC/LibreELEC.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SITE="https://github.com/CoreELEC/brcmfmac_sdio-firmware-aml"
+PKG_URL="https://github.com/CoreELEC/brcmfmac_sdio-firmware-aml/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Firmware for brcm bluetooth chips used in some Amlogic based devices."
 
