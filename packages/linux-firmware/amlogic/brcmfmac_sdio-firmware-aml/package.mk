eed3f771d6c4a601ae8998fced3a17191710a564
adamg <adamg88@users.noreply.github.com>
2019-07-08 07:25:12 +0100

    brcmfmac_sdio-firmware-aml: bump to 15e79c9 and add rules for bcm4359c0

--- packages/linux-firmware/amlogic/brcmfmac_sdio-firmware-aml/package.mk
+++ packages/linux-firmware/amlogic/brcmfmac_sdio-firmware-aml/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="brcmfmac_sdio-firmware-aml"
-PKG_VERSION="2bc4ef3797d87cad16e76cb8cff920a3c749392b"
-PKG_SHA256="355ac6b4b8d4545713928994131fa58af182af1ee4986fde2eab927663d7ae91"
+PKG_VERSION="15e79c977a136efc0bae87a2190e5bc6f7e524a9"
+PKG_SHA256="867f30fb0f3a9ce10cb047663aeaf8a322d39f391a6979db40ee97070fcd18ad"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC/brcmfmac_sdio-firmware-aml"
 PKG_URL="https://github.com/CoreELEC/brcmfmac_sdio-firmware-aml/archive/$PKG_VERSION.tar.gz"
