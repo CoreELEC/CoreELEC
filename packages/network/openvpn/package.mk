0b56f5e859abe1d50c444ba2922c9e7d1ded6918
JimmyS83 <github.jpeter@spamgourmet.com>
2019-07-16 18:11:46 +0200

    Enable server feature for OpenVPN

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -12,7 +12,6 @@ PKG_DEPENDS_TARGET="toolchain lzo openssl"
 PKG_LONGDESC="A full featured SSL VPN software solution that integrates OpenVPN server capabilities."
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_have_decl_TUNSETPERSIST=no \
-                           --disable-server \
                            --disable-plugins \
                            --enable-iproute2 IPROUTE=/sbin/ip \
                            --enable-management \
