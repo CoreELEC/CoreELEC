d2ab1ca2febbc33b7f9372dea87aed5562542394
Shanti Gilbert <shantic@gmail.com>
2019-07-17 11:13:15 -0500

    Munt: Bump package to 538a3b6

--- packages/sx05re/tools/munt/package.mk
+++ packages/sx05re/tools/munt/package.mk
@@ -3,13 +3,13 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="munt"
-PKG_VERSION="939cc98"
-PKG_SHA256="fb6577e09bd373875464c54ea64f572c97b642da00c84a9d47af9db2c00d1dba"
+PKG_VERSION="538a3b6b0e9f3e1f9b37dd1512fa3035883aab91"
+PKG_SHA256="24117694973e50fa952745790cf57aafc4f8185694172579373727c3e978cd8b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/munt/munt"
-PKG_URL="https://github.com/munt/munt/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/munt/munt/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="A multi-platform software synthesiser emulating (currently inaccurately) pre-GM MIDI devices such as the Roland MT-32, CM-32L, CM-64 and LAPC-I. In no way endorsed by or affiliated with Roland Corp."
+PKG_LONGDESC="A software synthesiser emulating pre-GM MIDI devices such as the Roland MT-32."
 
 PKG_CMAKE_OPTS_TARGET="-Dmunt_WITH_MT32EMU_QT=0 \
                        -Dmunt_WITH_MT32EMU_SMF2WAV=0 \
