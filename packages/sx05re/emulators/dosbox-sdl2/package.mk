51337f5c9b8d9d6450acbfcb735c2dccf3385ec0
Shanti Gilbert <shantic@gmail.com>
2019-07-28 01:10:10 -0500

    DOSboxSDL: update and cleanup

--- packages/sx05re/emulators/dosbox-sdl2/package.mk
+++ packages/sx05re/emulators/dosbox-sdl2/package.mk
@@ -1,57 +1,40 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
-# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dosbox-sdl2"
 PKG_VERSION="d4380b09810f5d07bd86328b9da6b6a82d8e583b"
 PKG_SHA256="ceda5ea24ee42dad1867236144d2e3a3f242ee7fc2aef634f4ac9bce9008a809"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/duganchen/dosbox"
-PKG_URL="https://github.com/duganchen/dosbox/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/duganchen/dosbox/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc glib systemd dbus alsa-lib SDL2-git SDL2_net SDL_sound libpng zlib libvorbis flac libogg fluidsynth-git munt"
-PKG_LONGDESC="This is an enhanced SDL2 fork of DOSBox emulator by duganchen. It is currently in sync with revision 4006."
+PKG_LONGDESC="This is an enhanced fork of DOSBox. It is currently in sync with revision 4156."
 PKG_TOOLCHAIN="autotools"
-
-configure_package() {
-  # Displayserver Support
-  if [ "${DISPLAYSERVER}" = "x11" ]; then
-    PKG_DEPENDS_TARGET+=" xorg-server"
-  fi
-
-  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
-    PKG_DEPENDS_TARGET+="  ${OPENGL} dosbox-sdl2-shaders glew"
-  fi
-}
+PKG_BUILD_FLAGS="+lto"
 
 pre_configure_target() {
+  # Clean up build directory
+  cd ${PKG_BUILD}
+  rm -rf .${TARGET_NAME}
+
   PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                              --enable-core-inline \
                              --enable-dynrec \
                              --enable-unaligned_memory \
-                             --with-sdl-prefix=$SYSROOT_PREFIX/usr"
-  cd ..
-  rm -rf .$TARGET_NAME
+                             --with-sdl-prefix=${SYSROOT_PREFIX}/usr"
+
+   PKG_CONFIGURE_OPTS_TARGET+=" --host=armv7l"
+
 }
 
 pre_make_target() {
-  # Enable DynaRec for ARMv7 cpus
-  if [ "$TARGET_CPU" = "cortex-a7" ] || [ "$TARGET_CPU" = "cortex-a53" ] && [ ! $TARGET_CPU = "arm1176jzf-s" ]; then
-    sed -i 's|/\* #undef C_DYNREC \*/|#define C_DYNREC 1|' config.h
-    sed -i s/C_TARGETCPU.*/C_TARGETCPU\ ARMV7LE/g config.h
-    sed -i 's|/\* #undef C_UNALIGNED_MEMORY \*/|#define C_UNALIGNED_MEMORY 1|' config.h
-  fi
-  # Change version SVN to SDL2
-  sed -i s/SVN/SDL2/g config.h
+  # Define DOSBox version
+  sed -e "s/SVN/SDL2/" -i ${PKG_BUILD}/config.h
 }
 
 post_makeinstall_target() {
   # Create config directory & install config
-  mkdir -p $INSTALL/usr/config/dosbox
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin/
-  cp $PKG_DIR/config/* $INSTALL/usr/config/dosbox/
-
-  # Enable OpenGL output if supported
-  if [ "$OPENGL_SUPPORT" = "yes" ]; then
-    sed -i s/output=texture/output=opengl/g $INSTALL/usr/config/dosbox/dosbox-SDL2.conf
-  fi
+  mkdir -p ${INSTALL}/usr/config/dosbox
+  cp -a ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin/
+  cp -a ${PKG_DIR}/config/*  ${INSTALL}/usr/config/dosbox/
 }
--- packages/sx05re/tools/fluidsynth-git/package.mk
+++ packages/sx05re/tools/fluidsynth-git/package.mk
@@ -1,18 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fluidsynth-git"
-PKG_VERSION="2.0.4"
-PKG_SHA256="2c065de87e9c9ba0311ebf2f4828a4fd76f1f5cc7d1d93dd80d7a048d7d2a76c"
+PKG_VERSION="2.0.5"
+PKG_SHA256="69b244512883491e7e66b4d0151c61a0d6d867d4d2828c732563be0f78abcc51"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://fluidsynth.org/"
-PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glib soundfont-generaluser"
 PKG_LONGDESC="FluidSynth is a software real-time synthesizer based on the Soundfont 2 specifications."
 PKG_BUILD_FLAGS="+pic"
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-Denable-readline=0 \
+  PKG_CMAKE_OPTS_TARGET="-DLIB_SUFFIX= \
+                         -Denable-readline=0 \
                          -Denable-oss=0 \
                          -Denable-pulseaudio=1 \
                          -Denable-libsndfile=0"
@@ -20,14 +21,14 @@ pre_configure_target() {
 
 post_makeinstall_target() {
   # Create directories
-  mkdir -p $INSTALL/etc/fluidsynth
-  mkdir -p $INSTALL/usr/config/fluidsynth/soundfonts
+  mkdir -p ${INSTALL}/etc/fluidsynth
+  mkdir -p ${INSTALL}/usr/config/fluidsynth/soundfonts
 
   # Create symlinks & install config file
-  cp $PKG_DIR/config/* $INSTALL/usr/config/fluidsynth/
-  ln -s /storage/.config/fluidsynth/fluidsynth.conf $INSTALL/etc/fluidsynth/
-  echo "Place your SoundFonts here!" >> $INSTALL/usr/config/fluidsynth/soundfonts/readme.txt
+  cp -a ${PKG_DIR}/config/* ${INSTALL}/usr/config/fluidsynth/
+  ln -s /storage/.config/fluidsynth/fluidsynth.conf ${INSTALL}/etc/fluidsynth/
+  echo "Place your SoundFonts here!" >> ${INSTALL}/usr/config/fluidsynth/soundfonts/readme.txt
 
   # Create symlink to SoundFont
-  ln -s /usr/share/soundfonts/GeneralUser.sf2  $INSTALL/usr/config/fluidsynth/soundfonts/
+  ln -s /usr/share/soundfonts/GeneralUser.sf2  ${INSTALL}/usr/config/fluidsynth/soundfonts/
 }
