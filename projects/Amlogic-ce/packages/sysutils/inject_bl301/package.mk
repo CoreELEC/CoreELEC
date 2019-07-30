0df838152f72163193e9181752ec28d4769dcff9
Portisch <hugo.portisch@yahoo.de>
2019-07-30 16:09:06 +0200

    Amlogic-ng: introduce inject_bl301 package

---
+++ projects/Amlogic-ce/packages/sysutils/inject_bl301/package.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="inject_bl301"
+PKG_VERSION="0.1"
+PKG_LICENSE="GPL"
+PKG_SITE="https://coreelec.org"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Tool to inject bootloader blob BL301.bin on internal eMMC"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/sbin
+    cp -P $PKG_BUILD/$PKG_NAME $INSTALL/usr/sbin
+}
