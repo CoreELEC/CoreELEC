5a4d8f1f6a110090b6833510d43fb515d4938274
Shanti Gilbert <shantic@gmail.com>
2019-08-03 12:21:35 -0500

    amiberry: Bump to 0201782 / cleanup

--- packages/sx05re/emulators/amiberry/package.mk
+++ packages/sx05re/emulators/amiberry/package.mk
@@ -1,46 +1,29 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="amiberry"
-PKG_VERSION="928b4e2bf2047bbb2d174fd58c7450153b4d94d5"
+PKG_VERSION="0201782dcb7e28adadaa0cf52da9a7ed8ad1017f" # v2.25+
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/midwan/amiberry"
 PKG_URL="https://github.com/midwan/amiberry.git"
-PKG_DEPENDS_TARGET="toolchain linux glibc bzip2 zlib SDL2-git SDL2_image SDL2_ttf capsimg freetype libxml2 flac libogg mpg123-compat libpng libmpeg2"
+PKG_DEPENDS_TARGET="toolchain linux glibc bzip2 zlib SDL2-git SDL2_image SDL2_ttf capsimg freetype libxml2 flac libogg mpg123 libpng libmpeg2 retroarch-joypad-autoconfig"
 PKG_LONGDESC="Amiberry is an optimized Amiga emulator for ARM-based boards."
 GET_HANDLER_SUPPORT="git"
 PKG_TOOLCHAIN="make"
 
-PKG_PATCH_DIRS="${PROJECT}"
-
 PKG_MAKE_OPTS_TARGET="all"
 
 pre_configure_target() {
-
-# TEMP MAKE SURE TO REMOVE
-# sed -i "s|#DEBUG=1|DEBUG=1|" $PKG_BUILD/Makefile
-
   cd ${PKG_BUILD}
   export SYSROOT_PREFIX=${SYSROOT_PREFIX}
 
   case ${PROJECT} in
-    Amlogic*)
-      AMIBERRY_PLATFORM="amlogic"
+    Amlogic)
+        AMIBERRY_PLATFORM="AMLGX"
       ;;
-    Rockchip)
-      if [ "${DEVICE}" = "RK3399" ]; then
-        AMIBERRY_PLATFORM="rockpro64"
-      else
-        AMIBERRY_PLATFORM="tinker"
-      fi
-      ;;
-    RPi)
-      if [ "${DEVICE}" = "RPi2" ]; then
-        AMIBERRY_PLATFORM="rpi2-sdl2"
-      else
-        AMIBERRY_PLATFORM="rpi1-sdl2"
-      fi
+    Amlogic-ng)
+        AMIBERRY_PLATFORM="AMLG12B"
       ;;
   esac
 
@@ -67,7 +50,7 @@ makeinstall_target() {
   ln -s "/tmp/joypads" "${INSTALL}/usr/config/amiberry/controller"
 
   # Copy binary, scripts & link libcapsimg
-  cp -a amiberry ${INSTALL}/usr/bin/amiberry
+  cp -a amiberry-aml* ${INSTALL}/usr/bin/amiberry
   cp -a ${PKG_DIR}/scripts/*          ${INSTALL}/usr/bin
   ln -sf /usr/lib/libcapsimage.so.5.1 ${INSTALL}/usr/config/amiberry/capsimg.so
 }
