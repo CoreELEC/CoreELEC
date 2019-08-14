ab6c176642642e8e0021c347afaf523778790836
Shanti Gilbert <shantic@gmail.com>
2019-08-14 01:37:57 -0500

    mba.mini.plus: bump to 8b8f623

--- packages/sx05re/emulators/mba.mini.plus/package.mk
+++ packages/sx05re/emulators/mba.mini.plus/package.mk
@@ -2,28 +2,33 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mba.mini.plus"
-PKG_VERSION="663228687bec9ef0a4a72cbe170efc166b7357ef"
-PKG_SHA256="b25fe171c787680dd8b5ef895a6bd4d33060de313ab2d58b23cda2a4d5de65a7"
+PKG_VERSION="8b8f623ef241c7aa21f2f775eb1b27364e0bd556"
+PKG_SHA256="55240dc16b483a4ff410c84ad209fc6768bd0dafe0a97bb1853db07adfb303b3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/SumavisionQ5/MBA.mini.Plus-libretro"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_PRIORITY="optional"
-PKG_SECTION="libretro"
+PKG_SECTION="emuelec"
 PKG_SHORTDESC="M.B.A = MAME's skeleton + FBA's romsets"
 PKG_LONGDESC="M.B.A-mini from MAME2010-libretro (https://github.com/libretro/mame2010-libretro) after the codes is streamlined, only CPS 1/2, NEOGEO, IREM M92 machines && roms is supported."
-
-PKG_IS_ADDON="no"
 PKG_TOOLCHAIN="make"
-PKG_AUTORECONF="no"
+
 
 pre_configure_target() {
-  PKG_MAKE_OPTS_TARGET="platform=armv CC=$CC LD=$CC"
-
+  
+  if [ ${PROJECT} = "Amlogic-ng" ]; then
+	PKG_MAKE_OPTS_TARGET="platform=AMLG12B"
+  elif [ "${PROJECT}" = "Amlogic" ]; then
+	PKG_MAKE_OPTS_TARGET="platform=AMLGX"
+  fi
+  
+  PKG_MAKE_OPTS_TARGET+=" CC=$CC LD=$CC"
+  
   sed -i -e "s|uname -a|echo armv|" \
          -e "s|uname -m|echo armv|" \
+         -e "s|LIBS = -lm|LIBS = |g" \
          -e "s|LIBS = |LIBS = -lm|g" \
     makefile
 }
