4cb83507c695317ed2ad904cb31c7baf81013388
Shanti Gilbert <shantic@gmail.com>
2019-08-17 02:01:43 -0500

    Retroarch: Bump to 77d2ce9

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="3c2df78caf53fa5575187e9ff0f9ea150b16f74a"
+PKG_VERSION="77d2ce95f1c30c2e5b0d4a8ba97f2c4391b7cc99"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
