0ff3ba503990df31a6c019a3449ad817f97c659e
Shanti Gilbert <shantic@gmail.com>
2019-08-17 15:57:35 -0500

    jslisten: Bump to f08e985

--- packages/sx05re/tools/jslisten/package.mk
+++ packages/sx05re/tools/jslisten/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="jslisten"
-PKG_VERSION="5e09d97963198051c8ccfdd56e6f99e9d2b03de8"
-PKG_SHA256="90ddc5a05a76a237736eded42d14a50851f82db164e10f64e1e84f9521d44304"
+PKG_VERSION="f08e98533b1c77bd9d250348ffc40c24abe1af32"
+PKG_SHA256="d211c26c0815ca77c692b246f02178994bcfbf3917aa39d15ccb2992e1a7e5c4"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/shantigilbert/jslisten"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
