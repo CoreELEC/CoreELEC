0ed33d5ab016a247ba2af9ce87a2118d68ff6765
Shanti Gilbert <shantic@gmail.com>
2019-08-26 17:27:07 -0500

    Advmame: add 8bitdo SN30 Pro+

--- packages/sx05re/emulators/advancemame/joverride/retroarch-joypad-autoconfig/package.mk
+++ packages/sx05re/emulators/advancemame/joverride/retroarch-joypad-autoconfig/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="4b44318f363a2261b1ef8827f0e4f80e6c536c35"
+PKG_VERSION="4f80e58b2b34bd3107e12cf615f955a87e0a83a4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
