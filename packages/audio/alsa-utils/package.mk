0d13afcd1eb3c43e2d0dd5fca0d5c162cea09873
Shanti Gilbert <shantic@gmail.com>
2019-09-05 13:48:40 -0500

    alsa-utils: Don't remove tools

--- packages/audio/alsa-utils/package.mk
+++ packages/audio/alsa-utils/package.mk
@@ -30,9 +30,9 @@ post_makeinstall_target() {
 # so we avoid resetting our soundconfig
   rm -rf $INSTALL/usr/lib/udev/rules.d/90-alsa-restore.rules
 
-  for i in aconnect alsamixer amidi aplaymidi arecord arecordmidi aseqdump aseqnet iecset; do
-    rm -rf $INSTALL/usr/bin/$i
-  done
+#  for i in aconnect alsamixer amidi aplaymidi arecord arecordmidi aseqdump aseqnet iecset; do
+#    rm -rf $INSTALL/usr/bin/$i
+#  done
 
   mkdir -p $INSTALL/usr/lib/udev
     cp $PKG_DIR/scripts/soundconfig $INSTALL/usr/lib/udev
