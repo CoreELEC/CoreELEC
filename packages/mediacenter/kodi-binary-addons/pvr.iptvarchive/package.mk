00933facb30edd5666a747ecf06aefbb51997635
Arthur Liberman <arthur_liberman@hotmail.com>
2019-09-06 18:17:20 +0300

    pvr.iptvarchive, pvr.iptvsimple: bump to 3.8.0

--- packages/mediacenter/kodi-binary-addons/pvr.iptvarchive/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvarchive/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pvr.iptvarchive"
-PKG_VERSION="73feb2f4a3106cb3cf7f3f5432438ad1af2590a3"
-PKG_SHA256="332cd69cf8991d8e789125b294fe877c3d742d8ce83afb189f366461c67b5a86"
-PKG_REV="3"
+PKG_VERSION="3.8.0-Leia"
+PKG_SHA256="648fd6e3d6b52f2328cfffd313a3638a3e0809fa46f0fe382a81c0997fa83838"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.kodi.tv"
+PKG_SITE="https://github.com/kodi-pvr/pvr.iptvsimple"
 PKG_URL="https://github.com/kodi-pvr/pvr.iptvsimple/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform rapidxml zlib"
 PKG_SECTION=""
