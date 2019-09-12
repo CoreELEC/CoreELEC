1bb678122b7c0c5dd9e4a66d3c926d2ff6bf45b5
Shanti Gilbert <shantic@gmail.com>
2019-09-12 13:32:56 -0500

    PPSSPPSDL: bump to edcd5e1

--- packages/sx05re/emulators/PPSSPPSDL/package.mk
+++ packages/sx05re/emulators/PPSSPPSDL/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="9db48075769d37076429404ec4b7fee2e9084c0c"
+PKG_VERSION="edcd5e1c7b42d2b0ac7092bbbe138f6b81800d22"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
