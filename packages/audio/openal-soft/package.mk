f560a070d9abafd4ddf710671c23c60e22ea756e
Shanti Gilbert <shantic@gmail.com>
2019-09-15 21:00:58 -0500

    Use pulseaudio as default
    for mixing sounds, thanks to 5schatten

--- packages/audio/openal-soft/package.mk
+++ packages/audio/openal-soft/package.mk
@@ -2,17 +2,34 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openal-soft"
-PKG_VERSION="1.18.2"
-PKG_SHA256="9f8ac1e27fba15a59758a13f0c7f6540a0605b6c3a691def9d420570506d7e82"
+PKG_VERSION="1.19.1"
+PKG_SHA256="9f3536ab2bb7781dbafabc6a61e0b34b17edd16bd6c2eaf2ae71bc63078f98c7"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openal.org/"
-PKG_URL="http://kcat.strangesoft.net/openal-releases/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain alsa-lib"
+PKG_URL="https://github.com/kcat/openal-soft/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain alsa-lib openal-soft:host"
 PKG_LONGDESC="OpenAL the Open Audio Library"
 
-PKG_CMAKE_OPTS_TARGET="-DALSOFT_BACKEND_OSS=off \
+configure_package() {
+  PKG_CMAKE_OPTS_HOST="-DALSOFT_NATIVE_TOOLS=on \
+                       -DALSOFT_BACKEND_OSS=off \
                        -DALSOFT_BACKEND_WAVE=off \
-                       -DALSOFT_BACKEND_PULSEAUDIO=off \
+                       -DALSOFT_BACKEND_PULSEAUDIO=on \
                        -DALSOFT_EXAMPLES=off \
                        -DALSOFT_TESTS=off \
                        -DALSOFT_UTILS=off"
+
+  PKG_CMAKE_OPTS_TARGET="-DALSOFT_NATIVE_TOOLS=off -DNATIVE_BIN_DIR=$PKG_BUILD/.$HOST_NAME/native-tools/ \
+                         -DALSOFT_BACKEND_OSS=off \
+                         -DALSOFT_BACKEND_WAVE=off \
+                         -DALSOFT_BACKEND_PULSEAUDIO=on \
+                         -DALSOFT_EXAMPLES=off \
+                         -DALSOFT_TESTS=off \
+                         -DALSOFT_UTILS=off"
+}
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/etc/openal
+  sed s/^#drivers.*/drivers=alsa/ $INSTALL/usr/share/openal/alsoftrc.sample > $INSTALL/etc/openal/alsoft.conf
+  rm -rf $INSTALL/usr/share/openal
+}
---
+++ packages/sx05re/emuelec/config/emuelec/scripts/rr_audio.sh
@@ -0,0 +1,163 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking@gmail.com)
+
+# Set common paths
+export PULSE_RUNTIME_PATH=/run/pulse
+	RR_AUDIO_DEVICE="sysdefault:CARD=AMLM8AUDIO"
+    RR_AUDIO_BACKEND="PulseAudio"
+    RR_PA_UDEV="true"
+    RR_PA_TSCHED="true"
+    RR_AUDIO_VOLUME="100"
+
+
+asound_load() {
+	
+ASOUND_CONF="/storage/.config/asound.conf"
+ASOUND_CONF_DEFAULT="/storage/.config/emuelec/configs/rr_asound.conf"
+
+# If selected card is not active then pick first active card
+CARDS="$(aplay -L | grep "^[^ ]*:CARD=")"
+if ! echo "${CARDS}" | grep -q "^$RR_AUDIO_DEVICE$"; then
+  RR_AUDIO_DEVICE="$(echo "${CARDS}" | head -n 1)"
+fi
+
+if [ -n "${RR_AUDIO_DEVICE}" ]; then
+  # Remove ALSA configuration
+  if [ -f "${ASOUND_CONF}" ]; then
+    rm -f "${ASOUND_CONF}"
+  fi
+  # Add ALSA configuration
+  sed -e "s/default \".*/default \"${RR_AUDIO_DEVICE}\"/" ${ASOUND_CONF_DEFAULT} >> "${ASOUND_CONF}"
+fi
+
+# Output configuration
+echo "rr-config-script: ALSA & PulseAudio will use "${RR_AUDIO_DEVICE}" as audio output device"
+
+}
+
+# If a card is found use PulseAudio sink
+pulseaudio_sink_load() {
+  
+  if [ ${RR_AUDIO_BACKEND} = "PulseAudio" ];then
+    if [ "${RR_PA_TSCHED}" = "false" ]; then
+      TSCHED="tsched=0"
+      echo "rr-config-script: PulseAudio will disable timer-based audio scheduling"
+    else
+      TSCHED="tsched=1"
+      echo "rr-config-script: PulseAudio will enable timer-based audio scheduling"
+    fi
+
+    if [ ! -z "$(pactl list modules short | grep module-null-sink)" ];then
+      if [ "${RR_PA_UDEV}" = "true" ]; then
+        pactl load-module module-udev-detect $TSCHED > /dev/null
+        pactl set-sink-volume "$(pactl info | grep 'Default Sink:' | cut -d ' ' -f 3)" ${RR_AUDIO_VOLUME}%
+        if [ ! -z "$(pactl list modules short | grep module-alsa-card)" ];then
+          echo "rr-config-script: PulseAudio module-udev-detect loaded, setting a volume of "${RR_AUDIO_VOLUME}"%"
+          echo "rr-config-script: PulseAudio will use sink "$(pactl list sinks short)
+        else
+          echo "rr-config-script: PulseAudio module-udev-detect failed to load"
+        fi
+      else
+        pactl load-module module-alsa-sink device="${RR_AUDIO_DEVICE}" name="temp_sink" ${TSCHED} > /dev/null
+        pactl set-sink-volume alsa_output.temp_sink ${RR_AUDIO_VOLUME}%
+        if [ ! -z "$(pactl list modules short | grep module-alsa-sink)" ];then
+          echo "rr-config-script: PulseAudio module-alsa-sink loaded, setting a volume of "${RR_AUDIO_VOLUME}"%"
+          echo "rr-config-script: PulseAudio will use sink "$(pactl list sinks short)
+        else
+          echo "rr-config-script: PulseAudio module-alsa-sink failed to load"
+        fi
+      fi
+    fi
+  fi
+}
+
+# Unload PulseAudio sink
+pulseaudio_sink_unload() {
+  
+
+  if [ ${RR_AUDIO_BACKEND} = "PulseAudio" ]; then
+    if [ "${RR_PA_UDEV}" = "true" ] && [ ! -z "$(pactl list modules short | grep module-alsa-card)" ]; then
+      pactl set-sink-volume "$(pactl info | grep 'Default Sink:' | cut -d ' ' -f 3)" 100%  
+      pactl unload-module module-udev-detect
+      pactl unload-module module-alsa-card
+      echo "rr-config-script: PulseAudio module-udev-detect unloaded"
+    elif [ "${RR_PA_UDEV}" = "false" ] && [ ! -z "$(pactl list modules short | grep module-alsa-sink)" ]; then
+      pactl set-sink-volume alsa_output.temp_sink 100%
+      NUMBER="$(pactl list modules short | grep "name=temp_sink" | awk '{print $1;}')"
+      if [ -n "${NUMBER}" ]; then
+        pactl unload-module "${NUMBER}"
+      fi
+      echo "rr-config-script: PulseAudio module-alsa-sink unloaded"
+    else
+      echo "rr-config-script: neither the PulseAudio module module-alsa-card or module-alsa-sink was found. Nothing to unload"
+    fi
+
+    # Restore ALSA Master volume to 100%
+    if [ ! -z "$(amixer | grep "'Master',0")" ] && [ ! $(amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]%') = "100" ]; then
+      amixer -q set Master,0 100% unmute
+      echo "rr-config-script: ALSA mixer restore volume to 100%"
+    fi
+  fi
+}
+
+# Start FluidSynth
+fluidsynth_service_start() {
+  
+
+  if [ ${RR_AUDIO_BACKEND} = "PulseAudio" ] && [ ! "$(systemctl is-active fluidsynth)" = "active" ]; then
+    systemctl start fluidsynth
+    if [ "$(systemctl is-active fluidsynth)" = "active" ]; then 
+      echo "rr-config-script: FluidSynth service loaded successfully"
+    else
+      echo "rr-config-script: FluidSynth service failed to load"
+    fi
+  fi
+}
+
+# Stop FluidSynth
+fluidsynth_service_stop() {
+  
+
+  if [ "$(systemctl is-active fluidsynth)" = "active" ]; then
+    systemctl stop fluidsynth
+    if [ ! "$(systemctl is-active fluidsynth)" = "active" ]; then 
+      echo "rr-config-script: FluidSynth service successfully stopped"
+    else
+      echo "rr-config-script: FluidSynth service failed to stop"
+    fi
+  fi
+}
+
+# SDL2: Set audio driver to Pulseaudio or ALSA
+set_SDL_audiodriver() {
+  
+
+  if [ ${RR_AUDIO_BACKEND} = "PulseAudio" ]; then
+    export SDL_AUDIODRIVER=pulseaudio
+  else
+    export SDL_AUDIODRIVER=alsa
+  fi
+  echo "rr-config-script: SDL2 set environment variable SDL_AUDIODRIVER="${SDL_AUDIODRIVER}
+}
+
+# RETROARCH: Set audio & midi driver
+set_RA_audiodriver() {
+  
+
+  RETROARCH_HOME=/storage/.config/retroarch
+  RETROARCH_CONFIG=${RETROARCH_HOME}/retroarch.cfg
+
+  if [ -f ${RETROARCH_CONFIG} ]; then
+    if [ ${RR_AUDIO_BACKEND} = "PulseAudio" ]; then
+      sed -e "s/audio_driver = \"alsathread\"/audio_driver = \"pulse\"/" -i ${RETROARCH_CONFIG}
+      sed -e "s/midi_driver = \"null\"/midi_driver = \"alsa\"/" -i          ${RETROARCH_CONFIG}
+      sed -e "s/midi_output = \"Off\"/midi_output = \"FluidSynth\"/" -i     ${RETROARCH_CONFIG}
+      echo "rr-config-script: Retroarch force audio driver to PulseAudio & MIDI output to FluidSynth"
+    else
+      sed -e "s/audio_driver = \"pulse\"/audio_driver = \"alsathread\"/" -i ${RETROARCH_CONFIG}
+      sed -e "s/midi_driver = \"alsa\"/midi_driver = \"null\"/" -i          ${RETROARCH_CONFIG}
+      sed -e "s/midi_output = \"FluidSynth\"/midi_output = \"Off\"/" -i     ${RETROARCH_CONFIG}
+      echo "rr-config-script: Retroarch force audio driver to ALSA & disable MIDI output"
+    fi
+  fi
+}
