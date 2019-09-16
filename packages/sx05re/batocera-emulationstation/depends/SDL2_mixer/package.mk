1dfd81962a7f08fb2a4017f5f9ce62e8ef0da963
Shanti Gilbert <shantic@gmail.com>
2019-09-15 21:34:34 -0500

    SDL@_mixer: bump to 2.0.4
    Fixes mp3 playback

--- packages/sx05re/batocera-emulationstation/depends/SDL2_mixer/package.mk
+++ packages/sx05re/batocera-emulationstation/depends/SDL2_mixer/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="SDL2_mixer"
-PKG_VERSION="2.0.1"
+PKG_VERSION="2.0.4"
 PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
@@ -15,7 +15,8 @@ pre_configure_target() {
   PKG_CONFIGURE_OPTS_TARGET="--disable-fluidsynth \
 							 --disable-music-flac \
 							 --disable-music-mod-modplug \
-							 --disable-music-ogg-tremor"
+							 --disable-music-ogg-tremor \
+							 --enable-music-mp3"
 							 
 
 }
