cf3384a64c7ece4c77d2808899cce6b8516b18a3
Shanti Gilbert <shantic@gmail.com>
2019-09-16 22:07:42 -0500

    Move package fbterm
    Just move it to the unused pile for now

--- packages/sx05re/emuelec/package.mk
+++ packages/sx05re/emuelec/package.mk
@@ -19,7 +19,7 @@ PKG_TOOLCHAIN="make"
 # Thanks to magicseb  Reicast SA now WORKS :D
 PKG_EMUS="$LIBRETRO_CORES advancemame PPSSPPSDL reicastsa amiberry hatarisa openbor dosbox-sdl2 mupen64plus-nx mba.mini.plus scummvmsa"
 PKG_TOOLS="common-shaders scraper Skyscraper MC libretro-bash-launcher fbida mpv SDL_GameControllerDB linux-utils xmlstarlet CoreELEC-Debug-Scripts sixaxis jslisten evdev_tools"
-PKG_RETROPIE_DEP="bash pyudev dialog six fbterm git dbus-python pygobject coreutils fbpad"
+PKG_RETROPIE_DEP="bash pyudev dialog six git dbus-python pygobject coreutils fbpad"
 PKG_DEPENDS_TARGET+=" $PKG_EMUS $PKG_TOOLS $PKG_RETROPIE_DEP"
 
 # Removed cores for space and/or performance
