4349764fab48a18e6689bc598f46d19c86068f63
Shanti Gilbert <shantic@gmail.com>
2019-09-21 00:45:27 -0500

    Fix for Bezels not showing if platform is LIBRETRO

--- packages/sx05re/emuelec/config/emuelec/scripts/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/config/emuelec/scripts/emuelecRunEmu.sh
@@ -49,6 +49,8 @@ else
 	PLATFORM="$1"
 fi
 
+[ "$1" = "LIBRETRO" ] && ROMNAME="$3" || ROMNAME="$2"
+
 # Evkill setup
 /storage/.emulationstation/scripts/configscripts/z_getkillkeys.sh
 . ${JSLISTENCONF}
@@ -66,7 +68,7 @@ VERBOSE="-v"
 fi
 
 # if the emulator is in es_settings this is the line that will run 
-RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/${EMU}_libretro.so "$2"'
+RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/${EMU}_libretro.so "${ROMNAME}"'
 
 # very WIP {
 
@@ -77,8 +79,8 @@ PAT="s|\s*<bool name=\"EmuELEC_SPLASH\" value=\"\(.*\)\" />|\1|p"
 SPL=$(sed -n "$PAT" "$CFG")
 [ "$SPL" == "true" ] && SHOW_SPLASH="Yes" || SHOW_SPLASH="No"
 
-[ "$SHOW_BEZELS" = "Yes" ] && ${TBASH} /emuelec/scripts/bezels.sh "$PLATFORM" "$2" || ${TBASH} /emuelec/scripts/bezels.sh "default"
-[ "$SHOW_SPLASH" = "Yes" ] && ${TBASH} /emuelec/scripts/show_splash.sh "$PLATFORM" "$2" || ${TBASH} /emuelec/scripts/show_splash.sh "default" 
+[ "$SHOW_BEZELS" = "Yes" ] && ${TBASH} /emuelec/scripts/bezels.sh "$PLATFORM" "${ROMNAME}" || ${TBASH} /emuelec/scripts/bezels.sh "default"
+[ "$SHOW_SPLASH" = "Yes" ] && ${TBASH} /emuelec/scripts/show_splash.sh "$PLATFORM" "${ROMNAME}" || ${TBASH} /emuelec/scripts/show_splash.sh "default" 
 
 # } very WIP 
 
@@ -87,47 +89,47 @@ case $1 in
 "HATARI")
 	if [ "$EMU" = "HATARISA" ]; then
 	set_kill_keys "hatari"
-	RUNTHIS='${TBASH} /usr/bin/hatari.start "$2"'
+	RUNTHIS='${TBASH} /usr/bin/hatari.start "${ROMNAME}"'
 	fi
 	;;
 "OPENBOR")
 	set_kill_keys "OpenBOR"
-	RUNTHIS='${TBASH} /usr/bin/openbor.sh "$2"'
+	RUNTHIS='${TBASH} /usr/bin/openbor.sh "${ROMNAME}"'
 	;;
 "RETROPIE")
-	RUNTHIS='${TBASH} /emuelec/scripts/fbterm.sh "$2"'
+	RUNTHIS='${TBASH} /emuelec/scripts/fbterm.sh "${ROMNAME}"'
 	EMUELECLOG="$LOGSDIR/ee_script.log"
 	;;
 "LIBRETRO")
-	RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/$2_libretro.so "$3"'
+	RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/$2_libretro.so "${ROMNAME}"'
 		;;
 "REICAST")
     if [ "$EMU" = "REICASTSA" ]; then
     set_kill_keys "reicast"
-	RUNTHIS='${TBASH} /usr/bin/reicast.sh "$2"'
+	RUNTHIS='${TBASH} /usr/bin/reicast.sh "${ROMNAME}"'
 	LOGEMU="No" # ReicastSA outputs a LOT of text, only enable for debugging.
 	fi
 	;;
 "MAME"|"ARCADE")
 	if [ "$EMU" = "AdvanceMame" ]; then
 	set_kill_keys "advmame"
-	RUNTHIS='${TBASH} /usr/bin/advmame.sh "$2"'
+	RUNTHIS='${TBASH} /usr/bin/advmame.sh "${ROMNAME}"'
 	fi
 	;;
 "DRASTIC")
 	set_kill_keys "drastic"
-	RUNTHIS='${TBASH} /storage/.emulationstation/scripts/drastic.sh "$2"'
+	RUNTHIS='${TBASH} /storage/.emulationstation/scripts/drastic.sh "${ROMNAME}"'
 		;;
 "N64")
     if [ "$EMU" = "M64P" ]; then
     set_kill_keys "mupen64plus"
-	RUNTHIS='${TBASH} /usr/bin/m64p.sh "$2"'
+	RUNTHIS='${TBASH} /usr/bin/m64p.sh "${ROMNAME}"'
 	fi
 	;;
 "AMIGA")
     if [ "$EMU" = "AMIBERRY" ]; then
     set_kill_keys "amiberry"
-	RUNTHIS='${TBASH} /usr/bin/amiberry.start "$2"'
+	RUNTHIS='${TBASH} /usr/bin/amiberry.start "${ROMNAME}"'
 	fi
 	;;
 "SCUMMVM")
@@ -141,19 +143,19 @@ case $1 in
 "DOSBOX")
     if [ "$EMU" = "DOSBOXSDL2" ]; then
     set_kill_keys "dosbox"
-	RUNTHIS='${TBASH} /usr/bin/dosbox.start "$2"'
+	RUNTHIS='${TBASH} /usr/bin/dosbox.start "${ROMNAME}"'
 	fi
 	;;		
 "PSP")
 	if [ "$EMU" = "PPSSPPSA" ]; then
 	#PPSSPP can run at 32BPP but only with buffered rendering, some games need non-buffered and the only way they work is if I set it to 16BPP
 	# /emuelec/scripts/setres.sh 16 # This was only needed for S912, but PPSSPP does not work on S912 
-	RUNTHIS='${TBASH} /usr/bin/ppsspp.sh "$2"'
+	RUNTHIS='${TBASH} /usr/bin/ppsspp.sh "${ROMNAME}"'
 	fi
 	;;
 "NEOCD")
 	if [ "$EMU" = "fbneo" ]; then
-	RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/fbneo_libretro.so --subsystem neocd "$2"'
+	RUNTHIS='/usr/bin/retroarch $VERBOSE -L /tmp/cores/fbneo_libretro.so --subsystem neocd "${ROMNAME}"'
 	fi
 	;;
 esac
@@ -162,7 +164,8 @@ esac
 echo "EmuELEC Run Log" > $EMUELECLOG
 
 # Write the command to the log file.
-echo "PLATFORM: $PLATFORM" >> $EMUELECLOG 
+echo "PLATFORM: $PLATFORM" >> $EMUELECLOG
+echo "ROM NAME: ${ROMNAME}" >> $EMUELECLOG
 echo "1st Argument: $1" >> $EMUELECLOG 
 echo "2nd Argument: $2" >> $EMUELECLOG
 echo "3rd Argument: $3" >> $EMUELECLOG 
