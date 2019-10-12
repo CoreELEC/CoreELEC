6a661c0ae3a37c8ceea38264138948c048a1e8bf
cdu13a <cdu13a@gmail.com>
2019-10-12 02:15:42 -0400

    dav1d: updated to 0.5.0

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="0.3.1"
-PKG_SHA256="be511d465223cc74aab507fe3a6e4e764501662ebf1a5e233854ed064a7ae204"
+PKG_VERSION="0.5.0"
+PKG_SHA256="b29c159bf7c56e8b6ae81bb24704599819fa89399ec3d6db3dbc052d7bc5baf8"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.jbkempf.com/blog/post/2018/Introducing-dav1d"
 PKG_URL="https://code.videolan.org/videolan/dav1d/-/archive/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.bz2"
@@ -14,5 +14,5 @@ if [ "${TARGET_ARCH}" = "x86_64" ]; then
   PKG_DEPENDS_TARGET+=" nasm:host"
 fi
 
-PKG_MESON_OPTS_TARGET="-Dbuild_tools=false \
-                       -Dbuild_tests=false"
+PKG_MESON_OPTS_TARGET="-Denable_tools=false \
+                       -Denable_tests=false"
