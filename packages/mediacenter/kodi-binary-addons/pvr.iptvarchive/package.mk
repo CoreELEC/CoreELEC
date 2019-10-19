d0afb0da6cf7c2be4d2cd806f706e42d44468249
Arthur Liberman <arthur_liberman@hotmail.com>
2019-10-19 04:12:31 +0300

    pvr.iptvarchive: hide HasArchive icon when archive is disabled

--- packages/mediacenter/kodi-binary-addons/pvr.iptvarchive/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvarchive/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="pvr.iptvarchive"
 PKG_VERSION="3.8.3-Leia"
 PKG_SHA256="feac27e3bd97d115ad9f3920be6f43447972194246d0f33fa295d9a4f33e4c48"
-PKG_REV="1"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.iptvsimple"
