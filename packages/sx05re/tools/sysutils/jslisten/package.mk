05a5ce1d84a8530d9a166c5373136d8926d82281
Shanti Gilbert <shantic@gmail.com>
2019-10-20 20:44:25 -0500

    jslisten: add path for EmuELEC addon

--- packages/sx05re/tools/sysutils/jslisten/package.mk
+++ packages/sx05re/tools/sysutils/jslisten/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="jslisten"
-PKG_VERSION="f08e98533b1c77bd9d250348ffc40c24abe1af32"
-PKG_SHA256="d211c26c0815ca77c692b246f02178994bcfbf3917aa39d15ccb2992e1a7e5c4"
+PKG_VERSION="039084cb2c006586f87a4f407da4762977065104"
+PKG_SHA256="03a3f8ae41d174d6f326a32f6a9f0f0bcbf04d26e2d26f472b90a1621015bdf6"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/shantigilbert/jslisten"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
