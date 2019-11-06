67b72745e9cb5cd0df929b87001dd22f655104b5
Portisch <hugo.portisch@yahoo.de>
2019-11-06 11:53:06 +0100

    libamcodec: bump to 5faa7d4d

--- projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="libamcodec"
-PKG_VERSION="5fa8d401f3d00e7cc0e0b34bf19a0d6a51561603"
-PKG_SHA256="1dfede5dee6c112170327c31d164c776e464aac3abfb3bc4e0cafd0c75ab12ef"
+PKG_VERSION="5faa7d4d554640dd6343900e24f43be327cea314"
+PKG_SHA256="464e1e6e225b0b41f615fff8537e471c6f48046063e352f95431450d37c1574b"
 PKG_LICENSE="proprietary"
 PKG_SITE="http://openlinux.amlogic.com"
-PKG_URL="https://github.com/CoreELEC/libamcodec/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://sources.coreelec.org/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="libamplayer: Interface library for Amlogic media codecs"
 PKG_TOOLCHAIN="manual"
