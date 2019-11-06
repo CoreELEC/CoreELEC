1e1936a1e781a34c54255600f9c88d57f6e66dd4
Portisch <hugo.portisch@yahoo.de>
2019-11-06 13:12:23 +0100

    ceemmc: changed package URL

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -2,10 +2,11 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="ceemmc"
-PKG_VERSION="0.1"
+PKG_VERSION="f905079f320da9d0d95d3601b4fc0f5a5e7217bb"
+PKG_SHA256="d39ba4c8bc178ffdcf0bf9424fec12fff0c25552a2031c4c5f3b2dc7b592a7b0"
 PKG_LICENSE="proprietary"
 PKG_SITE="https://coreelec.org"
-PKG_URL=""
+PKG_URL="https://sources.coreelec.org/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Tool to install CoreELEC on internal eMMC"
 PKG_TOOLCHAIN="manual"
