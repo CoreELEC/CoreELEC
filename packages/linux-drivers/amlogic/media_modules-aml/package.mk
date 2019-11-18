ce5b5cdc42a6df1896b4a5ebe66d77ce7bf1289b
adamg <adamg88@users.noreply.github.com>
2019-11-18 09:09:31 +0000

    media_modules-aml: introduce new package

---
+++ packages/linux-drivers/amlogic/media_modules-aml/package.mk
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="media_modules-aml"
+PKG_VERSION="fe556771e66fdac71ca11529de92f8d7c33ebe80"
+PKG_SHA256="dcbb76757095dc9329e784f8288f6c6d45d8ae52dacfd32ab23426df6f592517"
+PKG_LICENSE="GPL"
+PKG_SITE="https://coreelec.org"
+PKG_URL="https://github.com/CoreELEC/media_modules-aml/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="$PKG_NAME: Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="manual"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  kernel_make -C $(kernel_path) M=$PKG_BUILD/drivers \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MPEG12=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MPEG2_MULTI=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MPEG4=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MPEG4_MULTI=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_VC1=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_H264=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_H264_MULTI=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_H264_MVC=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_H265=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_VP9=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MJPEG=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_MJPEG_MULTI=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_REAL=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_AVS=m \
+    CONFIG_AMLOGIC_MEDIA_VDEC_AVS2=m \
+    CONFIG_AMLOGIC_MEDIA_VENC_H264=m \
+    CONFIG_AMLOGIC_MEDIA_VENC_H265=m
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+}
