1c171927e9350a5628237734a3e36e69fe7d71ab
adamg <adamg88@users.noreply.github.com>
2019-11-18 09:25:42 +0000

    libamcodec: bump to 31cd6ec for multi decoder support

--- projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="libamcodec"
-PKG_VERSION="5faa7d4d554640dd6343900e24f43be327cea314"
-PKG_SHA256="464e1e6e225b0b41f615fff8537e471c6f48046063e352f95431450d37c1574b"
+PKG_VERSION="31cd6eceaa1402b9f4ff5cc349e53899860fe9b9"
+PKG_SHA256="9052aaa00c827d3bfef7de83f55678aa1674368f06911a003b138953409f4d7c"
 PKG_LICENSE="proprietary"
 PKG_SITE="http://openlinux.amlogic.com"
 PKG_URL="https://sources.coreelec.org/$PKG_NAME-$PKG_VERSION.tar.xz"
