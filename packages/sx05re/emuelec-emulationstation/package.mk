af115b4f2e0c7aaab332a4c91a51b9caa5c9ec4d
Shanti Gilbert <shantic@gmail.com>
2019-11-25 01:53:53 -0600

    Emuelec-emulationstation: Bump to 8f1bc65
    Fixes gamepads with special characters in the name

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="a4611c7da5d3cf439c6d69cf0b3757a47fc22010"
+PKG_VERSION="8f1bc65221a7cb229ab2a12507aeb9d30df9184f"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
