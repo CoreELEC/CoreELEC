6795fb0f08e4d015bfe6ca519394f1009dbf8ac4
Shanti Gilbert <shantic@gmail.com>
2019-12-09 01:39:27 -0600

    Add stealla (standalone) package

---
+++ packages/sx05re/emulators/stellasa/package.mk
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="stellasa"
+PKG_VERSION="1ac4f8e3620ba22c1a571810723aa0093f3de23c"
+PKG_SHA256="836e20b073dc2463383ef7e5e45fbf423bf5f3c26677db4ea169374c5d0fc61f"
+PKG_REV="1"
+PKG_LICENSE="GPL2"
+PKG_SITE="https://github.com/stella-emu/stella"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="A multi-platform Atari 2600 Emulator"
+PKG_TOOLCHAIN="configure"
+
+pre_configure_target() { 
+TARGET_CONFIGURE_OPTS="--host=${TARGET_NAME} --with-sdl-prefix=${SYSROOT_PREFIX}/usr/bin --disable-windowed"
+}
+
+make_target() {
+cd $PKG_BUILD
+mv $PKG_BUILD/.${TARGET_NAME}/* $PKG_BUILD
+make 
+}
+
+makeinstall_target() {
+mkdir -p $INSTALL/usr/bin
+cp -rf $PKG_BUILD/stella $INSTALL/usr/bin
+}
