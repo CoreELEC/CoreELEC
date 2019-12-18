994b8d7a7db7bf4aeb503f77dfbe6508942260b7
adamg <adamg88@users.noreply.github.com>
2019-12-18 14:31:33 +0000

    systemd: re-enable debug-shell.service, removed in previous merge

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -283,4 +283,5 @@ post_install() {
   enable_service network-base.service
   enable_service systemd-timesyncd.service
   enable_service systemd-timesyncd-setup.service
+  enable_service debug-shell.service
 }
