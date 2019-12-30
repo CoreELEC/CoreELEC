d098029d7b5196bc30826afa0e20e7caf848edca
Shanti Gilbert <shantic@gmail.com>
2019-12-30 17:11:53 -0600

    Parallel-n64: Revert to ab155da1
    Newer versions don't seem to compile

--- packages/sx05re/libretro/parallel-n64/package.mk
+++ packages/sx05re/libretro/parallel-n64/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="parallel-n64"
-PKG_VERSION="6822bdc5ca89e701adb37bb31716142af19daa97"
-#PKG_SHA256="42940f899efd36e75d9b02d79069393a5ef0c9891f6d03350f6cda83791e62df"
+PKG_VERSION="ab155da1"
+PKG_SHA256="42940f899efd36e75d9b02d79069393a5ef0c9891f6d03350f6cda83791e62df"
 PKG_REV="2"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/parallel-n64"
@@ -16,7 +16,7 @@ PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-lto"
 
 make_target() {
-  make platform=armv8
+  make platform=emuelec64-armv8
 }
 
 makeinstall_target() {
