7521ded5d8cf0d44f36bcd0d62ec0cf28c4f5dd2
cdu13a <cdu13a@gmail.com>
2019-12-29 21:20:05 -0500

    Amlogic-ng:u-boot: add u-boot for the Le Potato SBC

---
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-LePotato/package.mk
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="u-boot-LePotato"
+PKG_VERSION="b497a82d864d07e4d4e8ffe7f05870536b2b38d0"
+PKG_SHA256="bcdac5c884493aa9b906e635fa12c5f549753ecea78416dc93a49e152ff343a7"
+PKG_LICENSE="GPL"
+PKG_SITE="https://www.denx.de/wiki/U-Boot"
+PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-elf:host gcc-linaro-arm-eabi:host"
+PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
+PKG_TOOLCHAIN="manual"
+
+configure_package() {
+  PKG_UBOOT_CONFIG="libretech_cc_defconfig"
+}
+
+pre_make_target() {
+  sed -i "s|arm-none-eabi-|arm-eabi-|g" $PKG_BUILD/Makefile $PKG_BUILD/arch/arm/cpu/armv8/gx*/firmware/scp_task/Makefile 2>/dev/null || true
+}
+
+make_target() {
+  [ "${BUILD_WITH_DEBUG}" = "yes" ] && PKG_DEBUG=1 || PKG_DEBUG=0
+  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make mrproper
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make $PKG_UBOOT_CONFIG
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="$HOST_CC" HOSTSTRIP="true"
+}
+
+makeinstall_target() {
+  : # nothing
+}
