d01a2b32e12a5368b56762c01aa7133df86c67a8
Shanti Gilbert <shantic@gmail.com>
2019-12-31 15:21:43 -0600

    Emuelec-emulationstation: Bump to d5093d3

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="cc35c0e5d3d89a92a94478f16edfd5f5ca9586aa"
+PKG_VERSION="d5093d3565a3f6085fe54b16ad4c657823033b6e"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
