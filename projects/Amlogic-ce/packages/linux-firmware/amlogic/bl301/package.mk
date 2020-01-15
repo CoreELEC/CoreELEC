11aed65ff4b7f1f57b9a2e6dbea29fa2d1db5590
Portisch <hugo.portisch@yahoo.de>
2020-01-15 12:57:20 +0100

    bl301: bump to 4ad6f792

--- projects/Amlogic-ce/packages/linux-firmware/amlogic/bl301/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/amlogic/bl301/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="bl301"
-PKG_VERSION="a824623258d3f92b89303f6ae4e4d1047022c032"
-PKG_SHA256="b451dd91649a6e6ecc689bc42e563491be39d292fd383bf0aba988499c30d6e8"
+PKG_VERSION="4ad6f7920bd9412aa758e432d82761c7642ac7ce"
+PKG_SHA256="a19f20f95074cda71fc6dc46c47eb083d76ac27ce7d217acf1e8807393ec2744"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/bl301/archive/$PKG_VERSION.tar.gz"
@@ -16,23 +16,23 @@ make_target() {
   export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
   DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make mrproper
 
-  for PKG_BL301_SUBDEVICE in ${BL301_SUBDEVICES}; do
-    PKG_UBOOT_CONFIG=${PKG_BL301_SUBDEVICE}_defconfig
-
-    if [[ -f "${PKG_BUILD}/configs/${PKG_UBOOT_CONFIG,,}" ]]; then
-      echo Building bl301 for ${PKG_BL301_SUBDEVICE}
-      DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make ${PKG_UBOOT_CONFIG,,}
-      DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="${HOST_CC}" HOSTSTRIP="true" bl301.bin
-      mv ${PKG_BUILD}/build/scp_task/bl301.bin ${PKG_BUILD}/build/${PKG_BL301_SUBDEVICE}_bl301.bin
-      echo "moved to: " ${PKG_BUILD}/build/${PKG_BL301_SUBDEVICE}_bl301.bin
-    fi
+  for f in $(find ${PKG_BUILD}/configs -mindepth 1); do
+    PKG_UBOOT_CONFIG=$(basename -- "$f")
+    PKG_BL301_SUBDEVICE=${PKG_UBOOT_CONFIG%_defconfig}
+    echo Building bl301 for ${PKG_BL301_SUBDEVICE}
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make ${PKG_UBOOT_CONFIG}
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="${HOST_CC}" HOSTSTRIP="true" bl301.bin
+    mv ${PKG_BUILD}/build/scp_task/bl301.bin ${PKG_BUILD}/build/${PKG_BL301_SUBDEVICE}_bl301.bin
+    echo "moved blob to: " ${PKG_BUILD}/build/${PKG_BL301_SUBDEVICE}_bl301.bin
   done
 }
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/bootloader/bl301
 
-  for PKG_BL301_SUBDEVICE in ${BL301_SUBDEVICES}; do
+  for f in $(find ${PKG_BUILD}/configs -mindepth 1); do
+    PKG_UBOOT_CONFIG=$(basename -- "$f")
+    PKG_BL301_SUBDEVICE=${PKG_UBOOT_CONFIG%_defconfig}
     PKG_BIN=${PKG_BUILD}/build/${PKG_BL301_SUBDEVICE}_bl301.bin
     cp -av ${PKG_BIN} ${INSTALL}/usr/share/bootloader/bl301/${PKG_BL301_SUBDEVICE}_bl301.bin
   done
