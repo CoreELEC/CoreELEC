cc8b4449fc89ad377261e90a8bcc56649e45005c
Portisch <hugo.portisch@yahoo.de>
2020-01-16 15:35:41 +0100

    inject_bl301: bump to f3653e6d   The tool is now able to inject the user defined wake up keys from   config.ini directly to the bl301 blob. The tool will run on every boot   and update the injected blob if needed (like if config.ini key changed).

--- projects/Amlogic-ce/packages/sysutils/inject_bl301/package.mk
+++ projects/Amlogic-ce/packages/sysutils/inject_bl301/package.mk
@@ -2,19 +2,39 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="inject_bl301"
-PKG_VERSION="2313a5b8fc6fae7251e5f02b6161da12468fa035"
-PKG_SHA256="47d59141dae79ccb332d9608498e521d33febac2eda3e5056dbc129122a6efc6"
+PKG_VERSION="86adad8949a337f30665c6b2a589138fdbcf378e"
+PKG_SHA256="3bd3ba2cbd054c1ef0203bde1bbfdaf4ecb70f3e2b072bf05a308f1937168376"
 PKG_LICENSE="proprietary"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://sources.coreelec.org/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain bl301"
 PKG_LONGDESC="Tool to inject bootloader blob BL301.bin on internal eMMC"
 PKG_TOOLCHAIN="manual"
 
+pre_make_target() {
+  cp -av ${PKG_DIR}/config/bl301.conf ${PKG_BUILD}/bl301.conf
+  for f in $(find $(get_build_dir bl301) -mindepth 1 -name 'coreelec_config.c'); do
+    cat ${f} | awk -F'[(),"]' '/.config_id\s*=\s*HASH/ {printf("%s %s\n", $2, $3)}' | \
+      while read id name; do
+        if ! grep -Fwq "${id}" ${PKG_BUILD}/bl301.conf; then
+          echo -e '\n['${id}']' >> ${PKG_BUILD}/bl301.conf;
+          cat ${f%.*}.h | awk -v id="HASHSTR_${id} " '$0 ~ id {printf("config_id=%s\n", $3)}' >> ${PKG_BUILD}/bl301.conf;
+          echo -e "config_name=${name}" >> ${PKG_BUILD}/bl301.conf;
+        fi
+      done
+  done
+}
+
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/sbin
-  mkdir -p $INSTALL/etc/inject_bl301
-    install -m 0755 inject_bl301 $INSTALL/usr/sbin/inject_bl301
-    install -m 0755 $PKG_DIR/scripts/checkbl301.sh $INSTALL/usr/sbin/checkbl301
-    install -m 0644 $PKG_DIR/config/bl301.conf $INSTALL/etc/inject_bl301/bl301.conf
+  mkdir -p ${INSTALL}/usr/sbin
+  mkdir -p ${INSTALL}/usr/lib/coreelec
+  mkdir -p ${INSTALL}/etc/inject_bl301
+    install -m 0755 inject_bl301 ${INSTALL}/usr/sbin/inject_bl301
+    install -m 0755 ${PKG_DIR}/scripts/check-bl301.sh ${INSTALL}/usr/lib/coreelec/check-bl301
+    install -m 0755 ${PKG_DIR}/scripts/update-bl301.sh ${INSTALL}/usr/lib/coreelec/update-bl301
+    install -m 0644 ${PKG_BUILD}/bl301.conf ${INSTALL}/etc/inject_bl301/bl301.conf
+}
+
+post_install() {
+  enable_service update-bl301.service
 }
