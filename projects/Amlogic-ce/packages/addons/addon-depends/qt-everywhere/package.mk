4fa8ed3f6758282065c4911b67ba89009b4b8332
Paulchen-Panther <Paulchen-Panter@protonmail.com>
2020-01-21 18:54:29 +0100

    Fix image formats in the program
    Signed-off-by: Paulchen-Panther <Paulchen-Panter@protonmail.com>

--- projects/Amlogic-ce/packages/addons/addon-depends/qt-everywhere/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/qt-everywhere/package.mk
@@ -27,8 +27,8 @@ PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
                            -no-sql-mysql
                            -system-zlib
                            -no-mtdev
-                           -no-gif
-                           -no-libpng
+                           -qt-libjpeg
+                           -qt-libpng
                            -no-harfbuzz
                            -no-libproxy
                            -system-pcre
