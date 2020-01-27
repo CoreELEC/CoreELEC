051513dbb1b969e2ac2b0d1c69cba871cf3487d0
Portisch <hugo.portisch@yahoo.de>
2020-01-27 15:29:26 +0100

    ceemmc: bump to c3d23cfa

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="ceemmc"
-PKG_VERSION="574211d9ac1384de314e4d3daf5e1d6951971f32"
-PKG_SHA256="ff1fe96be37f1128171c6acfbbf0cc642b23b5c478d221e885c5869e665435ee"
+PKG_VERSION="c3d23cfa851840bc1896165bf0ba42cfd592db59"
+PKG_SHA256="a9df8c71d278f40cb088344c9627c7cca9a99c054d7885a66a595c7d37b952ec"
 PKG_LICENSE="proprietary"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://sources.coreelec.org/$PKG_NAME-$PKG_VERSION.tar.xz"
