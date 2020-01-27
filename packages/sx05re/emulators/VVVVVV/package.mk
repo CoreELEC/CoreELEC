f861b6ba66f4485035a3deb5b225b6de44423cc8
Shanti Gilbert <shantic@gmail.com>
2020-01-26 19:40:10 -0600

    VVVVVV: Bump to 4ea4a1e

--- packages/sx05re/emulators/VVVVVV/package.mk
+++ packages/sx05re/emulators/VVVVVV/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="VVVVVV"
-PKG_VERSION="916f182ce6df764283a7dd37ad681f5752e7503a"
-PKG_SHA256="8afcf58f15d5f24610b4470ff359b2ae64ae44350fb2021fdf08104dbbdaa239"
+PKG_VERSION="4ea4a1e6154c249d8ea7b9b31651e94366553c99"
+PKG_SHA256="10067071dd9118cba49a486d21d091af3fe543f6e9e01097518f7b4848492648"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/TerryCavanagh/VVVVVV"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain SDL2-git"
 PKG_SHORTDESC="VVVVVV License: https://github.com/TerryCavanagh/VVVVVV/blob/master/LICENSE.md"
 PKG_LONGDESC="VVVVVV is a platform game all about exploring one simple mechanical idea - what if you reversed gravity instead of jumping?"
 PKG_TOOLCHAIN="cmake"
