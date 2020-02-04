8b6d8d1eee4a04d84167fcefc1c9ba9b4118a8d2
Shanti Gilbert <shantic@gmail.com>
2020-02-04 01:08:13 -0600

    Introduce triggerhappy package
    Still not in use

---
+++ packages/sx05re/tools/sysutils/triggerhappy/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="triggerhappy"
+PKG_VERSION="b822888066129350e51ad79f1cf307fa38dae4f7"
+PKG_SHA256="1b3221963b9adca26eacc5442d8c2422cf2072f8adde1dbd691a69b37a583fb1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/wertarbyte/triggerhappy"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="A lightweight hotkey daemon "
+PKG_TOOLCHAIN="make"
