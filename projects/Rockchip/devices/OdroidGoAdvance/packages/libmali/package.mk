9bab7cb66f8eb7c5906b234fd95012a298375d4b
Shanti Gilbert <shantic@gmail.com>
2020-02-15 23:48:20 -0600

    Odroidgoadvance: Make sure libmali does not overwrite libs

---
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/libmali/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libmali"
+PKG_VERSION="d4000def121b818ae0f583d8372d57643f723fdc"
+PKG_SHA256="4f2103fc927cc006ee5c9b647e899f50b0dcaeee127fec713387d06a333eb404"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/LibreELEC/libmali"
+PKG_URL=""
+PKG_LONGDESC="OpenGL ES user-space binary for the ARM Mali GPU family"
+PKG_DEPENDS_TARGET="mali-bifrost alverga"
+PKG_TOOLCHAIN="manual"
