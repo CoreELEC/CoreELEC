1951bb2251191cde37f13a3b68406a61d916991e
adamg <adamg88@users.noreply.github.com>
2020-02-21 13:34:22 +0000

    rtkbt-firmware-aml: fix rtl8822bs
    The config file the Amlogic repo does not seem to work with rtk_hciattach
    
    After trying many different versions, this appears to be the only
    one that will bring the device up.
    
    https://github.com/ChalesYu/rtl8822bs-aml/blob/master/bluetooth/rtl8822b_config.bin

--- projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
@@ -20,7 +20,11 @@ makeinstall_target() {
   FWDIR=$INSTALL/$(get_full_firmware_dir)/rtlbt
 
   mkdir -p $FWDIR
-    cp -a $PKG_BUILD/rtl8723bs_* $FWDIR
-    cp -a $PKG_BUILD/rtl8822b_* $FWDIR
+    cp -a $PKG_BUILD/rtl8723bs_config $FWDIR/rtl8723b_config
+    cp -a $PKG_BUILD/rtl8723bs_fw $FWDIR/rtl8723b_fw
+
+    cp -a $PKG_BUILD/rtl8822bs_config $FWDIR/rtl8822b_config
+    cp -a $PKG_BUILD/rtl8822bs_fw $FWDIR/rtl8822b_fw
+
     cp -a $PKG_BUILD/rtl8822cs_* $FWDIR
 }
