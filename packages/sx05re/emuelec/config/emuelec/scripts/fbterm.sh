955b02865e2ed41d67e63cd6c8bc619011f0c3b1
Shanti Gilbert <shantic@gmail.com>
2020-02-29 00:41:34 -0600

    Kmscon: introduce package
    Used to display scripts on OdroidGoAdvance

--- packages/sx05re/emuelec/config/emuelec/scripts/fbterm.sh
+++ packages/sx05re/emuelec/config/emuelec/scripts/fbterm.sh
@@ -9,11 +9,23 @@ if [ -e /proc/device-tree/t82x@d00c0000/compatible ]; then
 	/emuelec/scripts/setres.sh 16
 fi
 
+EE_DEVICE=$(cat /ee_arch)
 
-if [[ "$1" == *"13 - Launch Terminal (kb).sh"* ]]; then
-fbterm -s 32 --verbose < /dev/tty1
+if [ "$EE_DEVICE" == "OdroidGoAdvance" ]; then
+	#kmscon
+	if [[ "$1" == *"13 - Launch Terminal (kb).sh"* ]]; then
+	kmscon --font-size 8 --login /usr/bin/bash
+	else
+	kmscon --font-size 8 --login /usr/bin/bash "$1"
+	fi 
+
 else
-fbterm "$1" -s 24 < /dev/tty1
-fi 
+
+	if [[ "$1" == *"13 - Launch Terminal (kb).sh"* ]]; then
+	fbterm -s 32 --verbose < /dev/tty1
+	else
+	fbterm "$1" -s 24 < /dev/tty1
+	fi 
+fi
 
 joy2keyStop
--- packages/sx05re/emuelec/package.mk
+++ packages/sx05re/emuelec/package.mk
@@ -20,7 +20,7 @@ PKG_TOOLCHAIN="make"
 PKG_EXPERIMENTAL="munt_neon nestopiaCV quasi88 xmil np2kai hypseus triggerhappy"
 PKG_EMUS="$LIBRETRO_CORES advancemame PPSSPPSDL reicastsa reicastsa_old amiberry hatarisa openbor dosbox-sdl2 mupen64plus-nx mba.mini.plus scummvmsa residualvm commander-genius stellasa VVVVVV devilutionX"
 PKG_TOOLS="common-shaders scraper Skyscraper MC libretro-bash-launcher SDL_GameControllerDB linux-utils xmlstarlet CoreELEC-Debug-Scripts sixaxis jslisten evtest"
-PKG_RETROPIE_DEP="bash pyudev dialog six git dbus-python pygobject coreutils fbterm"
+PKG_RETROPIE_DEP="bash pyudev dialog six git dbus-python pygobject coreutils"
 PKG_DEPENDS_TARGET+=" $PKG_EMUS $PKG_TOOLS $PKG_RETROPIE_DEP $PKG_EXPERIMENTAL"
 
 # Removed cores for space and/or performance
@@ -29,6 +29,12 @@ PKG_DEPENDS_TARGET+=" $PKG_EMUS $PKG_TOOLS $PKG_RETROPIE_DEP $PKG_EXPERIMENTAL"
 # These packages are only meant for S922x, S905x2 and A311D devices as they run poorly on S905, S912, etc"
 if [ "$PROJECT" == "Amlogic-ng" ]; then
 PKG_DEPENDS_TARGET+=" $LIBRETRO_S922X_CORES mame2016 steam-controller"
+fi
+
+if [ "$DEVICE" == "OdroidGoAdvance" ]; then
+	PKG_DEPENDS_TARGET+=" kmscon odroidgoa-utils"
+	else
+	PKG_DEPENDS_TARGET+=" fbterm"
 fi
 
 make_target() {
