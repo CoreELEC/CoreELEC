e97cca1c9f9edd244e32ece27ff217473243b729
Kegg1701 <40126590+Kegg1701@users.noreply.github.com>
2020-03-01 17:28:40 +0000

    addition of Pokemini  (#125)
    * KEgg VVVVVV icon update
    
    * Pokemini add to build list
    
    Added Pokemini to EmuELEC options libretro core list
    
    * conflict resolve
    
    pokemini and snes conflicted because a dual edit
    
    * pokemini_libretro core added
    
    * change options pokemini lpl

--- packages/sx05re/emuelec-emulationstation/config/scripts/getcores.sh
+++ packages/sx05re/emuelec-emulationstation/config/scripts/getcores.sh
@@ -103,6 +103,9 @@ case "$1" in
 "mame")
 	CORES="AdvanceMame,mame2003_plus_libretro,mame2010_libretro,fbneo_libretro,mba_mini_libretro"
 	;;
+"pokemini")
+	CORES="pokemini_libretro"
+	;;
 "psp"|"pspminis")
 	CORES="PPSSPPSA,ppsspp_libretro"
 	;;
