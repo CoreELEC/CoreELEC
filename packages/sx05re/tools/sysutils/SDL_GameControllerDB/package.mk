b9973f08fdbd0f94b7b1a2b46b17d19d94a3c568
Shanti Gilbert <shantic@gmail.com>
2020-03-01 16:45:10 -0600

    SDL_GameControllerDB: Fix compilation

--- packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
+++ packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL_GameControllerDB"
-PKG_VERSION="ef8542c9f354e0f65cb861b67f0aa81366060874"
-PKG_SHA256="0e00c189d46a96682065cb6e506b97f5c2c89ff54244f069b7a98155a49ffeae"
+PKG_VERSION="c5992e0edc141be1fd0764c810792b4ead26e25c"
+PKG_SHA256="8e86c7b05d442b114f150d96958d7e310b28b9bb0b802bc54d0fad26ddcbc2af"
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
-PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/$PKG_VERSION.tar.gz"
+#PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
+PKG_SITE="https://github.com/gabomdq/gamecontrollerdb"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
 PKG_TOOLCHAIN="manual"
