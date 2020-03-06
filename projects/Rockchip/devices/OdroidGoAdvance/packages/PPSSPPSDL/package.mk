63718327e0233c9ec4e2e0757bbcecba5127b583
Shanti Gilbert <shantic@gmail.com>
2020-03-06 12:05:52 -0600

    Revert "PPSSPPSDL: Use upstream version "
    This reverts commit 51363c9da33fc6b2bfa32855c891f34602b5d52b.

--- projects/Rockchip/devices/OdroidGoAdvance/packages/PPSSPPSDL/package.mk
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/PPSSPPSDL/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="3bd0b3fa7410b5aa7c72ecde68f41f533952eba9"
+PKG_VERSION="c2d8437c2d0ba1a7958b6627fbd5cf3e5877e8eb"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
-PKG_SITE="https://github.com/hrydgard/ppsspp"
-PKG_URL="https://github.com/hrydgard/ppsspp.git"
-PKG_DEPENDS_TARGET="toolchain ffmpeg libzip libpng SDL2-git zlib zip"
+PKG_SITE="https://github.com/OtherCrashOverride/ppsspp-go2"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain ffmpeg libzip libpng SDL2-git zlib zip libgo2"
 PKG_SHORTDESC="PPSSPPDL"
 PKG_LONGDESC="PPSSPP Standalone"
 GET_HANDLER_SUPPORT="git"
@@ -21,6 +21,9 @@ PKG_CMAKE_OPTS_TARGET+="-DUSE_SYSTEM_FFMPEG=ON \
                         -DUSING_GLES2=ON \
                         -DUSING_X11_VULKAN=OFF \
                         -DUSE_DISCORD=OFF"
+pre_configure_target() {
+sed -i "s|include_directories(/usr/include/drm)|include_directories(${SYSROOT_PREFIX}/usr/include/drm)|" $PKG_BUILD/CMakeLists.txt
+}
 pre_make_target() {
   # fix cross compiling
   find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
