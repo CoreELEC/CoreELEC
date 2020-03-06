28a8d86e4cd6e81ce09c64ee46ac4cdf8d227bef
Shanti Gilbert <shantic@gmail.com>
2020-03-06 12:21:08 -0600

    Introduce sdljoytest package
    Useful to map new gamepads to gamecontrollerdb.txt

---
+++ packages/sx05re/tools/sysutils/sdljoytest/package.mk
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="sdljoytest"
+PKG_VERSION="c316d9e87a102e1d42882fc862f7953e578ae257"
+PKG_SHA256="b5812d24990eb4092ce3b46dc5b1670ba1d80bfee6c52a5ae4eb24abbf48359d"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/Wintermute0110/sdljoytest"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2-git"
+PKG_LONGDESC="Test joystick with SDL2 in Linux"
+PKG_TOOLCHAIN="make"
+
+pre_configure_target() {
+sed -i "s|gcc|${CC}|" Makefile
+}
+
+makeinstall_target() {
+mkdir -p $INSTALL/usr/config/emuelec/bin
+cp -rf test_gamepad_SDL2 $INSTALL/usr/config/emuelec/bin
+cp -rf map_gamepad_SDL2 $INSTALL/usr/config/emuelec/bin
+}
