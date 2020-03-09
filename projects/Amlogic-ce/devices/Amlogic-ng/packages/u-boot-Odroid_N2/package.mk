1ad471fd77a9998d3a5c2ab18b8f1d50d7f09106
Portisch <hugo.portisch@yahoo.de>
2020-03-09 20:15:12 +0100

    u-boot-Odroid_N2: bump to 191b5170

--- projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Odroid_N2/package.mk
@@ -2,14 +2,18 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Odroid_N2"
-PKG_VERSION="ff5c8ef69cfe050ceb20375555e3db6138fc1149"
-PKG_SHA256="ff5a3a6db1689aeec7baa68be53a00df091265b799f52e93505a31d21ce9a331"
+PKG_VERSION="191b5170d0955699b0341c6bc3655636d20d6a25"
+PKG_SHA256="5db877266bb6eaa1dbd072c86e44c7729b85655c63c115417f4f5802d8168e42"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-elf:host gcc-linaro-arm-eabi:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
 PKG_TOOLCHAIN="manual"
+
+pre_make_target() {
+  sed -i "s|arm-none-eabi-|arm-eabi-|g" $PKG_BUILD/Makefile $PKG_BUILD/arch/arm/cpu/armv8/*/firmware/scp_task/Makefile 2>/dev/null || true
+}
 
 configure_package() {
   PKG_UBOOT_CONFIG="odroidn2_defconfig"
