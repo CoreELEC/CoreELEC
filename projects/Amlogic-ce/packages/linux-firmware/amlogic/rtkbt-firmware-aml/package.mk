b59c44a85dd915e3617f18eeb12eb7f98c3ee5da
adamg <adamg88@users.noreply.github.com>
2020-03-29 23:52:52 +0100

    rtkbt-firmware-aml: use CE repo for firmware

--- projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
@@ -2,29 +2,18 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="rtkbt-firmware-aml"
-PKG_VERSION="28865e18c4dde6efc5ef34a7bb77ad5e9b85ba1c"
-PKG_SHA256="4621a45cb5a66d05f93fa5710386b74bd6cbcb4e931531f5e22fc4d05131f5ea"
+PKG_VERSION="763c7195bb0328b45a5679e212ab05150361cc3a"
+PKG_SHA256="2996dd867990a14b8d431c789285363ff35f4f2304ce8c92db401122ec43db9a"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/khadas/android_hardware_amlogic_bluetooth"
-PKG_URL="https://github.com/khadas/android_hardware_amlogic_bluetooth/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/rtkbt-firmware-aml"
+PKG_URL="https://github.com/CoreELEC/rtkbt-firmware-aml/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain rtk_hciattach"
 PKG_LONGDESC="Realtek BT Linux firmware"
 PKG_TOOLCHAIN="manual"
-
-unpack() {
-  mkdir -p $PKG_BUILD
-  tar --strip-components=5 -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $PKG_BUILD android_hardware_amlogic_bluetooth-$PKG_VERSION/realtek/rtkbt/Firmware/TV
-}
 
 makeinstall_target() {
   FWDIR=$INSTALL/$(get_full_firmware_dir)/rtlbt
 
   mkdir -p $FWDIR
-    ln -sf /usr/lib/kernel-overlays/base/lib/firmware/rtl_bt/rtl8723bs_config-OBDA8723.bin $FWDIR/rtl8723b_config
-    ln -sf /usr/lib/kernel-overlays/base/lib/firmware/rtl_bt/rtl8723bs_fw.bin $FWDIR/rtl8723b_fw
-
-    cp -a $PKG_BUILD/rtl8822bs_config $FWDIR/rtl8822b_config
-    cp -a $PKG_BUILD/rtl8822bs_fw $FWDIR/rtl8822b_fw
-
-    cp -a $PKG_BUILD/rtl8822cs_* $FWDIR
+    cp -a $PKG_BUILD/* $FWDIR
 }
