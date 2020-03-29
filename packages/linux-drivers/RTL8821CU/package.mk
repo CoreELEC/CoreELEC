83d53566b263f67af8757685f312742edb0d931b
Arthur Liberman <arthur_liberman@hotmail.com>
2020-03-29 09:26:45 +0000

    RTL8821CU: bump package to f791028 - new driver

--- packages/linux-drivers/RTL8821CU/package.mk
+++ packages/linux-drivers/RTL8821CU/package.mk
@@ -2,17 +2,28 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="RTL8821CU"
-PKG_VERSION="fb7fd86822da12522499f01df34f980e5cb337e5"
-PKG_SHA256="c29424be8f5c241444228c0cb2c8dd2db086420b77ff21a78d207ddf51302636"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/smp79/rtl8821CU"
-PKG_URL="https://github.com/smp79/rtl8821CU/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="rtl8821CU-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_SECTION="driver"
 PKG_LONGDESC="Realtek RTL8821CU Linux driver"
 PKG_IS_KERNEL_PKG="yes"
+
+case "$LINUX" in
+  amlogic-3.14)
+    PKG_VERSION="178fcbf4f1bf5b94580b5708016d0b2c2ded1720"
+    PKG_SHA256="29d3e053dd1fad37ee03de65e4ed2b25a4fb9aaf8bb6bd435da477753d03ad26"
+    PKG_URL="https://github.com/smp79/rtl8821CU/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="rtl8821CU-$PKG_VERSION*"
+    ;;
+  amlogic-4.9)
+    PKG_VERSION="f7910283478ac1b508ff163d30e4b374bf99f7cb"
+    PKG_SHA256="b2128cbc23ecf9b17bbbd9652a2453d73403276a56b11eb8a795d168156cd53e"
+    PKG_URL="https://github.com/smp79/rtl8821CU/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="rtl8821CU-$PKG_VERSION*"
+    ;;
+esac
 
 pre_make_target() {
   unset LDFLAGS
