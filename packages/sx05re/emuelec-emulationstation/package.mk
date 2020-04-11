21fa31114003ac84ebadd3ad3403e1a68ce33f0c
Shanti Gilbert <shantic@gmail.com>
2020-04-11 12:57:48 -0500

    Emulationstation: Bump to 0730448
    Add double tap to shutdown, for OGA and GTKING-pro

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="505eed66d7929cbb4fdf6ad969d1a1957c8ba5cc"
+PKG_VERSION="0730448a7beb78fb9ca1bba0d40e39287aea21aa"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
