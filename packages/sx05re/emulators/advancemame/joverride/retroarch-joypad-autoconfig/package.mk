41f52cd060ed2f405fc988f58ab6984ce01e6af5
Shanti Gilbert <shantic@gmail.com>
2020-04-23 15:37:00 -0500

    joypad autoconfig: bumnp to 3d78a9d

--- packages/sx05re/emulators/advancemame/joverride/retroarch-joypad-autoconfig/package.mk
+++ packages/sx05re/emulators/advancemame/joverride/retroarch-joypad-autoconfig/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="652bfed517970427b2b65ebe7da49ebaa8397617"
+PKG_VERSION="3d78a9d01d72f2a7b521344ff64658b0cd4006fa"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
