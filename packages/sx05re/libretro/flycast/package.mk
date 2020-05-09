332d0bc023a20aea2b0edb75f79341d55ae59aa1
Shanti Gilbert <shantic@gmail.com>
2020-05-09 12:43:50 -0500

    Flycast: Bump to 6b34166 / cleanup

--- packages/sx05re/libretro/flycast/package.mk
+++ packages/sx05re/libretro/flycast/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="flycast"
-PKG_VERSION="8288faeb4cefabc5a65a8334322dfa7cc5fdf81f"
-PKG_SHA256="8d4d961fe5b0986098babc22c9086a10555dbceafd5342d45f996f4bdc402fe7"
+PKG_VERSION="6b341667e88581c65cb5d4ad200cf3df665b2a35"
+PKG_SHA256="bb4dddb9a9f184a6c54d982c220520f384f4b808d4b0004af477c82cdab7f1d4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/flycast"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
@@ -45,9 +45,6 @@ pre_make_target() {
   esac
   
  if [ "$DEVICE" == "OdroidGoAdvance" ]; then
- #temp fix
- sed -i "s|-flto -fuse-linker-plugin|-fuse-linker-plugin|g" Makefile
- sed -i "s|HAVE_OPENMP = 0|HAVE_OPENMP = 1|g" Makefile
 	PKG_MAKE_OPTS_TARGET+=" platform=classic_armv8_a35"
  fi 
 }
