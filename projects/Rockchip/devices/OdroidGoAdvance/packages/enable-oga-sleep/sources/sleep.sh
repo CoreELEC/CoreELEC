595f3a85735f3ae619d200ae146221d0295d1bbd
Shanti Gilbert <shantic@gmail.com>
2020-05-22 12:13:24 -0500

    OdroidGoAdvance: Fix Sleep for v1.0

--- projects/Rockchip/devices/OdroidGoAdvance/packages/enable-oga-sleep/sources/sleep.sh
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/enable-oga-sleep/sources/sleep.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+OGA=$(cat /proc/device-tree/compatible)
+
 case $1 in
    pre)
     # unload esp8090 WiFi module
-    modprobe -r esp8089
+[[ "${OGA}" == *"v11"* ]] && modprobe -r esp8089
     # Store sound state. Try to avoid having max volume after resume
     alsactl store -f /tmp/asound.state
 	# workaround until dwc2 is fixed
@@ -18,7 +20,7 @@ case $1 in
 	modprobe -r dwc2
 	modprobe -i dwc2
 	# re-load WiFi module
-	modprobe esp8089
+[[ "${OGA}" == *"v11"* ]] &&  modprobe esp8089
     # re-detect and reapply sound, brightness and hp state
     systemctl start odroidgoa-headphones.service
 	;;
