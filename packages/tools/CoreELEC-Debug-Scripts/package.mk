53b2d9e2a2a44b0f252b26dbfc94e2f4fd29a7de
Shanti Gilbert <shantic@gmail.com>
2020-05-22 01:43:03 -0500

    Debug-scripts: Replace printf for echo

--- packages/tools/CoreELEC-Debug-Scripts/package.mk
+++ packages/tools/CoreELEC-Debug-Scripts/package.mk
@@ -11,9 +11,9 @@ PKG_SOURCE_NAME="$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_LONGDESC="A set of scripts to help debug user issues with CoreELEC"
 PKG_TOOLCHAIN="manual"
 
-
 makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
+    find . -type f -name "*.sh" -print0 | xargs -0 sed -i "s/printf \"/echo \"/g"
     install -m 0755 dispinfo.sh $INSTALL/usr/bin/dispinfo
     install -m 0755 remoteinfo.sh $INSTALL/usr/bin/remoteinfo
     install -m 0755 audinfo.sh $INSTALL/usr/bin/audinfo
