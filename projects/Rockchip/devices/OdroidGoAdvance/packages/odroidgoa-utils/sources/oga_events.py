ea9c8a72e4024e03e64e627da3891a2fcc505d2a
Shanti Gilbert <shantic@gmail.com>
2020-05-24 17:48:43 -0500

    OdroidGoAdvance: Better way to handle gamepad on v1.1
    Thanks to @valadaa48 for the patch!

--- projects/Rockchip/devices/OdroidGoAdvance/packages/odroidgoa-utils/sources/oga_events.py
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/odroidgoa-utils/sources/oga_events.py
@@ -6,7 +6,7 @@ import time
 from subprocess import check_output
 
 pwrkey = evdev.InputDevice("/dev/input/event0")
-odroidgo2_joypad = evdev.InputDevice("/dev/input/event2")
+odroidgo2_joypad = evdev.InputDevice("/dev/input/by-path/platform-odroidgo2-joypad-event-joystick")
 
 need_to_swallow_pwr_key = False # After a resume, we swallow the pwr input that triggered the resume
 
@@ -47,7 +47,7 @@ async def handle_event(device):
                     need_to_swallow_pwr_key = False
 
 
-        elif device.name == "odroidgo2_joypad":
+        elif device.name.find('odroidgo2') != -1:
             keys = odroidgo2_joypad.active_keys()
             print(keys)
             if event.value == 1 and Joypad.f3 in keys:
