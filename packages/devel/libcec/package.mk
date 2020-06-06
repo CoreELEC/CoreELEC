b391bc911119de625d2843d4a711372ded987d94
Portisch <hugo.portisch@yahoo.de>
2020-06-06 09:39:30 +0200

    Revert "libcec: bump to 72ae683f"
    This reverts commit c2b8dba080e0ea99a5ace33ee83eb1d19ce185b7.

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libcec"
-PKG_VERSION="72ae683fbf827175068290671312a3d1ed087404"
-PKG_SHA256="a40e082f2880dca792f79be88eacc67d265b9b17de2424a9ffd25073e49aa94c"
+PKG_VERSION="libcec-4.0.4"
+PKG_SHA256="4382a964bf8c511c22c03cdab5ba2d81c241536e6479072a61516966804f400a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_URL="https://github.com/Pulse-Eight/libcec/archive/$PKG_VERSION.tar.gz"
