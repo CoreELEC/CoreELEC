8a595bd3412d4fb534a4812611725c6f33a2aeed
Shanti Gilbert <shantic@gmail.com>
2020-06-08 00:09:00 -0500

    Kmscon: Fix missing dependency libxkbcommon

--- projects/Rockchip/devices/OdroidGoAdvance/packages/kmscon/package.mk
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/kmscon/package.mk
@@ -7,20 +7,9 @@ PKG_SHA256="6b7efdb4f9b6715208898ee4757364c04d1bb903182bba1667644dd68c11524d"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/dvdhrm/kmscon"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libtsm"
+PKG_DEPENDS_TARGET="toolchain libtsm libxkbcommon"
 PKG_LONGDESC="Linux KMS/DRM based virtual Console Emulator"
 PKG_TOOLCHAIN="autotools"
 
 
 PKG_CONFIGURE_OPTS_TARGET=" --disable-debug --with-video=fbdev,drm2d,drm3d --disable-multi-seat --with-sessions=dummy,terminal"
-
-
-#pre_configure_target() {
- 
-#}
-
-#makeinstall_target() { 
-#}
-
-
-
