686e15af186bc64047960d1820507442936dbc06
Portisch <hugo.portisch@yahoo.de>
2020-06-19 21:31:41 +0200

    linux: bump amlogic-4.9 to 00fc9eba

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -26,8 +26,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-4.9)
-    PKG_VERSION="8426535bb9c7502eab94e4eb41e985aaa2916b32"
-    PKG_SHA256="9f48e4bc9fbe4956b889cd9e816fb070bdcbf1e6326d8f494eed9544479080c8"
+    PKG_VERSION="00fc9eba7b8836341d84e02a2e60ec78fc648ce8"
+    PKG_SHA256="2a769c4bc9c4066e3732ecab47bfb9ddb51f23cf6e7019bafe2954ebed55fbf8"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
