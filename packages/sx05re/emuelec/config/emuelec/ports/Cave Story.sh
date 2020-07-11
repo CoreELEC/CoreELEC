82190ff8caba6a2f49c76cb14859576f98b1df00
shantigilbert <shantic@gmail.com>
2020-07-11 01:53:02 -0500

    fbterm.sh: add "error" to display a dialog box with an error for 10 secs
    Useful for ports or other stuff, based on the modifications on #218

--- packages/sx05re/emuelec/config/emuelec/ports/Cave Story.sh
+++ packages/sx05re/emuelec/config/emuelec/ports/Cave Story.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [[ ! -f /storage/roms/ports/CaveStory/Doukutsu.exe ]] || [[ ! -d /storage/roms/ports/CaveStory/data ]]; then
-    /emuelec/scripts/fbterm.sh "echo Could not find Doukutsu.exe or the data directory, please make sure you copied the game into /storage/roms/ports/CaveStory.; sleep 10" &
+    /emuelec/scripts/fbterm.sh error "Cavestory" "Could not find Doukutsu.exe or the data directory, please make sure you copied the game into /storage/roms/ports/CaveStory \n\n This box will close in 10 seconds" &
     error_process="$!"
     sleep 10
-    # Kill fbterm and all children
-    pkill -P "$!"
+    pkill -P $error_process
     exit 1
 fi
 
