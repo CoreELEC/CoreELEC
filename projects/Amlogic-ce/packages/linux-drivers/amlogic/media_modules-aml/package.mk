47af67703e17cd242c5b5881602699aa28a41fbf
cdu13a <cdu13a@gmail.com>
2020-07-23 19:58:51 -0400

    media_modules-aml: bump to 7d4cf61
    Improvement for N2+

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/media_modules-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/media_modules-aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_modules-aml"
-PKG_VERSION="73fef6910edc202d335648dc74576c274dd8b807"
-PKG_SHA256="c4e5a6e33f6b55b4ac90962306fc3d1f7ccfc75f4a5b115761f9426a6e966fa8"
+PKG_VERSION="7d4cf61ca34bd8273cbc81dfd46bb89138ef9481"
+PKG_SHA256="9b09841de64a5a4111dc8580b6776773a635726e51df262a9df1845e31ea4124"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/media_modules-aml/archive/$PKG_VERSION.tar.gz"
