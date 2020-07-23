7aff0cedfa733d00b1cdb7b0b5fe803bba2e010e
shantigilbert <shantic@gmail.com>
2020-07-23 14:09:30 -0500

    xow: bump to 9e86c52

--- packages/sx05re/tools/sysutils/xow/package.mk
+++ packages/sx05re/tools/sysutils/xow/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="xow"
-PKG_VERSION="5c7ead6eb4785ad30e6adbcf62a36a35db9f6f53"
-PKG_SHA256="bce29513afff4b696f9ce3f175fc1ec96f05ee14f4de32c366ee8073077176d2"
+PKG_VERSION="9e86c529afab1ffde40e2554e5eee1ecfcbf8c7a"
+PKG_SHA256="6112e8a0f9246dec746beb11f191f89b7078dcba2b7f1460b628e031b3b1fd33"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/medusalix/xow"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
