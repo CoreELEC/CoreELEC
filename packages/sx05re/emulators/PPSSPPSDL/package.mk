ee899250023de5444a5e05900b35da3a3e906d5b
shantigilbert <shantic@gmail.com>
2020-07-23 14:03:55 -0500

    PPSSPPSDL: Bump to 3403e28

--- packages/sx05re/emulators/PPSSPPSDL/package.mk
+++ packages/sx05re/emulators/PPSSPPSDL/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="c739d4e16424751a4dde73fe769dd783ec5037b5"
+PKG_VERSION="3403e28470966c11ff37d459677c8bba2f2c25d0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
