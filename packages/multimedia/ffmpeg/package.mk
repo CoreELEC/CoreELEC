511090c0e8c7252bc0e103656df495daa23c6334
shantigilbert <shantic@gmail.com>
2020-07-23 22:06:16 -0500

    ffmpeg: fix SDL2 dependency for ffplay

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -9,7 +9,7 @@ PKG_SHA256="e11e7594af35f36ab2711252c3d6bb106908f26605498aef4a9be2d7bc001db2"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://ffmpeg.org"
 PKG_URL="https://github.com/xbmc/FFmpeg/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib bzip2 openssl speex"
+PKG_DEPENDS_TARGET="toolchain zlib bzip2 openssl speex SDL2-git"
 PKG_LONGDESC="FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video."
 PKG_BUILD_FLAGS="-gold"
 
