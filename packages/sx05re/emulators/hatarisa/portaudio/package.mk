7b3e25e02a5b12e4769081bdc4404e4ea0107c1b
shantigilbert <shantic@gmail.com>
2020-07-30 23:51:23 -0500

    Portaudio: Use github
    Since portaudio.com seems to be down 
    Closes #242

--- packages/sx05re/emulators/hatarisa/portaudio/package.mk
+++ packages/sx05re/emulators/hatarisa/portaudio/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="portaudio"
-PKG_VERSION="190600_20161030"
-PKG_SHA256="f5a21d7dcd6ee84397446fa1fa1a0675bb2e8a4a6dceb4305a8404698d8d1513"
+PKG_VERSION="64d1bf913433d7a2835526b80a93487d5038cb8e"
+PKG_SHA256="c33e48217a844cca3b66ab88571b90beefb8fcb6967177951d4dc283587bf834"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.portaudio.com"
-PKG_URL="http://www.portaudio.com/archives/pa_stable_v$PKG_VERSION.tgz"
+PKG_SITE="https://portaudio.com"
+PKG_URL="https://github.com/zhang-ray/portaudio/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain alsa-lib"
 PKG_LONGDESC="PortAudio is a free, cross-platform, open-source, audio I/O library."
