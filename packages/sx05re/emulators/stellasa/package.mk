d9ba8baa8dfd85009f12971e43f7e33eca691396
shantigilbert <shantic@gmail.com>
2020-08-13 01:15:13 -0500

    stellaSA: Bump to 7964c93

--- packages/sx05re/emulators/stellasa/package.mk
+++ packages/sx05re/emulators/stellasa/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="stellasa"
-PKG_VERSION="5f5ac9500961b828997341f76d874d36ecfd5f46"
-PKG_SHA256="bc37d7f289adefa1aa92e7235a856f08e8419e17b6170ad681227bb2ea4da29f"
+PKG_VERSION="7964c93a2fc3f0690f03ed39c6f7c2a9293e3f5f"
+PKG_SHA256="3de1b3479a64140b163c5f55a2ed27cd1642ae76b0fe5845a2e77451fac86a57"
 PKG_REV="1"
 PKG_LICENSE="GPL2"
 PKG_SITE="https://github.com/stella-emu/stella"
