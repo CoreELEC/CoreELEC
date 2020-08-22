81fecf2e833bd3629ceedcf199107aedfb6a23dc
shantigilbert <shantic@gmail.com>
2020-08-22 18:10:23 -0500

    Ports builder: Fix and enable
    Add new host package jinja2 and eable pyyaml on host

--- packages/sx05re/tools/sysutils/pyyaml/package.mk
+++ packages/sx05re/tools/sysutils/pyyaml/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="pyyaml"
-PKG_VERSION="0f64cbfa54b0b22dc7b776b7b98a7cd657e84d78"
+PKG_VERSION="d0d660d035905d9c49fc0f8dafb579d2cc68c0c8"
 PKG_SHA256=""
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/yaml/pyyaml"
@@ -10,6 +10,14 @@ PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python2 distutilscross:host"
 PKG_LONGDESC="Python 2 and 3 compatibility library "
 PKG_TOOLCHAIN="manual"
+
+make_host() {
+  python3 setup.py build
+}
+
+makeinstall_host() {
+  python3 setup.py install --prefix=$TOOLCHAIN
+}
 
 pre_make_target() {
   export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
