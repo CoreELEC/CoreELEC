964aa98e3f001e600eb42c8aaaed77e13fd9659e
shantigilbert <shantic@gmail.com>
2020-09-01 02:38:08 -0500

    OdroidGoAdvance: Enable rs97-commander-sdl2

--- packages/sx05re/emuelec/config/emuelec/scripts/modules/02 - File Manager (kb).sh
+++ packages/sx05re/emuelec/config/emuelec/scripts/modules/02 - File Manager (kb).sh
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
-source /emuelec/scripts/env.sh
-joy2keyStart
-
-mc -a
+if [[ "$EE_DEVICE" == "OdroidGoAdvance" ]]; then
+    DinguxCommander
+else
+    source /emuelec/scripts/env.sh
+    joy2keyStart
+    mc -a
+fi
--- packages/sx05re/tools/sysutils/rs97-commander-sdl2/package.mk
+++ packages/sx05re/tools/sysutils/rs97-commander-sdl2/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="rs97-commander-sdl2"
-PKG_VERSION="4bc1bf2b08e94bcd17d5ed93fb0c3d4193b05d28"
-PKG_SHA256="8b09adc1204cb9b18232f38baf98943a7aa02deb932e1c3327a9f6417ef7848d"
+PKG_VERSION="30a9f6a588d220ac233b4c02c8c55fb1e24aeeeb"
+PKG_SHA256="441aa06ca35ac0e56cfbfa45522832f50794f2f95475305ce3dddc6100e6693d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/zfteam/rs97-commander-sdl2"
+PKG_SITE="https://github.com/EmuELEC/rs97-commander-sdl2"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain SDL2_image SDL2_gfx"
 PKG_PRIORITY="optional"
@@ -17,6 +17,11 @@ PKG_SHORTDESC="Two-pane commander for RetroFW and RG-350 (fork of Dingux Command
 pre_configure_target() {
 sed -i "s|sdl2-config|${SYSROOT_PREFIX}/usr/bin/sdl2-config|" Makefile
 sed -i "s|CC=g++|CC=${CXX}|" Makefile
+
+[[ "$DEVICE" == "OdroidGoAdvance" ]] &&	OGA=1 || OGA=0
+
+PKG_MAKE_OPTS_TARGET=" ODROIDGO=${OGA} CC=$CXX"
+	
 }
 
 makeinstall_target() {
