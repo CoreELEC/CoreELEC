626818f5a7d8567cbef089a2d7fbb556747275f9
shantigilbert <shantic@gmail.com>
2020-10-09 01:46:21 -0500

    Add Pico-8 Support
    Just add the contents (sans folders) of the raspberry pi version into /storage/roms/bios/pico-8 and restart emulationstation

---
+++ packages/sx05re/emuelec/config/emuelec/scripts/pico8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+
+# If there is a new version copy the files
+if [[ -e "/storage/roms/bios/pico-8" ]]; then
+    cp -rf /storage/roms/bios/pico-8 /emuelec/bin/
+    rm -rf /storage/roms/bios/pico-8
+    chmod +x /emuelec/bin/pico-8/pico8_dyn
+    touch /storage/roms/pico-8/splore.p8
+fi 
+
+mkdir -p /emuelec/configs/pico-8
+
+if [[ ! -L "/emuelec/configs/pico-8/sdl_controllers.txt" ]]; then
+    mv sdl_controllers.txt sdl_controllers.txt.bak
+    ln -sf /storage/.config/SDL-GameControllerDB/gamecontrollerdb.txt sdl_controllers.txt
+fi
+
+CART=name=$(echo "${1}" | cut -f 1 -d '.')
+
+if [[ "${CART}" == *"/splore"* ]]; then
+    /emuelec/bin/pico-8/pico8_dyn -splore -home /emuelec/configs/pico-8 -root_path /storage/roms/pico-8 -joystick 0
+else
+    /emuelec/bin/pico-8/pico8_dyn -run ${CART} -home /emuelec/configs/pico-8 -root_path /storage/roms/pico-8 -joystick 0
+fi
