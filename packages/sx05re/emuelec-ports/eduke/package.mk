114a9663ccc213d344625d7ddbbe728e2aafa842
Shanti Gilbert <shantic@gmail.com>
2020-10-24 02:53:25 -0500

    eduke: Fix sdl2-config path

--- packages/sx05re/emuelec-ports/eduke/package.mk
+++ packages/sx05re/emuelec-ports/eduke/package.mk
@@ -9,11 +9,11 @@ PKG_SITE="https://eduke32.com"
 PKG_URL="https://voidpoint.io/terminx/eduke32.git"
 PKG_DEPENDS_TARGET="toolchain SDL2-git SDL2_mixer timidity"
 PKG_LONGDESC="EDuke32 is an awesome, free homebrew game engine and source port of the classic PC first person shooter Duke Nukem 3D"
-PKG_TOOLCHAIN="auto"
 GET_HANDLER_SUPPORT="git"
 PKG_TOOLCHAIN="make"
 
 pre_configure_target() {
+sed -i "s|sdl2-config|$SYSROOT_PREFIX/usr/bin/sdl2-config|" Common.mak
 PKG_MAKE_OPTS_TARGET=" duke3d LTO=0 SDL_TARGET=2 NOASM=1 HAVE_GTK2=0 POLYMER=1 USE_OPENGL=0"
 }
 
