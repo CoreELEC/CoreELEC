26daf40c81d32ece0f6479234c19cb48a473d563
Portisch <hugo.portisch@yahoo.de>
2020-10-27 19:55:22 +0100

    libcec: bump to 6.0.2

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libcec"
-PKG_VERSION="4.0.7"
-PKG_SHA256="bcd92c376993a5721d346edcc09eb17289451f9156b1d1d113c9663c2046315a"
+PKG_VERSION="6.0.2"
+PKG_SHA256="090696d7a4fb772d7acebbb06f91ab92e025531c7c91824046b9e4e71ecb3377"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_URL="https://github.com/Pulse-Eight/libcec/archive/libcec-${PKG_VERSION}.tar.gz"
@@ -16,9 +16,18 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
                        -DCMAKE_INSTALL_LIBDIR_NOARCH:STRING=lib \
                        -DSKIP_PYTHON_WRAPPER=1 \
                        -DHAVE_IMX_API=0 \
-                       -DHAVE_AOCEC_API=0 -DHAVE_AMLOGIC_API=0 \
                        -DHAVE_GIT_BIN=0 \
                        -DHAVE_RPI_LIB=0"
+
+if [ "${KODIPLAYER_DRIVER}" = "libamcodec" ]; then
+  if [ "${TARGET_KERNEL_ARCH}" = "arm64" ]; then
+    PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} -DHAVE_AOCEC_API=1"
+  else
+    PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} -DHAVE_AMLOGIC_API=1"
+  fi
+else
+  PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} -DHAVE_AOCEC_API=0 -DHAVE_AMLOGIC_API=0"
+fi
 
 # libX11 and xrandr to read the sink's EDID, used to determine the PC's HDMI physical address
 if [ "${DISPLAYSERVER}" = "x11" ]; then
@@ -35,7 +44,11 @@ post_makeinstall_target() {
   # Remove the Python3 demo - useless for us
   rm -f ${INSTALL}/usr/bin/pyCecClient
 
-  PYTHON_DIR=${INSTALL}/usr/lib/${PKG_PYTHON_VERSION}
+  # Remove the sysmlink and redirect to /var/lib so that we can change libcec versions at run time
+  rm -f ${INSTALL}/usr/lib/libcec.so.6
+  ln -sf /var/lib/libcec.so.6 ${INSTALL}/usr/lib/libcec.so.6
+
+	PYTHON_DIR=${INSTALL}/usr/lib/${PKG_PYTHON_VERSION}
   if [ -d ${PYTHON_DIR}/dist-packages ]; then
     mv ${PYTHON_DIR}/dist-packages ${PYTHON_DIR}/site-packages
   fi
