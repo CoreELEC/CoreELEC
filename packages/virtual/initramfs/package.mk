9afd202a023986e2f575cf8e2e6b5f5ba582e42a
Portisch <hugo.portisch@yahoo.de>
2020-11-04 08:50:59 +0100

    initramfs: update package for kernel 4.9

--- packages/virtual/initramfs/package.mk
+++ packages/virtual/initramfs/package.mk
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="initramfs"
 PKG_VERSION=""
@@ -23,3 +24,19 @@ fi
 for i in ${PKG_DEPENDS_INIT}; do
   PKG_NEED_UNPACK+=" $(get_pkg_directory ${i})"
 done
+
+post_install() {
+  if [ "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
+  ( 
+    cd $BUILD/initramfs
+
+    ln -sfn /usr/lib  $BUILD/initramfs/lib
+    ln -sfn /usr/bin  $BUILD/initramfs/bin
+    ln -sfn /usr/sbin $BUILD/initramfs/sbin
+
+    mkdir -p $BUILD/image
+    fakeroot -- sh -c \
+      "mkdir -p dev; mknod -m 600 dev/console c 5 1; find . | cpio -H newc -ov -R 0:0 > $BUILD/image/initramfs.cpio"
+  )
+  fi
+} 
