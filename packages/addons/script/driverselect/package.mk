6cc2fd62784fcfc8092e56b9ee2d857a30ac81c2
Portisch <hugo.portisch@yahoo.de>
2020-11-08 19:22:15 +0100

    Amlogic-ce: merge and update driverselect package

--- packages/addons/script/driverselect/package.mk
+++ packages/addons/script/driverselect/package.mk
@@ -1,14 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="driverselect"
-PKG_VERSION="8fe0e3dce8e717f00d8c0e5e4dadb7e9306be624"
-PKG_SHA256="42ca062bbb885f84472f91fa0b3354fff6ffb7ace3ae37cddca0b9923d066c8a"
+PKG_VERSION="17d69cfa9781b9f987428cccd22a240bec4ebf9b"
+PKG_SHA256="fd9e73b5618b52be9e95a7c2392ac49544a7d153e418c515dcff9915aaee7e2c"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="https://libreelec.tv"
-PKG_URL="https://github.com/b-jesch/script.program.driverselect/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/CoreELEC/script.program.driverselect/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="script.program"
 PKG_SHORTDESC="script.program.driverselect"
