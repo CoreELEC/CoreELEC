320e4da2da76adcdecdc5479311fb24df6daaf8e
Shanti Gilbert <shantic@gmail.com>
2020-12-02 12:34:11 -0600

    Youtube-dl: Back to Github and bump to 2020.12.02

--- packages/sx05re/tools/video/youtube-dl/package.mk
+++ packages/sx05re/tools/video/youtube-dl/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="youtube-dl"
-PKG_VERSION="latest"
-PKG_SHA256="b5660fa8441337c6b75448550bd6dcc7f97fe966b4ebb9bf47ff2b1cd4e3633a"
+PKG_VERSION="2020.12.02"
+PKG_SHA256="e315af9979143c28910bbaf8390b6c2b68c74b2e636001807234187636aa963f"
 PKG_LICENSE="The Unlicense"
-PKG_SITE="https://youtube-dl.org"
-PKG_URL="${PKG_SITE}/downloads/${PKG_VERSION}/youtube-dl"
+PKG_SITE="https://github.com/ytdl-org/youtube-dl"
+PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/youtube-dl"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Command-line program to download videos from YouTube.com and other video sites"
 PKG_TOOLCHAIN="manual"
