8248c26b7bf845633207d70a511cb4611c2390e0
Portisch <hugo.portisch@yahoo.de>
2020-12-06 13:44:24 +0100

    dtc: include zlib library for Amlogic multidtb compression

--- packages/tools/dtc/package.mk
+++ packages/tools/dtc/package.mk
@@ -8,18 +8,31 @@ PKG_SHA256="af720893485b02441f8812773484b286f969d1b8c98769d435a75c6ad524104b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/"
 PKG_URL="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="toolchain:host"
+PKG_DEPENDS_HOST="toolchain:host zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="The Device Tree Compiler"
 
 PKG_MAKE_OPTS_TARGET="dtc fdtput fdtget fdtdump libfdt"
 PKG_MAKE_OPTS_HOST="dtc libfdt"
 
+pre_configure_host() {
+  export LDLIBS_dtc="-lz"
+  export EXTRA_CFLAGS="-I${TOOLCHAIN}/include"
+}
+
+pre_make_host() {
+  make clean
+}
+
 makeinstall_host() {
   mkdir -p ${TOOLCHAIN}/bin
     cp -P ${PKG_BUILD}/dtc ${TOOLCHAIN}/bin
   mkdir -p ${TOOLCHAIN}/lib
     cp -P ${PKG_BUILD}/libfdt/libfdt.so ${TOOLCHAIN}/lib
+}
+
+pre_make_target() {
+  make clean
 }
 
 makeinstall_target() {
