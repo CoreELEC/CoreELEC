67de164d7fda6c97f80a7b64515eb7bf80e3e419
Portisch <hugo.portisch@yahoo.de>
2020-12-08 20:00:45 +0100

    Amlogic-ce: merge and update misc-packages package

--- packages/virtual/misc-packages/package.mk
+++ packages/virtual/misc-packages/package.mk
@@ -9,3 +9,9 @@ PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain ${ADDITIONAL_PACKAGES}"
 PKG_SECTION="virtual"
 PKG_LONGDESC="misc-packages: Metapackage for miscellaneous packages"
+
+# Entware support
+if [ "$ENTWARE_SUPPORT" = "yes" ]; then
+  ln -sf /storage/.opt $INSTALL/opt
+  PKG_DEPENDS_TARGET+=" entware"
+fi
