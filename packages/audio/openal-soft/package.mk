58aa1dc2c9519c14006e2d6529d717973c4dd29f
valadaa48 <61368862+valadaa48@users.noreply.github.com>
2021-01-06 22:11:24 -0500

    openal: bump from 1.19.1 to 1.20.1 (#388)

--- packages/audio/openal-soft/package.mk
+++ packages/audio/openal-soft/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openal-soft"
-PKG_VERSION="1.19.1"
-PKG_SHA256="9f3536ab2bb7781dbafabc6a61e0b34b17edd16bd6c2eaf2ae71bc63078f98c7"
+PKG_VERSION="1.20.1"
+PKG_SHA256="c32d10473457a8b545aab50070fe84be2b5b041e1f2099012777ee6be0057c13"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openal.org/"
 PKG_URL="https://github.com/kcat/openal-soft/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -11,15 +11,14 @@ PKG_DEPENDS_TARGET="toolchain alsa-lib openal-soft:host"
 PKG_LONGDESC="OpenAL the Open Audio Library"
 
 configure_package() {
-  PKG_CMAKE_OPTS_HOST="-DALSOFT_NATIVE_TOOLS=on \
-                       -DALSOFT_BACKEND_OSS=off \
+  PKG_CMAKE_OPTS_HOST="-DALSOFT_BACKEND_OSS=off \
                        -DALSOFT_BACKEND_WAVE=off \
                        -DALSOFT_BACKEND_PULSEAUDIO=on \
                        -DALSOFT_EXAMPLES=off \
                        -DALSOFT_TESTS=off \
                        -DALSOFT_UTILS=off"
 
-  PKG_CMAKE_OPTS_TARGET="-DALSOFT_NATIVE_TOOLS=off -DNATIVE_BIN_DIR=$PKG_BUILD/.$HOST_NAME/native-tools/ \
+  PKG_CMAKE_OPTS_TARGET="-DALSOFT_NATIVE_TOOLS_PATH=$PKG_BUILD/.$HOST_NAME/native-tools \
                          -DALSOFT_BACKEND_OSS=off \
                          -DALSOFT_BACKEND_WAVE=off \
                          -DALSOFT_BACKEND_PULSEAUDIO=on \
