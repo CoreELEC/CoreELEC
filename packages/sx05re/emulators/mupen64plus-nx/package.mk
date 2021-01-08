73565c51735d4d8bcd9b332b63f7593fc8a72772
shantigilbert <shantic@gmail.com>
2021-01-08 16:52:37 -0600

    mupen64plus-nx: Use older version on all devices
    New one has a lot of performance issues.

--- packages/sx05re/emulators/mupen64plus-nx/package.mk
+++ packages/sx05re/emulators/mupen64plus-nx/package.mk
@@ -2,12 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mupen64plus-nx"
-PKG_VERSION="ea1c677c1e61ce1d95809c09cf26ffa75cd7e9dc"
-PKG_SHA256="b2b06332523aef0fb44cb3b97cfab2122b627ca4ef11708b0e40c8699a2b288e"
-if [ $PROJECT = "Amlogic" ]; then
 PKG_VERSION="b785150465048fa88f812e23462f318e66af0be0"
 PKG_SHA256="456c433f45b0e2ba15a587978234e3e1300301d431b6823747ad0e779331c97e"
-fi
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
@@ -24,35 +20,24 @@ pre_configure_target() {
   sed -e "s|^GIT_VERSION ?.*$|GIT_VERSION := \" ${PKG_VERSION:0:7}\"|" -i Makefile
 
 if [ $ARCH == "arm" ]; then
-if [ ${PROJECT} = "Amlogic-ng" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=AMLG12B"
-	sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
-	sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
-elif [ "${PROJECT}" = "Amlogic" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=amlogic"
-elif [ "${DEVICE}" = "OdroidGoAdvance" ] || [ "$DEVICE" == "GameForce" ]; then
-	sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
-	sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
-	sed -i "s|cortex-a53|cortex-a35|g" Makefile
-	PKG_MAKE_OPTS_TARGET+=" platform=odroidgoa"
+	if [ ${PROJECT} = "Amlogic-ng" ]; then
+		PKG_MAKE_OPTS_TARGET+=" platform=AMLG12B"
+	elif [ "${PROJECT}" = "Amlogic" ]; then
+		PKG_MAKE_OPTS_TARGET+=" platform=amlogic"
+	elif [ "${DEVICE}" = "OdroidGoAdvance" ] || [ "$DEVICE" == "GameForce" ]; then
+		sed -i "s|cortex-a53|cortex-a35|g" Makefile
+		PKG_MAKE_OPTS_TARGET+=" platform=odroidgoa"
 	fi
 else
 	if [ ${PROJECT} = "Amlogic-ng" ]; then
 		PKG_MAKE_OPTS_TARGET+=" platform=odroid64 BOARD=N2"
-		sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
-		sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
 	elif [ "${PROJECT}" = "Amlogic" ]; then 
-		sed -i "s|GLES = 1|GLES = 1|g" Makefile
-		sed -i "s|-lGLESv2|-lGLESv2|g" Makefile
 		PKG_MAKE_OPTS_TARGET+=" platform=amlogic64"
 	elif [ "${DEVICE}" = "OdroidGoAdvance" ] || [ "$DEVICE" == "GameForce" ]; then
-		sed -i "s|GLES = 1|GLES3 = 1|g" Makefile
-		sed -i "s|-lGLESv2|-lGLESv3|g" Makefile
 		PKG_MAKE_OPTS_TARGET+=" platform=amlogic64"
 	fi
 fi
 }
-
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
