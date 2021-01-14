537d26f0867ac904502fea9e66f14c5848096ab9
shantigilbert <shantic@gmail.com>
2021-01-14 12:52:37 -0600

    Flycast: Disable HAVE_OPENMP
    This fixes #395

--- packages/sx05re/libretro/flycast/package.mk
+++ packages/sx05re/libretro/flycast/package.mk
@@ -38,7 +38,7 @@ PKG_BUILD_FLAGS="-gold"
 
 pre_configure_target() {
 # Flycast defaults to -O3 but then CHD v5 do not seem to work on EmuELEC so we change it to -O2 to fix the issue
-PKG_MAKE_OPTS_TARGET="ARCH=arm HAVE_OPENMP=1 GIT_VERSION=${PKG_VERSION:0:7} FORCE_GLES=1 SET_OPTIM=-O2 HAVE_LTCG=0"
+PKG_MAKE_OPTS_TARGET="ARCH=arm HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} FORCE_GLES=1 SET_OPTIM=-O2 HAVE_LTCG=0"
 
 if [ "${ARCH}" == "aarch64" ]; then
 PKG_MAKE_OPTS_TARGET+=" WITH_DYNAREC=arm64"
