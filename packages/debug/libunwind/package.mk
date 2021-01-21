b8c599d59019bee31bf1ad4f092b528a4e891801
shantigilbert <shantic@gmail.com>
2021-01-21 12:35:10 -0600

    libunwind: bump to 1.4.0

--- packages/debug/libunwind/package.mk
+++ packages/debug/libunwind/package.mk
@@ -2,16 +2,20 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libunwind"
-PKG_VERSION="1.2.1"
-PKG_SHA256="3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb"
+PKG_VERSION="1.4.0"
+PKG_SHA256="df59c931bd4d7ebfd83ee481c943edf015138089b8e50abed8d9c57ba9338435"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nongnu.org/libunwind/"
 PKG_URL="http://download.savannah.nongnu.org/releases/libunwind/libunwind-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="gcc:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="library to determine the call-chain of a program"
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-			   --disable-shared"
+                           --disable-shared \
+                           --disable-minidebuginfo \
+                           --disable-documentation \
+                           --disable-tests"
 
 makeinstall_target() {
   make DESTDIR=$SYSROOT_PREFIX install
