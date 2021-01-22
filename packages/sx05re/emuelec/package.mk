00d918df59218820b535990d75023d1b37893d9a
shantigilbert <shantic@gmail.com>
2021-01-22 10:51:39 -0600

    Enable building Sonic 1,2 and CD

--- packages/sx05re/emuelec/package.mk
+++ packages/sx05re/emuelec/package.mk
@@ -17,7 +17,7 @@ PKG_AUTORECONF="no"
 PKG_TOOLCHAIN="make"
 
 PKG_EXPERIMENTAL="munt nestopiaCV quasi88 xmil np2kai hypseus dosbox-x"
-PKG_EMUS="$LIBRETRO_CORES advancemame PPSSPPSDL amiberry hatarisa openbor dosbox-sdl2 mupen64plus-nx scummvmsa stellasa solarus dosbox-pure"
+PKG_EMUS="$LIBRETRO_CORES advancemame PPSSPPSDL amiberry hatarisa openbor dosbox-sdl2 mupen64plus-nx scummvmsa stellasa solarus dosbox-pure sonic2013 soniccd"
 PKG_TOOLS="emuelec-tools"
 PKG_DEPENDS_TARGET+=" $PKG_TOOLS $PKG_EMUS $PKG_EXPERIMENTAL emuelec-ports"
 
