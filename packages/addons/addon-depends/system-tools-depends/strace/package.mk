d28913e1b6e9f7628cbfb6e6986b62358ae2ad8e
shantigilbert <shantic@gmail.com>
2021-01-31 12:34:44 -0600

    strace: bump to 5.9

--- packages/addons/addon-depends/system-tools-depends/strace/package.mk
+++ packages/addons/addon-depends/system-tools-depends/strace/package.mk
@@ -2,15 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="strace"
-PKG_VERSION="5.8"
-PKG_SHA256="df4a669f7fff9cc302784085bd4b72fab216a426a3f72c892b28a537b71e7aa9"
+PKG_VERSION="5.9"
+PKG_SHA256="39473eb8465546c3e940fb663cb381eba5613160c7302794699d194a4d5d66d9"
 PKG_LICENSE="BSD"
 PKG_SITE="https://strace.io/"
 PKG_URL="https://strace.io/files/$PKG_VERSION/strace-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="strace is a diagnostic, debugging and instructional userspace utility"
 PKG_TOOLCHAIN="autotools"
+PKG_BUILD_FLAGS="-sysroot"
 
-makeinstall_target() {
-  :
-}
+PKG_CONFIGURE_OPTS_TARGET=" --enable-mpers=check"
