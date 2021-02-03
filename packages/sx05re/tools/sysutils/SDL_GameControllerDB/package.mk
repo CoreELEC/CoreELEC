86d87c587ac532b331815e7beac2c4e75f7f4e65
shantigilbert <shantic@gmail.com>
2021-02-03 15:51:29 -0600

    SDL_GameControllerDB: Use upstream version

--- packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
+++ packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL_GameControllerDB"
-PKG_VERSION="c5992e0edc141be1fd0764c810792b4ead26e25c"
-PKG_SHA256="8e86c7b05d442b114f150d96958d7e310b28b9bb0b802bc54d0fad26ddcbc2af"
+PKG_VERSION="cead9cccf79c0865cf8c2b7b652867372d63cd6e"
+PKG_SHA256="af45411e7b4a24b91f267cf2281c63df209e7552f41f29d9a4261a50363811e5"
 PKG_LICENSE="OSS"
-#PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
-PKG_SITE="https://github.com/gabomdq/gamecontrollerdb"
+PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
