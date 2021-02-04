041269e4c80ddcb7fa1b5708b0518fdb56d116ca
shantigilbert <shantic@gmail.com>
2021-02-04 16:26:26 -0600

    Crystal: bump to 6141520

--- packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
+++ packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="Crystal"
-PKG_VERSION="848d8f99d523b9266e500dc57ed313a2fce14cb1"
+PKG_VERSION="61415205cacdaf02174c9c45e80381cfef6b9daf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
