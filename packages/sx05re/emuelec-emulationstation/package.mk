4c3c8408acc41d316b517a9fa6b94f3b3c8d25e1
shantigilbert <shantic@gmail.com>
2021-02-08 03:08:24 -0600

    emuelec-emulationstation: Bump to 87d5ece
    X = Random, Y = Game Menu, R3 =  Favorite Toggle

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="3c817ff8c7eff8c8407be98ead35ff252e7391ed"
+PKG_VERSION="87d5ece0dc6efa9dd3352d836e78627d9d39f3ad"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
