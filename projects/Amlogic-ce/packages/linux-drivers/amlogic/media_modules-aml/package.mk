0250b4a1769ecf16b014513f05519caec993525e
Arthur Liberman <arthur_liberman@hotmail.com>
2021-02-16 22:51:07 +0200

    media_modules-aml: bump to 0181804

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/media_modules-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/media_modules-aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="media_modules-aml"
-PKG_VERSION="8829ade6b43b4f2acc934513e2b8b7a05a730586"
-PKG_SHA256="6a9544a867e91d278a4d2bfa0bc58baa3267b726d92f0b813a5b48bd0b31d13b"
+PKG_VERSION="01818043189006db511004b8ac2118c3db26f07b"
+PKG_SHA256="2e0a7ed4521ab08c5a3959fbd880e5482a8dcbefa3723fe42a5ef84b7de2bce5"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/media_modules-aml/archive/$PKG_VERSION.tar.gz"
