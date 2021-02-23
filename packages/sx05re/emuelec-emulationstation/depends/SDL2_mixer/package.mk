522529dfd7c8481c98a7d35634e75391d4e1c00a
shantigilbert <shantic@gmail.com>
2021-02-23 14:13:02 -0600

    emuelec-emulationstation: Fix ogg BGM not working

--- packages/sx05re/emuelec-emulationstation/depends/SDL2_mixer/package.mk
+++ packages/sx05re/emuelec-emulationstation/depends/SDL2_mixer/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git mpg123-compat"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git mpg123-compat libogg"
 PKG_LONGDESC="SDL_mixer 2.0.1"
 
 pre_configure_target() {
@@ -15,7 +15,8 @@ pre_configure_target() {
   PKG_CONFIGURE_OPTS_TARGET="--disable-fluidsynth \
 							 --disable-music-flac \
 							 --disable-music-mod-modplug \
-							 --disable-music-ogg-tremor \
+							 --enable-music-ogg-tremor \
+							 --enable-music-ogg \
 							 --enable-music-mp3"
 							 
 
