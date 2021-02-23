3683c68398185232a917f4a3d84a327bf113b10e
shantigilbert <shantic@gmail.com>
2021-02-22 18:22:59 -0600

    SonicCD: Bump to 8e970fd

--- packages/sx05re/emuelec-ports/soniccd/package.mk
+++ packages/sx05re/emuelec-ports/soniccd/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="soniccd"
-PKG_VERSION="ce80f3599ea9fe8ef06bf2aef2876246d7d904a0"
+PKG_VERSION="8e970fd30e1b401ee1dd3d81f58c1f4e46d30802"
 PKG_ARCH="any"
 PKG_SITE="https://github.com/Rubberduckycooly/Sonic-CD-11-Decompilation"
 PKG_URL="$PKG_SITE.git"
@@ -12,7 +12,7 @@ PKG_TOOLCHAIN="make"
 
 makeinstall_target() {
 mkdir -p $INSTALL/usr/bin
-cp soniccd $INSTALL/usr/bin
+cp bin/soniccd $INSTALL/usr/bin
 
 mkdir -p $INSTALL/usr/config/emuelec/configs/sonic
 cp $PKG_DIR/config/* $INSTALL/usr/config/emuelec/configs/sonic
