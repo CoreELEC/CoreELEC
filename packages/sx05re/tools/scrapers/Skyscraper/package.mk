d9578dc4f222bf0b941f13d2be61769a28f3e10c
shantigilbert <shantic@gmail.com>
2021-02-24 12:50:25 -0600

    SkyScraper: Bump to 7022edc

--- packages/sx05re/tools/scrapers/Skyscraper/package.mk
+++ packages/sx05re/tools/scrapers/Skyscraper/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="Skyscraper"
-PKG_VERSION="90a200438f0214fd8c78d89093d3373438055720"
+PKG_VERSION="7022edcc95054557a3b918ae75ce3eb827f1f6c4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
