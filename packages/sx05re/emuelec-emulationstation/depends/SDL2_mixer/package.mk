0fa27ce3d2c76072125d5a9eebc7d451cf5c6e0f
shantigilbert <shantic@gmail.com>
2021-02-27 09:22:36 -0600

    SDL2_mixer: Add missing dependency for OGG

--- packages/sx05re/emuelec-emulationstation/depends/SDL2_mixer/package.mk
+++ packages/sx05re/emuelec-emulationstation/depends/SDL2_mixer/package.mk
@@ -7,17 +7,16 @@ PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git mpg123-compat libogg"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git mpg123-compat libvorbis libogg"
 PKG_LONGDESC="SDL_mixer 2.0.1"
 
 pre_configure_target() {
 
-  PKG_CONFIGURE_OPTS_TARGET="--disable-fluidsynth \
+  PKG_CONFIGURE_OPTS_TARGET="--disable-sdltest \
+							 --disable-music-midi-fluidsynth \
 							 --disable-music-flac \
 							 --disable-music-mod-modplug \
 							 --enable-music-ogg-tremor \
 							 --enable-music-ogg \
 							 --enable-music-mp3"
-							 
-
 }
