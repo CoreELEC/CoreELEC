230ad35f13e65b07fee36deebe817b96b53d25b5
Portisch <hugo.portisch@yahoo.de>
2021-03-09 13:37:09 +0100

    bl301: add option to use multiple bl301 packages Some package status are not compatible with the vendor bootloader. Add multiple bl301 packages and use BL2_MIN_VERSION to be able to identify the correct bl301 package.

---
+++ packages/tools/bl301_221119/package.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2021-present Team CoreELEC (https://coreelec.org)
+
+. $(get_pkg_directory bl301_xxxxxx)/package.mk
+
+BL2_MIN_VERSION="221119"
+PKG_NAME="bl301_${BL2_MIN_VERSION}"
+PKG_VERSION="b04911fe96980613f0b0dfd8b9233f613d4874be"
+PKG_SHA256="0aec99de5abc581d7e38b8f07f27b9e9fdc4e4cc5b549dc54ee204da1b20fd5f"
+PKG_URL="https://github.com/CoreELEC/bl301/archive/$PKG_VERSION.tar.gz"
