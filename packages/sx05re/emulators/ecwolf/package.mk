104846af418430adc4ca29f147cd4e884a53b86b
shantigilbert <shantic@gmail.com>
2021-03-11 02:07:51 -0600

    ecwolf: Add package

---
+++ packages/sx05re/emulators/ecwolf/package.mk
@@ -0,0 +1,47 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="ecwolf"
+PKG_VERSION="3ce6e4d064b54eec72386fe949ec7be20746c16c"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://bitbucket.org/ecwolf/ecwolf"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain SDL2-git SDL2_mixer SDL2_net ecwolf:host"
+PKG_LONGDESC="ECWolf is a port of the Wolfenstein 3D engine based of Wolf4SDL. It combines the original Wolfenstein 3D engine with the user experience of ZDoom to create the most user and mod author friendly Wolf3D source port."
+PKG_TOOLCHAIN="cmake-make"
+
+pre_build_host() {
+HOST_CMAKE_OPTS=""
+}
+
+make_host() {
+  cmake . -DNO_GTK=ON
+  make
+}
+
+makeinstall_host() {
+: #no 
+}
+
+pre_configure_target() {
+PKG_CMAKE_OPTS_TARGET=" -DNO_GTK=ON \
+						-DFORCE_CROSSCOMPILE=ON \
+						-DIMPORT_EXECUTABLES=$PKG_BUILD/.$HOST_NAME/ImportExecutables.cmake						
+						-DCMAKE_BUILD_TYPE=Release"
+
+ cd $PKG_BUILD/deps/gdtoa
+ $HOST_CC -o rithchk arithchk.c -Wall -Wextra
+ ./rithchk > $PKG_BUILD/deps/gdtoa/arith.h
+
+ $HOST_CC -o qnan qnan.c -Wall -Wextra
+ ./qnan > $PKG_BUILD/deps/gdtoa/gd_qnan.h
+ cd $PKG_BUILD
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+  cp $PKG_BUILD/ecwolf $INSTALL/usr/bin/ecwolf
+  
+  mkdir -p $INSTALL/usr/config/emuelec/configs/ecwolf
+  cp $PKG_BUILD/ecwolf.pk3 $INSTALL/usr/config/emuelec/configs/ecwolf/ecwolf.pk3
+}
