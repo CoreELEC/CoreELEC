4b4891789ce1164982ecdfa9e43231bfdf8fc0bc
miwasp <39096757+miwasp@users.noreply.github.com>
2021-03-20 20:18:09 +0100

    Support for mods with doom similar to ECWolf (#528)
    * Support for mods similar to ECWolf. This works really good but somehow
    'breaks' the usage of libretro prboom. I had to copy the wad, iwad, pwad
    files to the subdir games. This way at least those still work with prboom.
    Calling .choco files with prboom gives an error.
    
    * Better handling of possible typos in .choco files for entry GAMETYPE
    
    * Mods are now placed in subdir "doom-mods" under ports. This way placing
    wad files in folder doom works like before, you can switch between prboom
    and chocolate-doom without a problem. No need to copy any files any more.

--- packages/sx05re/emuelec-ports/chocolate-doom/scripts/chocodoom.sh
+++ packages/sx05re/emuelec-ports/chocolate-doom/scripts/chocodoom.sh
@@ -5,19 +5,62 @@
 
 . /etc/profile
 
-CONFIG="/emuelec/configs/chocolate-doom"
+RUN_DIR="/storage/roms/ports/doom"
+CONFIG_DIR="/emuelec/configs/chocolate-doom"
 LOCALCONFIG="/storage/.local/share/chocolate-doom"
+
+params=" --savedir ${CONFIG_DIR}"
 
 if [ ! -L "${LOCALCONFIG}" ]; then 
 [[ -d "${LOCALCONFIG}" ]] && rm -rf "${LOCALCONFIG}"
-ln -sf "${CONFIG}" "${LOCALCONFIG}"
+ln -sf "${CONFIG_DIR}" "${LOCALCONFIG}"
 fi
 
 GUID=$(echo "${2}" | grep -Ewo '[[:xdigit:]]{32}' | head -n1)
 # echo "Will use controller with ${GUID}"
 # Try to set up gamepad
-sed -i "s|joystick_guid.*|joystick_guid                 \"${GUID}\"|" "${CONFIG}/chocolate-doom.cfg"
+sed -i "s|joystick_guid.*|joystick_guid                 \"${GUID}\"|" "${CONFIG_DIR}/chocolate-doom.cfg"
 
-/usr/bin/chocolate-doom -iwad "${1}"
+# EXT can be wad, WAD, iwad, IWAD, pwad, PWAD or choco
+FILE=$(basename -- "${1}")
+EXT=${1#*.}
 
+# If its not a simple wad (extension .choco) read the file and parse the data
+if [ ${EXT} == "choco" ]; then
+    while IFS== read -r key value; do
+	if [ "$key" == "GAMETYPE" ]; then
+	    case ${value} in
+		"doom"|"DOOM")
+		    PROGRAM="chocolate-doom"
+		;;
+		"heretic"|"HERETIC")
+		    PROGRAM="chocolate-heretic"
+		;;
+		"hexen"|"HEXEN")
+		    PROGRAM="chocolate-hexen"
+		;;
+		"strife"|"STRIFE")
+		    PROGRAM="chocolate-strife"
+		;;
+		*)
+		    PROGRAM="chocolate-doom"
+		;;
+	    esac
+	fi
+
+        if [ "$key" == "SUBDIR" ]; then
+	    RUN_DIR="/storage/roms/ports/doom/$value"
+        fi
+
+        if [ "$key" == "PARAMS" ]; then
+            params+=" $value"
+        fi
+    done < "${1}"
+else
+    PROGRAM="chocolate-doom"
+    params+=" -iwad ${1}"
+fi
+
+cd "${RUN_DIR}"
+/usr/bin/${PROGRAM} ${params} >/emuelec/logs/emuelec.log 2>&1
 
