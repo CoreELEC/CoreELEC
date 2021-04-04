362d703801f0ca545a69adffa568d7da807fe887
shantigilbert <shantic@gmail.com>
2021-04-04 13:00:18 -0500

    libsodium: bump to 1.0.18

--- packages/sx05re/emuelec-ports/devilutionX/libsodium/package.mk
+++ packages/sx05re/emuelec-ports/devilutionX/libsodium/package.mk
@@ -2,13 +2,12 @@
 # Copyright (C) 2020 Trond Haugland (github.com/escalade)
 
 PKG_NAME="libsodium"
-PKG_VERSION="45bca21a954799d64d9ad86f245916737cdb9971"
+PKG_VERSION="1.0.18"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/jedisct1/libsodium"
-PKG_URL="https://github.com/jedisct1/libsodium/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://libsodium.org"
+PKG_URL="https://download.libsodium.org/libsodium/releases/libsodium-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A modern, portable, easy to use crypto library"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+speed"
 
-PKG_CONFIGURE_OPTS_TARGET=" --disable-neon"
