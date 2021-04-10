1ce43a39f43e1395147ab6825ab295587e9ad7bb
Portisch <hugo.portisch@yahoo.de>
2021-04-10 22:50:10 +0200

    qca9377-aml: update driver

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/qca9377-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/qca9377-aml/package.mk
@@ -3,14 +3,14 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="qca9377-aml"
-PKG_VERSION="311e24939b53d0cbaf303cde3ed1dc22349602a9"
-PKG_SHA256="238e0174ec5ca3f87c184fade5eb7d1e4522d6af279aac6bd87f8bab24fc13ad"
+PKG_VERSION="b16df7d323458c461c9f6487af9afbf83bba3459"
+PKG_SHA256="397676ef19c1f4c7efa4c177e3c866632948bcdea1bf04e9707ba0707b6025c5"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://boundarydevices.com/new-silex-wifi-802-11ac-bt4-1-module/"
-PKG_URL="https://github.com/boundarydevices/qcacld-2.0/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/qca9377-aml"
+PKG_URL="https://github.com/CoreELEC/qca9377-aml/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
-PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="qca9377 Linux Driver"
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
@@ -18,8 +18,8 @@ PKG_TOOLCHAIN="manual"
 PKG_PATCH_DIRS="$LINUX"
 
 post_unpack() {
-  sed -i 's,-Wall,,g; s,-Werror,,g' $PKG_BUILD/Kbuild
-  sed -i 's,CDEFINES :=,CDEFINES := -Wno-misleading-indentation -Wno-unused-variable -Wno-unused-function,g' $PKG_BUILD/Kbuild
+  sed -i 's,-Wall,,g; s,-Werror,,g' ${PKG_BUILD}/AIO/drivers/qcacld-new/Kbuild
+  sed -i 's,CDEFINES :=,CDEFINES := -Wno-misleading-indentation -Wno-unused-variable -Wno-unused-function,g' ${PKG_BUILD}/AIO/drivers/qcacld-new/Kbuild
 }
 
 pre_make_target() {
@@ -28,12 +28,15 @@ pre_make_target() {
 }
 
 make_target() {
-  make KERNEL_SRC="$(kernel_path)" \
-    ARCH=$TARGET_KERNEL_ARCH \
-    CROSS_COMPILE=$TARGET_KERNEL_PREFIX
+  cd AIO/build
+  make KERNELPATH="$(kernel_path)" \
+    ARCH=${TARGET_KERNEL_ARCH} \
+    CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
+    INSTALL_ROOT=${PKG_BUILD} \
+    CONFIG_BUILDROOT=y
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
-    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp ${PKG_BUILD}/lib/modules/wlan.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}/wlan-${PKG_NAME}.ko
 }
