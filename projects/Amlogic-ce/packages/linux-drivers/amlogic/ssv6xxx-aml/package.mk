83fe4df3d2979567a60e7ab63b7acf70f16e6309
Portisch <hugo.portisch@yahoo.de>
2021-04-12 13:11:26 +0200

    ssv6xxx-aml: bump package to 0dddfae9

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/ssv6xxx-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/ssv6xxx-aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ssv6xxx-aml"
-PKG_VERSION="d1708e72e7e69ed2b29aa7a48d05be76544c8f1d"
-PKG_SHA256="4d8ec5677bd7e61c0ec87deef723433f4c5da927de1df4c4d37fed1507303846"
+PKG_VERSION="0dddfae91c07bd11274c443b6480a0a88234deed"
+PKG_SHA256="59552fa1400c6c81d10d6fe75c70ea10358681c094aa77a3a130277ed58a5268"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/khadas/android_hardware_wifi_icomm_drivers_ssv6xxx/"
