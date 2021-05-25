cc02979074088f04dd8c7026d6489ed21c0d301f
Portisch <hugo.portisch@yahoo.de>
2021-05-25 11:45:58 +0200

    splash-image: bump package to 2807b667

--- packages/tools/splash-image/package.mk
+++ packages/tools/splash-image/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020 present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="splash-image"
-PKG_VERSION="76925fc7781dcffa5a4517f251ebd004816a3de6"
-PKG_SHA256="7e972aada5f5aa1f12e4add0757970c9dfddc53b01a209ce38581af791b7e75a"
+PKG_VERSION="2807b667817208f803e5fd8ba96c65703faaf6ae"
+PKG_SHA256="f2a5d0c7d4d8766fb0deb26864eb43058a70f05a9a31f992dde77b1a5bf3c76c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/splash-image/archive/${PKG_VERSION}.tar.gz"
