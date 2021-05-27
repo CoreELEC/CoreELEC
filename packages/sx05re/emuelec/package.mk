bfa39a51f6dbb1b6420394a6cabe5316b2df2371
shantigilbert <shantic@gmail.com>
2021-05-27 01:27:44 -0500

    Swanstation: Use libretro name
    From Duckstation to Swanstation

--- packages/sx05re/emuelec/package.mk
+++ packages/sx05re/emuelec/package.mk
@@ -46,7 +46,7 @@ if [ "$ARCH" == "aarch64" ]; then
 for discore in munt_neon quicknes reicastsa_old reicastsa parallel-n64 pcsx_rearmed; do
 		PKG_DEPENDS_TARGET=$(echo $PKG_DEPENDS_TARGET | sed "s|$discore||")
 	done
-PKG_DEPENDS_TARGET+=" duckstation emuelec-32bit-libs"
+PKG_DEPENDS_TARGET+=" swanstation emuelec-32bit-libs"
 
 if [ "$PROJECT" == "Amlogic-ng" ]; then
 	PKG_DEPENDS_TARGET+=" dolphinSA"
--- packages/sx05re/emulators/swanstation/package.mk
+++ packages/sx05re/emulators/swanstation/package.mk
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
-PKG_NAME="duckstation"
-PKG_VERSION="2be5b81b685a2064709ee6ed1f75f15c36752650"
-PKG_SHA256="9fc4adb7f1ad15a69972839b324946f0b30c48f27bf860f7741360f89d537ce6"
+PKG_NAME="swanstation"
+PKG_VERSION="a39c7512c47c125f554c4e4725217c16628683e4"
+PKG_SHA256="7e3ba31e90f6cf58c41d8aee6f7101a07b16d29b9e9fd0af6c31fb72aa177821"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/libretro/duckstation"
+PKG_SITE="https://github.com/libretro/swanstation"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain nasm:host $OPENGLES"
 PKG_SECTION="libretro"
-PKG_SHORTDESC="DuckStation - PlayStation 1, aka. PSX Emulator"
+PKG_SHORTDESC="Fast PlayStation 1 emulator for PC and Android"
 PKG_TOOLCHAIN="cmake"
 PKG_BUILD_FLAGS="-lto"
 
@@ -20,5 +20,5 @@ pre_configure_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/.$TARGET_NAME/duckstation_libretro.so $INSTALL/usr/lib/libretro/
+  cp $PKG_BUILD/.$TARGET_NAME/swanstation_libretro.so $INSTALL/usr/lib/libretro/
 }
