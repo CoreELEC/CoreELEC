e8d6ca1bc598cd73d846553f84b40c665aab947a
Portisch <hugo.portisch@yahoo.de>
2021-06-16 17:13:06 +0200

    linux: bump package to 640e3a39

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -17,8 +17,8 @@ PKG_PATCH_DIRS="$LINUX"
 
 case "$LINUX" in
   amlogic-4.9)
-    PKG_VERSION="62aef14426eb89cfeabe5e4de86fe111a5e2e00a"
-    PKG_SHA256="392f605e0ba792d8f325bff870b64a60c8c170bc3c590040e100e43a273fe657"
+    PKG_VERSION="640e3a39bff3a1bdf338938eb09a11dcfc7f34b4"
+    PKG_SHA256="269be38b086f8c6074199da6b9a16653455a8401b48a9f7a83e7ac0e4306dd42"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
