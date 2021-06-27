bc2bc4737ced25866ab1b76dd2f92b27d26b9be9
shantigilbert <shantic@gmail.com>
2021-06-27 13:27:12 -0500

    advmame.sh: unset DISPLAY for Amlogic and Amlogic-ng
    Fixes #652

--- packages/sx05re/emulators/advancemame/bin/advmame.sh
+++ packages/sx05re/emulators/advancemame/bin/advmame.sh
@@ -6,6 +6,7 @@
 . /etc/profile
 
 CONFIG_DIR="/storage/.advance"
+export DISPLAY=:0
 
 if [ ! -d "$CONFIG_DIR" ]; then
  mkdir -p $CONFIG_DIR
@@ -19,29 +20,29 @@ sed -i "s|/roms/arcade|/roms/mame|g" $CONFIG_DIR/advmame.rc
 fi 
 
 if [ "$EE_DEVICE" != "OdroidGoAdvance" ] && [ "$EE_DEVICE" != "GameForce" ]; then
+    unset DISPLAY
+    MODE=`cat /sys/class/display/mode`;
+    sed -i '/device_video_modeline/d' $CONFIG_DIR/advmame.rc
 
-MODE=`cat /sys/class/display/mode`;
-sed -i '/device_video_modeline/d' $CONFIG_DIR/advmame.rc
-
-case "$MODE" in
-"720p"*)
-if [ -f /ee_s905 ]; then
-	echo "device_video_modeline 1280x720-60 91.517 1280 1440 1531 1691 720 810 812 902 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
-fi
-	;;
-"1080p"*)
-	echo "device_video_modeline 1920x1080_60.00 153.234 1920 1968 2121 2168 1080 1127 1130 1178 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
-	;;
-"1280x1024p60hz"*)
-	echo "device_video_modeline 1280x1024_60.00 108.88 1280 1360 1496 1712 1024 1025 1028 1060 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
-	;;
-"1024x768p60hz"*)
-	echo "device_video_modeline 1024x768_60.00 64.11 1024 1080 1184 1344 768 769 772 795 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
-	;;
-"640x480p60hz"*)
-	echo "device_video_modeline 640x480_60.00 23.86 640 656 720 800 480 481 484 497 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
-	;;
-esac
+    case "$MODE" in
+        "720p"*)
+            if [ -f /ee_s905 ]; then
+                echo "device_video_modeline 1280x720-60 91.517 1280 1440 1531 1691 720 810 812 902 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+            fi
+        ;;
+        "1080p"*)
+            echo "device_video_modeline 1920x1080_60.00 153.234 1920 1968 2121 2168 1080 1127 1130 1178 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+        ;;
+        "1280x1024p60hz"*)
+            echo "device_video_modeline 1280x1024_60.00 108.88 1280 1360 1496 1712 1024 1025 1028 1060 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+        ;;
+        "1024x768p60hz"*)
+            echo "device_video_modeline 1024x768_60.00 64.11 1024 1080 1184 1344 768 769 772 795 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+        ;;
+        "640x480p60hz"*)
+            echo "device_video_modeline 640x480_60.00 23.86 640 656 720 800 480 481 484 497 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+        ;;
+    esac
 fi
 
 if [ "$EE_DEVICE" != "GameForce" ]; then
@@ -51,5 +52,4 @@ fi
 
 ARG=$(echo basename $1 | sed 's/\.[^.]*$//')
 ARG="$(echo $1 | sed 's=.*/==;s/\.[^.]*$//')"         
-
-DISPLAY=0 SDL_AUDIODRIVER=alsa /usr/bin/advmame $ARG -quiet
+SDL_AUDIODRIVER=alsa /usr/bin/advmame $ARG -quiet
