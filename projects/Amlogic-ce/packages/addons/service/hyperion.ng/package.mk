7726014688a3cbb6a37c17165c29648d79463a99
Portisch <hugo.portisch@yahoo.de>
2021-07-18 15:41:52 +0200

    hyperion.ng: bump package to 2.0.0-alpha.10

--- projects/Amlogic-ce/packages/addons/service/hyperion.ng/package.mk
+++ projects/Amlogic-ce/packages/addons/service/hyperion.ng/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="hyperion.ng"
-PKG_VERSION="2.0.0-alpha.9"
-PKG_SHA256="545a8a6f7474ef7c6c11d3f30c9e39a23b064e5e9004ac202845d7cdf51d0469"
-PKG_REV="106"
+PKG_VERSION="d3a912f014b12ce443516a6d8a78734228da7c8e"
+PKG_SHA256="18d4e257d182396c756f2377d826ca7d58555413238367eeaad4750ba882340c"
+PKG_REV="107"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/hyperion-project/hyperion.ng"
 PKG_URL="https://github.com/hyperion-project/hyperion.ng/archive/$PKG_VERSION.tar.gz"
@@ -56,6 +56,7 @@ PKG_CMAKE_OPTS_TARGET="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
                        -DENABLE_SPIDEV=1 \
                        -DENABLE_TINKERFORGE=0 \
                        -DENABLE_TESTS=0 \
+                       -DENABLE_DEPLOY_DEPENDENCIES=0 \
                        -Wno-dev \
                        -DQT_BIN_PATH=$TOOLCHAIN/bin"
 
