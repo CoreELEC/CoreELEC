71ac2fdd94117784c99ff009fd1a06f219d042f7
shantigilbert <shantic@gmail.com>
2021-07-22 10:57:57 -0500

    flycast.sh: make sure "/storage/.local/share/" exists

--- packages/sx05re/emulators/flycastsa/scripts/flycast.sh
+++ packages/sx05re/emulators/flycastsa/scripts/flycast.sh
@@ -6,10 +6,12 @@
 # Source predefined functions and variables
 . /etc/profile
 
+mkdir -p "/storage/.local/share/"
+
 if [ ! -L "/storage/.local/share/flycast" ]; then
     mkdir -p "/storage/roms/bios/dc"
     rm -rf "/storage/.local/share/flycast"
     ln -sf "/storage/roms/bios/dc" "/storage/.local/share/flycast"
-fi 
+fi
 
 flycast "${1}"
