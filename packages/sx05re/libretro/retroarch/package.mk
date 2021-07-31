a6cad345fd3d8908a24137de60d2bfd1c90346cd
shantigilbert <shantic@gmail.com>
2021-07-31 01:02:47 -0500

    Retroarch: Bump to 1.9.7

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="6738fa57aad810bc43ebcb4966e00bb37f48609d"
+PKG_VERSION="8373ac67460cd0e092453bad6bb83e674b5af31a"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="$PKG_SITE.git"
 PKG_LICENSE="GPLv3"
