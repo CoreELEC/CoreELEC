fcbf11b8065b071a5abda0d3b1ac9e13de30cb0c
shantigilbert <shantic@gmail.com>
2021-07-30 23:38:35 -0500

    libepoxy: bump to ecfa8e0

--- packages/graphics/libepoxy/package.mk
+++ packages/graphics/libepoxy/package.mk
@@ -9,14 +9,15 @@
 # in Xorg.log
 
 PKG_NAME="libepoxy"
-PKG_VERSION="1.5.3"
-PKG_SHA256="002958c5528321edd53440235d3c44e71b5b1e09b9177e8daf677450b6c4433d"
+PKG_VERSION="ecfa8e0f083084181d36966fa084aca9a6c97d53"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/anholt/libepoxy"
-PKG_URL="https://github.com/anholt/libepoxy/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Epoxy is a library for handling OpenGL function pointer management for you."
-PKG_TOOLCHAIN="autotools"
+
+
+PKG_MESON_OPTS_TARGET+="-Dtests=false -Ddocs=false"
 
 if [ "$OPENGL" != "no" ]; then
   PKG_DEPENDS_TARGET+=" $OPENGL"
@@ -27,5 +28,5 @@ if [ "$OPENGLES" != "no" ]; then
 fi
 
 if [ "$DISPLAYSERVER" != "x11" ]; then
-  PKG_CONFIGURE_OPTS_TARGET="--disable-glx"
+  PKG_MESON_OPTS_TARGET+=" -Dx11=false"
 fi
