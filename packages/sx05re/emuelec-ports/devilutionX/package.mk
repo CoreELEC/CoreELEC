4c583f4f343f3dc826f6ddae6baaf4efac5a38dc
shantigilbert <shantic@gmail.com>
2021-08-23 16:02:44 -0500

    DevilutionX: Bump to ac0a690
    * Enable Hellfire
    * Integrate multi language support

--- packages/sx05re/emuelec-ports/devilutionX/package.mk
+++ packages/sx05re/emuelec-ports/devilutionX/package.mk
@@ -2,37 +2,31 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="devilutionX"
-PKG_VERSION="cb62e7bf2d9797d74363c7350eaf631144d2d2a5"
+PKG_VERSION="ac0a6907685a71e4c9691d8f83bf9f3c5e6bc7fd"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="unlicense"
 PKG_SITE="https://github.com/diasurgical/devilutionX"
 PKG_URL="$PKG_SITE.git"
-PKG_DEPENDS_TARGET="toolchain SDL2-git SDL2_mixer SDL2_ttf libsodium"
+PKG_DEPENDS_TARGET="toolchain SDL2-git SDL2_mixer SDL2_ttf"
 PKG_LONGDESC="Diablo build for modern operating systems "
 GET_HANDLER_SUPPORT="git"
 PKG_BUILD_FLAGS="-lto"
 
 pre_configure_target() {
-PKG_CMAKE_OPTS_TARGET=" -DBINARY_RELEASE=1 -DCMAKE_BUILD_TYPE="Release" -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON"
+PKG_CMAKE_OPTS_TARGET=" -DBINARY_RELEASE=1 -DCMAKE_BUILD_TYPE="Release" -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON -DDEVILUTIONX_SYSTEM_LIBSODIUM=OFF -DMO_LANG_DIR=\"/emuelec/configs/devilution/langs/\""
 sed -i "s|;-static-libstdc++>|;-lstdc++>|" $PKG_BUILD/CMakeLists.txt
 }
 
 makeinstall_target() { 
 mkdir -p $INSTALL/usr/bin
 cp -rf $PKG_BUILD/.$TARGET_NAME/devilutionx $INSTALL/usr/bin
-cp -rf $PKG_BUILD/Packaging/resources/CharisSILB.ttf $PKG_BUILD/.$TARGET_NAME/devilutionx $INSTALL/usr/bin
+cp -rf $PKG_BUILD/Packaging/resources/CharisSILB.ttf $INSTALL/usr/bin
+cp -rf $PKG_DIR/scripts/* $INSTALL/usr/bin
 
-mkdir -p ${INSTALL}/usr/share/fonts/truetype
-cp ../Packaging/resources/*.ttf ${INSTALL}/usr/share/fonts/truetype/
-mkfontscale ${INSTALL}/usr/share/fonts/truetype
-mkfontdir ${INSTALL}/usr/share/fonts/truetype
-    
-ninja -t clean
-  cmake ${CMAKE_GENERATOR_NINJA} ${TARGET_CMAKE_OPTS} ${PKG_CMAKE_OPTS_TARGET} -DHELLFIRE=1 $(dirname ${PKG_CMAKE_SCRIPT})
-  ninja ${NINJA_OPTS}
+mkdir -p ${INSTALL}/usr/config/emuelec/configs/devilution/langs
+cp $PKG_BUILD/.$TARGET_NAME/*.gmo ${INSTALL}/usr/config/emuelec/configs/devilution/langs
 
-  cp devilutionx $INSTALL/usr/bin/devilutionx.hf
-
- 
+mkdir -p ${INSTALL}/usr/local/share/diasurgical/devilutionx/
+cp $PKG_BUILD/.$TARGET_NAME/devilutionx.mpq ${INSTALL}/usr/local/share/diasurgical/devilutionx/
 }
---
+++ packages/sx05re/emuelec-ports/devilutionX/scripts/devilutionx.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
+
+# Source predefined functions and variables
+. /etc/profile
+
+DIABLOPATH="/storage/.local/share/diasurgical/devilution"
+DIABLOROMPATH="/storage/roms/ports/diablo"
+
+EELANG=$(echo ${LANG} | cut -d= -f2 | cut -d_ -f1)
+
+if [ "${EELANG}" == "en" ]; then
+    LANG=""
+fi
+
+mkdir -p ${DIABLOPATH}
+
+if [ -e ${DIABLOPATH}/diablo.ini ]; then
+    sed -i "s|Code=.*|Code=${EELANG}|g" ${DIABLOPATH}/diablo.ini
+fi
+
+if [ -e ${DIABLOROMPATH}/diabdat.mpq ]; then
+    if [ ! -L ${DIABLOPATH}/diabdat.mpq ]; then
+        ln -sf ${DIABLOROMPATH}/diabdat.mpq ${DIABLOPATH}/diabdat.mpq
+    fi
+else
+    exit 21
+fi
+
+if [ "${1}" == "hellfire" ]; then
+    for hell in diabdat hellfire hfmonk hfmusic hfvoice; do
+        if [ -e ${DIABLOROMPATH}/${hell}.mpq ]; then
+            if [ ! -L ${DIABLOPATH}/${hell}.mpq ]; then
+                ln -sf ${DIABLOROMPATH}/${hell}.mpq ${DIABLOPATH}/${hell}.mpq
+            fi
+        else
+            exit 21
+        fi
+    done
+else
+PARAMS=" --diablo"
+fi 
+
+devilutionx --verbose ${PARAMS}
