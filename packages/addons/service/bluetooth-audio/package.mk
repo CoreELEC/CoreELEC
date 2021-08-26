00b7a372ce90e936e2befe928adc92fb4c4ad7c0
Portisch <hugo.portisch@yahoo.de>
2021-08-26 16:51:51 +0200

    Revert "bluetooth-audio: drop package as the function is now inbuilt in CoreELEC-settings"
    This reverts commit 2d6e94060137a90e280bcb85a5252abcfbe12277.
    
    Revert the remove to xbmc.broken to inform the user to uninstall the addon.

---
+++ packages/addons/service/bluetooth-audio/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="bluetooth-audio"
+PKG_VERSION="0"
+PKG_REV="103"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="service"
+PKG_SHORTDESC="bluetooth-audio: Add-on removed"
+PKG_LONGDESC="bluetooth-audio: Add-on removed"
+PKG_TOOLCHAIN="manual"
+
+PKG_ADDON_BROKEN="Add-on removed as the bluetooth audio auto connect is handled by CoreELEC settings."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Bluetooth Audio Device Changer"
+PKG_ADDON_TYPE="xbmc.broken"
+
+addon() {
+  :
+}
