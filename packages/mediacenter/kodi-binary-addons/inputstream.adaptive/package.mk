dd414ec39e74f03360da1ac361286dc559932fa7
Portisch <hugo.portisch@yahoo.de>
2021-09-15 09:06:27 +0200

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="2.6.23-Matrix"
-PKG_SHA256="eeb68250725c4e6c990fdb24365188733b6ef944965792bb5b0e42ce306ad836"
+PKG_VERSION="20.0.0-Nexus"
+PKG_SHA256="257ef5905447807052d6f377691a99ac24cce55bc63d059d31fde2a80cb15c3b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
