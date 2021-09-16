70ace8e2c3bbf83da98c8d075046ccba708f311d
Dmitry_L <freebox_2002@mail.ru>
2021-09-16 17:59:06 +0300

    libnfs: update to latest versions
    This commit below required more updated libnfs than 4.0.0
    https://github.com/xbmc/xbmc/pull/17512

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnfs"
-PKG_VERSION="4.0.0"
-PKG_SHA256="6ee77e9fe220e2d3e3b1f53cfea04fb319828cc7dbb97dd9df09e46e901d797d"
+PKG_VERSION="f88cca547dc0d6c2115dd520826fb905b7389b01"
+PKG_SHA256="70bed047800bf83e78907fee005f683f6c6e37739f519e173d0545e8b88c2e1d"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="https://github.com/sahlberg/libnfs"
-PKG_URL="https://github.com/sahlberg/libnfs/archive/libnfs-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/sahlberg/libnfs/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A client library for accessing NFS shares over a network."
 PKG_TOOLCHAIN="autotools"
