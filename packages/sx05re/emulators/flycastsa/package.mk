b0e8786bc523301e25d9effeb251326f8c1a8ea9
Joshua L <Langerz82@users.noreply.github.com>
2021-09-23 14:19:10 +1000

    minor update to flycastsa for sdl game controller support on launch. (#724)

--- packages/sx05re/emulators/flycastsa/package.mk
+++ packages/sx05re/emulators/flycastsa/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="flycastsa"
-PKG_VERSION="4d5c93adfa538b7d23d734ccf160afd91c6bf54d"
+PKG_VERSION="17348dae8abfeab6e99702aea6614532f6545ec4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain $OPENGLES alsa SDL2-git libzip zip"
 PKG_LONGDESC="Flycast is a multiplatform Sega Dreamcast, Naomi and Atomiswave emulator"
 PKG_TOOLCHAIN="cmake"
-PKG_GIT_CLONE_BRANCH="libretro"
+PKG_GIT_CLONE_BRANCH="master"
 
 
 if [ "${ARCH}" == "arm" ]; then
