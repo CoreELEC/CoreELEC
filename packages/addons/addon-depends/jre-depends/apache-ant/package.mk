43d23c2d5e8a682da86daf7f81992321d21a5f7a
shantigilbert <shantic@gmail.com>
2021-10-05 13:20:14 -0500

    Apache-ant: bump to 1.10.11

--- packages/addons/addon-depends/jre-depends/apache-ant/package.mk
+++ packages/addons/addon-depends/jre-depends/apache-ant/package.mk
@@ -3,27 +3,20 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="apache-ant"
-PKG_VERSION="1.10.7"
-PKG_SHA256="c8d68b396d9e44b49668bafe0c82f8c89497915254b5395d73d6f6e41d7a0e25"
+PKG_VERSION="1.10.11"
+PKG_SHA256="baa049855cdecbefa62539555824058e52412e5ebe8f102e1db944cb762e06d9"
 PKG_LICENSE="Apache License 2.0"
 PKG_SITE="https://ant.apache.org/"
-PKG_URL="https://archive.apache.org/dist/ant/source/${PKG_NAME}-${PKG_VERSION}-src.tar.xz"
-PKG_DEPENDS_HOST="jdk-x86_64-zulu:host"
+PKG_URL="https://downloads.apache.org/ant/binaries/${PKG_NAME}-${PKG_VERSION}-bin.tar.xz"
+PKG_DEPENDS_UNPACK="jdk-x86_64-zulu"
 PKG_LONGDESC="Apache Ant is a Java library and command-line tool that help building software."
 PKG_TOOLCHAIN="manual"
 
-make_host() {
-  (
-  export JAVA_HOME=$(get_build_dir jdk-x86_64-zulu)
-
-  ./bootstrap.sh
-  ./bootstrap/bin/ant -f fetch.xml -Ddest=optional
-  ./build.sh -Ddist.dir=${PKG_BUILD}/binary dist
-  )
-}
-
 makeinstall_host() {
+  mkdir -p ${TOOLCHAIN}/apache-ant/bin
+  mkdir -p ${TOOLCHAIN}/apache-ant/lib
+    cp bin/ant ${TOOLCHAIN}/apache-ant/bin
+    cp lib/*.jar ${TOOLCHAIN}/apache-ant/lib
   mkdir -p ${TOOLCHAIN}/bin
-    cp binary/bin/ant ${TOOLCHAIN}/bin
-    cp -r binary/lib ${TOOLCHAIN}
+    ln -sf ${TOOLCHAIN}/apache-ant/bin/ant ${TOOLCHAIN}/bin/ant
 }
