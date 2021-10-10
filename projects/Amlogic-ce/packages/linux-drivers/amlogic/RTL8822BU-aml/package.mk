001ca7f9923a83816896f15a164df70b36bf4f52
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2021-10-10 17:15:33 +0200

    update rtl88x2bu

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/RTL8822BU-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/RTL8822BU-aml/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8822BU-aml"
-PKG_VERSION="b655ba8114d7def7e6181bfe4635fbb831e2b8d6"
-PKG_SHA256="bbb6dab43a0e21654a7796ce07796677bbfda322d74b6a55dd08e3bde4953208"
+PKG_VERSION="f2bccb96ac8e54b63ef35a3c873075f21fb95030"
+PKG_SHA256="9ff7b6d7ef6c37576edb2d6ea2765385dd1512f476f6a49202d11b7516194f96"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/khadas/android_hardware_wifi_realtek_drivers_8822bu"
-PKG_URL="https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/cilynx/rtl88x2bu/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_LONGDESC="Realtek RTL8822BU Linux driver"
