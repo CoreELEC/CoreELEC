332edf8d31bfe664158f2a3cee790027fbf523c2
Portisch <hugo.portisch@yahoo.de>
2021-11-08 07:58:09 +0100

    Revert "Amlogic-ng: Fix smp-affinity for aocec"
    This reverts commit cc8c5acb97f6470143776ff19bdf9dd77f3e9121.
    
    Use CECB again since 02/2021

--- projects/Amlogic-ce/devices/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
+++ projects/Amlogic-ce/devices/Amlogic-ng/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -10,9 +10,9 @@ VDEC0="`find /proc/irq/ -name vdec-0`"
 VDEC1="`find /proc/irq/ -name vdec-1`"
 PREDI="`find /proc/irq/ -name pre_di`"
 AFIFO0="`find /proc/irq/ -name afifo0`"
-AOCEC="`find /proc/irq/ -name hdmi_aocec`"
+AOCECB="`find /proc/irq/ -name hdmi_aocecb`"
 USB3="`find /proc/irq/ -name xhci-hcd:usb1`"
-IRQ="$AOCEC $ETH0 $USB3 $VDEC0 $VDEC1 $AFIFO0 $MESONIR $PREDI"
+IRQ="$AOCECB $ETH0 $USB3 $VDEC0 $VDEC1 $AFIFO0 $MESONIR $PREDI"
 
 cpu=0
 aff=1
