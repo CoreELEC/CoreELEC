b9cc8b2ea87d3101dcad8458d18aa8ff9483fe73
shantigilbert <shantic@gmail.com>
2021-11-14 13:47:38 -0600

    gptokeyb: Revert until upstream is fixed

--- packages/sx05re/tools/sysutils/gptokeyb/package.mk
+++ packages/sx05re/tools/sysutils/gptokeyb/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="gptokeyb"
-PKG_VERSION="17e0fc1889604944b87757057d7a51363c6e6eed"
+PKG_VERSION="ded03731ccf8e65e855dd776aade021425afc53b"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/EmuELEC/gptokeyb"
