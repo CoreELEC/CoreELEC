41ff03d1f9116b9d8a3478c60a4a67d6173f4283
shantigilbert <shantic@gmail.com>
2021-11-18 15:28:31 -0600

    mame: Bump to 031ac78
    Due to size, this core is NOT included in EmuELEC

--- packages/sx05re/libretro/mame/package.mk
+++ packages/sx05re/libretro/mame/package.mk
@@ -2,26 +2,24 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="mame"
-PKG_VERSION="0b8f5a56162e839b94ecaa7124df58b471bc3c31"
-PKG_SHA256=""
+PKG_VERSION="031ac783585e7d5156a6f87a9ba20d88caf94ad6"
+PKG_SHA256="bb91e074f96bcbbfef4f1da852f12fda5f9e3f83dc480f8901b088e93e06fdaa"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame"
-PKG_URL="$PKG_SITE.git"
+PKG_URL="https://github.com/libretro/mame/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib flac sqlite expat"
 PKG_SECTION="libretro"
 PKG_SHORTDESC="MAME - Multiple Arcade Machine Emulator"
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="-lto"
 
+pre_configure_target() {
 
 PTR64="1"
 NOASM="0"
 
-if [ "$ARCH" == "arm" ]; then
+if [ "${ARCH}" == "arm" ]; then
   NOASM="1"
-elif [ "$ARCH" == "x86_64" ]; then
-  PTR64="1"
 fi
 
 PKG_MAKE_OPTS_TARGET="REGENIE=1 \
@@ -29,10 +27,10 @@ PKG_MAKE_OPTS_TARGET="REGENIE=1 \
 		      NOWERROR=1 \
 		      OPENMP=1 \
 		      CROSS_BUILD=1 \
-		      TOOLS=1 \
+		      TOOLS=0 \
 		      RETRO=1 \
-		      PTR64=$PTR64 \
-		      NOASM=$NOASM \
+		      PTR64=${PTR64} \
+		      NOASM=${NOASM} \
 		      PYTHON_EXECUTABLE=python3 \
 		      CONFIG=libretro \
 		      LIBRETRO_OS=unix \
@@ -40,23 +38,30 @@ PKG_MAKE_OPTS_TARGET="REGENIE=1 \
 		      PLATFORM=arm64 \
 		      ARCH= \
 		      TARGET=mame \
-		      SUBTARGET=arcade \
+		      SUBTARGET=mame \
 		      OSD=retro \
 		      USE_SYSTEM_LIB_EXPAT=1 \
 		      USE_SYSTEM_LIB_ZLIB=1 \
 		      USE_SYSTEM_LIB_FLAC=1 \
 		      USE_SYSTEM_LIB_SQLITE3=1"
 
+export ARCHOPTS="-D__aarch64__ -DASMJIT_BUILD_X86"
+
+sed -i "s/-static-libstdc++//g" scripts/genie.lua
+
+unset ARCH
+unset DISTRO
+unset PROJECT
+
+}
+
 make_target() {
-  unset ARCH
-  unset DISTRO
-  unset PROJECT
-  make $PKG_MAKE_OPTS_TARGET OVERRIDE_CC=$CC OVERRIDE_CXX=$CXX OVERRIDE_LD=$LD AR=$AR $MAKEFLAGS
+  make $PKG_MAKE_OPTS_TARGET OVERRIDE_CC=$CC OVERRIDE_CXX=$CXX OVERRIDE_LD=$LD AR=$AR $MAKEFLAGS -j4
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp *.so $INSTALL/usr/lib/libretro/mame_libretro.so
+  cp *.so $INSTALL/usr/lib/libretro/
   mkdir -p $INSTALL/usr/config/retroarch/savefiles/mame/hi
   cp plugins/hiscore/hiscore.dat $INSTALL/usr/config/retroarch/savefiles/mame/hi
 }
