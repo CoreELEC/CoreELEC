7c12b6e344d0ad2da8b0212cc4ab24c8a04b24c1
shantigilbert <shantic@gmail.com>
2021-12-12 15:59:04 -0600

    Crystal: bump to db45cef

--- packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
+++ packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="Crystal"
-PKG_VERSION="e7d7439b376a0e9e52a01b94d83a7479083b2fbe"
+PKG_VERSION="db45cef9fafb84fc1da066b2e3e3bf3ede72327a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
