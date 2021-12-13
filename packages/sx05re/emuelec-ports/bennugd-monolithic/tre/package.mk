69f333b904543061e106f2639fc9844c0e018bd8
shantigilbert <shantic@gmail.com>
2021-12-12 23:47:53 -0600

    tre: Use gettext 17 or higher

--- packages/sx05re/emuelec-ports/bennugd-monolithic/tre/package.mk
+++ packages/sx05re/emuelec-ports/bennugd-monolithic/tre/package.mk
@@ -10,3 +10,7 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SHORTDESC="The approximate regex matching library and agrep command line tool."
 PKG_TOOLCHAIN="autotools"
 
+pre_configure_target() {
+sed -i "s|AM_GNU_GETTEXT_VERSION(0.17)|AM_GNU_GETTEXT_REQUIRE_VERSION(0.17)|" ${PKG_BUILD}/configure.ac
+}
+
