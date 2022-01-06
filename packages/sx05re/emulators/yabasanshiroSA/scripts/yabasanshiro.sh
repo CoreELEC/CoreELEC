0dcb32f4d1f6ac55b2fe2d423577e9616eeaaebe
shantigilbert <shantic@gmail.com>
2022-01-06 17:46:13 -0600

    yabasanshiroSA: Use ROMs partition for config

--- packages/sx05re/emulators/yabasanshiroSA/scripts/yabasanshiro.sh
+++ packages/sx05re/emulators/yabasanshiroSA/scripts/yabasanshiro.sh
@@ -6,10 +6,12 @@
 # Source predefined functions and variables
 . /etc/profile
 
-mkdir -p "/emuelec/configs/yabasanshiro"
+mkdir -p "/storage/roms/saturn/yabasanshiro/"
 
-if [ ! -e "/emuelec/configs/yabasanshiro/input.cfg" ]; then
-    cp -rf "/usr/config/emuelec/configs/yabasanshiro/input.cfg" "/emuelec/configs/yabasanshiro/input.cfg"
+if [ ! -e "/storage/roms/saturn/yabasanshiro/input.cfg" ]; then
+    cp -rf "/emuelec/configs/yabasanshiro/input.cfg" "/storage/roms/saturn/yabasanshiro/input.cfg"
 fi
 
-yabasanshiro -i "${1}" -b "/storage/roms/bios/saturn_bios.bin" -r 2 -a -nf > /emuelec/logs/emuelec.log 2>&1
+# We use { } to avoid SIGUSR signal showing text and messing up with the error handling
+
+{ yabasanshiro -r 2 -a -nf -i "${1}" -b "/storage/roms/bios/saturn_bios.bin"; } > /emuelec/logs/emuelec.log 2>&1
