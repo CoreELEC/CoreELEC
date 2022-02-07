e4bbc0546b05367c9bab8e0d678773e6b2402b80
shantigilbert <shantic@gmail.com>
2022-02-07 14:20:46 -0600

    Remove "Bluetooth controller management" from setup
    It is no longer working

--- packages/sx05re/emuelec/bin/scripts/setup/bt_controller_management.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Source predefined functions and variables
-. /etc/profile
-
-ee_console enable
-python /usr/bin/scripts/scriptmodules/supplementary/bluetoothcontroller.py ES 2>/dev/null
-ee_console disable
