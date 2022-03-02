a85f7af1a3f4f0c4ad07e7996836c2db2ad29267
shantigilbert <shantic@gmail.com>
2022-03-02 16:09:41 -0600

    WIP: Add cloud sync option
    This is experimental and not fully tested! 
    
    Please read: https://rclone.org/remote_setup/
    Name remote as `EmuELEC_Remote` and copy rclone.conf to `/emuelec/configs/rclone/` finally create a folder named `EmuELEC_Backup` on your cloud service

--- packages/sx05re/tools/sysutils/rclone/package.mk
+++ packages/sx05re/tools/sysutils/rclone/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="e47d769185971d3d414d4ca5f9532e5b476e4fc5b1d4da89a8437ed3049bf908"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/rclone/rclone"
-PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_LONGDESC="rsync for cloud storage - Google Drive, S3, Dropbox, Backblaze B2, One Drive, Swift, Hubic, Wasabi, Google Cloud Storage, Yandex Files"
 PKG_TOOLCHAIN="manual"
@@ -39,12 +39,15 @@ configure_target() {
 
 make_target() {
   mkdir -p bin
-  cd $PKG_BUILD
+  cd ${PKG_BUILD}
   ${GOLANG} get $(echo ${PKG_SITE} | sed s/'http[s]\?:\/\/'//)
   ${GOLANG} build -ldflags "$LDFLAGS" $(echo ${PKG_SITE} | sed s/'http[s]\?:\/\/'//)
 }
 
 makeinstall_target() {
-mkdir -p $INSTALL/usr/bin/
-    cp $PKG_BUILD/rclone $INSTALL/usr/bin/
+	mkdir -p ${INSTALL}/usr/bin/
+	mkdir -p ${INSTALL}/usr/config/emuelec/configs/rclone
+	cp ${PKG_BUILD}/rclone $INSTALL/usr/bin/
+	cp ${PKG_DIR}/config/emuelec-cloud-filter.cfg ${INSTALL}/usr/config/emuelec/configs/rclone/
+	ln -sf /emuelec/configs/rclone ${INSTALL}/usr/config/rclone
 }
