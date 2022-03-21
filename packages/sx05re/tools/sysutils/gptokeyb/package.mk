dee76f218ffe46e027d091d2e90d4aa7cfdcc0dd
shantigilbert <shantic@gmail.com>
2022-03-21 13:51:23 -0600

    gptokeyb: Bump to 0a90050

--- packages/sx05re/tools/sysutils/gptokeyb/package.mk
+++ packages/sx05re/tools/sysutils/gptokeyb/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="gptokeyb"
-PKG_VERSION="0d807e67d9dab1f23b9593467ea5d0c1c1bf2e50"
+PKG_VERSION="0a9005074b6355bab8bc10a72a8302204e692862"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/EmuELEC/gptokeyb"
