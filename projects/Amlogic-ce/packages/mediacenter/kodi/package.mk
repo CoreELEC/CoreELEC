51264813c8f08b8c4799b8f4fa74440be05a5772
Portisch <hugo.portisch@yahoo.de>
2022-03-22 07:51:54 +0100

    kodi: bump package to d5bd3400

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2020-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="ce4a27c063ea0cbdb240dfa3c2f2cfbfc0bc5f03"
-KG_SHA256="5b25dd184a250711d3d97d2a7ff0807bbf20dbc08a3cfcff08a399737bff6082"
+PKG_VERSION="d5bd3400ee6f6d2226d069d7ab0cd2334b665292"
+KG_SHA256="3fdf90fba022892921e3e74677d8be86db310c110b736649b14bec8b8f93a9e8"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/CoreELEC/xbmc/archive/$PKG_VERSION.tar.gz"
