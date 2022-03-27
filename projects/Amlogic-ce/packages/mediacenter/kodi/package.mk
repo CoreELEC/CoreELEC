8262a00011b7b320bebe90f2da258429d0e092c4
Portisch <hugo.portisch@yahoo.de>
2022-03-27 17:41:18 +0200

    kodi: bump package to fd069084

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2020-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="d5bd3400ee6f6d2226d069d7ab0cd2334b665292"
-KG_SHA256="3fdf90fba022892921e3e74677d8be86db310c110b736649b14bec8b8f93a9e8"
+PKG_VERSION="fd069084d23a53f231e509db3d0697bbc98a08c3"
+KG_SHA256="2e9147f2a91f5000ce9a57428ffafe6019f33c7ee5ccad139adbb11a9578a982"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/CoreELEC/xbmc/archive/$PKG_VERSION.tar.gz"
