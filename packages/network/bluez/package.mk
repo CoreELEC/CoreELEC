ef29c2a66c9771ae6037758665e73614cd955322
shantigilbert <shantic@gmail.com>
2022-04-17 22:50:03 -0500

    bluez: set FastConnectable
    Maybe this will fix issues with some bt chips

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -61,7 +61,9 @@ post_makeinstall_target() {
     sed -i ${INSTALL}/etc/bluetooth/main.conf \
         -e "s|^#\[Policy\]|\[Policy\]|g" \
         -e "s|^#AutoEnable.*|AutoEnable=true|g" \
-        -e "s|^#JustWorksRepairing.*|JustWorksRepairing=always|g"
+        -e "s|^#JustWorksRepairing.*|JustWorksRepairing=always|g" \
+        -e "s|^#FastConnectable.*|FastConnectable=true|g" \
+        -e "s|^# Privacy =.*|Privacy = device|g"
 
   mkdir -p ${INSTALL}/usr/share/services
     cp -P ${PKG_DIR}/default.d/*.conf ${INSTALL}/usr/share/services
