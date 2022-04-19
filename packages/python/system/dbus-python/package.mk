4308097da56b6de6451cb6ce092ec1ed03882bfa
Peter Vicman <peter.vicman@gmail.com>
2022-04-19 09:52:12 +0200

    dbus-python: bump to 1.2.18

--- packages/python/system/dbus-python/package.mk
+++ packages/python/system/dbus-python/package.mk
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="dbus-python"
-PKG_VERSION="1.2.16"
-PKG_SHA256="11238f1d86c995d8aed2e22f04a1e3779f0d70e587caffeab4857f3c662ed5a4"
+PKG_VERSION="1.2.18"
+PKG_SHA256="92bdd1e68b45596c833307a5ff4b217ee6929a1502f5341bae28fd120acf7260"
 PKG_LICENSE="GPL"
 PKG_SITE="https://freedesktop.org/wiki/Software/dbus"
 PKG_URL="https://dbus.freedesktop.org/releases/dbus-python/${PKG_NAME}-${PKG_VERSION}.tar.gz"
