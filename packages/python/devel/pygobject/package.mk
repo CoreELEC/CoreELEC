9adfb18855927297d418b7a01bafb75508b4e1ff
Peter Vicman <peter.vicman@gmail.com>
2022-04-19 09:51:48 +0200

    Revert "pygobject: bump to 3.42.1 and switch to meson"
    This reverts commit e189b261176b72a3ff9666caad851a47a07be934.
    
    Problemsn when actually using it: 
        from gi.repository import GLib
    ModuleNotFoundError: No module named 'gi.repository'

--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -1,26 +1,33 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
-# Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pygobject"
-PKG_VERSION="3.42.1"
-PKG_SHA256="1f34b5f7624de35e44eb5a7eb428353285bd03004d55131a5f7f7fa9b90f3cc9"
+PKG_VERSION="3.28.3"
+PKG_SHA256="3dd3e21015d06e00482ea665fc1733b77e754a6ab656a5db5d7f7bfaf31ad0b0"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.pygtk.org/"
 PKG_URL="http://ftp.gnome.org/pub/GNOME/sources/pygobject/$(get_pkg_version_maj_min)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain Python3 glib libffi gobject-introspection pgi"
 PKG_LONGDESC="A convenient wrapper for the GObject+ library for use in Python programs."
-PKG_TOOLCHAIN="meson"
+PKG_TOOLCHAIN="autotools"
+PKG_IS_ADDON="no"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-cairo \
+                           --enable-shared \
+                           --with-python=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION}"
 
 pre_configure_target() {
-  PKG_CONFIG_PATH="${SYSROOT_PREFIX}/usr/lib/pkgconfig"
+  export PYTHON_INCLUDES="$(${SYSROOT_PREFIX}/usr/bin/python3-config --includes)"
+}
 
-  PKG_MESON_OPTS_TARGET=" \
-    -Dpython=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION} \
-    -Dpycairo=disabled \
-    -Dtests=false"
+post_unpack() {
+  sed -i "s|@CODE_COVERAGE_RULES@||" ${PKG_BUILD}/Makefile.am
+
+  # https://docs.python.org/3/whatsnew/3.9.html
+  #   the tp_print slot of PyTypeObject has been removed
+  sed -i "s|                              offsetof(PyTypeObject, tp_print) ||" ${PKG_BUILD}/gi/pygobject-object.c
 }
 
 post_makeinstall_target() {
