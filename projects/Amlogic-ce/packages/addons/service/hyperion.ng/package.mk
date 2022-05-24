910f83bf159655f2d590a021111f3981bbeff58e
Portisch <hugo.portisch@yahoo.de>
2022-05-24 21:23:51 +0200

    hyperion.ng: bump package to 2.0.13 added new dependency package qmdnsengine for mDNS support

--- projects/Amlogic-ce/packages/addons/service/hyperion.ng/package.mk
+++ projects/Amlogic-ce/packages/addons/service/hyperion.ng/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="hyperion.ng"
-PKG_VERSION="2.0.12"
-PKG_SHA256="755771f17114611722f0b0879322b8c7cf1aa7e7ff8e8b592e544d5b26412aa6"
-PKG_REV="109"
+PKG_VERSION="2.0.13"
+PKG_SHA256="ed286d43a470c7263e189fdfd224d6594d85d5b5880001d4274a80e1dd8ea465"
+PKG_REV="110"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/hyperion-project/hyperion.ng"
 PKG_URL="https://github.com/hyperion-project/hyperion.ng/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 avahi libusb qt-everywhere protobuf flatbuffers:host flatbuffers libcec"
+PKG_DEPENDS_TARGET="toolchain Python3 avahi libusb qt-everywhere protobuf flatbuffers:host flatbuffers libcec libjpeg-turbo qmdnsengine"
 PKG_SECTION="service"
 PKG_SHORTDESC="Hyperion.NG: an AmbiLight controller"
 PKG_LONGDESC="Hyperion.NG($PKG_VERSION) is an modern opensource AmbiLight implementation."
@@ -17,52 +17,25 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Hyperion.NG"
 PKG_ADDON_TYPE="xbmc.service"
 
-# Setting default values
-PKG_PLATFORM="-DPLATFORM=x86"
-PKG_AMLOGIC_SUPPORT="-DENABLE_AMLOGIC=0"
-PKG_DISPMANX_SUPPORT="-DENABLE_DISPMANX=0"
-PKG_FB_SUPPORT="-DENABLE_FB=1"
-PKG_X11_SUPPORT="-DENABLE_X11=0"
-PKG_DENABLE_WS281XPWM="-DENABLE_WS281XPWM=0"
-
-if [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
-  PKG_PLATFORM="-DPLATFORM=amlogic"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libamcodec"
-  PKG_AMLOGIC_SUPPORT="-DENABLE_AMLOGIC=1"
-elif [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
-  PKG_PLATFORM="-DPLATFORM=rpi"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver rpi_ws281x"
-  PKG_DISPMANX_SUPPORT="-DENABLE_DISPMANX=1"
-  PKG_FB_SUPPORT="-DENABLE_FB=0"
-  PKG_DENABLE_WS281XPWM="-DENABLE_WS281XPWM=1"
-elif [ "$DISPLAYSERVER" = "x11" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xorg-server xrandr"
-  PKG_X11_SUPPORT="-DENABLE_X11=1"
-fi
-
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
-                       -DCMAKE_BUILD_TYPE=Release\
-                       -DUSE_SHARED_AVAHI_LIBS=OFF\
-                       -DUSE_SYSTEM_PROTO_LIBS=1 \
-                       -DUSE_SYSTEM_FLATBUFFERS_LIBS=ON\
-                       $PKG_PLATFORM \
-                       $PKG_AMLOGIC_SUPPORT \
-                       $PKG_DISPMANX_SUPPORT \
-                       $PKG_FB_SUPPORT \
-                       $PKG_DENABLE_WS281XPWM \
-                       $PKG_X11_SUPPORT \
-                       -DENABLE_V4L2=1 \
-                       -DENABLE_OSX=0 \
-                       -DENABLE_SPIDEV=1 \
-                       -DENABLE_TINKERFORGE=0 \
-                       -DENABLE_TESTS=0 \
-                       -DENABLE_DEPLOY_DEPENDENCIES=0 \
-                       -Wno-dev \
-                       -DQT_BIN_PATH=$TOOLCHAIN/bin"
-
-makeinstall_target() {
-  : # nothing to do here
-}
+                       -DCMAKE_BUILD_TYPE=Release \
+                       -DUSE_SYSTEM_PROTO_LIBS=ON \
+                       -DUSE_SYSTEM_FLATBUFFERS_LIBS=ON \
+                       -DUSE_SYSTEM_QMDNS_LIBS=ON \
+                       -DPLATFORM=amlogic \
+                       -DENABLE_AMLOGIC=ON \
+                       -DENABLE_DISPMANX=OFF \
+                       -DENABLE_FB=ON \
+                       -DENABLE_DEV_WS281XPWM=OFF \
+                       -DENABLE_X11=OFF \
+                       -DENABLE_V4L2=ON \
+                       -DENABLE_OSX=OFF \
+                       -DENABLE_DEV_SPI=ON \
+                       -DENABLE_MDNS=ON \
+                       -DENABLE_DEV_TINKERFORGE=OFF \
+                       -DENABLE_TESTS=OFF \
+                       -DENABLE_DEPLOY_DEPENDENCIES=OFF \
+                       -Wno-dev"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
