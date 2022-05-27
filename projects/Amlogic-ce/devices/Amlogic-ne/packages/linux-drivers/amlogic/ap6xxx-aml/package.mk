67ed1dd28215ab3d0e98ea7bc738db71eca92e7f
Portisch <hugo.portisch@yahoo.de>
2022-05-27 11:06:54 +0200

    ap6xxx-aml: add 5.4 compatible driver

---
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/linux-drivers/amlogic/ap6xxx-aml/package.mk
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="ap6xxx-aml"
+PKG_VERSION="098c1541313588e64b25e6beb1ca59cec9656f13"
+PKG_SHA256="b4dabe28bd6e11b66bb02f078e7777ceeb1bc3b5a622670cb79f9ee624a5e8be"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/CoreELEC/ap6xxx-aml"
+PKG_URL="https://github.com/CoreELEC/ap6xxx-aml/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
+PKG_LONGDESC="ap6xxx: Linux drivers for AP6xxx WLAN chips used in some devices based on Amlogic SoCs"
+PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  echo
+  echo "building ap6275s"
+  kernel_make -C  ${PKG_BUILD}/bcmdhd.101.10.361.x \
+       M=${PKG_BUILD}/bcmdhd.101.10.361.x \
+       PWD=${PKG_BUILD}/bcmdhd.101.10.361.x \
+       KERNEL_SRC=$(kernel_path) \
+       CONFIG_BCMDHD_DISABLE_WOWLAN=y \
+       CONFIG_BCMDHD_SDIO=y \
+       bcmdhd_sdio
+
+  echo
+  echo "building ap6356s and others"
+  kernel_make -C  ${PKG_BUILD}/bcmdhd.100.10.315.x \
+       M=${PKG_BUILD}/bcmdhd.100.10.315.x \
+       PWD=${PKG_BUILD}/bcmdhd.100.10.315.x \
+       KERNEL_SRC=$(kernel_path) \
+       CONFIG_BCMDHD_DISABLE_WOWLAN=y \
+       CONFIG_BCMDHD_SDIO=y \
+       bcmdhd_sdio
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+  cp ${PKG_BUILD}/*/*.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
