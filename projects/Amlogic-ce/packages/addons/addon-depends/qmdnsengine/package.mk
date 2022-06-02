230158b37940dda25cabf988b82b05d511b5e886
Portisch <hugo.portisch@yahoo.de>
2022-06-02 07:45:36 +0200

    qmdnsengine: add missing dependency

--- projects/Amlogic-ce/packages/addons/addon-depends/qmdnsengine/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/qmdnsengine/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="3ae288458e3fc1c1e636869aaca0fd5c77bdd6aec6fd4d62217d0f46acd4042c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/nitroshare/qmdnsengine"
 PKG_URL="https://github.com/nitroshare/qmdnsengine/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain qt-everywhere"
 PKG_LONGDESC="Library provides an implementation of multicast DNS as per RFC 6762."
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF"
