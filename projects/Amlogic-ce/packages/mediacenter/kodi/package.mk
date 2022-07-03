4e2f793a935e22f421963af37031683638818788
Peter Vicman <peter.vicman@gmail.com>
2022-07-03 13:46:31 +0200

    kodi: check the checksum of libdvd* libraries before using them if checksum does match then local tarball is used by Kodi if checksum doesn't match new tarball is downloaded by Kodi

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -238,26 +238,32 @@ configure_package() {
                          $KODI_PULSEAUDIO"
 }
 
+prepare_libdvd_library() {
+  # arg1 is library name libdvdcss/libdvdnav/libdvdread
+  local LIBRARY_VERSION="$(awk -F= '/VERSION=/ {print $2}' ${PKG_BUILD}/tools/depends/target/${1}/${1^^}-VERSION)"
+  local LIBRARY_SHA512="$(awk  -F= '/SHA512=/  {print $2}' ${PKG_BUILD}/tools/depends/target/${1}/${1^^}-VERSION)"
+  local LIBRARY_ARCHIVE="${SOURCES}/${1}/${1}-${LIBRARY_VERSION}.tar.gz"
+
+  if [ -f "${LIBRARY_ARCHIVE}" ]; then
+    local LIBRARY_ARCHIVE_SHA512="$(sha512sum "${LIBRARY_ARCHIVE}" | cut -d ' ' -f 1)"
+    if [ "${LIBRARY_ARCHIVE_SHA512}" = "${LIBRARY_SHA512}" ]; then
+      KODI_LIBDVD+=" -D${1^^}_URL=${LIBRARY_ARCHIVE}"
+    fi
+  fi
+}
+
 pre_configure_target() {
   export LIBS="$LIBS -lncurses"
 
   if [ "${KODI_DVDCSS_SUPPORT}" = yes ]; then
-    LIBDVDCSS_VERSION="$(grep VERSION= ${PKG_BUILD}/tools/depends/target/libdvdcss/LIBDVDCSS-VERSION)"
-    LIBDVDCSS_ARCHIVE="${SOURCES}/libdvdcss/libdvdcss-${LIBDVDCSS_VERSION#*=}.tar.gz"
     KODI_LIBDVD="-DENABLE_DVDCSS=ON"
-    [ -f "${LIBDVDCSS_ARCHIVE}" ] && KODI_LIBDVD+=" -DLIBDVDCSS_URL=${LIBDVDCSS_ARCHIVE}"
+    prepare_libdvd_library libdvdcss
   else
     KODI_LIBDVD="-DENABLE_DVDCSS=OFF"
   fi
 
-  LIBDVDNAV_VERSION="$(grep VERSION= ${PKG_BUILD}/tools/depends/target/libdvdnav/LIBDVDNAV-VERSION)"
-  LIBDVDNAV_ARCHIVE="${SOURCES}/libdvdnav/libdvdnav-${LIBDVDNAV_VERSION#*=}.tar.gz"
-  [ -f "${LIBDVDNAV_ARCHIVE}" ] && KODI_LIBDVD+=" -DLIBDVDNAV_URL=${LIBDVDNAV_ARCHIVE}"
-
-  LIBDVDREAD_VERSION="$(grep VERSION= ${PKG_BUILD}/tools/depends/target/libdvdread/LIBDVDREAD-VERSION)"
-  LIBDVDREAD_ARCHIVE="${SOURCES}/libdvdread/libdvdread-${LIBDVDREAD_VERSION#*=}.tar.gz"
-  [ -f "${LIBDVDREAD_ARCHIVE}" ] && KODI_LIBDVD+=" -DLIBDVDREAD_URL=${LIBDVDREAD_ARCHIVE}"
-
+  prepare_libdvd_library libdvdnav
+  prepare_libdvd_library libdvdread
   PKG_CMAKE_OPTS_TARGET+=" ${KODI_LIBDVD}"
 }
 
