d77df70d7b4725e34ec58a4fbaaa7a4cda49ef43
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-14 11:00:29 +0000

    vdr-plugin-epgsearch: update to 2.4.2
    move to using upstream PCRE2
    
    log:
    - https://github.com/vdr-projects/vdr-plugin-epgsearch/compare/v2.4.1...v2.4.2
    - From the current version in the LE11 repo.
      https://github.com/vdr-projects/vdr-plugin-epgsearch/compare/cff8dfe58853aa0cc4f6e9a8896104a920b98730...v2.4.2
    
    2023-01-09; Version 2.4.2
    new:
    - Use perlregex-library pcre2 if available
    fixes:
    - timers not marked as done if 2 recordings stop same time
    - remove dead code
    - update spanish translation
    - fix memory leaks
    - do not lose recordingtime at change to winter time (kfb77)

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-epgsearch/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-epgsearch/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-epgsearch"
-PKG_VERSION="cff8dfe58853aa0cc4f6e9a8896104a920b98730"
-PKG_SHA256="ad6521003ab41b76ba22cac3b086901700ed2b46ea3d48f71d28b0545cf17e94"
+PKG_VERSION="2.4.2"
+PKG_SHA256="7c0a03c22fedbc73a34220da0edf3293a903185c412d5b20fb48d72f2e4fd118"
 PKG_LICENSE="GPL"
 PKG_SITE="http://winni.vdr-developer.org/epgsearch/"
-PKG_URL="https://github.com/vdr-projects/vdr-plugin-epgsearch/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain vdr pcre"
+PKG_URL="https://github.com/vdr-projects/vdr-plugin-epgsearch/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain vdr pcre2"
 PKG_NEED_UNPACK="$(get_pkg_directory vdr)"
 PKG_LONGDESC="EPGSearch is a plugin for the Video-Disc-Recorder (VDR)."
 PKG_TOOLCHAIN="manual"
