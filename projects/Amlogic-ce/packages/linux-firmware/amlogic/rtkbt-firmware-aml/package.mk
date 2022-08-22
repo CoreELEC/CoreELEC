563910beb743a0aafcbcfa4bf45acec39ed8808e
Stanislav Vlasic <svlasic@gmail.com>
2022-08-22 18:02:22 +0000

    rtkbt-firmware-aml: Bump package to support RTL8852BS chip

--- projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="rtkbt-firmware-aml"
-PKG_VERSION="763c7195bb0328b45a5679e212ab05150361cc3a"
-PKG_SHA256="2996dd867990a14b8d431c789285363ff35f4f2304ce8c92db401122ec43db9a"
+PKG_VERSION="36f9ce681687223a8138c711e7a3d8271a4435db"
+PKG_SHA256="506eaefab6413a18f1508734e8a33b3d31ad3cc50db77ddf71a435035229e22f"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/CoreELEC/rtkbt-firmware-aml"
-PKG_URL="https://github.com/CoreELEC/rtkbt-firmware-aml/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/Stane1983/rtkbt-firmware-aml"
+PKG_URL="https://github.com/Stane1983/rtkbt-firmware-aml/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain rtk_hciattach"
 PKG_LONGDESC="Realtek BT Linux firmware"
 PKG_TOOLCHAIN="manual"
