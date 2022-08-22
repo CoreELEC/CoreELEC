6d2c88164a7f0a8cb1fb7f36d18dbcd12e07d66a
shantigilbert <shantic@gmail.com>
2022-08-22 15:12:25 -0500

    emuelec-emulationstation: Bump to 6c5b34d

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="9a40995d4eb4510af15d020f469097d8781b8061"
+PKG_VERSION="6c5b34d2603106c2978f22be048483efbb9dd5f4"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
