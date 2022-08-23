a6301b3d66a5558b425d39ba630cdb4bfc0a445e
Stanislav Vlasic <svlasic@gmail.com>
2022-08-23 09:38:19 +0000

    rtkbt-firmware-aml: Move repo back to CoreELEC

--- projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-firmware/amlogic/rtkbt-firmware-aml/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="rtkbt-firmware-aml"
-PKG_VERSION="36f9ce681687223a8138c711e7a3d8271a4435db"
-PKG_SHA256="506eaefab6413a18f1508734e8a33b3d31ad3cc50db77ddf71a435035229e22f"
+PKG_VERSION="4d95579f256383af2df39d796f38a91ee6ec0b80"
+PKG_SHA256="7a2884532969f84596f469cde599b8ecc026e80263ae64c9faf0e22a0332f569"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/Stane1983/rtkbt-firmware-aml"
-PKG_URL="https://github.com/Stane1983/rtkbt-firmware-aml/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/rtkbt-firmware-aml"
+PKG_URL="https://github.com/CoreELEC/rtkbt-firmware-aml/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain rtk_hciattach"
 PKG_LONGDESC="Realtek BT Linux firmware"
 PKG_TOOLCHAIN="manual"
