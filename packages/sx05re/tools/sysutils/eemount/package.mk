6aa99e91342b160794caf8fca6d57a9ff8251fd0
Guoxin Pu <pugokushin@gmail.com>
2022-08-27 13:55:10 +0800

    eemount: bump 4adf313; init: fix disabled systemds units (#983)

--- packages/sx05re/tools/sysutils/eemount/package.mk
+++ packages/sx05re/tools/sysutils/eemount/package.mk
@@ -2,10 +2,11 @@
 # Copyright (C) 2022-present 7Ji (https://github.com/7Ji)
 
 PKG_NAME="eemount"
-PKG_VERSION="1e83ccd4f76fbb7c08f686e2eff0682fbefd9e40"
-PKG_SHA256="10d8d4b40ee5daa335521ea3d3c0626bde214536f52c69f22bd2ab35f3d40b3e"
+PKG_VERSION="4adf31387a9019cb07db861374839d496f2c95e5"
+PKG_SHA256="84b0acb5f3c34a28ddc9e933b50fb2a395b0c044fcc300c79a97f405b0c03773"
 PKG_SITE="https://github.com/7Ji/eemount"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_LONGDESC="Multi-source ROMs mounting utility for EmuELEC"
 PKG_TOOLCHAIN="make"
+PKG_MAKE_OPTS_TARGET="LOGGING_ALL_TO_STDOUT=1"
