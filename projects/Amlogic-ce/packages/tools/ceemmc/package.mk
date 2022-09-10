6a35b67f91fdd5add4de31094903739b31987822
Peter <peter.vicman@gmail.com>
2022-09-10 14:09:29 +0200

    ceemmc: bump package to e7be656a

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="ceemmc"
-PKG_VERSION="5c89919264c75edade85cd3ad0f0756f42f2ad4a"
-PKG_SHA256="bc4ddba5a5fd17cacccbe4200fa8f4b3d89084b01efeec3caad56fc8c8a7e9c8"
+PKG_VERSION="e7be656a035a85da07fdfae4761e031120826943"
+PKG_SHA256="6c923ac1c022228533b0d19ebb58fc665c3a53a1924c5b5269d6b8795c025c57"
 PKG_SOURCE_NAME="$PKG_NAME-$ARCH-$PKG_VERSION.tar.xz"
 PKG_LICENSE="proprietary"
 PKG_SITE="https://coreelec.org"
