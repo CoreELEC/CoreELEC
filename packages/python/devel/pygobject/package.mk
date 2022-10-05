0ca712d2c43ffb82533ef7ef45c5767176ec8ec4
Peter <peter.vicman@gmail.com>
2022-10-05 16:33:34 +0200

    pygobject: bump to 3.42.2

--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pygobject"
-PKG_VERSION="3.42.1"
-PKG_SHA256="1f34b5f7624de35e44eb5a7eb428353285bd03004d55131a5f7f7fa9b90f3cc9"
+PKG_VERSION="3.42.2"
+PKG_SHA256="ade8695e2a7073849dd0316d31d8728e15e1e0bc71d9ff6d1c09e86be52bc957"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.pygtk.org/"
