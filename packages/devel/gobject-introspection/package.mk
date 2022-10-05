11c16bcadfd000b6cbf7767c59216ec08a78fced
Peter <peter.vicman@gmail.com>
2022-10-05 16:33:48 +0200

    gobject-introspection: bump to 1.74.0

--- packages/devel/gobject-introspection/package.mk
+++ packages/devel/gobject-introspection/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2020-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="gobject-introspection"
-PKG_VERSION="1.72.0"
-PKG_SHA256="07c30d7f465dabd861c03a950430343e976d445179ad451a4dd1b17329e1cbae"
+PKG_VERSION="1.74.0"
+PKG_SHA256="79ed5d764d288f046b027ff064be174d7904904565de150a94841740a2a0455d"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.gtk.org/"
