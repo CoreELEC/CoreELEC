09d2180dae8e54624b3c35b2e254125df23afef8
Peter <peter.vicman@gmail.com>
2022-10-05 16:34:02 +0200

    dbus-python: bump to 1.3.2

--- packages/python/system/dbus-python/package.mk
+++ packages/python/system/dbus-python/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="dbus-python"
-PKG_VERSION="1.2.18"
-PKG_SHA256="92bdd1e68b45596c833307a5ff4b217ee6929a1502f5341bae28fd120acf7260"
+PKG_VERSION="1.3.2"
+PKG_SHA256="ad67819308618b5069537be237f8e68ca1c7fcc95ee4a121fe6845b1418248f8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://freedesktop.org/wiki/Software/dbus"
 PKG_URL="https://dbus.freedesktop.org/releases/dbus-python/${PKG_NAME}-${PKG_VERSION}.tar.gz"
@@ -14,6 +14,7 @@ PKG_LONGDESC="D-BUS is a message bus, used for sending messages between applicat
 PKG_BUILD_FLAGS="+lto"
 
 pre_configure_target() {
+  export PKG_CONFIG_PATH="${SYSROOT_PREFIX}/usr/lib/pkgconfig"
   export PYTHON_CONFIG="${SYSROOT_PREFIX}/usr/bin/python3-config"
   export PYTHON_INCLUDES="$(${SYSROOT_PREFIX}/usr/bin/python3-config --includes)"
   export PYTHON_LIBS="$(${SYSROOT_PREFIX}/usr/bin/python3-config --ldflags --embed)"
