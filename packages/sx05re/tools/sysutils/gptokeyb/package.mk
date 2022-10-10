20615fe7ae47e1846a53dbe1b3bdf437fd75ae58
shantigilbert <shantic@gmail.com>
2022-10-10 13:18:05 -0500

    gptokeyb: use correct git hash 490b758

--- packages/sx05re/tools/sysutils/gptokeyb/package.mk
+++ packages/sx05re/tools/sysutils/gptokeyb/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="gptokeyb"
-PKG_VERSION="30b65b0e54e520adfc0b3ce8d13a1ccfaf944595"
+PKG_VERSION="490b75876dedfd24ab526d58da30e17c8eff5b11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/EmuELEC/gptokeyb"
