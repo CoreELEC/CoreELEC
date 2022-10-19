9d05829524b1130d2ea614c95150c28a9b439eca
shantigilbert <shantic@gmail.com>
2022-10-19 17:12:26 -0500

    Retroarch: Bump to 6ade323

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="1e4042d08b2514bca0e9b03fe721ad52cd071de1"
+PKG_VERSION="6ade32336a990dfc9d25369596418a9fb505f367"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="$PKG_SITE.git"
 PKG_LICENSE="GPLv3"
