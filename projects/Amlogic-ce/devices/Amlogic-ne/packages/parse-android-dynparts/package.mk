d4a5dea1516a7f061e2ab25699e4390f50360e6c
Peter <peter.vicman@gmail.com>
2022-10-20 16:53:23 +0200

    Amlogic-ne: add package parse-android-dynparts allow mounting Android super partition with a standard Linux userspace tools https://source.android.com/docs/core/ota/dynamic_partitions/implement

---
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/parse-android-dynparts/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="parse-android-dynparts"
+PKG_VERSION="c8837c1cd0c4fbc29641980b71079fc4f3cabcc0"
+PKG_SHA256="5a9df34c2078ff947a478723e8b06dc427bc71c238125657a913536acb29146a"
+PKG_LICENSE="Apache-2.0 license"
+PKG_SITE="https://github.com/tchebb/parse-android-dynparts"
+PKG_URL="https://github.com/tchebb/parse-android-dynparts/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Tool mounts super.img files with a standard Linux userspace tools."
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_CXX_FLAGS=-D_FILE_OFFSET_BITS=64"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/sbin
+  cp ${PKG_BUILD}/.${TARGET_NAME}/parse-android-dynparts ${INSTALL}/usr/sbin
+}
