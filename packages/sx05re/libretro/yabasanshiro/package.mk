96750dfbcab4e7e120bfa489a7d9772dff6cd974
shantigilbert <shantic@gmail.com>
2022-11-17 10:56:29 -0600

    Remove lr-yabasanshiro, set YabasanshiroSA as default

--- packages/sx05re/libretro/yabasanshiro/package.mk
+++ packages/sx05re/libretro/yabasanshiro/package.mk
@@ -51,6 +51,10 @@ else
 		sed -i "s|-mtune=cortex-a73.cortex-a53|-mtune=cortex-a53|g" $PKG_BUILD/yabause/src/libretro/Makefile
 	fi
 		PKG_MAKE_OPTS_TARGET+=" -C yabause/src/libretro platform=odroid-n2"
+
+# yabasanshiro seems to only work in debug mode, it is not recomended for use as it will be slow. But until a fix is found this will have to do
+sed -i "s|DEBUG = 0|DEBUG = 1|g" $PKG_BUILD/yabause/src/libretro/Makefile
+
 fi
 }
 
