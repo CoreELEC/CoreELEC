375bbf25a89e0e8a528e9ffad35201632105fef2
Portisch <hugo.portisch@yahoo.de>
2022-12-07 10:07:55 +0100

    opentee_linuxdriver: bump package to 3108eb27

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/opentee_linuxdriver/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/opentee_linuxdriver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="opentee_linuxdriver"
-PKG_VERSION="423100e389fafd31da16530594c1b6171857d58c"
-PKG_SHA256="bf382a4e698215458ec1dfb5a44eea2ce2644552772c1c0efb1b716639c1d7e2"
+PKG_VERSION="3108eb27eb23937c4bc7a0a05e72e2aaf1ae6800"
+PKG_SHA256="abafdbe20673635289edbc3c93d903dc7752a52d8a8f78582a738ec09b929ee0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC/opentee_linuxdriver"
 PKG_URL="https://github.com/CoreELEC/opentee_linuxdriver/archive/${PKG_VERSION}.tar.gz"
