8b8ec5078b2cdf6fc3e54e68c99c0b81d9f86bcb
Portisch <hugo.portisch@yahoo.de>
2022-12-13 16:23:39 +0100

    dvb-latest: add support for Amlogic-ne

--- projects/Amlogic-ce/packages/addons/driver/dvb-latest/package.mk
+++ projects/Amlogic-ce/packages/addons/driver/dvb-latest/package.mk
@@ -3,13 +3,12 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="dvb-latest"
-PKG_VERSION="0f25e6fb13b6bc345218800ad9ac863deb2ee9c8"
-PKG_SHA256="71294995cff2bc556bcf9aa7ade595a4ccd91a1972b73d1e537d99d2ce86dbed"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.linuxtv.org/media_build.git"
-PKG_URL="https://git.linuxtv.org/media_build.git/snapshot/${PKG_VERSION}.tar.gz"
+PKG_URL="https://git.linuxtv.org/media_build.git"
+GET_HANDLER_SUPPORT="git"
 PKG_DEPENDS_TARGET="toolchain linux media_tree"
-PKG_NEED_UNPACK="$LINUX_DEPENDS $(get_pkg_directory media_tree)"
+PKG_NEED_UNPACK="${LINUX_DEPENDS} $(get_pkg_directory media_tree)"
 PKG_SECTION="driver.dvb"
 PKG_LONGDESC="DVB drivers from the latest kernel (media_build)"
 
@@ -21,19 +20,19 @@ PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
 PKG_ADDON_REQUIRES="script.program.driverselect:0.0.0"
 
-if [ "$PROJECT" = "Amlogic-ce" ]; then
-  PKG_PATCH_DIRS="amlogic-common"
-  case "$LINUX" in
-    amlogic-3.14)
-      PKG_PATCH_DIRS+=" amlogic-3.14"
-      ;;
-    amlogic-4.9)
-      PKG_PATCH_DIRS+=" amlogic-4.9"
-      ;;
-  esac
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET media_tree_aml"
-  PKG_NEED_UNPACK="$PKG_NEED_UNPACK $(get_pkg_directory media_tree_aml)"
-fi
+PKG_PATCH_DIRS="amlogic-common"
+case "${LINUX}" in
+  amlogic-4.9)
+    PKG_VERSION="0f25e6fb13b6bc345218800ad9ac863deb2ee9c8"
+    PKG_DEPENDS_TARGET+=" media_tree_aml"
+    PKG_NEED_UNPACK+=" $(get_pkg_directory media_tree_aml)"
+    PKG_PATCH_DIRS+=" amlogic-4.9"
+    ;;
+  amlogic-5.4)
+    PKG_VERSION="680a07be51069bee47a07a4bcf36c5176f1290a4"
+    PKG_PATCH_DIRS+=" amlogic-5.4"
+    ;;
+esac
 
 pre_make_target() {
   export KERNEL_VER=$(get_module_dir)
@@ -41,40 +40,24 @@ pre_make_target() {
 }
 
 make_target() {
-  cp -RP $(get_build_dir media_tree)/* $PKG_BUILD/linux
+  cp -RP $(get_build_dir media_tree)/* ${PKG_BUILD}/linux
 
-  if [ "$PROJECT" = "Amlogic-ce" ]; then
-    cp -Lr $(get_build_dir media_tree_aml)/* $PKG_BUILD/linux
+  if [ "${DEVICE}" = "Amlogic-ng" ]; then
+    cp -Lr $(get_build_dir media_tree_aml)/* ${PKG_BUILD}/linux
 
     # compile modules
-    echo "obj-y += video_dev/" >> "$PKG_BUILD/linux/drivers/media/platform/meson/Makefile"
-    echo "obj-y += dvb/" >> "$PKG_BUILD/linux/drivers/media/platform/meson/Makefile"
-    echo 'source "drivers/media/platform/meson/dvb/Kconfig"' >>  "$PKG_BUILD/linux/drivers/media/platform/Kconfig"
-    sed -e 's/ && RC_CORE//g' -i $PKG_BUILD/linux/drivers/media/usb/dvb-usb/Kconfig
+    echo "obj-y += video_dev/" >> "${PKG_BUILD}/linux/drivers/media/platform/meson/Makefile"
+    echo "obj-y += dvb/" >> "${PKG_BUILD}/linux/drivers/media/platform/meson/Makefile"
+    echo 'source "drivers/media/platform/meson/dvb/Kconfig"' >>  "${PKG_BUILD}/linux/drivers/media/platform/Kconfig"
+    sed -e 's/ && RC_CORE//g' -i ${PKG_BUILD}/linux/drivers/media/usb/dvb-usb/Kconfig
   fi
 
   # make config all
   kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path) allyesconfig
 
-  # hack to workaround media_build bug
-  if [ "$PROJECT" = "Amlogic" ]; then
-    sed -e 's/CONFIG_DVB_LGDT3306A=m/# CONFIG_DVB_LGDT3306A is not set/g' -i v4l/.config
-    sed -e 's/CONFIG_VIDEO_S5C73M3=m/# CONFIG_VIDEO_S5C73M3 is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_SAA7146_VV=m/# CONFIG_VIDEO_SAA7146_VV is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_OV2659=m/# CONFIG_VIDEO_OV2659 is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_OV5647=m/# CONFIG_VIDEO_OV5647 is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_S5K5BAF=m/# CONFIG_VIDEO_S5K5BAF is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_VIVID=m/# CONFIG_VIDEO_VIVID is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_TVP514X=m/# CONFIG_VIDEO_TVP514X is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_TVP7002=m/# CONFIG_VIDEO_TVP7002 is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_CADENCE_CSI2RX=m/# CONFIG_VIDEO_CADENCE_CSI2RX is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/CONFIG_VIDEO_CADENCE_CSI2TX=m/# CONFIG_VIDEO_CADENCE_CSI2TX is not set/g' -i $PKG_BUILD/v4l/.config
-    sed -e 's/# CONFIG_MEDIA_TUNER_TDA18250 is not set/CONFIG_MEDIA_TUNER_TDA18250=m/g' -i $PKG_BUILD/v4l/.config
-  fi
-
   kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path)
 }
 
 makeinstall_target() {
-  install_driver_addon_files "$PKG_BUILD/v4l/"
+  install_driver_addon_files "${PKG_BUILD}/v4l/"
 }
