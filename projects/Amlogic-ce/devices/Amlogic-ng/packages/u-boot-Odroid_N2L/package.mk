a3499ad4fe0573c3818be1622250e2e144f51168
Peter <peter.vicman@gmail.com>
2022-12-17 15:41:36 +0100

    u-boot: switch to public and bump Odroid N2L to 25aa5907

--- projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Odroid_N2L/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Odroid_N2L/package.mk
@@ -2,12 +2,11 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Odroid_N2L"
-PKG_VERSION="9a921f676ca1dbdddcdd7edee95294d142643faa"
-PKG_SHA256="e4008d12076205103dbcd94ad276d8614a59f21c29d31030aecc9c62c688eb1e"
+PKG_VERSION="25aa59079acfc84fec943e18f2240581f6afb8ca"
+PKG_SHA256="cdc7f418655d45a22d278f568104950cac6970758f4537fbb251e076dc392455"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
-PKG_URL="https://github.com/CoreELEC/u-boot-private/archive/$PKG_VERSION.tar.gz"
-PKG_USETOKEN="yes"
+PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-elf:host gcc-linaro-arm-eabi:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
 PKG_TOOLCHAIN="manual"
