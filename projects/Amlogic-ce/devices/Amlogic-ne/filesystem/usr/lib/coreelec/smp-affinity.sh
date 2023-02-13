8c4b87de49a376055848ddbd92d0522364de24e3
Portisch <hugo.portisch@yahoo.de>
2023-02-13 08:49:39 +0100

    Amlogic-ce: split smp-affinity.sh meson-ir IRQ name does not match between 4.9 and 5.4 kernel fix aff overflow on second loop like if irqs count > (cpu count - 1) * 2

--- projects/Amlogic-ce/devices/Amlogic-ne/filesystem/usr/lib/coreelec/smp-affinity.sh
+++ projects/Amlogic-ce/devices/Amlogic-ne/filesystem/usr/lib/coreelec/smp-affinity.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+# Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 SMP="../smp_affinity"
-MESONIR="`find /proc/irq/ -name ir-meson`"
+MESONIR="`find /proc/irq/ -name *meson*ir`"
 ETH0="`find /proc/irq/ -name eth0`"
 VDEC0="`find /proc/irq/ -name vdec-0`"
 VDEC1="`find /proc/irq/ -name vdec-1`"
@@ -14,21 +14,15 @@ AOCEC="`find /proc/irq/ -name hdmi_aocec*`"
 USB3="`find /proc/irq/ -name xhci-hcd:usb1`"
 IRQ="$AOCEC $ETH0 $USB3 $VDEC0 $VDEC1 $AFIFO0 $MESONIR $PREDI"
 
-cpu=0
-aff=1
+cpu=1
 for i in $IRQ; do
     if [ -f "$i/$SMP" ];then
-        cpu=$((cpu + 1))
-        if [ -d "/sys/devices/system/cpu/cpu$cpu" ];then
-            aff=$((aff * 2))
-            haff=`printf '%x\n' $aff`
-        else
-            aff=2
-            haff=2
-            cpu=0
-        fi
+        [ ! -d "/sys/devices/system/cpu/cpu$cpu" ] && cpu=1
+        aff=$((1 << $cpu))
+        haff=`printf '%x\n' $aff`
         echo "echo $haff > $i/$SMP"
         echo $haff > $i/$SMP
+        cpu=$((cpu + 1))
     fi
 done
 
