edee276d88e75daca68bdd4706f0b1e81ba8a239
Portisch <hugo.portisch@yahoo.de>
2023-02-16 09:38:54 +0100

    inject_bl301: add ARCH dependent PKG_SHA256

--- projects/Amlogic-ce/devices/Amlogic-ne/packages/sysutils/inject_bl301/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/sysutils/inject_bl301/package.mk
@@ -3,7 +3,6 @@
 
 PKG_NAME="inject_bl301"
 PKG_VERSION="fdd519917b8c257ea56cced7e962a2a89f60fb37"
-PKG_SHA256="5be388fd4728b5d44a9f68321aa840bba94b5e050d58a04b77320885f2851281"
 PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
 PKG_LICENSE="proprietary"
 PKG_SITE="https://coreelec.org"
@@ -11,6 +10,15 @@ PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
 PKG_DEPENDS_TARGET="toolchain bl30"
 PKG_LONGDESC="Tool to inject bootloader blob BL30.bin on internal eMMC"
 PKG_TOOLCHAIN="manual"
+
+case "${ARCH}" in
+  arm)
+    PKG_SHA256="5be388fd4728b5d44a9f68321aa840bba94b5e050d58a04b77320885f2851281"
+    ;;
+  aarch64)
+    PKG_SHA256="04a38b47264cccf2dbdf8f9f1a9606740687d3e30d4d21a02b0845901dcde456"
+    ;;
+esac
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/sbin
