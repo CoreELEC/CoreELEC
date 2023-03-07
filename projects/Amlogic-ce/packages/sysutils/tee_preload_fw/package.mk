d34b35e11ba5fc6ca20c353de52d76804968b599
Portisch <hugo.portisch@yahoo.de>
2023-03-07 07:44:02 +0100

    tee_preload_fw: update aarch64 to 3425325d061f34cdfa5c52c5359aeb223fb9025d

--- projects/Amlogic-ce/packages/sysutils/tee_preload_fw/package.mk
+++ projects/Amlogic-ce/packages/sysutils/tee_preload_fw/package.mk
@@ -12,11 +12,17 @@ PKG_LONGDESC="SECPU FW Loader"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/lib
+  mkdir -p ${INSTALL}/usr/lib/ta
     ln -sf /var/lib/teetz ${INSTALL}/usr/lib/teetz
-    cp -rP $(get_pkg_directory ${PKG_NAME})/filesystem/noarch/. ${INSTALL}
-    cp -rP $(get_pkg_directory ${PKG_NAME})/filesystem/${ARCH}/. ${INSTALL}
+
+    for soc in ${TEE_SOC}; do
+      cp -rP $(get_pkg_directory ${PKG_NAME})/filesystem/${ARCH}/ta/v3.8/dev/${soc} ${INSTALL}/usr/lib/ta
+    done
+
+  mkdir -p ${INSTALL}/usr/sbin
     cp ${PKG_DIR}/scripts/trusted-application-setup ${INSTALL}/usr/sbin
+
+  cp -rP $(get_pkg_directory ${PKG_NAME})/filesystem/${ARCH}/usr/. ${INSTALL}/usr
 }
 
 post_install() {
