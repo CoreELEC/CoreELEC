7378a9841816508c25fc9e0272ce432af5266487
Peter <peter.vicman@gmail.com>
2023-03-07 09:06:06 +0100

    mbedtls: fix package dependencies

--- projects/Amlogic-ce/packages/addons/addon-depends/jsonschema/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/jsonschema/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="0f864437ab8b6076ba6707453ef8f98a6a0d512a80e93f8abdb676f737ecb60d"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pypi.org/project/jsonschema/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="Python3:host"
+PKG_DEPENDS_HOST="Python3:host attrs:host pyrsistent:host"
 PKG_LONGDESC="An implementation of JSON Schema validation for Python."
 PKG_TOOLCHAIN="manual"
 
--- projects/Amlogic-ce/packages/addons/addon-depends/mbedtls/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/mbedtls/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="113fa84bc3cf862d56e7be0a656806a5d02448215d1e22c98176b1c372345d33"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/Mbed-TLS/mbedtls"
 PKG_URL="https://github.com/Mbed-TLS/mbedtls/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain jsonschema:host attrs:host pyrsistent:host Jinja2:host"
+PKG_DEPENDS_TARGET="toolchain jsonschema:host Jinja2:host"
 PKG_LONGDESC="Mbed TLS is a C library that implements cryptographic primitives, X.509 certificate manipulation and the SSL/TLS and DTLS protocols."
 
 PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
