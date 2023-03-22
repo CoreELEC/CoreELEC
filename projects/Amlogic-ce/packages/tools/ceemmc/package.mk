4318688d21df75ad9c1a409574778f48eb5d143a
Portisch <hugo.portisch@yahoo.de>
2023-03-22 14:13:15 +0100

    ceemmc: bump package to b8b56af2

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -7,18 +7,8 @@ PKG_SITE="https://coreelec.org"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Tool to install CoreELEC on internal eMMC"
 PKG_TOOLCHAIN="manual"
-
-case "${DEVICE}" in
-  Amlogic-ng)
-    PKG_VERSION="e7be656a035a85da07fdfae4761e031120826943"
-    PKG_SHA256="6c923ac1c022228533b0d19ebb58fc665c3a53a1924c5b5269d6b8795c025c57"
-    ;;
-  Amlogic-ne)
-    PKG_VERSION="b494d2a5e8f5b83728005b39b87e6f0243236c9f"
-    PKG_SHA256="0804967f3e0c742c4cb9d4f8e90b076663fe9ff0bb647ff273ac33158f532c3f"
-    ;;
-esac
-
+PKG_VERSION="b8b56af20b04e5d2e7b0968a257ec7ce5b0c779a"
+PKG_SHA256="4cf138ac8b52ebd08ef7c7823e9e90dd37cbf6a69e5699adb2becb315c1dc826"
 PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
 PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
 
