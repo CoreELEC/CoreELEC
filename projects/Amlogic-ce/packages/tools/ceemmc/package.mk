11b06d7eb947c56e6b122f02671eeab4466cdcc4
Portisch <hugo.portisch@yahoo.de>
2023-04-07 10:15:30 +0200

    ceemmc: bump package to 3143cf2a

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -7,14 +7,14 @@ PKG_SITE="https://coreelec.org"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Tool to install CoreELEC on internal eMMC"
 PKG_TOOLCHAIN="manual"
-PKG_VERSION="b8b56af20b04e5d2e7b0968a257ec7ce5b0c779a"
+PKG_VERSION="3143cf2ab3dec9f47f822a3c5adbaf8640bc65da"
 
 case "${ARCH}" in
   arm)
-    PKG_SHA256="4cf138ac8b52ebd08ef7c7823e9e90dd37cbf6a69e5699adb2becb315c1dc826"
+    PKG_SHA256="493a809c95c3021c87597fb77ea9e9c39d174b5d5c4f81c40810ceebc71ce1ba"
     ;;
   aarch64)
-    PKG_SHA256="907f652754c611cd39caf562508fba1d25c35cc661e2349102510660ccaa13e5"
+    PKG_SHA256="0665182ad9a5ab2de330fc55559c7b1dd23554000507d7a532b5b000ba2cbac1"
     ;;
 esac
 
