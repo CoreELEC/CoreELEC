272da75fc89063be80b9a6e6f6b2ad418755fdfb
Portisch <hugo.portisch@yahoo.de>
2023-04-07 09:42:37 +0200

    libamcodec: bump package to eb874808

--- projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="libamcodec"
-PKG_VERSION="dcd4999ba708fc84e4662c39262bbbcf92219ed9"
+PKG_VERSION="eb874808303936404027f3fc7f7434285d0a7d2f"
 PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
 PKG_LICENSE="proprietary"
 PKG_SITE="http://openlinux.amlogic.com"
@@ -14,10 +14,10 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   arm)
-    PKG_SHA256="3b987f85a11a19835d471757f5eab13b0bb7356202275c08ff4a6b6fd7cf3758"
+    PKG_SHA256="9465b1029aa8ca7e2d1c5ffc3c2c9c5c524682e1c84321d91766dbdc3f26ccb6"
     ;;
   aarch64)
-    PKG_SHA256="693bd42257286a463a616617208b9cf284a56b94c7b8059ee9c4833cfaaa2fed"
+    PKG_SHA256="b6620dea6fe1856695bfd032b3ddb2310c9bd5556920bc818d656c1a325b9e1a"
     ;;
 esac
 
