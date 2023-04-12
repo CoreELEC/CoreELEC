7fb819c80a8f4fed0e15297b69390a7c9478e671
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-12 14:46:20 +0000

    openal-soft: update to 1.23.1

--- packages/audio/openal-soft/package.mk
+++ packages/audio/openal-soft/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openal-soft"
-PKG_VERSION="1.23.0"
-PKG_SHA256="af2abf9cb539c9d66296a83ba63a75eb5868658d0f0b28b27c556f45e70c5231"
+PKG_VERSION="1.23.1"
+PKG_SHA256="dfddf3a1f61059853c625b7bb03de8433b455f2f79f89548cbcbd5edca3d4a4a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openal.org/"
 PKG_URL="https://github.com/kcat/openal-soft/archive/${PKG_VERSION}.tar.gz"
