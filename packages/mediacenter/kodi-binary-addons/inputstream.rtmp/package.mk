7b27eb41b32af52a8f3ce9341070b949eb81f638
Peter Vicman <peter.vicman@gmail.com>
2023-04-23 10:02:01 +0200

    inputstream.rtmp: update to 21.0.1-Omega

--- packages/mediacenter/kodi-binary-addons/inputstream.rtmp/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.rtmp/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.rtmp"
-PKG_VERSION="20.3.0-Nexus"
-PKG_SHA256="6a6129dca822e1447c0945ddf9cc6dbff1203dab313395d27efb4669a0ef3370"
-PKG_REV="6"
+PKG_VERSION="21.0.1-Omega"
+PKG_SHA256="38c84aa98f9c0323559b70eae62a18c72ea67769cd5fef650d6fb262323fb0bc"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/inputstream.rtmp"
