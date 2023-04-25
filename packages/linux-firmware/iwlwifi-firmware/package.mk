11f97d7a57a232450f68c4ec2fd9420b2f236cc9
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-25 04:52:54 +0000

    iwlwifi-firmware: update to 83d522a

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="2eb6baa08b3a3c9bf3f9f12ec6d9b94eca1d36e7"
-PKG_SHA256="2641d8587536f646e28468dad6cda962fbdee9c45571075546135c4ca8776162"
+PKG_VERSION="83d522a7b8c7fab580a8a7cbe6a8637d206d0cc3"
+PKG_SHA256="f7e0124f15461fefbd93b6323dcde451362d81fee9da331f6168dec028a453d4"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
