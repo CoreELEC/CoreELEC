f93b584776b906320222aba5683510ced345674f
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-26 17:41:02 +0000

    pvr.iptvsimple: update 21.2.1-Omega to 21.3.0-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="21.2.1-Omega"
-PKG_SHA256="62291307c0deaa82977a2c407831bb996ded3fd0574423b628d164d89c15bf9e"
-PKG_REV="2"
+PKG_VERSION="21.3.0-Omega"
+PKG_SHA256="ecdbae4f06bcc74fad260a11b048879256cd3457848f7af03731b36ce21a098b"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.iptvsimple"
