1e39e76fcc6099438dba2d1ea2eee27b17df8584
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-15 15:32:48 +0000

    ethtool: update to 6.3

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ethtool"
-PKG_VERSION="6.2"
-PKG_SHA256="86df0114064d4d73f6bf72bf03e85c33964a519ee0c1d1ba65005ad2d0e570e1"
+PKG_VERSION="6.3"
+PKG_SHA256="d9425f0a3df138734001fccc4175fe178c025f938460ac25c4ebc39960168822"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.kernel.org/pub/software/network/ethtool/"
 PKG_URL="https://www.kernel.org/pub/software/network/ethtool/${PKG_NAME}-${PKG_VERSION}.tar.xz"
