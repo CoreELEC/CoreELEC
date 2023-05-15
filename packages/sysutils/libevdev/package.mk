7573367ff546edc3f60d609460277a55c82f0070
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-15 15:34:01 +0000

    libevdev: update to 1.13.1

--- packages/sysutils/libevdev/package.mk
+++ packages/sysutils/libevdev/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libevdev"
-PKG_VERSION="1.13.0"
-PKG_SHA256="9edf2006cc86a5055279647c38ec923d11a821ee4dc2c3033e8d20e8ee237cd9"
+PKG_VERSION="1.13.1"
+PKG_SHA256="06a77bf2ac5c993305882bc1641017f5bec1592d6d1b64787bad492ab34f2f36"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libevdev/"
 PKG_URL="http://www.freedesktop.org/software/libevdev/${PKG_NAME}-${PKG_VERSION}.tar.xz"
