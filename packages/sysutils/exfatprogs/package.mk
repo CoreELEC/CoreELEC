73e809caa45fbb13c4496627db1b5f4b87248c14
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-20 12:42:13 +0000

    exfatprogs: update to 1.2.1

--- packages/sysutils/exfatprogs/package.mk
+++ packages/sysutils/exfatprogs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="exfatprogs"
-PKG_VERSION="1.2.0"
-PKG_SHA256="56d9a49465deafc367d428afc71c8098705a30ee19a3cdf3c5320650b8880742"
+PKG_VERSION="1.2.1"
+PKG_SHA256="a6f3b1fb4bd37835c8f8cb421aac4eb75b880a51342b29850c4063973162227b"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/exfatprogs/exfatprogs"
 PKG_URL="https://github.com/exfatprogs/exfatprogs/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
