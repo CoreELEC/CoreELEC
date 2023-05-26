629b65c8829f8b758933f5d833de3bfa47619d49
Peter <peter.vicman@gmail.com>
2023-05-26 17:32:15 +0200

    Amlogic-ng/Amlogic-ne: fix update

--- projects/Amlogic-ce/devices/Amlogic-ng/bootloader/canupdate.sh
+++ projects/Amlogic-ce/devices/Amlogic-ng/bootloader/canupdate.sh
@@ -9,8 +9,8 @@ if [ "$(uname -r)" = "3.14.29" ]; then
 fi
 
 # allow upgrades between aarch64 and arm images
-PROJECT=$("$1" | cut -d. -f1)
-if [ "$1" = "${PROJECT}.aarch64" -o "$1" = "${PROJECT}.arm" ]; then
+PROJECT=$(echo "${1}" | cut -d. -f1)
+if [ "${1}" = "${PROJECT}.aarch64" -o "${1}" = "${PROJECT}.arm" ]; then
   exit 0
 else
   exit 1
