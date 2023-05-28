e7ffe8ccbefdbcc109625c5edea6ad60cb6ca045
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-28 07:01:13 +0000

    cli: update to 24.0.2

--- packages/addons/addon-depends/docker/cli/package.mk
+++ packages/addons/addon-depends/docker/cli/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="cli"
 PKG_VERSION="$(get_pkg_version moby)"
-PKG_SHA256="f5916273959fb2df56424bb2c26d8b6feb9a148dd15eb400aedd5d3753e06959"
+PKG_SHA256="632357aa58d7f5e16ce87dbd73641c5f65c25b6501e3917ac4f0ce553a01e0bc"
 PKG_LICENSE="ASL"
 PKG_SITE="https://github.com/docker/cli"
 PKG_URL="https://github.com/docker/cli/archive/v${PKG_VERSION}.tar.gz"
@@ -12,7 +12,7 @@ PKG_LONGDESC="The Docker CLI"
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching release https://github.com/docker/cli/releases
-export PKG_GIT_COMMIT="680212238b47d4299b62ed55e3113a498cde3cef"
+export PKG_GIT_COMMIT="cb74dfcd853482dd43cb553106b1e0cd237acb3e"
 
 configure_target() {
   go_configure
