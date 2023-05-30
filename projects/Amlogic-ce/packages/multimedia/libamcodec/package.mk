5f6cc8e1a4ef41a67c8fd5b8d6708f2a08a35bad
Portisch <hugo.portisch@yahoo.de>
2023-05-30 13:17:32 +0200

    libamcodec:Amlogic-ne: bump package to 0f27efb9

--- projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libamcodec/package.mk
@@ -3,23 +3,25 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="libamcodec"
-PKG_VERSION="eb874808303936404027f3fc7f7434285d0a7d2f"
-PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
 PKG_LICENSE="proprietary"
 PKG_SITE="http://openlinux.amlogic.com"
-PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="libamplayer: Interface library for Amlogic media codecs"
 PKG_TOOLCHAIN="manual"
 
-case "${ARCH}" in
-  arm)
+case "${DEVICE}" in
+  Amlogic-ng*)
+    PKG_VERSION="eb874808303936404027f3fc7f7434285d0a7d2f"
     PKG_SHA256="9465b1029aa8ca7e2d1c5ffc3c2c9c5c524682e1c84321d91766dbdc3f26ccb6"
     ;;
-  aarch64)
-    PKG_SHA256="b6620dea6fe1856695bfd032b3ddb2310c9bd5556920bc818d656c1a325b9e1a"
+  Amlogic-ne)
+    PKG_VERSION="0f27efb9e958eaa66144709f1964179a019f5c32"
+    PKG_SHA256="9cd7497999bdf4a709aee8e0cefe51940b40b9091dc831dfc08509d82637da30"
     ;;
 esac
+
+PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
 
 make_target() {
   cp -PR * $SYSROOT_PREFIX
