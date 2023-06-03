5d3822911e8a5ebdf892a25ff41aeb5dde79c2d8
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-03 10:01:47 +0000

    MarkupSafe: update to 2.1.3

--- packages/python/devel/MarkupSafe/package.mk
+++ packages/python/devel/MarkupSafe/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="MarkupSafe"
-PKG_VERSION="2.1.2"
-PKG_SHA256="abcabc8c2b26036d62d4c746381a6f7cf60aafcc653198ad678306986b09450d"
+PKG_VERSION="2.1.3"
+PKG_SHA256="af598ed32d6ae86f1b747b82783958b1a4ab8f617b06fe68795c7f026abbdcad"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/MarkupSafe/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
