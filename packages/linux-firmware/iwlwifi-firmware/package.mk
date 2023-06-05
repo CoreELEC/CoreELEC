08c3799a9ebb8c42b17800ee800a6db8326d8fa9
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-05 11:12:25 +0000

    iwlwifi-firmware: update to 03bf32c

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="83d522a7b8c7fab580a8a7cbe6a8637d206d0cc3"
-PKG_SHA256="f7e0124f15461fefbd93b6323dcde451362d81fee9da331f6168dec028a453d4"
+PKG_VERSION="03bf32cb9e3d6545ceaf00be20caee89bae6b936"
+PKG_SHA256="81219e6618912b9591d3e7e7ddb42fa55786d9e1fbf881f8f1f5205c967973be"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
