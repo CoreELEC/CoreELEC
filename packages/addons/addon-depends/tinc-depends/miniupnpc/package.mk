e2fcd3c2d0019d7e7110487605b3d6c8456253b3
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-12 03:45:06 +0000

    miniupnpc: update to 2.2.5

--- packages/addons/addon-depends/tinc-depends/miniupnpc/package.mk
+++ packages/addons/addon-depends/tinc-depends/miniupnpc/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="miniupnpc"
-PKG_VERSION="2.2.4"
-PKG_SHA256="481a5e4aede64e9ef29895b218836c3608d973e77a35b4f228ab1f3629412c4b"
+PKG_VERSION="2.2.5"
+PKG_SHA256="38acd5f4602f7cf8bcdc1ec30b2d58db2e9912e5d9f5350dd99b06bfdffb517c"
 PKG_LICENSE="BSD"
 PKG_SITE="http://miniupnp.free.fr"
 PKG_URL="http://miniupnp.free.fr/files/${PKG_NAME}-${PKG_VERSION}.tar.gz"
