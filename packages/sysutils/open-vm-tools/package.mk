96bc7f427eb5d44aa2354c0b7d3c8b5eaf7eddca
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-14 17:20:10 +0000

    open-vm-tools: update to 12.2.5

--- packages/sysutils/open-vm-tools/package.mk
+++ packages/sysutils/open-vm-tools/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="open-vm-tools"
-PKG_VERSION="12.2.0"
-PKG_SHA256="5127dd8643e4c89a22a93728ea5420d236cd4083bc07d665f70fee08a581d635"
+PKG_VERSION="12.2.5"
+PKG_SHA256="8cf14932fde137d9079891c1c08ac8f41024de0af90e8ff3707fe0e66c687775"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/vmware/open-vm-tools"
