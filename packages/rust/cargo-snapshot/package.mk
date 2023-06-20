6edf498a8c61d3ea0415deb9252df1b3b4d16742
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-20 12:59:38 +0000

    cargo-snapshot: allow for MACHINE_HARDWARE_NAME of aarch64, arm in addition to x86_64

--- packages/rust/cargo-snapshot/package.mk
+++ packages/rust/cargo-snapshot/package.mk
@@ -3,9 +3,23 @@
 
 PKG_NAME="cargo-snapshot"
 PKG_VERSION="$(get_pkg_version rust)"
-PKG_SHA256="650e7a890a52869cd14e2305652bff775aec7fc2cf47fc62cf4a89ff07242333"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
-PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
 PKG_LONGDESC="cargo bootstrap package"
 PKG_TOOLCHAIN="manual"
+
+case "${MACHINE_HARDWARE_NAME}" in
+  "aarch64")
+    PKG_SHA256="8fd2d9806f0601feab1485f79e46d1441af2158c68abf56788ff355d5c6b4ab5"
+    PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
+    ;;
+  "arm")
+    PKG_SHA256="b932b2d562a1383b1fae5e2931f85fd5ea0cbb5da2c7605d5382d7d2680efd7f"
+    PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
+    ;;
+  "x86_64")
+    PKG_SHA256="650e7a890a52869cd14e2305652bff775aec7fc2cf47fc62cf4a89ff07242333"
+    PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
+    ;;
+esac
+PKG_SOURCE_NAME="cargo-snapshot_${PKG_VERSION}_${ARCH}.tar.xz"
