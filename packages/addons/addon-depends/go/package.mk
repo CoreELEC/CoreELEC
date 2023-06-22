25fc76a10de4608990daa3abbeacf76b803bd28f
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-22 10:05:49 +0000

    go: update to 1.21.0

--- packages/addons/addon-depends/go/package.mk
+++ packages/addons/addon-depends/go/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="go"
-PKG_VERSION="1.20.7"
-PKG_SHA256="5f68e08caf09ba1ea8c3c256206673aaf70fa6abce98e6873ef75f01fe69f486"
+PKG_VERSION="1.21.0"
+PKG_SHA256="cf1d9b0504983c1a4c044856c389555070fbc58c3ce1703193a2c3ffe877a8b9"
 PKG_LICENSE="BSD"
 PKG_SITE="https://golang.org"
 PKG_URL="https://github.com/golang/go/archive/${PKG_NAME}${PKG_VERSION}.tar.gz"
