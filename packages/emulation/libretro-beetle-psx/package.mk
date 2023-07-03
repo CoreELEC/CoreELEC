d3eef7fdba1883746b520b972de5bd01328ed4eb
Garrett Brown <themagnificentmrb@gmail.com>
2023-07-03 15:45:26 -0700

    libretro-beetle-psx: Only build software core

--- packages/emulation/libretro-beetle-psx/package.mk
+++ packages/emulation/libretro-beetle-psx/package.mk
@@ -11,29 +11,11 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Standalone port of Mednafen PSX to libretro."
 PKG_TOOLCHAIN="make"
 
-if [ "${OPENGL_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGL}"
-  PKG_LIBNAME="mednafen_psx_hw_libretro.so"
-else
-  PKG_LIBNAME="mednafen_psx_libretro.so"
-fi
-
-if [ "${VULKAN_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${VULKAN}"
-fi
-
+PKG_LIBNAME="mednafen_psx_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
 PKG_LIBVAR="BEETLE-PSX_LIB"
 
 PKG_MAKE_OPTS_TARGET="HAVE_CDROM=1 LINK_STATIC_LIBCPLUSPLUS=0"
-
-if [ "${OPENGL_SUPPORT}" = "yes" -a "${VULKAN_SUPPORT}" = "yes" ]; then
-  PKG_MAKE_OPTS_TARGET+=" HAVE_HW=1"
-elif [ "${OPENGL_SUPPORT}" = "yes" ]; then
-  PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=1"
-elif [ "${VULKAN_SUPPORT}" = "yes" ]; then
-  PKG_MAKE_OPTS_TARGET+=" HAVE_VULKAN=1"
-fi
 
 makeinstall_target() {
   mkdir -p ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}
