d4499e052d46d83bc686237c90debc37b882f12d
Portisch <hugo.portisch@yahoo.de>
2023-07-12 10:44:44 +0200

    u-boot-Khadas_VIM4: bump package to be24ec37 Add multiDTB support

--- projects/Amlogic-ce/devices/Amlogic-ne/packages/u-boot-Khadas_VIM4/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/u-boot-Khadas_VIM4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Khadas_VIM4"
-PKG_VERSION="3efc85a8370796bcec3bcadcdecec9aed973f4a9"
-PKG_SHA256="01214902dee5424e22f1a410741780a61cf8ec8a15274c9c00857fc406dcd66b"
+PKG_VERSION="be24ec376835d55d8897161c69ad21dbab50ca30"
+PKG_SHA256="36e02b0c607dcfd11949b3c85fa9d629f1d184e7d2835cb3668e4cc0e793a894"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="https://github.com/CoreELEC/u-boot/archive/${PKG_VERSION}.tar.gz"
