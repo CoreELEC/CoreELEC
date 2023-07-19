bfc4f8b5a88a7f0dbbd9126dbcf8e93a0af7cea5
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-19 08:38:53 +0000

    libpthread-stubs: update to 0.5

--- packages/devel/libpthread-stubs/package.mk
+++ packages/devel/libpthread-stubs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="libpthread-stubs"
-PKG_VERSION="0.4"
-PKG_SHA256="50d5686b79019ccea08bcbd7b02fe5a40634abcfd4146b6e75c6420cc170e9d9"
+PKG_VERSION="0.5"
+PKG_SHA256="593196cc746173d1e25cb54a93a87fd749952df68699aab7e02c085530e87747"
 PKG_LICENSE="OSS"
 PKG_SITE="http://xcb.freedesktop.org/"
 PKG_URL="http://xcb.freedesktop.org/dist/${PKG_NAME}-${PKG_VERSION}.tar.gz"
