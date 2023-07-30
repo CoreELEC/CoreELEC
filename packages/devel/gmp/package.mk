66e5030783480d0f04c4dff662eebbe30abb0f31
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-30 14:28:15 +0000

    gmp: update to 6.3.0
    ann:
    - https://gmplib.org/list-archives/gmp-announce/2023-July/000050.html

--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmp"
-PKG_VERSION="6.2.1"
-PKG_SHA256="fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2"
+PKG_VERSION="6.3.0"
+PKG_SHA256="a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898"
 PKG_LICENSE="LGPLv3+"
 PKG_SITE="http://gmplib.org/"
 PKG_URL="https://gmplib.org/download/gmp/${PKG_NAME}-${PKG_VERSION}.tar.xz"
