2b2b3d017d47f4ae357de8e52249c895a87a68a3
Rudi Heitbaum <rudi@heitbaum.com>
2023-08-01 10:22:15 +0000

    libxcrypt: initial package

---
+++ packages/security/libxcrypt/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libxcrypt"
+PKG_VERSION="4.4.36"
+PKG_SHA256="e5e1f4caee0a01de2aee26e3138807d6d3ca2b8e67287966d1fefd65e1fd8943"
+PKG_LICENSE="LGPL-2.1"
+PKG_SITE="https://github.com/besser82/libxcrypt"
+PKG_URL="https://github.com/besser82/libxcrypt/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="glibc"
+PKG_LONGDESC="Extended crypt library for descrypt, md5crypt, bcrypt, and others"
