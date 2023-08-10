6d6eab2278cbeef3e3e4f9baeb64045874ef2431
Rudi Heitbaum <rudi@heitbaum.com>
2023-08-10 11:11:30 +0000

    libxml2: update to 2.11.5

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxml2"
-PKG_VERSION="2.11.4"
-PKG_SHA256="a9493ae091f58037dd5e73fc6035a4907eae58e2cc4756abc4e6253ee6036166"
+PKG_VERSION="2.11.5"
+PKG_SHA256="ab3699b697e917c7d5035e81d237a9bc895eb4bf70c0a2259c6d1f125c1b77b6"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org"
 PKG_URL="https://gitlab.gnome.org/GNOME/${PKG_NAME}/-/archive/v${PKG_VERSION}/${PKG_NAME}-v${PKG_VERSION}.tar.bz2"
