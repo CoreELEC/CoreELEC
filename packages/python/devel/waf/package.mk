48fbdb0b775c57d5500144098d8c212aadba9a84
Rudi Heitbaum <rudi@heitbaum.com>
2023-08-12 09:05:44 +0000

    waf: update to 2.0.26

--- packages/python/devel/waf/package.mk
+++ packages/python/devel/waf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="waf"
-PKG_VERSION="2.0.25"
-PKG_SHA256="66cff7beed0e77db874e9232cc08874abb3e866c7f0f1f34ba2f959fde44fdd4"
+PKG_VERSION="2.0.26"
+PKG_SHA256="c33d19c1bdfae1b078edaef2fab19b1bc734294edd4cc005d4881e9d53ed219c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://waf.io"
 PKG_URL="https://waf.io/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
