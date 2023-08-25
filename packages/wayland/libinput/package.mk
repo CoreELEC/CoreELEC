3fac6155b0e6b35722aba046d78ff9364359b4c9
Rudi Heitbaum <rudi@heitbaum.com>
2023-08-25 09:02:29 +0000

    libinput: update to 1.24.0

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libinput"
-PKG_VERSION="1.23.0"
-PKG_SHA256="fad7011705a21f500229199f789f3e3e794b4c9826b70073745cdaec23bc1d0b"
+PKG_VERSION="1.24.0"
+PKG_SHA256="c07cd0f3f464e8d2e07dc9479fd5b9340e637408517b77e7e96b2245f37f6fe6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.freedesktop.org/wiki/Software/libinput/"
 PKG_URL="https://gitlab.freedesktop.org/libinput/libinput/-/archive/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
