13135355d8fb93d8ba444d309cbdeba72a1be0b2
Matthias Reichl <hias@horus.com>
2023-08-29 17:39:11 +0200

    kodi: update to aaef1a0
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="21.0a2-Omega"
-PKG_SHA256="b4433b6d0c165b3de82d3594224c7297a89cc9fb2c12c5cc56b899f60912fda6"
+PKG_VERSION="aaef1a0d1e37ff87361011fd48c89d9e69ec9a9d"
+PKG_SHA256="2ee1ac405cf451cacf5c5e63ccd170cd3e044cb2d4cfe350ab0673289c350e8c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
