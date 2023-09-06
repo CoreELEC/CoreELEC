be172594d52a8fab2783b9590400d8975746bb0d
Portisch <hugo.portisch@yahoo.de>
2023-09-06 07:49:46 +0200

    libMali: bump bifrost to r37p0 and add valhall r41p0 opengl-meson: load valhall libMali on S5

--- projects/Amlogic-ce/devices/Amlogic-ne/packages/linux-drivers/gpu-aml/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/linux-drivers/gpu-aml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="gpu-aml"
-PKG_VERSION="1e963e9e181df78a6fb318010b0553084ed1523d"
-PKG_SHA256="968703f7845be782fb410bbf8f616919e2eb775b32be7669ae60f4ee790feb5c"
+PKG_VERSION="ecf394cb42126b08da5b2abbbc3e07ae10850024"
+PKG_SHA256="86715698649650478f107210f285d4308ce28c472102c9322f580ef7a5051f27"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/gpu-aml/archive/${PKG_VERSION}.tar.gz"
@@ -13,14 +13,33 @@ PKG_LONGDESC="gpu-aml: Linux drivers for Mali GPUs found in Amlogic Meson SoCs"
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
+pre_make_target() {
+  GPU_DRIVERS_ARCHITECTURE_REVISION="bifrost/r37p0 valhall/r41p0"
+}
+
 make_target() {
-  kernel_make -C $(kernel_path) M=${PKG_BUILD}/bifrost/r25p0/kernel/drivers/gpu/arm \
-    CONFIG_MALI_MIDGARD=m CONFIG_MALI_PLATFORM_NAME="devicetree" \
-    EXTRA_CFLAGS="-I${PKG_BUILD}/dvalin/kernel/include -DCONFIG_MALI_LOW_MEM=0"
+  for driver_arch_rev in ${GPU_DRIVERS_ARCHITECTURE_REVISION}; do
+    echo
+    echo "building ${driver_arch_rev}"
+
+    kernel_make -C $(kernel_path) M=${PKG_BUILD}/${driver_arch_rev}/kernel/drivers/gpu/arm \
+      CONFIG_MALI_MIDGARD=m CONFIG_MALI_PLATFORM_NAME="devicetree" \
+      EXTRA_CFLAGS="-I${PKG_BUILD}/${driver_arch_rev}/kernel/include -DCONFIG_MALI_LOW_MEM=0"
+  done
 }
 
 makeinstall_target() {
-  kernel_make -C $(kernel_path) M=${PKG_BUILD}/bifrost/r25p0/kernel/drivers/gpu/arm \
-    INSTALL_MOD_PATH=${INSTALL}/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
-  modules_install
+  for driver_arch_rev in ${GPU_DRIVERS_ARCHITECTURE_REVISION}; do
+    echo
+    echo "modules install ${driver_arch_rev}"
+
+    driver_arch=${driver_arch_rev%%/*}
+
+    kernel_make -C $(kernel_path) M=${PKG_BUILD}/${driver_arch_rev}/kernel/drivers/gpu/arm \
+      INSTALL_MOD_PATH=${INSTALL}/$(get_kernel_overlay_dir) INSTALL_MOD_STRIP=1 DEPMOD=: \
+      modules_install
+
+    mv ${INSTALL}/$(get_kernel_overlay_dir)/lib/modules/$(get_module_dir)/extra/midgard/mali_kbase.ko \
+       ${INSTALL}/$(get_kernel_overlay_dir)/lib/modules/$(get_module_dir)/extra/midgard/mali_kbase_${driver_arch}.ko
+  done
 }
--- projects/Amlogic-ce/devices/Amlogic-ne/packages/opengl-meson/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ne/packages/opengl-meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="opengl-meson"
-PKG_VERSION="80d86501568a39f7d521bee204e65c9805af600d"
-PKG_SHA256="06e03211fd5766e55e201e6d13b7fd0f4c543c04b803a554eec565ab7649f488"
+PKG_VERSION="8bfb8ebe38f615907852ada7ff375a04f53f3e81"
+PKG_SHA256="9468dad254ef7b2486f738f594c5a78fbb1926a6661923ae10054d9960978ec7"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/filesystem/"
 PKG_URL="https://github.com/CoreELEC/opengl-meson/archive/${PKG_VERSION}.tar.gz"
@@ -22,8 +22,9 @@ makeinstall_target() {
   esac
 
   mkdir -p ${INSTALL}/usr/lib
-    cp -p lib/${lib_arch}/gondul/r25p0/fbdev/libMali.so ${INSTALL}/usr/lib/libMali.gondul.so
-    cp -p lib/${lib_arch}/dvalin/r25p0/fbdev/libMali.so ${INSTALL}/usr/lib/libMali.dvalin.so
+    cp -p lib/${lib_arch}/gondul/r37p0/fbdev/libMali_r1p0.so ${INSTALL}/usr/lib/libMali.gondul.so
+    cp -p lib/${lib_arch}/dvalin/r37p0/fbdev/libMali.so ${INSTALL}/usr/lib/libMali.dvalin.so
+    cp -p lib/${lib_arch}/valhall/r41p0/fbdev/libMali.so ${INSTALL}/usr/lib/libMali.valhall.so
 
     ln -sf /var/lib/libMali.so ${INSTALL}/usr/lib/libMali.so
 
