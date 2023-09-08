c097c93323f458fd99331fefeb84c1be39eff5a9
Arthur Liberman <arthur_liberman@hotmail.com>
2023-09-08 15:59:05 +0300

    openvfd-driver: bump to f8d4510 controllers: fd650 - fix3 for icons with the FD650

--- projects/Amlogic-ce/packages/linux-drivers/openvfd-driver/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/openvfd-driver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Arthur Liberman (arthur_liberman@hotmail.com)
 
 PKG_NAME="openvfd-driver"
-PKG_VERSION="95e33a1f7f48145f4759366a32bf46dc52c5a1b8"
-PKG_SHA256="6c589afbdb98de6fb51f380d208af1fd941fb5054dca4b5518b3b8b9eac69766"
+PKG_VERSION="f8d45107a2238470344189bc1f9545ee5987071c"
+PKG_SHA256="9e52c7f1c6d776e1756e9a378cb27e0709721599930e5993cc88a2e63551ec65"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/arthur-liberman/linux_openvfd"
 PKG_URL="https://github.com/arthur-liberman/linux_openvfd/archive/$PKG_VERSION.tar.gz"
