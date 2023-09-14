85c519d29a7fd2ee3a13772992e808f3a712f7fc
Rudi Heitbaum <rudi@heitbaum.com>
2023-09-14 07:58:44 +0000

    libglvnd: update to 1.7.0

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libglvnd"
-PKG_VERSION="1.6.0"
-PKG_SHA256="e31dd71441604cf7467397e740af8070caa84133b5ed71241d3d5ca84c950baf"
+PKG_VERSION="1.7.0"
+PKG_SHA256="073e7292788d4d3eeb45ea6c7bdcce9bfdb3b3eef8d7dbd47f2f30dce046ef98"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/NVIDIA/libglvnd"
 PKG_URL="https://github.com/NVIDIA/libglvnd/archive/v${PKG_VERSION}.tar.gz"
