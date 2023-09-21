4834e24f558a30497ba34db390b83e2934a01543
Rudi Heitbaum <rudi@heitbaum.com>
2023-09-21 09:39:07 +0000

    rust-std-snapshot: update to 1.72.1

--- packages/rust/rust-std-snapshot/package.mk
+++ packages/rust/rust-std-snapshot/package.mk
@@ -10,15 +10,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${MACHINE_HARDWARE_NAME}" in
   "aarch64")
-    PKG_SHA256="41d259c6f84280fd0e7719fea03a7583ba54e33e8ac32a2a7b703ffb0aebb7d9"
+    PKG_SHA256="178fb79ab40f9608a642b1a7f1c864aa1943f95064195cc95df17b1a8fd775d5"
     PKG_URL="https://static.rust-lang.org/dist/rust-std-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
   "arm")
-    PKG_SHA256="4ef1c8df3dbc0160212afafc8bf643e5bbb40b0147b163f2f5e964a52fc8c217"
+    PKG_SHA256="f17670e870cacebcb96010e6bd48b6830cffeae9f276de3e3bf48650a2efbbc2"
     PKG_URL="https://static.rust-lang.org/dist/rust-std-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
     ;;
   "x86_64")
-    PKG_SHA256="36f27513a6e4381f15b0cd14097c885af537f990cb6193cec3337c429367bf23"
+    PKG_SHA256="35d7f3099c801fc0899453318a538ec55b9a4641279943479912d7784236338b"
     PKG_URL="https://static.rust-lang.org/dist/rust-std-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
 esac
