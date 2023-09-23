f22e0b52396fa31f3a847a3a0c927afa52d1de10
Matthias Reichl <hias@horus.com>
2023-09-24 01:09:24 +0200

    inputstream.adaptive: update to 21.3.0-Omega
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="21.2.1-Omega"
-PKG_SHA256="136ebdd1836a68fb586cc3f4e688ad926342b6fde5691aaf299b8f56c6da263c"
+PKG_VERSION="21.3.0-Omega"
+PKG_SHA256="799fa681148ba3860517c699d805e469b4a222818a54eca178ef021a3a718d29"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -19,6 +19,8 @@ PKG_IS_ADDON="yes"
 addon() {
   install_binary_addon ${PKG_ADDON_ID}
 
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
-  cp -P ${PKG_BUILD}/.${TARGET_NAME}/wvdecrypter/libssd_wv.so ${ADDON_BUILD}/${PKG_ADDON_ID}
+  if [ "${ARCH}" = "aarch64" ]; then
+    mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+    cp -P ${PKG_BUILD}/.${TARGET_NAME}/lib/cdm_aarch64/libcdm_aarch64_loader.so ${ADDON_BUILD}/${PKG_ADDON_ID}
+  fi
 }
