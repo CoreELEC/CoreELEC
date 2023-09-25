fbe4853ddc45614009d4e9e7307b12307a1dd022
Rudi Heitbaum <rudi@heitbaum.com>
2023-09-25 11:05:26 +0000

    pv: update to 1.8.0

--- packages/addons/addon-depends/system-tools-depends/pv/package.mk
+++ packages/addons/addon-depends/system-tools-depends/pv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pv"
-PKG_VERSION="1.7.24"
-PKG_SHA256="3bf43c5809c8d50066eaeaea5a115f6503c57a38c151975b710aa2bee857b65e"
+PKG_VERSION="1.8.0"
+PKG_SHA256="5cec4f737826a0eddab471dd3b75a587bd29a2e7cfa30068d57f29439a251fdf"
 PKG_LICENSE="GNU"
 PKG_SITE="http://www.ivarch.com/programs/pv.shtml"
 PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.gz"
