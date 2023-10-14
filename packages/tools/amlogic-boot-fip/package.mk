8d693bc782fc9cc47f800913b44f8cdd80451c2b
Rudi Heitbaum <rudi@heitbaum.com>
2023-10-14 00:56:19 +0000

    amlogic-boot-fip: update to githash c4bf0e3

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="0312a79cc65bf7bb3d66d33ad0660b66146bd36d"
-PKG_SHA256="437d479c0bb4694113051aadbab393b705fcc0df4a3a5bf3543235226a443c4d"
+PKG_VERSION="c4bf0e3b1ab1246c3176d6c3e420a5e1cdf40c4e"
+PKG_SHA256="77ba6437f105ae8feab25bfe8536e6741dd184975dc082d8ff73284d2037027d"
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
