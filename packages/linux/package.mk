b081a7c973caee545ada7be1ae9f798bbb579362
Christian Hewitt <christianshewitt@gmail.com>
2023-10-26 14:06:43 +0000

    linux: bump Amlogic kernel and patches to 6.6.y
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -16,8 +16,8 @@ PKG_PATCH_DIRS="${LINUX}"
 
 case "${LINUX}" in
   amlogic)
-    PKG_VERSION="8bbe7c640d76724e9cfd8aa130b8d36ad6db77a9" # 6.5.8
-    PKG_SHA256="9d6f2b0bfd3571b7fe2cebe2537ea9cc627293e1c4c9c0b3e7cd0b15629e5f87"
+    PKG_VERSION="ffc253263a1375a65fa6c9f62a893e9767fbebfa" # 6.6
+    PKG_SHA256="2fa48537a32d233ec8f8f0ba36fdd19c07a9ee78a574034876b39939429ced84"
     PKG_URL="https://github.com/torvalds/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="default"
