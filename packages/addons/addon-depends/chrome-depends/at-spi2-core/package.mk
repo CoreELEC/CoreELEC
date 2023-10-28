14d2eab82dfce654c6659105b570299c6e5363e2
Rudi Heitbaum <rudi@heitbaum.com>
2023-10-28 12:29:20 +0000

    at-spi2-core: add dependency of libxml2

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="53ed9eb77e4c48b3bf6ac4afb5689391e0d7d0f44f7ca4443d8b13c7dd26119c"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.gnome.org/"
 PKG_URL="https://download.gnome.org/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain atk dbus glib"
+PKG_DEPENDS_TARGET="toolchain atk dbus glib libxml2"
 PKG_LONGDESC="Protocol definitions and daemon for D-Bus at-spi."
 
 configure_package() {
