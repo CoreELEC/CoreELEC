52286410739d49f9f28c9a349e3f6493ee226081
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-02 12:30:19 +0000

    xz: update to 5.4.5

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xz"
-PKG_VERSION="5.4.4"
-PKG_SHA256="705d0d96e94e1840e64dec75fc8d5832d34f6649833bec1ced9c3e08cf88132e"
+PKG_VERSION="5.4.5"
+PKG_SHA256="da9dec6c12cf2ecf269c31ab65b5de18e8e52b96f35d5bcd08c12b43e6878803"
 PKG_LICENSE="GPL"
 PKG_SITE="https://tukaani.org/xz/"
 PKG_URL="https://tukaani.org/xz/${PKG_NAME}-${PKG_VERSION}.tar.xz"
