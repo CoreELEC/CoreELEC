89076978d64011a70f87540d22296e7f790b3b38
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-02 12:25:57 +0000

    llvm: update to 17.0.4

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="17.0.3"
-PKG_SHA256="be5a1e44d64f306bb44fce7d36e3b3993694e8e6122b2348608906283c176db8"
+PKG_VERSION="17.0.4"
+PKG_SHA256="a225eb96f52e7d8c6c275b351fcc66d7a21d925eecff53730900404f244ff16a"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
