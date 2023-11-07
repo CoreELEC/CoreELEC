8fca16ecfc4d0f8c22999bf1bcdb8037b8dc1b8c
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-07 00:54:14 +0000

    systemd: update to 255
    Release notes:
    - https://github.com/systemd/systemd/releases

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-PKG_VERSION="254.7"
-PKG_SHA256="9d7b4b21d2db3b16286b9ec6657ba86bd797388c532bdc63862826f6d39fe8cb"
+PKG_VERSION="255"
+PKG_SHA256="a3eb766ee96eb9f4cc25c2a6c933f3299e1b7ae22e72507dade0a5c86d92534f"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd-stable/archive/v${PKG_VERSION}.tar.gz"
