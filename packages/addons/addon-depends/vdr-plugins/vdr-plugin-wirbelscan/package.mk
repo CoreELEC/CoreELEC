5a4db493ff555e8aa27d85f78faf369d8745abd1
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-10 10:25:22 +0000

    vdr-plugin-wirbelscan: update to 2021.10.09
    last version before migrating to librepfunc

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-wirbelscan/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-wirbelscan/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-wirbelscan"
-PKG_VERSION="2021.07.14"
-PKG_SHA256="cd1ce8c8b2b57ce4043fcfcfc0775a18353444ff8a2010810b6d97c1cb47ab1d"
+PKG_VERSION="2021.10.09"
+PKG_SHA256="e6f7e38402e0853b0460d723fdc745c110417bfab3c94c9883f49126eca680a8"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gen2vdr.de/wirbel/wirbelscan/index2.html"
 PKG_URL="https://www.gen2vdr.de/wirbel/wirbelscan/vdr-wirbelscan-${PKG_VERSION}.tgz"
