bf43260f970b60930620d0a1a5f06d960e59cd5d
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-29 09:09:49 +0000

    nlohmann-json: update to 3.11.3

--- packages/textproc/nlohmann-json/package.mk
+++ packages/textproc/nlohmann-json/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nlohmann-json"
-PKG_VERSION="3.11.2"
-PKG_SHA256="d69f9deb6a75e2580465c6c4c5111b89c4dc2fa94e3a85fcd2ffcd9a143d9273"
+PKG_VERSION="3.11.3"
+PKG_SHA256="0d8ef5af7f9794e3263480193c491549b2ba6cc74bb018906202ada498a79406"
 PKG_LICENSE="MIT"
 PKG_SITE="https://nlohmann.github.io/json/"
 PKG_URL="https://github.com/nlohmann/json/archive/v${PKG_VERSION}.tar.gz"
