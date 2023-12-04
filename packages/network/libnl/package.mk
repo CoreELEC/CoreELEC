be772332b55dd7712559c612038ae7a23274efa5
Rudi Heitbaum <rudi@heitbaum.com>
2023-12-04 21:45:25 +0000

    libnl: update to 3.9.0

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnl"
-PKG_VERSION="3.8.0"
-PKG_SHA256="bb726c6d7a08b121978d73ff98425bf313fa26a27a331d465e4f1d7ec5b838c6"
+PKG_VERSION="3.9.0"
+PKG_SHA256="aed507004d728a5cf11eab48ca4bf9e6e1874444e33939b9d3dfed25018ee9bb"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/thom311/libnl"
 PKG_URL="https://github.com/thom311/libnl/releases/download/libnl${PKG_VERSION//./_}/libnl-${PKG_VERSION}.tar.gz"
