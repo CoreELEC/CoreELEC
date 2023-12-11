0441331eb36403a676c0d22825d630b8150d40bd
Rudi Heitbaum <rudi@heitbaum.com>
2023-12-11 09:32:15 +0000

    libusb: update to 1.0.27

--- packages/sysutils/libusb/package.mk
+++ packages/sysutils/libusb/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libusb"
-PKG_VERSION="1.0.26"
-PKG_SHA256="12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5"
+PKG_VERSION="1.0.27"
+PKG_SHA256="ffaa41d741a8a3bee244ac8e54a72ea05bf2879663c098c82fc5757853441575"
 PKG_LICENSE="LGPLv2.1"
 PKG_SITE="http://libusb.info/"
 PKG_URL="https://github.com/libusb/libusb/releases/download/v${PKG_VERSION}/libusb-${PKG_VERSION}.tar.bz2"
