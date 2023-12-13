d11f0ffd6174458c3c5f3adb1820ec5978ad8c54
Rudi Heitbaum <rudi@heitbaum.com>
2023-12-13 12:45:16 +0000

    groovy: initial package

---
+++ packages/devel/groovy/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="groovy"
+PKG_VERSION="4.0.16"
+PKG_SHA256="b8c3bec88a3f5a62235d9429a97e371032bf7216f3e28724823a9169dd10befc"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://groovy.apache.org"
+PKG_URL="https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-${PKG_VERSION}.zip"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Apache Groovy is a Java-syntax-compatible object-oriented programming language for the Java platform."
+PKG_TOOLCHAIN="manual"
