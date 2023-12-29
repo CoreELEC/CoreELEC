7e7fba5f452ea88658cde1231947cbb7e649c33d
dtechsrv <dee.gabor@gmail.com>
2023-12-29 12:54:28 +0100

    aml-vnc: replace source repository and update addon main files

--- projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
+++ projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="aml-vnc"
-PKG_VERSION="c34903e68695110c6af5df6fb43797950a0f335b"
-PKG_SHA256="650b664c7fb402f37bb9f2001f3a26bc85ad04d9eb0f2c273c446514cc3c12f4"
-PKG_REV="4"
+PKG_VERSION="1.1.0"
+PKG_SHA256="50a0040b46019c2781f671401907be4b24fb9f5749a7e439c4111c0853a4fe58"
+PKG_REV="5"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/CoreELEC/aml-vnc"
-PKG_URL="https://github.com/CoreELEC/aml-vnc/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/dtechsrv/aml-vnc-server/"
+PKG_URL="https://github.com/dtechsrv/aml-vnc-server/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libvncserver"
 PKG_SECTION="service"
 PKG_SHORTDESC="Amlogic VNC server"
@@ -17,14 +17,6 @@ PKG_LONGDESC="Amlogic VNC server is a Virtual Network Computing (VNC) server for
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Amlogic VNC"
 PKG_ADDON_TYPE="xbmc.service"
-
-unpack() {
-  mkdir -p ${PKG_BUILD}/addon
-  tar --strip-components=3 -xf $SOURCES/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz \
-    -C ${PKG_BUILD}       ${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}/sources
-  tar --strip-components=3 -xf $SOURCES/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz \
-    -C ${PKG_BUILD}/addon ${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}/source
-}
 
 pre_configure_target() {
   export CFLAGS+=" -Wno-stringop-truncation"
@@ -35,11 +27,7 @@ makeinstall_target() {
 }
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-    cp -P ${PKG_BUILD}/aml-vnc ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
-    cp $(get_build_dir libvncserver)/.${TARGET_NAME}/libvncserver.so.? ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
-
-  cp -PR ${PKG_BUILD}/addon/* ${ADDON_BUILD}/${PKG_ADDON_ID}
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib}
+  cp -P ${PKG_BUILD}/aml-vnc ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  cp $(get_build_dir libvncserver)/.${TARGET_NAME}/libvncserver.so.? ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
 }
---
+++ projects/Amlogic-ce/packages/addons/service/aml-vnc/source/default.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-2018 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
