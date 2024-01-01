c0d71cced731396e5d634f307dc34c2ddb6df87a
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-01 03:11:00 +0000

    tinyxml2: update to 10.0.0

--- packages/textproc/tinyxml2/package.mk
+++ packages/textproc/tinyxml2/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tinyxml2"
-PKG_VERSION="9.0.0"
-PKG_SHA256="cc2f1417c308b1f6acc54f88eb70771a0bf65f76282ce5c40e54cfe52952702c"
+PKG_VERSION="10.0.0"
+PKG_SHA256="3bdf15128ba16686e69bce256cc468e76c7b94ff2c7f391cc5ec09e40bff3839"
 PKG_LICENSE="zlib"
 PKG_SITE="http://www.grinninglizard.com/tinyxml2/index.html"
 PKG_URL="https://github.com/leethomason/tinyxml2/archive/${PKG_VERSION}.tar.gz"
