dfd3c291182acc691465f4b077d1fae22ece927b
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-04 20:28:26 +0000

    pixman: update to 0.43.0 and meson

--- packages/x11/lib/pixman/package.mk
+++ packages/x11/lib/pixman/package.mk
@@ -3,38 +3,42 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pixman"
-PKG_VERSION="0.42.2"
-PKG_SHA256="5747d2ec498ad0f1594878cc897ef5eb6c29e91c53b899f7f71b506785fc1376"
+PKG_VERSION="0.43.0"
+PKG_SHA256="5ba00c2879932c32d2ef878b6e802e3f898dda14c07e881dcfcc6a8f670e0cdc"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.x.org/"
 PKG_URL="https://xorg.freedesktop.org/archive/individual/lib/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain util-macros"
 PKG_LONGDESC="Pixman is a generic library for manipulating pixel regions, contains low-level pixel manipulation routines."
-PKG_TOOLCHAIN="configure"
 
 if [ "${TARGET_ARCH}" = arm ]; then
   if target_has_feature neon; then
-    PIXMAN_NEON="--enable-arm-neon"
+    PIXMAN_NEON="-Dneon=enabled"
   else
-    PIXMAN_NEON="--disable-arm-neon"
+    PIXMAN_NEON="-Dneon=disabled"
   fi
-  PIXMAN_CONFIG="--disable-mmx --disable-sse2 --disable-vmx --enable-arm-simd ${PIXMAN_NEON} --disable-arm-iwmmxt"
+  PIXMAN_CONFIG="-Dmmx=disabled -Dsse2=disabled -Dvmx=disabled -Darm-simd=enabled ${PIXMAN_NEON} -Diwmmxt=disabled"
 elif [ "${TARGET_ARCH}" = aarch64 ]; then
-  PIXMAN_CONFIG="--disable-mmx --disable-sse2 --disable-vmx --disable-arm-simd --disable-arm-neon --disable-arm-iwmmxt"
+  PIXMAN_CONFIG="-Dmmx=disabled -Dsse2=disabled -Dvmx=disabled -Darm-simd=disabled -Dneon=disabled -Diwmmxt=disabled"
 elif [ "${TARGET_ARCH}" = x86_64  ]; then
-  PIXMAN_CONFIG="--enable-mmx --enable-sse2 --disable-ssse3 --disable-vmx --disable-arm-simd --disable-arm-neon"
+  PIXMAN_CONFIG="-Dmmx=enabled -Dsse2=enabled -Dssse3=disabled -Dvmx=disabled -Darm-simd=disabled -Dneon=disabled"
 fi
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-openmp \
-                           --disable-loongson-mmi \
-                           ${PIXMAN_CONFIG} \
-                           --disable-mips-dspr2 \
-                           --enable-gcc-inline-asm \
-                           --disable-timers \
-                           --disable-gtk \
-                           --disable-libpng \
-                           --with-gnu-ld"
+PKG_MESON_OPTS_HOST="-Dgtk=disabled \
+                     -Dtests=disabled \
+                     -Ddemos=disabled"
+
+PKG_MESON_OPTS_TARGET="-Dopenmp=disabled \
+                       -Dloongson-mmi=disabled \
+                       ${PIXMAN_CONFIG} \
+                       -Dmips-dspr2=disabled \
+                       -Dgnu-inline-asm=enabled \
+                       -Dtimers=false \
+                       -Dgtk=disabled \
+                       -Dlibpng=disabled \
+                       -Dtests=disabled \
+                       -Ddemos=disabled"
 
 post_makeinstall_target() {
   cp ${SYSROOT_PREFIX}/usr/lib/pkgconfig/pixman-1.pc \
