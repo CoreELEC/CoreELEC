b824f49d2dae3092a99d2bacc4e955420c1f4ee4
Portisch <hugo.portisch@yahoo.de>
2024-01-07 21:36:03 +0100

    common_drivers: bump package and add DT for Libre Computer Alta AML-A311D-CC

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="f7c93ab617a5ab5d08db728ec59d22ad9ff1b56d"
+PKG_VERSION="22ee27bdf33c3b8c4fab5b92963d0ef638fc0a2b"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
