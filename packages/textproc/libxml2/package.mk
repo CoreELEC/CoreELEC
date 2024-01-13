34dd53f77ddf5605041274601e6ab8b2fa312d46
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-13 11:24:18 +0000

    libxml2: migrate to cmake

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -8,26 +8,19 @@ PKG_SHA256="5c3dbade38b64d7fecf628b92b3485868d21516a4a86465294e5919e6aa309a4"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org"
 PKG_URL="https://gitlab.gnome.org/GNOME/${PKG_NAME}/-/archive/v${PKG_VERSION}/${PKG_NAME}-v${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_HOST="zlib:host Python3:host"
+PKG_DEPENDS_HOST="zlib:host ninja:host Python3:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="The libxml package contains an XML library, which allows you to manipulate XML files."
-PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_ALL="ac_cv_header_ansidecl_h=no \
-                        --enable-static \
-                        --enable-shared \
-                        --disable-silent-rules \
-                        --enable-ipv6 \
-                        --without-lzma"
+PKG_CMAKE_OPTS_ALL="-DBUILD_SHARED_LIBS=ON \
+                    -DLIBXML2_WITH_LZMA=OFF \
+                    -DLIBXML2_WITH_TESTS=OFF"
 
-PKG_CONFIGURE_OPTS_HOST="${PKG_CONFIGURE_OPTS_ALL} \
-                         --with-zlib=${TOOLCHAIN} \
-                         --with-python"
+PKG_CMAKE_OPTS_HOST="${PKG_CMAKE_OPTS_ALL} \
+                     -DLIBXML2_WITH_PYTHON=ON"
 
-PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_ALL} \
-                           --with-zlib=${SYSROOT_PREFIX}/usr \
-                           --without-python \
-                           --with-sysroot=${SYSROOT_PREFIX}"
+PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_ALL} \
+                       -DLIBXML2_WITH_PYTHON=OFF"
 
 post_makeinstall_target() {
   sed -e "s:\(['= ]\)/usr:\\1${SYSROOT_PREFIX}/usr:g" -i ${SYSROOT_PREFIX}/usr/bin/xml2-config
