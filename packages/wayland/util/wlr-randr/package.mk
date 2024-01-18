c1c0dddbc20955634217d5b1394fc9a210614e9a
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-18 08:45:52 +0000

    wlr-randr: update to 0.4.0

--- packages/wayland/util/wlr-randr/package.mk
+++ packages/wayland/util/wlr-randr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wlr-randr"
-PKG_VERSION="0.3.1"
-PKG_SHA256="918420f134fc094da0cd9cc5112b65f623abd773964bff5fa57e221204506ab2"
+PKG_VERSION="0.4.0"
+PKG_SHA256="3bf5446487a165cb071e24197206bb11d5db35ac05445d76184f4ef6b999ac1c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://git.sr.ht/~emersion/wlr-randr"
 PKG_URL="https://git.sr.ht/~emersion/wlr-randr/archive/v${PKG_VERSION}.tar.gz"
