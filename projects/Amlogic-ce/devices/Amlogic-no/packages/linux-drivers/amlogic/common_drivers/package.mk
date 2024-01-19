07b44485f6c736bc1d9806351be76be7b38db225
Portisch <hugo.portisch@yahoo.de>
2024-01-19 09:22:55 +0100

    common_drivers: bump package and add DT for Libre Computer Solitude AML-S905D3-CC

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="22ee27bdf33c3b8c4fab5b92963d0ef638fc0a2b"
+PKG_VERSION="39089ce36b5909006b040157d47b2170ff012c20"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
