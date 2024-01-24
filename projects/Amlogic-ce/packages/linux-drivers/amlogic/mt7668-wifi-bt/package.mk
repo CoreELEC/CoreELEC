4b0f9d6fe20165d263d7f61a5ebf53b4a808b7e4
shantigilbert <shantic@gmail.com>
2024-01-24 02:16:51 -0600

    Add support for MT7668 Wifi/Bluetooth Combo
    Replaces old MT7668-aml package
    This closes #346.

---
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/mt7668-wifi-bt/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="mt7668-wifi-bt"
+PKG_VERSION="cea368147cdb316973aa45d274b4490740fb3296"
+PKG_SHA256="aeba356c9eef49782982ea7cce268efceb971fe5ad0989539d206359fc797464"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/noob404yt/mt7668-wifi-bt"
+PKG_URL="https://github.com/CoreELEC/MT7668/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
+PKG_LONGDESC="WiFi & Bluetooth Drivers for MT7668"
+PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  cd ${PKG_BUILD}/MT7668-Bluetooth
+    kernel_make EXTRA_CFLAGS="-w" \
+      KERNEL_SRC=$(kernel_path)
+
+  cd ${PKG_BUILD}/MT7668-WiFi
+    kernel_make EXTRA_CFLAGS="-w" \
+      KERNELDIR=$(kernel_path)
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    find ${PKG_BUILD}/ -name \*.ko -not -path '*/\.*' -exec cp {} ${INSTALL}/$(get_full_module_dir)/${PKG_NAME} \;
+
+  mkdir -p ${INSTALL}/$(get_full_firmware_dir)
+    cp ${PKG_BUILD}/MT7668-WiFi/7668_firmware/* ${INSTALL}/$(get_full_firmware_dir)
+}
