ac331d462f46dabdeb419b5fce7c2fce2ba49103
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-28 01:35:39 +0000

    libidn2: update to 2.3.7
    ann:
    - https://lists.gnu.org/archive/html/info-gnu/2024-01/msg00015.html

--- packages/textproc/libidn2/package.mk
+++ packages/textproc/libidn2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libidn2"
-PKG_VERSION="2.3.4"
-PKG_SHA256="93caba72b4e051d1f8d4f5a076ab63c99b77faee019b72b9783b267986dbb45f"
+PKG_VERSION="2.3.7"
+PKG_SHA256="4c21a791b610b9519b9d0e12b8097bf2f359b12f8dd92647611a929e6bfd7d64"
 PKG_LICENSE="LGPL3"
 PKG_SITE="https://www.gnu.org/software/libidn/"
 PKG_URL="https://ftpmirror.gnu.org/gnu/libidn/libidn2-${PKG_VERSION}.tar.gz"
