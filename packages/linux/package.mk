45003882b313771ac3bfbaf49183a9bf4825947c
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-01 06:00:25 +0000

    linux: update to 6.6.16

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.6.14"
-    PKG_SHA256="fbe96b2db3f962cd2a96a849d554300e7a4555995160082d4f323c2a1dfa1584"
+    PKG_VERSION="6.6.16"
+    PKG_SHA256="b21d5795a3bead4f112916423222faa8a0f519e4201df343e3eb88dc9e4aaa30"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
