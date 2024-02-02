87e232353676bbd60893df9650695cc0d95ba4e2
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-02 12:52:49 +0000

    stress-ng: update to 0.17.05

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.17.04"
-PKG_SHA256="60c37d8b1effc5772fb30f638e20b1de01e0488e274e283301c3fd6c707d8538"
+PKG_VERSION="0.17.05"
+PKG_SHA256="48964a0de5838acfed5c78d78d5f4a1d86974883d5537ccc55df019a0186a1b5"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/ColinIanKing/stress-ng"
 PKG_URL="https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V${PKG_VERSION}.tar.gz"
