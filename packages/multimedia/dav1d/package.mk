b275c142e7d6729e0468ff1a158fe92ce8643b91
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-15 07:14:50 +0000

    dav1d: update to 1.4.0

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="1.3.0"
-PKG_SHA256="6d8be2741c505c47f8f1ced3c9cc427759243436553d01d1acce201f87b39e71"
+PKG_VERSION="1.4.0"
+PKG_SHA256="ac8f8ff581906c8ef6479370cf6c1465f282fd2e7cfc2c6366c772f84ad9d30e"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.videolan.org/projects/dav1d.html"
 PKG_URL="https://downloads.videolan.org/pub/videolan/dav1d/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.xz"
