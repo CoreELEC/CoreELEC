09a8c4c88d64dd498376a80854145ab038f491cf
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-16 06:20:12 +0000

    gpiozero: update to 2.0.1

--- packages/addons/addon-depends/rpi-tools-depends/gpiozero/package.mk
+++ packages/addons/addon-depends/rpi-tools-depends/gpiozero/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gpiozero"
-PKG_VERSION="2.0"
-PKG_SHA256="403bcc9e7f24f0877653e7fced91ba51508d5197c9d1f80e383fe6693b9c3c27"
+PKG_VERSION="2.0.1"
+PKG_SHA256="d4ea1952689ec7e331f9d4ebc9adb15f1d01c2c9dcfabb72e752c9869ab7e97e"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/RPi-Distro/python-gpiozero"
