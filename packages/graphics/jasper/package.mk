6766f859a669a53a3b7fd02059974f4212d0f545
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-21 08:10:07 +0000

    jasper: update to 4.2.1
    log:
    - https://github.com/jasper-software/jasper/compare/version-4.2.0...version-4.2.1
    
    release notes:
    - https://github.com/jasper-software/jasper/releases/tag/version-4.2.1

--- packages/graphics/jasper/package.mk
+++ packages/graphics/jasper/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jasper"
-PKG_VERSION="4.2.0"
-PKG_SHA256="c9a3e35c95447f530b006fab6634a2dadec70276cc3e42c343b9e5ce5a1f2b6b"
+PKG_VERSION="4.2.1"
+PKG_SHA256="970002b774b91edd9d2dedf76d0b8d5a88af28e0c6d603cc51988311a99a869f"
 PKG_LICENSE="OpenSource"
 PKG_SITE="http://www.ece.uvic.ca/~mdadams/jasper/"
 PKG_URL="https://github.com/jasper-software/jasper/archive/refs/tags/version-${PKG_VERSION}.tar.gz"
