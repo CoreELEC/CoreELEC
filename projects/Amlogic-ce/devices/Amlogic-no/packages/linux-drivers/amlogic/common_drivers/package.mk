a5eb365a43066fea1c41b7e39f8084c41565beec
Portisch <hugo.portisch@yahoo.de>
2024-02-22 15:14:20 +0100

    common_drivers: bump to hdmitx21 4k fix

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="a6c3b3dec8fa3cc4dffb5224b46cd6764f516a6e"
+PKG_VERSION="b633f63260a98a30f52ff6e61ca82736f4da6d0f"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
