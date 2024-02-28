080f715c175a14795764596e514d4d71df7a5f2d
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-28 07:47:14 +0000

    rsyslog: update to 8.2402.0 and addon (2)
    - librepfunc: update to 1.9.1

--- packages/addons/service/rsyslog/package.mk
+++ packages/addons/service/rsyslog/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rsyslog"
-PKG_VERSION="8.2312.0"
-PKG_SHA256="774032006128a896437f5913e132aa27dbfb937cd8847e449522d5a12d63d03e"
-PKG_REV="1"
+PKG_VERSION="8.2402.0"
+PKG_SHA256="acbdd8579489df36b4a383dc6909a61b7623807f0aff54c062115f2de7ea85ba"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/rsyslog"
