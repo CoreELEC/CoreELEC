6d5562dd5b82c42501c4c445cf2f1dc496673e74
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-02 02:33:23 +0000

    elfutils: update to 0.191
    includes fix
    - https://sourceware.org/bugzilla/show_bug.cgi?id=31439

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.190"
-PKG_SHA256="8e00a3a9b5f04bc1dc273ae86281d2d26ed412020b391ffcc23198f10231d692"
+PKG_VERSION="0.191"
+PKG_SHA256="df76db71366d1d708365fc7a6c60ca48398f14367eb2b8954efc8897147ad871"
 PKG_LICENSE="GPL"
 PKG_SITE="https://sourceware.org/elfutils/"
 PKG_URL="https://sourceware.org/elfutils/ftp/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
