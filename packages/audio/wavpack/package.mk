b1ad04e563464dcfb8f45979cee7d78dcca9225f
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-02 12:58:53 +0000

    wavpack: update to 5.7.0
    release notes:
    - https://github.com/dbry/WavPack/releases/tag/5.7.0

--- packages/audio/wavpack/package.mk
+++ packages/audio/wavpack/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wavpack"
-PKG_VERSION="5.6.0"
-PKG_SHA256="af8035f457509c3d338b895875228a9b81de276c88c79bb2d3e31d9b605da9a9"
+PKG_VERSION="5.7.0"
+PKG_SHA256="e81510fd9ec5f309f58d5de83e9af6c95e267a13753d7e0bbfe7b91273a88bee"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.wavpack.com"
 PKG_URL="https://www.wavpack.com/wavpack-${PKG_VERSION}.tar.xz"
@@ -13,6 +13,5 @@ PKG_LONGDESC="Audio compression format providing lossless, high-quality lossy an
 PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTING=OFF \
                        -DCMAKE_INSTALL_INCLUDEDIR=include/wavpack \
                        -DWAVPACK_BUILD_PROGRAMS=OFF \
-                       -DWAVPACK_BUILD_DOCS=OFF \
                        -DWAVPACK_ENABLE_ASM=OFF \
                        -DWAVPACK_INSTALL_DOCS=OFF"
