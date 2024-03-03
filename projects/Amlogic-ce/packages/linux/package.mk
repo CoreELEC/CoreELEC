a2ecc4f39bf84019a5664f13baa77836d1cf9e36
Arthur Liberman <arthur_liberman@hotmail.com>
2024-03-03 18:34:38 +0200

    linux:Amlogic-ng: bump package to 76e87af

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -17,8 +17,8 @@ PKG_PATCH_DIRS="$LINUX"
 
 case "$LINUX" in
   amlogic-4.9)
-    PKG_VERSION="3724fe5532aa400c6b90bc096cbbfd102be89c2a"
-    PKG_SHA256="81a22683e5dc73d93d3eaba4ce54a07aa5b8724d9ae5830af541b870a2bb701f"
+    PKG_VERSION="76e87afd60014a9fb11b8a978efe029b6eb2c690"
+    PKG_SHA256="bf701227551495c0ff9302b3e8701ffbee9502d700d2b91e6167e55ac9735ff2"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
