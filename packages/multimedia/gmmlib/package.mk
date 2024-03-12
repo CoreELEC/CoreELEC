310a9a8d181b450f980ae64170d5b2530aa6802c
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-12 20:50:24 +0000

    gmmlib: update to 22.3.18
    log:
    - https://github.com/intel/gmmlib/compare/intel-gmmlib-22.3.17...intel-gmmlib-22.3.18

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="22.3.17"
-PKG_SHA256="5fa23407d4780c4ee8acd68e9ec9186e1721238150dc36ca9ab469a335891d2a"
+PKG_VERSION="22.3.18"
+PKG_SHA256="381f7ad104e3d0d8dd6c3ec0c8304e410a479c439e72db35c83fb7e2b0ec556f"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
