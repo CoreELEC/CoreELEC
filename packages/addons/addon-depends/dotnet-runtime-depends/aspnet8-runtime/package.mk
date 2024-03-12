a55c9ef62771147f484db459cc603bf891f42a75
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-12 20:49:13 +0000

    aspnet8-runtime: update to 8.0.4

--- packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aspnet8-runtime"
-PKG_VERSION="8.0.2"
+PKG_VERSION="8.0.4"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="107074b613c48ffcd311670bc34df93f63fce33938ac35770c3db1969da770ef"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/bdfd0216-539e-4dfd-81ea-1b7a77dda929/59a62884bdb8684ef0e4f434eaea0ca3/aspnetcore-runtime-8.0.2-linux-arm64.tar.gz"
+    PKG_SHA256="e3b666aa7836a83b71575df0680d0e57bf46b356668521db174e5686fec28cf0"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/80ec12e5-b26f-466c-a20c-f96772ea709d/606e7203912400b44cb35d6fcecf60bf/aspnetcore-runtime-8.0.4-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="940a9e0f89330924a5aed8dedaadb6220f4504f42b5c676e1f6a77060595bb49"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/272dbea2-057e-4032-9857-7e00b476ceec/3c472df94b1c3f5e0d009cbccc9256a6/aspnetcore-runtime-8.0.2-linux-arm.tar.gz"
+    PKG_SHA256="f0d65259cb6bdf3324fe0ef2a1b72418ed3db67dbe404debd44bffb352d02875"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/f12a9449-04ba-454c-bc35-4cdb426accf6/2729a371b61d59794845eb309a46fba2/aspnetcore-runtime-8.0.4-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="9748a8c44b20219b7529b8dde8c17aa07dfadb0206609ea6ce7329269379a121"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/d6d79cc3-df2f-4680-96ff-a7198f461139/df025000eaf5beb85d9137274a8c53ea/aspnetcore-runtime-8.0.2-linux-x64.tar.gz"
+    PKG_SHA256="ec817fa8f7a1c0f9c627425b1eda6ffe7a49bd180cb15d9c59ace2a051dd83e4"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/0b0bc7f4-c6e5-4cec-a7ed-45c2fac0da4b/ae2090564274152b5a4be9f1e66c5d30/aspnetcore-runtime-8.0.4-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
