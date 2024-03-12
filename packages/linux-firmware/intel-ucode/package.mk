3ce98f92b4e410cfe240050c1e0534404487e161
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-12 20:41:39 +0000

    intel-ucode: update to 20240312
    ref:
    - https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/releases/tag/microcode-20240312
    
    before:
    [    0.000000] microcode: updated early: 0x42a -> 0x430, date = 2023-06-07
    after:
    [    0.000000] microcode: updated early: 0x42a -> 0x432, date = 2023-09-19
    
    running on:
    Linux version 6.6.21 (docker@8f3d0261cf31) (x86_64-libreelec-linux-gnu-gcc-13.2.0 (GCC) 13.2.0,
      GNU ld (GNU Binutils) 2.42) #1 SMP Sat Mar  9 13:52:27 UTC 2024
    DMI: Intel(R) Client Systems NUC12WSKi7/NUC12WSBi7, BIOS WSADL357.0088.2023.0505.1623 05/05/2023
    smpboot: CPU0: 12th Gen Intel(R) Core(TM) i7-1260P (family: 0x6, model: 0x9a, stepping: 0x3)

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20231114"
-PKG_SHA256="cee26f311f7e2c039dd48cd30f995183bde9b98fb4c3039800e2ddaf5c090e55"
+PKG_VERSION="20240312"
+PKG_SHA256="9575c6d74491058bbf998c359d7f25f23655d97a31663a8ed6a98def2b0aaf2b"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
