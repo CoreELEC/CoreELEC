f5bcecf9635deb2323533bc8a9659c5f03e326eb
Portisch <hugo.portisch@yahoo.de>
2024-03-14 15:15:42 +0100

    common_drivers: bump to Dolby VSDB version 2 fix

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="b633f63260a98a30f52ff6e61ca82736f4da6d0f"
+PKG_VERSION="b5ded88c1eb2e48558f07702d27bf5d8ae30a237"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
