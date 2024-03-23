188cab75798b747b1cc25e879f72f03bf0557048
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-23 08:06:51 +0000

    pvr.plutotv: update 21.0.1-Omega to 21.0.2-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.plutotv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.plutotv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.plutotv"
-PKG_VERSION="21.0.1-Omega"
-PKG_SHA256="6f9ebee2da9f4f07f638cae3d45dc080dfbff85042753bbab44224f2bf31513d"
+PKG_VERSION="21.0.2-Omega"
+PKG_SHA256="9d20caf2bba5dc0cb87ff1d97f8fb7738e7a878adf7c93f3d4f030e5831995ea"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
