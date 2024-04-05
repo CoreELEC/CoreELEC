5dc3d0b1da4a24c004155b7b9d118f4ca1fbffee
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-05 09:30:59 +0000

    squeezelite: update to githash c751ef1 (2.0.0.1481)

--- packages/addons/addon-depends/multimedia-tools-depends/squeezelite/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/squeezelite/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="squeezelite"
-PKG_VERSION="fd89d67b1b9a17a6dd212be0c91d0417b440f60a"
-PKG_SHA256="40a97202d3018b5f36c3893e3e1c291d805eefdd9f26751b10f65c1592f5a2eb"
+PKG_VERSION="c751ef146265c243cdbd7c0353dd0b70ab51730c"
+PKG_SHA256="19fd23ca52039b4e216ff74e7d93b7fd8079ab8bfd63d5fb2286bfb743e334ed"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/ralph-irving/squeezelite"
 PKG_URL="https://github.com/ralph-irving/squeezelite/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain faad2 ffmpeg flac libmad libvorbis mpg123 soxr libogg"
 PKG_DEPENDS_CONFIG="mpg123"
-PKG_LONGDESC="A client for the Logitech Media Server."
+PKG_LONGDESC="A client for the Lyrion Music Server."
 PKG_BUILD_FLAGS="-sysroot"
 
 make_target() {
