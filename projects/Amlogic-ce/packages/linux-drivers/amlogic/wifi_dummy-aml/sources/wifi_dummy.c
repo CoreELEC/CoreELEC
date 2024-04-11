dd20eb50b7343873191b7324939cd824a2948c63
Portisch <hugo.portisch@yahoo.de>
2024-04-11 16:02:52 +0200

    wifi_dummy-aml: add support for PCIE reset

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/sources/wifi_dummy.c
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/sources/wifi_dummy.c
@@ -9,15 +9,30 @@ MODULE_DESCRIPTION("Amlogic WiFi power on and SDIO rescan module");
 
 extern void extern_wifi_set_enable(int);
 extern void sdio_reinit(void);
+#ifdef CONFIG_PCI
+extern void set_usb_wifi_power(int);
+extern void pci_remove(void);
+extern void pci_reinit(void);
+#endif
 
 static int __init wifi_dummy_init(void)
 {
-	printk(KERN_INFO "Triggered SDIO WiFi power on and bus rescan.\n");
+	printk(KERN_INFO "Triggered SDIO/PCI WiFi power on and bus rescan.\n");
+#ifdef CONFIG_PCI
+	pci_remove();
+	set_usb_wifi_power(0);
+#endif
 	extern_wifi_set_enable(0);
 	msleep(300);
 	extern_wifi_set_enable(1);
+#ifdef CONFIG_PCI
+	set_usb_wifi_power(1);
+#endif
 	msleep(300);
 	sdio_reinit();
+#ifdef CONFIG_PCI
+	pci_reinit();
+#endif
 	return 0;
 }
 
