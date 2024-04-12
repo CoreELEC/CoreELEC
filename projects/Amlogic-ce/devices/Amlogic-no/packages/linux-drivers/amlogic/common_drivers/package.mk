75a11e7e084d94df7d59e0e350e75589dcefc8d4
Portisch <hugo.portisch@yahoo.de>
2024-04-12 11:32:31 +0200

    common_drivers: bump to Kinhank-G1

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="8a8035b10c8e7ab5ad45f56a942bae1c650c6374"
-PKG_SHA256=""
+PKG_VERSION="9c77876acaf22a44dcc1dc412346f9678b490a6d"
+PKG_SHA256="84657bdd0e0bfce9a304ab840d8a6d4fb5bdc79c73bf95d0c8e68e0f6fd8225f"
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/common_drivers/archive/${PKG_VERSION}.tar.gz"
