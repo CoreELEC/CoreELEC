b63f5836d2f6cdb741c4c4d204dcdfcf3bef43cb
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-18 07:38:02 +0000

    wayland-protocols: update to 1.35

--- packages/wayland/wayland-protocols/package.mk
+++ packages/wayland/wayland-protocols/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wayland-protocols"
-PKG_VERSION="1.34"
-PKG_SHA256="c59b27cacd85f60baf4ee5f80df5c0d15760ead6a2432b00ab7e2e0574dcafeb"
+PKG_VERSION="1.35"
+PKG_SHA256="37a2716a28133dc819341c568a29d21e8cb72130e5c126a1fcfc9f42c23d95ab"
 PKG_LICENSE="OSS"
 PKG_SITE="https://wayland.freedesktop.org/"
 PKG_URL="https://gitlab.freedesktop.org/wayland/${PKG_NAME}/-/releases/${PKG_VERSION}/downloads/${PKG_NAME}-${PKG_VERSION}.tar.xz"
