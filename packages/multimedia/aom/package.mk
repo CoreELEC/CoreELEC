530b2ffb03750c48533343d63cf89cb106edf14a
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-24 11:17:22 +0000

    aom: update to 3.9.1

--- packages/multimedia/aom/package.mk
+++ packages/multimedia/aom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aom"
-PKG_VERSION="3.8.2"
-PKG_SHA256="98f7d6d7ddbf06d088735f5e51782df053fd1b08c553882c2924bd0b2021a202"
+PKG_VERSION="3.9.1"
+PKG_SHA256="dba99fc1c28aaade28dda59821166b2fa91c06162d1bc99fde0ddaad7cecc50e"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.webmproject.org"
 PKG_URL="https://storage.googleapis.com/aom-releases/libaom-${PKG_VERSION}.tar.gz"
