e01d6627ddf4236dfb9bf6c52506cde928347fae
Rudi Heitbaum <rudi@heitbaum.com>
2024-04-24 11:17:08 +0000

    mimalloc: update to 2.1.4

--- packages/devel/mimalloc/package.mk
+++ packages/devel/mimalloc/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mimalloc"
-PKG_VERSION="2.1.2"
-PKG_SHA256="2b1bff6f717f9725c70bf8d79e4786da13de8a270059e4ba0bdd262ae7be46eb"
+PKG_VERSION="2.1.4"
+PKG_SHA256="ef31a7c593866a35883b2090654a8d6136a1cf06f22b577b4e1c818b1b0a8796"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/microsoft/mimalloc"
 PKG_URL="https://github.com/microsoft/mimalloc/archive/refs/tags/v${PKG_VERSION}.tar.gz"
