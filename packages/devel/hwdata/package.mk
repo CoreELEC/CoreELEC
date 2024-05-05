d47ffa50d3763cdf89a1d0b93cc0a6aa947049e1
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-05 01:04:51 +0000

    hwdata: update to 0.382

--- packages/devel/hwdata/package.mk
+++ packages/devel/hwdata/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="hwdata"
-PKG_VERSION="0.381"
-PKG_SHA256="53435c73964ddc24ac53fa86e29e8b9244ca1cab0578ffdd82fd280f35863004"
+PKG_VERSION="0.382"
+PKG_SHA256="5e25457b562a5227eb77eac21d5e4344bd1183c7d62b41e7d3e780ae33e053c1"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://github.com/vcrhonek/hwdata"
 PKG_URL="https://github.com/vcrhonek/hwdata/archive/refs/tags/v${PKG_VERSION}.tar.gz"
