1c30c31432223e3576d5e63c4fd72a03855e5fe0
Portisch <hugo.portisch@yahoo.de>
2024-05-07 17:43:01 +0200

    linux:Amlogic-ne: bump package to 1d7dd2a7 linux:Amlogic-no: bump package to 69bd03e0

--- projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-no/packages/linux/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="linux"
-PKG_VERSION="e7a3ba1ca245f1324a8eecc6ca0f9cdacd4d630d"
-PKG_SHA256="98ed8276ea11e8338680d224d3e1c7afd41f1a90866edbc486f40400269de7a5"
+PKG_VERSION="69bd03e0a0eae9abb3aab3a108f416caf8f96fe4"
+PKG_SHA256="262796d7d737600b25e276de601995ce96f980a60a1d7e2ea03f3f7f1b2d53d9"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
 PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/${PKG_VERSION}.tar.gz"
