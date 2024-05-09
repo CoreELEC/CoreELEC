58853288caeaa6f82f6b6528cf6afadb3f457837
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-09 04:51:03 +0000

    pcsc-lite: update to 2.2.3 and meson

--- packages/addons/addon-depends/pcsc-lite/package.mk
+++ packages/addons/addon-depends/pcsc-lite/package.mk
@@ -2,24 +2,20 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="2.1.0"
-PKG_SHA256="85cab61cc744c81e2bc432656863293b8428d0136f079e3b12a84b335b5b35aa"
+PKG_VERSION="2.2.3"
+PKG_SHA256="cab1e62755713f62ce1b567954dbb0e9a7e668ffbc3bbad3ce85c53f8f4e00a4"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pcsclite.apdu.fr"
-PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libusb polkit"
 PKG_LONGDESC="Middleware to access a smart card using SCard API (PC/SC)."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-            --enable-static \
-            --disable-libudev \
-            --enable-libusb \
-            --enable-usbdropdir=/storage/.kodi/addons/service.pcscd/drivers"
+PKG_MESON_OPTS_TARGET="-Ddefault_library=static \
+                       -Dlibudev=false \
+                       -Dlibusb=true \
+                       -Dpolkit=true \
+                       -Dusbdropdir=/storage/.kodi/addons/service.pcscd/drivers"
 
 pre_configure_target() {
   export PKG_CONFIG_PATH="$(get_install_dir polkit)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
-}
-
-post_configure_target() {
-  libtool_remove_rpath libtool
 }
