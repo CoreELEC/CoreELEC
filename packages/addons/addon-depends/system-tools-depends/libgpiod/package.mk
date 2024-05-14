efae09ac84969a6dc5449f07b02e522848a8ebb6
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-14 12:57:36 +0000

    libgpiod: update to 2.1.2

--- packages/addons/addon-depends/system-tools-depends/libgpiod/package.mk
+++ packages/addons/addon-depends/system-tools-depends/libgpiod/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libgpiod"
-PKG_VERSION="2.1.1"
-PKG_SHA256="0af43a6089d69f9d075cf67ca2ae5972b9081e38e6b3d46cea37d67e2df6fb9b"
+PKG_VERSION="2.1.2"
+PKG_SHA256="b1bdf1e3f75238695f93e442062bafc069170f2bf4f0cd4b8e049ca67131a1f0"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/about/"
 PKG_URL="https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/snapshot/libgpiod-${PKG_VERSION}.tar.gz"
