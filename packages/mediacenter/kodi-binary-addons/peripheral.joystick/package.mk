d0da876d21a76babc6b914b91a65d43e3e03a1e0
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-28 09:46:50 +0000

    peripheral.joystick: update 21.1.15-Omega to 21.1.18-Omega

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="21.1.15-Omega"
-PKG_SHA256="b152c6f17291ede48850bf307e6d619ff803e1b26217ccec532d5c49a3ef7b84"
+PKG_VERSION="21.1.18-Omega"
+PKG_SHA256="226366b69536579d1adccb8548ba20c1b094876ce481bab81f744affa71bb305"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
