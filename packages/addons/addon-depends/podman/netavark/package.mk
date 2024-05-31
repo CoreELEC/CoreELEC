37327a04749695442773e23d34a8f3e6a9c474a0
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-31 22:22:14 +0000

    netavark: update to 1.12.2 and fix cross-compile build

--- packages/addons/addon-depends/podman/netavark/package.mk
+++ packages/addons/addon-depends/podman/netavark/package.mk
@@ -2,10 +2,24 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="netavark"
-PKG_VERSION="1.10.3"
-PKG_SHA256="fdc3010cb221f0fcef0302f57ef6f4d9168a61f9606238a3e1ed4d2e348257b7"
+PKG_VERSION="1.12.2"
+PKG_SHA256="d1e5a7e65b825724fd084b0162084d9b61db8cda1dad26de8a07be1bd6891dbc"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/containers/netavark"
 PKG_URL="https://github.com/containers/netavark/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cargo:host protobuf:host"
 PKG_LONGDESC="Container network stack"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  cargo build \
+    --target ${TARGET_NAME} \
+    --release \
+    --locked \
+    --all-features
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}
+  cp ${PKG_BUILD}/.${TARGET_NAME}/target/${TARGET_NAME}/release/netavark ${INSTALL}
+}
