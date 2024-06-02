2ed54bb715ba8c87988e702426b9440232c9c6ce
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-02 13:45:20 +0000

    mtools: update to 4.0.44
    ann:
    - https://lists.gnu.org/archive/html/info-gnu/2024-06/msg00000.html

--- packages/tools/mtools/package.mk
+++ packages/tools/mtools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtools"
-PKG_VERSION="4.0.43"
-PKG_SHA256="541e179665dc4e272b9602f2074243591a157da89cc47064da8c5829dbd2b339"
+PKG_VERSION="4.0.44"
+PKG_SHA256="37dc4df022533c3d4b2ec1c78973c27c7e8b585374c2d46ab64c6a3db31eddb8"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/mtools/"
 PKG_URL="http://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
