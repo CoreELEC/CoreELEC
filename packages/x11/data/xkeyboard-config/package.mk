6139f8a8e76d5e9b7b22330e50341574f91255c8
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-08 11:39:01 +0000

    xkeyboard-config: update to 2.42

--- packages/x11/data/xkeyboard-config/package.mk
+++ packages/x11/data/xkeyboard-config/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xkeyboard-config"
-PKG_VERSION="2.41"
-PKG_SHA256="f02cd6b957295e0d50236a3db15825256c92f67ef1f73bf1c77a4b179edf728f"
+PKG_VERSION="2.42"
+PKG_SHA256="a6b06ebfc1f01fc505f2f05f265f95f67cc8873a54dd247e3c2d754b8f7e0807"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.X.org"
 PKG_URL="https://www.x.org/releases/individual/data/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
