0b5ee8eb381bb25aef88a372fd9b60d9e58669f2
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-24 04:37:51 +0000

    pvr.stalker: update 21.0.1-Omega to 21.1.0-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.stalker/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.stalker/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.stalker"
-PKG_VERSION="21.0.1-Omega"
-PKG_SHA256="d90b6b5595a820197d097da8ee6698bea786359cfe0bfaeabf4985dbc2af3064"
-PKG_REV="2"
+PKG_VERSION="21.1.0-Omega"
+PKG_SHA256="9e6fdf2d3654d267135bff45c94d72b4c82b10483dbb46600a8e0df4b0676746"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.stalker"
