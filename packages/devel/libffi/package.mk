c64420fc262ef0440e90c88afa5ce015a3b45506
Ian Leonard <antonlacon@gmail.com>
2024-07-02 22:31:08 -0400

    libffi: replace toolchain depend
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/devel/libffi/package.mk
+++ packages/devel/libffi/package.mk
@@ -9,7 +9,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://sourceware.org/${PKG_NAME}/"
 PKG_URL="https://github.com/libffi/libffi/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host libtool:host pkg-config:host"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="autotools:host gcc:host"
 PKG_LONGDESC="Foreign Function Interface Library."
 PKG_TOOLCHAIN="autotools"
 
