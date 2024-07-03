3b8fd64ecc1bfae7bb1dc36ddd54b438f691cd26
Ian Leonard <antonlacon@gmail.com>
2024-07-02 22:35:11 -0400

    readline: replace toolchain depend
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.gnu.org/software/readline/"
 PKG_URL="http://ftpmirror.gnu.org/readline/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_DEPENDS_TARGET="autotools:host gcc:host ncurses"
 PKG_LONGDESC="The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in."
 PKG_BUILD_FLAGS="+pic"
 
