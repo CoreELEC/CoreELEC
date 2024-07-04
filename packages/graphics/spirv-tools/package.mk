d1276ae1f5d501a7066119071e26c14a95cab4d9
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-04 07:40:08 +0000

    spirv-tools: update to githash 0cfe9e7
    Supports build of glslang 14.3.0

--- packages/graphics/spirv-tools/package.mk
+++ packages/graphics/spirv-tools/package.mk
@@ -6,8 +6,8 @@ PKG_NAME="spirv-tools"
 # The SPIRV-Tools pkg_version needs to match the compatible (known_good) glslang pkg_version.
 # https://raw.githubusercontent.com/KhronosGroup/glslang/${PKG_VERSION}/known_good.json
 # When updating glslang pkg_version please update to the known_good spirv-tools pkg_version.
-PKG_VERSION="dd4b663e13c07fea4fbb3f70c1c91c86731099f7"
-PKG_SHA256="03ee1a2c06f3b61008478f4abe9423454e53e580b9488b47c8071547c6a9db47"
+PKG_VERSION="0cfe9e7219148716dfd30b37f4d21753f098707a"
+PKG_SHA256="a5277538fec0868452b1f616553c2fca80939a6a3ee0b7322e21ff063750a482"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-Tools"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-Tools/archive/${PKG_VERSION}.tar.gz"
