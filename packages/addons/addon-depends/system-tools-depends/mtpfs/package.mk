575b45eea1475f6bb811017f34849280e0f139ca
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-15 13:20:56 +0000

    mtpfs: update to githash 2bd9b5a

--- packages/addons/addon-depends/system-tools-depends/mtpfs/package.mk
+++ packages/addons/addon-depends/system-tools-depends/mtpfs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtpfs"
-PKG_VERSION="d228a21b07062170e05fb71a7a7bf4a74ad559e1"
-PKG_SHA256="4b89e014201a01634022a6348874361f5ca729e455b8c1f9990fa10647590b52"
+PKG_VERSION="2bd9b5a33ad70a2238e086ffb07907f20a1e0101"
+PKG_SHA256="732d5d450cfefd9df0e53ed6b188e1428298d8f81aaa8b5bf24ad31b9fddbe8f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.adebenham.com/mtpfs/"
 PKG_URL="https://github.com/cjd/mtpfs/archive/${PKG_VERSION}.tar.gz"
@@ -17,4 +17,5 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-mad"
 # TODO: mtpfs runs host utils while building, fix and set
 pre_configure_target() {
   export LIBS="-lusb-1.0 -ludev"
+  TARGET_CONFIGURE_OPTS=$(echo ${TARGET_CONFIGURE_OPTS} | sed -e "s|--disable-static||" -e "s|--enable-shared||")
 }
