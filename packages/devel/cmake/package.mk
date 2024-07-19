f2a3df0f8af1179d962e62a5df1e5e4f2124a8dd
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-19 08:49:09 +0000

    cmake: update to 3.30.1
    Release notes:
    - https://cmake.org/cmake/help/latest/release/index.html

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="3.30.0"
-PKG_SHA256="157e5be6055c154c34f580795fe5832f260246506d32954a971300ed7899f579"
+PKG_VERSION="3.30.1"
+PKG_SHA256="df9b3c53e3ce84c3c1b7c253e5ceff7d8d1f084ff0673d048f260e04ccb346e1"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
