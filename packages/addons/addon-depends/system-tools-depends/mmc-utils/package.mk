9042b84fcfc43b37e16c2c89153dacf72c82f4a8
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-21 01:50:37 +0000

    mmc-utils: update to githash 123fd8b (2024-08-01)

--- packages/addons/addon-depends/system-tools-depends/mmc-utils/package.mk
+++ packages/addons/addon-depends/system-tools-depends/mmc-utils/package.mk
@@ -2,11 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mmc-utils"
-PKG_VERSION="613495ecaca97a19fa7f8f3ea23306472b36453c"
-PKG_SHA256="1c76924aa3f636af70bd841bc1dcd85d5728ef1d4326921da30cbab7d643e2a7"
+PKG_VERSION="123fd8b2ac3933be1319486fb1f32236a4a86a7c"
+PKG_SHA256="d718338740cc75c8b0b54647a0522baff1824a31d4f9ee7d0d022405d07284f6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.kernel.org/doc/html/latest/driver-api/mmc/mmc-tools.html"
 PKG_URL="https://git.kernel.org/pub/scm/utils/mmc/mmc-utils.git/snapshot/mmc-utils-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Configure MMC storage devices from userspace."
 PKG_BUILD_FLAGS="-sysroot"
+
+PKG_MAKE_OPTS_TARGET+=" C="
