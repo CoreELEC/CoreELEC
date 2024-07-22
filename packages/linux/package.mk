24fb4e82b106f88fa8c8aefaec4de1150186b690
Peter Tuschy <sky42+git@mailbox.org>
2024-07-23 00:07:51 +0200

    Revert "linux: update to 6.6.39"
    This reverts commit 418963b2ef47e250ff7167521acc326aed2eb991.

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.6.39"
-    PKG_SHA256="2783d42112095f95c510e1b421f056df8cbfa845f9040c6115080434a77a776b"
+    PKG_VERSION="6.6.30"
+    PKG_SHA256="b66a5b863b0f8669448b74ca83bd641a856f164b29956e539bbcb5fdeeab9cc6"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
