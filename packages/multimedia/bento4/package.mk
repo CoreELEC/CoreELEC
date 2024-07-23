4ba78f23bb4391439387237b8dd2837f42b1b51f
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-23 10:40:52 +0000

    bento4: update to 1.6.0-641-2-Omega
    - Fixes for C++20
    - Other CVE fixes
    - Updated to upstream master branch (missing new release)
    
    log:
    - https://github.com/xbmc/Bento4/compare/1.6.0-641-Omega...1.6.0-641-2-Omega

--- packages/multimedia/bento4/package.mk
+++ packages/multimedia/bento4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bento4"
-PKG_VERSION="1.6.0-641-Omega"
-PKG_SHA256="f1f8cfa4f7cb651b609e587f825607cb4b06fe5b08af876fd0154a434f9a5315"
+PKG_VERSION="1.6.0-641-2-Omega"
+PKG_SHA256="3e4970a33700f1844513bb0c86c0c2de9f638bdc03547d2b7421f558962b85f1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.bento4.com"
 PKG_URL="https://github.com/xbmc/Bento4/archive/refs/tags/${PKG_VERSION}.tar.gz"
