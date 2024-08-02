9f062e4454e51fa68329db102770dbd624bb585e
Peter <peter.vicman@gmail.com>
2024-08-02 12:09:33 +0200

    linux: bump package to d79ca45e enable Amlogic IR transmitter

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -17,8 +17,8 @@ PKG_PATCH_DIRS="$LINUX"
 
 case "$LINUX" in
   amlogic-4.9)
-    PKG_VERSION="27b0df13f01b32a97f28a1b09fc6c8305c12d3d2"
-    PKG_SHA256="787e2432151bb3f9b4d2ff6b1488b8ee248c1549e9c9de894e740d6a8c3d8c07"
+    PKG_VERSION="d79ca45e3cc74d478f4c1580d4b2015f47643afb"
+    PKG_SHA256="97989367beb529c1f4c847d9a470ca869bf5584f64fe35837b3e3fe00f1fbab8"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
