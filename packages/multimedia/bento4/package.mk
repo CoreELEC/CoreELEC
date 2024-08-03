93ad56bd8f5222ecbf0ccfe81abba88accb68e0a
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-03 14:34:03 +0000

    bento4: update to 1.6.0-641-3-Omega

--- packages/multimedia/bento4/package.mk
+++ packages/multimedia/bento4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bento4"
-PKG_VERSION="1.6.0-641-Omega"
-PKG_SHA256="f1f8cfa4f7cb651b609e587f825607cb4b06fe5b08af876fd0154a434f9a5315"
+PKG_VERSION="1.6.0-641-3-Omega"
+PKG_SHA256="a9b231b63159b3a4d9e47c5328b476308852bf092ccb9ce98f7cf46a386465ce"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.bento4.com"
 PKG_URL="https://github.com/xbmc/Bento4/archive/refs/tags/${PKG_VERSION}.tar.gz"
