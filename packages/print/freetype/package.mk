b8760b33e06c6615c86c78ab93ef47cc3e125048
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-13 11:10:10 +0000

    freetype: update to 2.13.3

--- packages/print/freetype/package.mk
+++ packages/print/freetype/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="freetype"
-PKG_VERSION="2.13.2"
-PKG_SHA256="12991c4e55c506dd7f9b765933e62fd2be2e06d421505d7950a132e4f1bb484d"
+PKG_VERSION="2.13.3"
+PKG_SHA256="0550350666d427c74daeb85d5ac7bb353acba5f76956395995311a9c6f063289"
 PKG_LICENSE="GPL"
 PKG_SITE="https://freetype.org"
 PKG_URL="https://download.savannah.gnu.org/releases/freetype/freetype-${PKG_VERSION}.tar.xz"
