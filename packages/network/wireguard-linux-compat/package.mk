92d8cc7a9923d3e72035f8ce04a3a822bfa3a028
Peter <peter.vicman@gmail.com>
2024-08-19 15:40:21 +0200

    wireguard-linux-compat: bump package to v1.0.20220627

--- packages/network/wireguard-linux-compat/package.mk
+++ packages/network/wireguard-linux-compat/package.mk
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="wireguard-linux-compat"
-PKG_VERSION="v1.0.20210219"
-PKG_SHA256="11a75cf2387b6bb227c96241c911b5f48475def1ac84d0e758793cf9e677a6f0"
+PKG_VERSION="v1.0.20220627"
+PKG_SHA256="894f0e0792aa3cc74e93958c175f16ab7155b0049cec940a9000bf7971380f98"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.wireguard.com"
 PKG_URL="https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$PKG_VERSION.tar.xz"
@@ -13,15 +14,11 @@ PKG_LONGDESC="WireGuard VPN kernel module"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
 
-pre_make_target() {
-  unset LDFLAGS
-}
-
 make_target() {
   kernel_make KERNELDIR=$(kernel_path) -C src/ module
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
-    cp src/*.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+  cp src/*.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
 }
