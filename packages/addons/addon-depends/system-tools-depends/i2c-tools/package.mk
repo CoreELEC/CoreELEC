472dda0a4d0f90240f6645e0e8a743f184233937
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-08-20 21:28:29 +0200

    i2c-tools: Don't use distutilscross

--- packages/addons/addon-depends/system-tools-depends/i2c-tools/package.mk
+++ packages/addons/addon-depends/system-tools-depends/i2c-tools/package.mk
@@ -7,37 +7,26 @@ PKG_SHA256="1f899e43603184fac32f34d72498fc737952dbc9c97a8dd9467fadfdf4600cf9"
 PKG_LICENSE="GPL"
 PKG_SITE="https://i2c.wiki.kernel.org/index.php/I2C_Tools"
 PKG_URL="https://www.kernel.org/pub/software/utils/i2c-tools/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain Python3 distutilscross:host"
+PKG_DEPENDS_TARGET="toolchain Python3"
 PKG_LONGDESC="A heterogeneous set of I2C tools for Linux."
 PKG_BUILD_FLAGS="-sysroot"
-
-pre_make_target() {
-  export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
-  export LDSHARED="${CC} -shared"
-}
 
 make_target() {
   make  CC="${CC}" \
         AR="${AR}" \
         CFLAGS="${TARGET_CFLAGS}" \
-        CPPFLAGS="${TARGET_CPPFLAGS} -I${SYSROOT_PREFIX}/usr/include/${PKG_PYTHON_VERSION}" \
-        PYTHON=${TOOLCHAIN}/bin/python3 \
+        CPPFLAGS="${TARGET_CPPFLAGS}" \
         all
 
-  make  EXTRA="py-smbus" \
-        CC="${CC}" \
-        AR="${AR}" \
-        CFLAGS="${TARGET_CFLAGS}" \
-        CPPFLAGS="${TARGET_CPPFLAGS} -I${SYSROOT_PREFIX}/usr/include/${PKG_PYTHON_VERSION}" \
-        PYTHON=${TOOLCHAIN}/bin/python3 \
-        all-python
+  (
+    cd py-smbus
+    python_target_env python3 setup.py build
+  )
 }
 
 makeinstall_target() {
-  make  EXTRA="py-smbus" \
-        DESTDIR=${INSTALL} \
-        PREFIX="/usr" \
-        prefix="/usr" \
-        PYTHON=${TOOLCHAIN}/bin/python3 \
-        install
+  (
+    cd py-smbus
+    exec_thread_safe python_target_env python3 setup.py install --root=${INSTALL} --prefix=/usr
+  )
 }
