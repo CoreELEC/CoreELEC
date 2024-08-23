7890013454e6d52fe727920c72a0a35c4f587d72
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-08-23 16:50:45 +0200

    pycryptodome: switch to new build system

--- packages/python/graphics/glad/package.mk
+++ packages/python/graphics/glad/package.mk
@@ -7,11 +7,6 @@ PKG_SHA256="61a70234dc7da467cceb07fcdd6dec1213d6143a1b6b19ccc5d7b64cc247ea47"
 PKG_LICENSE="MIT"
 PKG_SITE="https://glad.dav1d.de"
 PKG_URL="https://github.com/Dav1dde/glad/archive/refs/tags/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="setuptools:host"
 PKG_LONGDESC="Multi-Language Vulkan/GL/GLES/EGL/GLX/WGL Loader-Generator based on the official specs"
-PKG_TOOLCHAIN="manual"
-
-makeinstall_host() {
-  export DONT_BUILD_LEGACY_PYC=1
-  exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN}
-}
+PKG_TOOLCHAIN="python"
--- packages/python/security/pycryptodome/package.mk
+++ packages/python/security/pycryptodome/package.mk
@@ -7,30 +7,22 @@ PKG_SHA256="35019dff66c25db80d0c739b9c7b59bb0f61551897681c6dbdbdd0f7198f780f"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.org/project/pycryptodome"
 PKG_URL="https://github.com/Legrandin/${PKG_NAME}/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3"
+PKG_DEPENDS_TARGET="toolchain Python3 setuptools:host"
 PKG_LONGDESC="PyCryptodome is a self-contained Python package of low-level cryptographic primitives."
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="python"
 
 pre_configure_target() {
   cd ${PKG_BUILD}
   rm -rf .${TARGET_NAME}
 }
 
-make_target() {
-  python_target_env python3 setup.py build
-}
-
-makeinstall_target() {
-  exec_thread_safe python_target_env python3 setup.py install --root=${INSTALL} --prefix=/usr
-
+post_makeinstall_target() {
   # Remove SelfTest bloat
   find ${INSTALL} -type d -name SelfTest -exec rm -fr "{}" \; 2>/dev/null || true
   find ${INSTALL} -name SOURCES.txt -exec sed -i "/\/SelfTest\//d;" "{}" \;
 
   # Create Cryptodome as an alternative namespace to Crypto (Kodi addons may use either)
   ln -sf /usr/lib/${PKG_PYTHON_VERSION}/site-packages/Crypto ${INSTALL}/usr/lib/${PKG_PYTHON_VERSION}/site-packages/Cryptodome
-}
 
-post_makeinstall_target() {
   python_remove_source
 }
