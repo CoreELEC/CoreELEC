eae21e687742ae9686ea26a1ac2719852498d185
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-08-23 16:54:18 +0200

    dbussy: switch to new build system

--- packages/python/system/dbussy/package.mk
+++ packages/python/system/dbussy/package.mk
@@ -7,17 +7,9 @@ PKG_SHA256="857104ef2fd1978323d7c87b32c753d2d178b79adbd13f52cea23511f5195ded"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://gitlab.com/ldo/dbussy"
 PKG_URL="https://github.com/ldo/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 dbus"
+PKG_DEPENDS_TARGET="toolchain Python3 dbus setuptools:host"
 PKG_LONGDESC="DBussy is a wrapper around libdbus, written in pure Python"
-PKG_TOOLCHAIN="manual"
-
-make_target() {
-  python3 setup.py build
-}
-
-makeinstall_target() {
-  python3 setup.py install --root=${INSTALL} --prefix=/usr
-}
+PKG_TOOLCHAIN="python"
 
 post_makeinstall_target() {
   python_remove_source
