cdf7cc33f98a1822277a7fd193986a85899546e5
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-08-23 16:35:38 +0200

    Mako: switch to new build system

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -10,9 +10,4 @@ PKG_SITE="https://pypi.org/project/Mako"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host MarkupSafe:host"
 PKG_LONGDESC="Mako is a super-fast templating language that borrows the best ideas from the existing templating languages."
-PKG_TOOLCHAIN="manual"
-
-makeinstall_host() {
-  export DONT_BUILD_LEGACY_PYC=1
-  exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN}
-}
+PKG_TOOLCHAIN="python"
