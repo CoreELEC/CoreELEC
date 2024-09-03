d04ab5e1051ad336e8ca58d499b35ccb93acf87a
Portisch <hugo.portisch@yahoo.de>
2024-09-03 13:57:06 +0200

    ap6xxx-aml:Amlogic-ng: bump package to c952e407 Add support for AP6275p

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/ap6xxx-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/ap6xxx-aml/package.mk
@@ -3,14 +3,14 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="ap6xxx-aml"
-PKG_VERSION="91a6e2bd2ff5e52377190fea04a0433bdbe4e948"
-PKG_SHA256="c1f96870858bd80adadb87a526286a2d82ed52da617b210af51db12def0d4725"
+PKG_VERSION="c952e40780e1278a5fe2105acf6d6f1622ea4a9b"
+PKG_SHA256="31c4928942f11b5e85a2d6dd19c0fae7f06bbd06284ec7a9dfe98caaf3b51ca9"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC/ap6xxx-aml"
-PKG_URL="https://github.com/CoreELEC/ap6xxx-aml/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/CoreELEC/ap6xxx-aml/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
-PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="ap6xxx: Linux drivers for AP6xxx WLAN chips used in some devices based on Amlogic SoCs"
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
@@ -22,37 +22,47 @@ pre_make_target() {
 make_target() {
   echo
   echo "building ap6356s and others"
-  make -C  $PKG_BUILD/bcmdhd.1.363.59.144.x.cn \
-       PWD=$PKG_BUILD/bcmdhd.1.363.59.144.x.cn \
+  kernel_make -C  ${PKG_BUILD}/bcmdhd.1.363.59.144.x.cn \
+       PWD=${PKG_BUILD}/bcmdhd.1.363.59.144.x.cn \
        KDIR=$(kernel_path) \
-       ARCH=$TARGET_KERNEL_ARCH \
-       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       ARCH=${TARGET_KERNEL_ARCH} \
+       CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
        CONFIG_BCMDHD_DISABLE_WOWLAN=y \
        dhd
 
   if [ "$DEVICE" = "Amlogic-ng" ]; then
     echo
     echo "building ap6275s"
-    make -C  $PKG_BUILD/bcmdhd.100.10.545.x \
-         PWD=$PKG_BUILD/bcmdhd.100.10.545.x \
+    kernel_make -C  ${PKG_BUILD}/bcmdhd.100.10.545.x \
+         PWD=${PKG_BUILD}/bcmdhd.100.10.545.x \
          KDIR=$(kernel_path) \
-         ARCH=$TARGET_KERNEL_ARCH \
-         CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+         ARCH=${TARGET_KERNEL_ARCH} \
+         CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
          CONFIG_BCMDHD_DISABLE_WOWLAN=y \
          bcmdhd_sdio
+
+    echo
+    echo "building ap6275p"
+    kernel_make -C  ${PKG_BUILD}/bcmdhd.101.10.591.x \
+         M=${PKG_BUILD}/bcmdhd.101.10.591.x \
+         PWD=${PKG_BUILD}/bcmdhd.101.10.591.x \
+         KERNEL_SRC=$(kernel_path) \
+         CONFIG_BCMDHD_DISABLE_WOWLAN=y \
+         CONFIG_BCMDHD_PCIE=y \
+         bcmdhd_pcie
   fi
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
 
-  find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' \
-    -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+  find ${PKG_BUILD}/ -name \*.ko -not -path '*/\.*' \
+    -exec cp {} ${INSTALL}/$(get_full_module_dir)/${PKG_NAME} \;
 }
 
 post_install() {
-  if [ "$PROJECT" = "Amlogic" ]; then
-    rm $INSTALL/usr/lib/modprobe.d/dhd_sdio.conf
-    rm $INSTALL/usr/lib/udev/rules.d/80-dhd_sdio.rules
+  if [ "${PROJECT}" = "Amlogic" ]; then
+    rm ${INSTALL}/usr/lib/modprobe.d/dhd_sdio.conf
+    rm ${INSTALL}/usr/lib/udev/rules.d/80-dhd_sdio.rules
   fi
 }
