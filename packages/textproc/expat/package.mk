ee1db2e749430f635699744d273996d83280d8c1
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-04 12:05:08 +0000

    expat: update to 2.6.3

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="expat"
-PKG_VERSION="2.6.2"
-PKG_SHA256="ee14b4c5d8908b1bec37ad937607eab183d4d9806a08adee472c3c3121d27364"
+PKG_VERSION="2.6.3"
+PKG_SHA256="274db254a6979bde5aad404763a704956940e465843f2a9bd9ed7af22e2c0efc"
 PKG_LICENSE="OSS"
 PKG_SITE="https://libexpat.github.io"
 PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
