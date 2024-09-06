6ddaf1963da7bef6664c7c57d2261e5bfe2a0dba
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-06 11:02:13 +0000

    glad: update to 2.0.7

--- packages/python/graphics/glad/package.mk
+++ packages/python/graphics/glad/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glad"
-PKG_VERSION="2.0.6"
-PKG_SHA256="61a70234dc7da467cceb07fcdd6dec1213d6143a1b6b19ccc5d7b64cc247ea47"
+PKG_VERSION="2.0.7"
+PKG_SHA256="45f5754fc91de428a840e6ad0237a5577c342b53448527ee073d055c9f8fc767"
 PKG_LICENSE="MIT"
 PKG_SITE="https://glad.dav1d.de"
 PKG_URL="https://github.com/Dav1dde/glad/archive/refs/tags/v${PKG_VERSION}.tar.gz"
