26045ddd483f8fae8ca555f03cdaa07ec5b8849a
Matthias Reichl <hias@horus.com>
2024-09-07 20:22:41 +0200

    rpi-eeprom: update to ccfc47b
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="ef2fc67d235d037b2b468813e646e20890fcea07"
-PKG_SHA256="c982ba891fcc63a9df711ae0c840e1a9cba083848658cd1f29a5f98d95d20dda"
+PKG_VERSION="ccfc47b215c05eaeb5b6f5549bbe51051a47f8e5"
+PKG_SHA256="dba4410f2a00926d8cd1cd13203e1d88389405b8cc5b8d5d07a647536586e92f"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
