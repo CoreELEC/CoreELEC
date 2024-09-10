3c8b9903522feb423634599c8279204563b494eb
Peter <peter.vicman@gmail.com>
2024-09-10 19:26:43 +0200

    wifi_dummy-aml: check DTB for enabled SDIO or PCIe device
    unload module after the actions

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/package.mk
@@ -8,20 +8,16 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain linux"
-PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="$PKG_NAME: Linux driver"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
+PKG_LONGDESC="${PKG_NAME}: Linux driver"
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
-pre_make_target() {
-  unset LDFLAGS
-}
-
 make_target() {
-  kernel_make -C $(kernel_path) M=$PKG_BUILD
+  kernel_make -C $(kernel_path) M=${PKG_BUILD}
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
-    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+  cp ${PKG_BUILD}/wifi_dummy.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
 }
--- projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/sources/wifi_dummy.c
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/wifi_dummy-aml/sources/wifi_dummy.c
@@ -2,43 +2,128 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/of_address.h>
+#include <linux/version.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("kszaq");
-MODULE_DESCRIPTION("Amlogic WiFi power on and SDIO rescan module");
+MODULE_DESCRIPTION("Amlogic WiFi power on and SDIO/PCIe rescan module");
 
 extern void extern_wifi_set_enable(int);
 extern void sdio_reinit(void);
+
 #ifdef CONFIG_PCI
 extern void set_usb_wifi_power(int);
 extern void pci_remove(void);
 extern void pci_reinit(void);
 #endif
 
+/*
+ linux 5.4.210, 5.15.119     linux 4.9.269
+
+			name sdio             			name sdio
+ full_name sdio@fe088000     full_name /sdio@ffe03000
+
+			name pcie             			name pcieA
+ full_name pcie@e0000000     full_name /pcieA@fc000000
+*/
+
+static bool device_enabled(const char *prefix)
+{
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 210))
+	const char *path = "/soc";
+#else
+	const char *path = "/";
+#endif
+
+struct device_node *parent_node;
+struct device_node *child;
+bool ret = false;
+int len;
+
+	parent_node = of_find_node_by_path(path);
+	if (parent_node) {
+		for_each_child_of_node(parent_node, child) {
+			/*pr_info("wifi_dummy: full_name=%s, name=%s\n", child->full_name, child->name);*/
+			if (!strncmp(child->name, prefix, strlen(prefix))) {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 210))
+				len = strlen(child->name);
+#else
+				len = strlen(child->name) + 1;  /* '/' */
+#endif
+				if (strlen(child->full_name) > len && child->full_name[len] == '@') {
+					if (of_device_is_available(child)) {
+						pr_info("wifi_dummy: found enabled %s\n", child->full_name);
+						ret = true;
+						break;
+					} else {
+						pr_info("wifi_dummy: found disabled %s\n", child->full_name);
+					}
+				}
+			}
+		}
+
+		of_node_put(parent_node);
+	}
+
+	return ret;
+}
+
 static int __init wifi_dummy_init(void)
 {
-	printk(KERN_INFO "Triggered SDIO/PCI WiFi power on and bus rescan.\n");
+bool sdio_en = false;
+bool pcie_en = false;
+
+	pr_info("wifi_dummy: Triggered SDIO/PCIe WiFi power on and bus rescan\n");
+
+	sdio_en = device_enabled("sdio");
+	pcie_en = device_enabled("pcie");
+
+	if (!sdio_en && !pcie_en) {
+		pr_info("wifi_dummy: SDIO/PCIe not enabled\n");
+		return -ENODEV;
+	}
+
+	pr_info("wifi_dummy: SDIO %s, PCIe %s\n",
+		sdio_en ? "enabled" : "disabled",
+		pcie_en ? "enabled" : "disabled");
+
 #ifdef CONFIG_PCI
-	pci_remove();
-	set_usb_wifi_power(0);
+	if (pcie_en) {
+		pci_remove();
+		set_usb_wifi_power(0);
+	}
 #endif
-	extern_wifi_set_enable(0);
+
+	if (sdio_en) {
+		extern_wifi_set_enable(0);
+		msleep(300);
+		extern_wifi_set_enable(1);
+	} else {
+		msleep(300);
+	}
+
+#ifdef CONFIG_PCI
+	if (pcie_en)
+		set_usb_wifi_power(1);
+#endif
+
 	msleep(300);
-	extern_wifi_set_enable(1);
+
+	if (sdio_en)
+		sdio_reinit();
+
 #ifdef CONFIG_PCI
-	set_usb_wifi_power(1);
+	if (pcie_en)
+		pci_reinit();
 #endif
-	msleep(300);
-	sdio_reinit();
-#ifdef CONFIG_PCI
-	pci_reinit();
-#endif
-	return 0;
+
+	return -ENODEV;
 }
 
 static void __exit wifi_dummy_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
+	/* unused */
 }
 
 module_init(wifi_dummy_init);
