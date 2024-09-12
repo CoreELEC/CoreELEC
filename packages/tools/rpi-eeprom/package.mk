0c731af4c205d69960db537c04375501cc82f4d6
Matthias Reichl <hias@horus.com>
2024-09-12 13:19:58 +0200

    rpi-eeprom: update to d53e3dd
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="171e47acb1d0ff0116f960dc882432b6041206c2"
-PKG_SHA256="a653f8448e139f55a9e2b577cc87090b7cb675e81ff9f5ecb45fb32a9074b750"
+PKG_VERSION="d53e3dd4b1b278010562220fcab341cccec81d75"
+PKG_SHA256="cec7c991fdd35092c066c9cb0e527ef16b5b9e25cb005eef5422bda629accbce"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
