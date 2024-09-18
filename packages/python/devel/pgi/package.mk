20a79d226f417f43aabebe17526ede2de3e6eeda
Peter <peter.vicman@gmail.com>
2024-09-18 16:53:11 +0200

    pgi: bump package to d8884858 and remove usage of distutilscross

--- packages/python/devel/pgi/package.mk
+++ packages/python/devel/pgi/package.mk
@@ -2,31 +2,26 @@
 # Copyright (C) 2020-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pgi"
-PKG_VERSION="0.0.11.2"
-PKG_SHA256="7a1ca8ac4e8bee6b663e6d556ecda8032584de753acd76ab3fc21c4f874fa738"
+PKG_VERSION="d88848581c68543f6d761205e689a2757fd1ffdd"
+PKG_SHA256="9db3fcf5215ea19f31bf790c935bda497769cf38fca0ee8a05cb0300b41b8d76"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/pygobject/pgi"
-PKG_URL="https://github.com/pygobject/pgi/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/pygobject/pgi/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python3"
 PKG_SECTION="python/devel"
 PKG_SHORTDESC="PGI - Pure Python GObject Introspection Bindings"
 PKG_LONGDESC="GObject Introspection bindings written in pure Python using ctypes and cffi (optional). API compatible with PyGObject."
 PKG_TOOLCHAIN="manual"
 
-pre_make_target() {
-  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
-}
-
 make_target() {
-  exec_thread_safe python3 setup.py build
+  python_target_env python3 -m build -n -w -x
 }
 
 makeinstall_target() {
-  exec_thread_safe python3 setup.py install --root=$INSTALL --prefix=/usr
+  python3 -m installer --overwrite-existing dist/*.whl -d ${INSTALL} -p /usr
 }
 
 post_makeinstall_target() {
-  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
-  rm -rf $INSTALL/usr/lib/python*/site-packages/*/tests
+  find ${INSTALL}/usr/lib -name "*.py" -exec rm -rf "{}" ";"
 }
