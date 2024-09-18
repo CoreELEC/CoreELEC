302ed5b4a14c2240f7087f693cf2d60b6ec29bf7
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-18 09:49:28 +0000

    spirv-llvm-translator: update to 19.1.10

---
+++ packages/graphics/spirv-llvm-translator/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="spirv-llvm-translator"
+PKG_VERSION="19.1.10"
+PKG_SHA256="c829a6090b7ea9cdebaa5d3dbad0972f75bccb46d09b2fe02db17afd7cf4eff2"
+PKG_LICENSE="LLVM"
+PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
+PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v${PKG_VERSION}.tar.gz"
+PKG_LONGDESC="SPIRV-LLVM-Translator"
+PKG_TOOLCHAIN="manual"
