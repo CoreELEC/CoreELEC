9918213159465c957cfcea0fbededb5dc27d0316
Peter <peter.vicman@gmail.com>
2024-09-20 17:29:59 +0200

    jsonschema: bump package to 4.23.0 and remove attrs and pyrsistent dependency

--- projects/Amlogic-ce/packages/addons/addon-depends/jsonschema/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/jsonschema/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="jsonschema"
-PKG_VERSION="4.17.3"
-PKG_SHA256="0f864437ab8b6076ba6707453ef8f98a6a0d512a80e93f8abdb676f737ecb60d"
+PKG_VERSION="4.23.0"
+PKG_SHA256="d71497fef26351a33265337fa77ffeb82423f3ea21283cd9467bb03999266bc4"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pypi.org/project/jsonschema/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="Python3:host attrs:host pyrsistent:host"
+PKG_DEPENDS_HOST="Python3:host"
 PKG_LONGDESC="An implementation of JSON Schema validation for Python."
 PKG_TOOLCHAIN="manual"
 
