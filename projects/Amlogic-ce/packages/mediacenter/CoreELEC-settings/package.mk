2ce79f627be03647967f7faf8ae5d78ddda7b6e0
Peter <peter.vicman@gmail.com>
2024-09-20 17:28:38 +0200

    CoreELEC-settings: bump package to fe37ff47

--- projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
@@ -2,33 +2,25 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="CoreELEC-settings"
-PKG_VERSION="d01117e46d6bbd0e1e74c6d367ca2f8e9cf60482"
-PKG_SHA256="fb16d17ba479b3e66c7fee662a0f8c8d894a6e06def29a25054f4b4e9639d1ea"
+PKG_VERSION="fe37ff477f39f182300f7b653278ad33d9ec83ea"
+PKG_SHA256="46330e94c78a19cbfa218fdbf0b89093d2df7cce60a066f8712f6779928d9d3f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/service.coreelec.settings/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 connman pygobject dbus-python"
+PKG_DEPENDS_TARGET="toolchain Python3 connman pygobject dbus-python bkeymaps"
 PKG_LONGDESC="CoreELEC-settings: is a settings dialog for CoreELEC"
 
-PKG_MAKE_OPTS_TARGET="DISTRONAME=${DISTRONAME} ADDON_VERSION=${ADDON_VERSION} ROOT_PASSWORD=${ROOT_PASSWORD}"
-
-if [ "${DISPLAYSERVER}" = "x11" ]; then
-  PKG_DEPENDS_TARGET="${PKG_DEPENDS_TARGET} setxkbmap"
-else
-  PKG_DEPENDS_TARGET="${PKG_DEPENDS_TARGET} bkeymaps"
-fi
+PKG_MAKE_OPTS_TARGET="DISTRONAME=${DISTRONAME} \
+                      ADDON_VERSION=${ADDON_VERSION} \
+                      ROOT_PASSWORD=${ROOT_PASSWORD}"
 
 post_makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/coreelec
-    cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/lib/coreelec
+  cp ${PKG_DIR}/scripts/* ${INSTALL}/usr/lib/coreelec
 
   ADDON_INSTALL_DIR=${INSTALL}/usr/share/kodi/addons/service.coreelec.settings
-
-  ${TOOLCHAIN}/bin/python -Wi -t -B ${TOOLCHAIN}/lib/${PKG_PYTHON_VERSION}/compileall.py ${ADDON_INSTALL_DIR}/resources/lib/ -f
-  rm -rf $(find ${ADDON_INSTALL_DIR}/resources/lib/ -name "*.py")
-
-  ${TOOLCHAIN}/bin/python -Wi -t -B ${TOOLCHAIN}/lib/${PKG_PYTHON_VERSION}/compileall.py ${ADDON_INSTALL_DIR}/oe.py -f
-  rm -rf ${ADDON_INSTALL_DIR}/oe.py
+  python_compile ${ADDON_INSTALL_DIR}/resources/lib/
+  python_compile ${ADDON_INSTALL_DIR}/oe.py
 }
 
 post_install() {
