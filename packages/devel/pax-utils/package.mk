fdcd87f710af9c6c736be9bd50f4a2da865e5330
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-24 03:13:44 +0000

    pax-utils: update to 1.3.8

--- packages/devel/pax-utils/package.mk
+++ packages/devel/pax-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pax-utils"
-PKG_VERSION="1.3.7"
-PKG_SHA256="907fdcfc6c6c2913a8e42847f8096027b0a953b9344208d14daf2324fd711638"
+PKG_VERSION="1.3.8"
+PKG_SHA256="3d826b7fa1d47003c0e8f68f5bb9848cb70bddfc1f0190492533ccea6d696f24"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://wiki.gentoo.org/wiki/Hardened/PaX_Utilities"
 PKG_URL="https://gitweb.gentoo.org/proj/pax-utils.git/snapshot/pax-utils-${PKG_VERSION}.tar.bz2"
