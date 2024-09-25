219e566e4f84f143b66f935e96386f0abcd1523d
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-25 11:35:17 +0000

    harfbuzz: update to 10.0.1

--- packages/graphics/harfbuzz/package.mk
+++ packages/graphics/harfbuzz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="harfbuzz"
-PKG_VERSION="9.0.0"
-PKG_SHA256="a41b272ceeb920c57263ec851604542d9ec85ee3030506d94662067c7b6ab89e"
+PKG_VERSION="10.0.1"
+PKG_SHA256="b2cb13bd351904cb9038f907dc0dee0ae07127061242fe3556b2795c4e9748fc"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
 PKG_URL="https://github.com/harfbuzz/harfbuzz/releases/download/${PKG_VERSION}/harfbuzz-${PKG_VERSION}.tar.xz"
