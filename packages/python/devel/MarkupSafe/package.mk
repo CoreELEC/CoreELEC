476454f0935e950b658b0651416d2447f2846123
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-08 12:08:19 +0000

    MarkupSafe: update to 3.0.1

--- packages/python/devel/MarkupSafe/package.mk
+++ packages/python/devel/MarkupSafe/package.mk
@@ -3,11 +3,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="MarkupSafe"
-PKG_VERSION="2.1.5"
-PKG_SHA256="d283d37a890ba4c1ae73ffadf8046435c76e7bc2247bbb63c00bd1a709c6544b"
+PKG_VERSION="3.0.1"
+PKG_SHA256="3e683ee4f5d0fa2dde4db77ed8dd8a876686e3fc417655c2ece9a90576905344"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/MarkupSafe/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/markupsafe-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host"
 PKG_LONGDESC="MarkupSafe implements a XML/HTML/XHTML Markup safe string for Python"
 PKG_TOOLCHAIN="python"
