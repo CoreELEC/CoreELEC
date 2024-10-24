8825b271a04156d4934737034d9fe8d73c35a78d
Portisch <hugo.portisch@yahoo.de>
2024-10-24 13:01:57 +0200

    cargo-c: bump package to v0.10.5

--- packages/rust/cargo-c/package.mk
+++ packages/rust/cargo-c/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="cargo-c"
-PKG_VERSION="v0.9.17"
-PKG_SHA256="95fcf50505516dfa6a941e666d1388810da9d7a9e1c623c09068faef5d50b3b9"
+PKG_VERSION="v0.10.5"
+PKG_SHA256="3f131a6a647851a617a87daaaf777a9e50817957be0af29806615613e98efc8a"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/lu-zero/cargo-c"
 PKG_URL="https://github.com/lu-zero/cargo-c/archive/refs/tags/${PKG_VERSION}.tar.gz"
