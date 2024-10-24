5386d65c7cbba3d054289c12c4bca45a26a50314
Portisch <hugo.portisch@yahoo.de>
2024-10-24 15:00:21 +0200

    libdovi: bump package to libdovi-3.3.1

--- projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
@@ -2,20 +2,20 @@
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="libdovi"
-PKG_VERSION="3ec01dccbf5a74dfc7c58e3864029187b715344f"
+PKG_VERSION="3.3.1"
 PKG_SITE="https://github.com/quietvoid/dovi_tool"
 PKG_DEPENDS_TARGET="toolchain"
 if [ "${BUILD_FROM_SRC}" = "yes" ]; then
-  PKG_SHA256="9483c81af5ca34ad8081ee71b0c31bdb92114d29bdcb664774ede4071c3fcea2"
-  PKG_URL="https://github.com/quietvoid/dovi_tool/archive/${PKG_VERSION}.tar.gz"
+  PKG_SHA256="4cd7a4c418fd8af1da13278ce7524c15b7fdf61e1fe53663aa291c68c5062777"
+  PKG_URL="https://github.com/quietvoid/dovi_tool/archive/${PKG_NAME}-${PKG_VERSION}.tar.gz"
   PKG_DEPENDS_TARGET+=" cargo-c:host"
 else
   case "${TARGET_ARCH}" in
     "arm")
-      PKG_SHA256="e034776d5e8fbad6f22092cd06c7a94252962467de7ae298cc98f2823281f8fa"
+      PKG_SHA256="9dc360de883c627014e8f36fe92950033e49ade1f94d596f58f0b26aab978fe1"
       ;;
     "aarch64")
-      PKG_SHA256="6807b37cb55d49953196cc150941e6fe26c7deb42e25d266d82bb37b29e3c612"
+      PKG_SHA256="e6e0bb82198a58a58cd38bbb2a6d286ff9d024ad35f490ff4b127ea415521457"
       ;;
   esac
   PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
