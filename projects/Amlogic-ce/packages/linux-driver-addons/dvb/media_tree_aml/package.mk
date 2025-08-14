153f878e11ed6d2b9e5150766c81ad8174f08eb6
Portisch <hugo.portisch@yahoo.de>
2025-08-14 14:02:08 +0200

    media_tree_aml:Amlogic-ng: bump package to 5de4194a

--- projects/Amlogic-ce/packages/linux-driver-addons/dvb/media_tree_aml/package.mk
+++ projects/Amlogic-ce/packages/linux-driver-addons/dvb/media_tree_aml/package.mk
@@ -5,24 +5,24 @@ PKG_NAME="media_tree_aml"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="Source of Linux Kernel amlogic drivers to build with media_build."
 PKG_TOOLCHAIN="manual"
 
-case "$LINUX" in
+case "${LINUX}" in
   amlogic-3.14)
     PKG_VERSION="f9a4b158183866c589ee8c06ab740f41aa08fa66"
     PKG_SHA256="ff629f20b39749e6523571409c74c38a27ba0101b0ffe2a1ecea3e4dafea6dd2"
     PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
     ;;
   amlogic-4.9)
-    PKG_VERSION="d3d8e036546c254471e3903d199a30f8977f9f9b"
-    PKG_SHA256="7d9dd87b075a31b27417c4a7b07586859766d06adee9300f7aadfbd9ec6085cc"
+    PKG_VERSION="5de4194a41241aea6c821aa8c39465859aaaf5f6"
+    PKG_SHA256="a11f17952c9826f4e43612fdf45924316488d66d4378791fa4ce2ce3ca1e8cad"
     PKG_URL="https://github.com/CoreELEC/media_tree_aml/archive/${PKG_VERSION}.tar.gz"
     ;;
 esac
 
 unpack() {
-  mkdir -p $PKG_BUILD/
-  tar -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $PKG_BUILD/../
+  mkdir -p ${PKG_BUILD}/
+  tar -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}/../
 }
