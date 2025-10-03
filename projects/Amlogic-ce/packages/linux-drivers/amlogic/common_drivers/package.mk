182b7eb540a5319ceac189a72617d348e9f6743d
Marek Czerski <m.czerski@ap-tech.pl>
2025-10-03 14:17:09 +0200

    introduce support for DVB hardware on sc2 (and newer) soc
    common_drivers: bump package to 1b77bc7bf
    AVL6862: bump package to 08558dc
    
    Currently supported demod/tuner is AVL6762 + MXL608 found on Mecool KT1
    Should work also with AVL6862 (but only one tuner is supported) and MXL603 but not tested.

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="a260d378d62c7126df963051218d3cf21525b73f"
+PKG_VERSION="1b77bc7bfcb31f57eebad4cc0d2b2fef604aa234"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
