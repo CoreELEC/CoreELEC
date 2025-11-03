6dbde8f27c30a082b0677a5590c876fb7fe428e6
Peter <peter.vicman@gmail.com>
2025-11-03 10:21:21 +0100

    dtv-scan-tables: fix checksum

--- packages/addons/addon-depends/dtv-scan-tables/package.mk
+++ packages/addons/addon-depends/dtv-scan-tables/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="dtv-scan-tables"
 PKG_VERSION="2024-03-24-7098bdd27548"
-PKG_SHA256="3f4c8981b3a4829066b396abc0da270e5c7e674b5d190e92532a06951a07e98e"
+PKG_SHA256="562b362f04a83940d1882049488c0c3c4f9dd5d1c2129d1af5b00cfa24493007"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.linuxtv.org/dtv-scan-tables.git"
 PKG_URL="https://linuxtv.org/downloads/dtv-scan-tables/dtv-scan-tables-${PKG_VERSION}.tar.bz2"
