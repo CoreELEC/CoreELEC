ca3d3ba6e1cdd839008612dce73c3fcf3a717018
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-11 10:00:49 +0000

    kodi-binary-addons: update to latest versions
    - inputstream.adaptive: update 21.5.17-Omega to 21.5.18-Omega
    - pvr.sledovanitv.cz: update 21.5.1-Omega to 21.6.0-Omega
    - pvr.waipu: update 21.12.5-Omega to 21.12.6-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.sledovanitv.cz/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.sledovanitv.cz/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.sledovanitv.cz"
-PKG_VERSION="21.5.1-Omega"
-PKG_SHA256="0f99179239f20e3ff1c920f08d5914c2a58383e9c15004604974a09eb14505fc"
+PKG_VERSION="21.6.0-Omega"
+PKG_SHA256="92587d655307f16c1d31e660e9ee73b3f7c21319bd6d65180464fd2abeeb22a4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
