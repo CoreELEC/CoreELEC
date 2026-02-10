9a80568e54d46fca70f5045c40590f06a712bbee
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-10 08:59:27 +0000

    binutils: update to 2.46 and refresh-patches

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="binutils"
-PKG_VERSION="2.45.1"
-PKG_SHA256="5fe101e6fe9d18fdec95962d81ed670fdee5f37e3f48f0bef87bddf862513aa5"
+PKG_VERSION="2.46"
+PKG_SHA256="a389850c2d3919f2cc96fb8b5e7711eacfc819259aaffb11615c9fb9756eaeae"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/binutils/"
-PKG_URL="https://ftp.gnu.org/gnu/binutils/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://ftp.gnu.org/gnu/binutils/binutils-with-gold-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_UNPACK="binutils-gold"
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host linux:host"
 PKG_DEPENDS_TARGET="toolchain zlib binutils:host"
