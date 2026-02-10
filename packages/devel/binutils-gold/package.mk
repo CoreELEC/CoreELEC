dd57cd6ad5511670e84bff303a9682a36c7f92ca
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-10 08:58:39 +0000

    binutils-gold: update to 2.46

--- packages/devel/binutils-gold/package.mk
+++ packages/devel/binutils-gold/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="binutils-gold"
-PKG_VERSION="2.44"
-PKG_SHA256="67be9198476cc37436e2801de649f4ad80bf0d02430d86aff63c6b59b6e23987"
+PKG_VERSION="2.46"
+PKG_SHA256="a389850c2d3919f2cc96fb8b5e7711eacfc819259aaffb11615c9fb9756eaeae"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/binutils/"
 PKG_URL="https://ftp.gnu.org/gnu/binutils/binutils-with-gold-${PKG_VERSION}.tar.xz"
