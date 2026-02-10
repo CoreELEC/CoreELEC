7064c1c39de1a35127bbf83ef919fe11c6ef92ef
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-10 10:31:07 +0000

    libpng: update to 1.6.55

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libpng"
-PKG_VERSION="1.6.54"
-PKG_SHA256="01c9d8a303c941ec2c511c14312a3b1d36cedb41e2f5168ccdaa85d53b887805"
+PKG_VERSION="1.6.55"
+PKG_SHA256="d925722864837ad5ae2a82070d4b2e0603dc72af44bd457c3962298258b8e82d"
 PKG_LICENSE="LibPNG2"
 PKG_SITE="http://www.libpng.org/"
 PKG_URL="${SOURCEFORGE_SRC}/libpng/${PKG_NAME}-${PKG_VERSION}.tar.xz"
