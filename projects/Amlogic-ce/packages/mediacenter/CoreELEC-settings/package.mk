5a7d97aee917165ad5b6e4cb6e0c5385a70096ca
Peter <peter.vicman@gmail.com>
2026-02-21 15:32:17 +0100

    CoreELEC-settings: bump package to 4d4539a7
    system: use info level for timezone settings

--- projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="CoreELEC-settings"
-PKG_VERSION="33a40f356ecaec7d4cfd0e9f8e7791114eea65eb"
-PKG_SHA256="906542ded836e912098b71548c1c842b7175f4168376820ddfcaa80634c89bd8"
+PKG_VERSION="4d4539a7301099a80438b0129c04a384b261ac66"
+PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/service.coreelec.settings/archive/${PKG_VERSION}.tar.gz"
