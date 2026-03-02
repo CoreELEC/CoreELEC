228afb97ed6d1a8a6e3974917ee41297a11a53b5
Deé Gábor <dee.gabor@gmail.com>
2026-03-02 19:05:56 +0100

    aml-vnc: bump package to 1.4.0_beta6

--- projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
+++ projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
@@ -2,25 +2,21 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="aml-vnc"
-PKG_VERSION="1.2.1"
-PKG_SHA256="751231c4e8c295a71e750b05e5f371395add0cb1853e7f8fcd41ef1067abc4ae"
-PKG_REV="0"
+PKG_VERSION="1.4.0_beta6"
+PKG_SHA256="ac6a971af5cc13a298e95b0b9dd7e2fddd9a66f2d6af06212a024dc41bcb9f46"
+PKG_REV="0~beta-6"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/dtechsrv/aml-vnc-server/"
 PKG_URL="https://github.com/dtechsrv/aml-vnc-server/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libvncserver"
+PKG_DEPENDS_TARGET="toolchain libvncserver libdrm"
 PKG_SECTION="service"
 PKG_SHORTDESC="Amlogic VNC server"
 PKG_LONGDESC="Amlogic VNC server is a Virtual Network Computing (VNC) server for Amlogic devices"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="Amlogic VNC"
+PKG_ADDON_NAME="Amlogic VNC Server"
 PKG_ADDON_TYPE="xbmc.service"
-
-pre_configure_target() {
-  export CFLAGS+=" -Wno-incompatible-pointer-types"
-}
 
 makeinstall_target() {
   :
