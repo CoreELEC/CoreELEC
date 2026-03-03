336aef8afc4447a139ac0b45c9d7cb2ac0e1f495
Peter <peter.vicman@gmail.com>
2026-03-03 16:54:48 +0100

    pygobject: bump package to 3.56.0

--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pygobject"
-PKG_VERSION="3.55.1"
-PKG_SHA256="1558d013f7d4c4f9dc47a7245b1dc6a8e91ea1d43ce67959aef4f7331a586eff"
+PKG_VERSION="3.56.0"
+PKG_SHA256="4fbb5bf47524e01026f8e309dd54233eb0f75f2281392c5bf0df5d9041cc7891"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.pygtk.org/"
