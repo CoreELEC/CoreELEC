8e5ff8197266e900d329e4493b077968c2afd4b8
Peter <peter.vicman@gmail.com>
2026-04-04 22:19:22 +0200

    qt6: simplify package

--- packages/addons/addon-depends/qt6/package.mk
+++ packages/addons/addon-depends/qt6/package.mk
@@ -3,187 +3,98 @@
 
 PKG_NAME="qt6"
 PKG_VERSION="6.11.0"
-PKG_SHA256=""
+PKG_SHA256="acf3b3db04c9e5d0820e8324b097320388954c297cee83d2bd698789234f68a4"
 PKG_LICENSE="GPL"
 PKG_SITE="https://qt-project.org"
 PKG_URL="https://download.qt.io/official_releases/qt/${PKG_VERSION%.*}/${PKG_VERSION}/single/qt-everywhere-src-${PKG_VERSION}.tar.xz"
 PKG_SOURCE_NAME="qt-everywhere-src-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_HOST="toolchain"
 PKG_DEPENDS_TARGET="toolchain freetype libjpeg-turbo libpng openssl sqlite zlib qt6:host"
 PKG_LONGDESC="A cross-platform application and UI framework."
 PKG_BUILD_FLAGS="-sysroot"
 PKG_TOOLCHAIN="manual"
 
-# hide warnings
-export LANG=C.UTF-8 LC_ALL=C.UTF-8
-
-#######################################################################################################
 unpack() {
   mkdir -p ${PKG_BUILD}
 
   # we don't use big qtwebengine, save space and unpack time
   tar --strip-components=1 \
-    --exclude='qtwebengine' --exclude='qt3d' --exclude='qtdatavis3d' \
-    --exclude='qtquick3d' --exclude='qtquick3dphysics' \
-    --exclude='qtcharts' --exclude='qtcoap' \
-    --exclude='qthttpserver' --exclude='qtmqtt' \
-    --exclude='qtserialbus' --exclude='qtspeech' \
-    --exclude='qtwayland' --exclude='qtvirtualkeyboard' \
+    --exclude='qtwebengine'      --exclude='qt3d' \
+    --exclude='qtdatavis3d'      --exclude='qtquick3d' \
+    --exclude='qtquick3dphysics' --exclude='qtcharts' \
+    --exclude='qtcoap'           --exclude='qthttpserver' \
+    --exclude='qtmqtt'           --exclude='qtserialbus'  \
+    --exclude='qtspeech'         --exclude='qtwayland' \
+    --exclude='qtvirtualkeyboard' \
     -xf ${SOURCES}/${PKG_NAME}/${PKG_SOURCE_NAME} -C ${PKG_BUILD}
+
+  # fix pcre2 detection
+  # https://github.com/ericLemanissier/cocorepo/commit/21abf03212100c1da7c17358b91732b624d4acc7
+  sed -e 's|set(__pcre2_target_name "PCRE2::16BIT")|set(__pcre2_target_name "PCRE2::pcre2-16")|' \
+      -i ${PKG_BUILD}/qtbase/cmake/FindWrapSystemPCRE2.cmake
 }
 
-#######################################################################################################
 configure_host() {
-  [ "${SKIP_QT_HOST_BUILD}" = "yes" ] && return
+  export LANG=C.UTF-8 LC_ALL=C.UTF-8  # hide warnings
 
   QT_OPTS_HOST="
+    -DBUILD_SHARED_LIBS=OFF
+    -DFEATURE_optimize_size=ON
+    -DFEATURE_reduce_exports=ON
     -DQT_BUILD_EXAMPLES=OFF
     -DQT_BUILD_TESTS=OFF
     -DQT_BUILD_DOCS=OFF
     -DQT_BUILD_BENCHMARKS=OFF
-    -DBUILD_SHARED_LIBS=OFF
-    -DFEATURE_optimize_size=ON
-    -DFEATURE_precompile_header=ON
-    -DFEATURE_reduce_exports=ON
-
-    -DFEATURE_printsupport=OFF
-    -DFEATURE_permissions=OFF
-    -DFEATURE_sqlmodel=OFF
-    -DFEATURE_androiddeployqt=OFF
-    -DBUILD_qtwayland=OFF
-    -DQT_BUILD_QTWAYLAND=OFF
-    -DFEATURE_emoji_segmenter=OFF
-    -DINPUT_emoji_segmenter=no
-    -DFEATURE_sessionmanager=OFF
-    -DFEATURE_itemmodeltester=OFF
-
-    -DFEATURE_xcb=OFF
-    -DFEATURE_wayland=OFF
-    -DFEATURE_eglfs=OFF
-    -DFEATURE_linuxfb=OFF
-    -DFEATURE_vnc=OFF
-    -DFEATURE_minimal=ON
-    -DFEATURE_offscreen=ON
-
-    -DFEATURE_concurrent=ON
-    -DFEATURE_dbus=ON
-    -DFEATURE_gui=ON
-    -DFEATURE_network=ON
-    -DFEATURE_sql=ON
-    -DFEATURE_testlib=ON
-    -DFEATURE_widgets=ON
-    -DFEATURE_xml=ON
-
-    -DFEATURE_doubleconversion=ON
-    -DFEATURE_system_doubleconversion=OFF
-    -DFEATURE_glib=OFF
-    -DFEATURE_iconv=OFF
-    -DFEATURE_icu=OFF
-    -DFEATURE_mimetype_database=ON
-    -DFEATURE_pcre2=ON
-    -DFEATURE_system_pcre2=OFF
-
-    -DFEATURE_openssl=ON
-    -DFEATURE_openssl_linked=ON
-    -DFEATURE_dtls=ON
-    -DFEATURE_ocsp=ON
-    -DFEATURE_sctp=OFF
-    -DFEATURE_gssapi=OFF
-    -DFEATURE_libproxy=OFF
-
-    -DFEATURE_openssl_hash=OFF
-
-    -DFEATURE_gif=ON
-    -DFEATURE_ico=OFF
-    -DFEATURE_jpeg=ON
-    -DFEATURE_system_jpeg=ON
-    -DFEATURE_png=ON
-    -DFEATURE_system_png=ON
-    -DFEATURE_freetype=OFF
-    -DFEATURE_system_freetype=OFF
-    -DFEATURE_harfbuzz=OFF
-    -DFEATURE_fontconfig=OFF
-    -DFEATURE_accessibility=OFF
-
-    -DFEATURE_texthtmlparser=ON
-    -DFEATURE_cssparser=ON
-    -DFEATURE_textodfwriter=ON
-    -DFEATURE_textmarkdownreader=ON
-    -DFEATURE_system_libmd4c=OFF
-    -DFEATURE_textmarkdownwriter=ON
-
-    -DFEATURE_opengl=OFF
-    -DFEATURE_opengles2=OFF
-    -DFEATURE_opengles3=OFF
-    -DFEATURE_vulkan=OFF
-    -DFEATURE_egl=OFF
+    -DQT_BUILD_SUBMODULES='qtbase;qtshadertools;qtdeclarative'
     -DINPUT_opengl=no
-
-    -DFEATURE_directfb=OFF
-    -DFEATURE_eglfs=OFF
-    -DFEATURE_linuxfb=OFF
-    -DFEATURE_vnc=OFF
-
-    -DFEATURE_evdev=OFF
-    -DFEATURE_libinput=OFF
-    -DFEATURE_mtdev=OFF
-    -DFEATURE_tslib=OFF
-    -DFEATURE_xkbcommon=OFF
-
-    -DFEATURE_gtk3=OFF
-
-    -DFEATURE_sql_sqlite=OFF
-    -DFEATURE_system_sqlite=OFF
-    -DFEATURE_sql_mysql=OFF
-    -DFEATURE_sql_psql=OFF
-    -DFEATURE_sql_odbc=OFF
-    -DFEATURE_sql_db2=OFF
-    -DFEATURE_sql_ibase=OFF
-    -DFEATURE_sql_oci=OFF
-
-    -DFEATURE_neon=OFF
-
-    -DFEATURE_system_zlib=ON
-    -DFEATURE_zstd=OFF
   "
 
-  cmake --fresh .. -G Ninja \
+  cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON \
     -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_C_COMPILER_LAUNCHER=${TOOLCHAIN}/bin/ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=${TOOLCHAIN}/bin/ccache \
-    -DFEATURE_ccache=ON \
+    -DCMAKE_C_COMPILER_LAUNCHER="${TOOLCHAIN}/bin/ccache" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="${TOOLCHAIN}/bin/ccache" \
     -DQT_USE_CCACHE=ON \
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,--start-group -Wl,--whole-archive -L${TOOLCHAIN}/lib -lffi -lpcre2-16 -lpcre2-8 -lglib-2.0 -lgmodule-2.0 -lgobject-2.0 -Wl,--no-whole-archive -Wl,--end-group" \
     ${QT_OPTS_HOST}
 
-  echo "configure host ok"
+  # CMAKE_EXE_LINKER_FLAGS is used to add extra libraries because
+  # ffi and pcre2 are not found (wrong order by default, bug in qt)
 }
 
 make_host() {
-  [ "${SKIP_QT_HOST_BUILD}" = "yes" ] && return
   cmake --build . --target host_tools --parallel
-  echo "make host ok"
 }
 
 makeinstall_host() {
-  : # no need to install
+  : # no need
 }
 
-#######################################################################################################
 configure_target() {
+  export LANG=C.UTF-8 LC_ALL=C.UTF-8  # hide warnings
+
   TOOLCHAIN_FILE="${PKG_REAL_BUILD}/toolchain-libreelec.cmake"
 
   cat >${TOOLCHAIN_FILE} <<EOF
-set(CMAKE_SYSTEM_NAME      Linux)
-set(CMAKE_SYSTEM_PROCESSOR ${TARGET_ARCH})
-set(CMAKE_C_COMPILER       "${CC}")
-set(CMAKE_CXX_COMPILER     "${CXX}")
+set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_PROCESSOR  ${TARGET_ARCH})
+set(CMAKE_SYSROOT           "${SYSROOT_PREFIX}")
+set(CMAKE_C_COMPILER        "${CC}")
+set(CMAKE_CXX_COMPILER      "${CXX}")
+set(CMAKE_C_COMPILER_LAUNCHER   "${TOOLCHAIN}/bin/ccache")
+set(CMAKE_CXX_COMPILER_LAUNCHER "${TOOLCHAIN}/bin/ccache")
+set(QT_USE_CCACHE ON)
 
-set(CMAKE_SYSROOT          "${SYSROOT_PREFIX}")
+set(CMAKE_FIND_ROOT_PATH "${SYSROOT_PREFIX}" "${TOOLCHAIN}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CMAKE_INSTALL_PREFIX    "/usr")
+set(CMAKE_STAGING_PREFIX    "${SYSROOT_PREFIX}/usr")
+set(QT_QMAKE_TARGET_MKSPEC  "devices/linux-libreelec-g++")
 EOF
 
   QMAKE_CONF_DIR="${PKG_BUILD}/qtbase/mkspecs/devices/linux-libreelec-g++"
@@ -216,149 +127,35 @@ EOF
 EOF
 
   QT_OPTS_TARGET="
+    -DBUILD_SHARED_LIBS=OFF
+    -DFEATURE_optimize_size=ON
+    -DFEATURE_reduce_exports=ON
     -DQT_BUILD_EXAMPLES=OFF
     -DQT_BUILD_TESTS=OFF
     -DQT_BUILD_DOCS=OFF
     -DQT_BUILD_BENCHMARKS=OFF
-    -DBUILD_SHARED_LIBS=OFF
-    -DFEATURE_optimize_size=ON
-    -DFEATURE_precompile_header=ON
-    -DFEATURE_reduce_exports=ON
-
-    -DFEATURE_printsupport=OFF
-    -DFEATURE_permissions=OFF
-    -DFEATURE_sqlmodel=OFF
-    -DFEATURE_androiddeployqt=OFF
-    -DBUILD_qtwayland=OFF
-    -DQT_BUILD_QTWAYLAND=OFF
-    -DFEATURE_emoji_segmenter=OFF
-    -DINPUT_emoji_segmenter=no
-    -DFEATURE_sessionmanager=OFF
-    -DFEATURE_itemmodeltester=OFF
-    
-    -DFEATURE_libudev=OFF
-
-    -DFEATURE_xcb=OFF
-    -DFEATURE_wayland=OFF
-    -DFEATURE_eglfs=OFF
-    -DFEATURE_linuxfb=OFF
-    -DFEATURE_vnc=OFF
-    -DFEATURE_minimal=ON
-    -DFEATURE_offscreen=ON
-
-    -DFEATURE_alsa=OFF \
-    -DFEATURE_pulseaudio=OFF \
-    -DFEATURE_coreaudio=OFF \
-    -DFEATURE_wmsdk=OFF \
-    -DFEATURE_mmrenderer=OFF
-
-    -DFEATURE_concurrent=ON
-    -DFEATURE_dbus=ON
+    -DQT_BUILD_SUBMODULES='qtbase;qtserialport;qtwebsockets'
     -DFEATURE_gui=ON
-    -DFEATURE_network=ON
-    -DFEATURE_sql=ON
-    -DFEATURE_testlib=ON
-    -DFEATURE_widgets=ON
-    -DFEATURE_xml=ON
-
-    -DFEATURE_doubleconversion=ON
-    -DFEATURE_system_doubleconversion=OFF
-    -DFEATURE_glib=OFF
-    -DFEATURE_iconv=OFF
-    -DFEATURE_icu=OFF
-    -DFEATURE_mimetype_database=ON
-    -DFEATURE_pcre2=ON
-    -DFEATURE_system_pcre2=OFF
-
-    -DFEATURE_openssl=ON
-    -DFEATURE_openssl_linked=ON
-    -DFEATURE_dtls=ON
-    -DFEATURE_ocsp=ON
-    -DFEATURE_sctp=OFF
-    -DFEATURE_gssapi=OFF
-    -DFEATURE_libproxy=OFF
-
-    -DFEATURE_openssl_hash=OFF
-
-    -DFEATURE_gif=ON
-    -DFEATURE_ico=OFF
-    -DFEATURE_jpeg=ON
-    -DFEATURE_system_jpeg=ON
-    -DFEATURE_png=ON
-    -DFEATURE_system_png=ON
-    -DFEATURE_freetype=OFF
-    -DFEATURE_system_freetype=OFF
-    -DFEATURE_harfbuzz=OFF
-    -DFEATURE_fontconfig=OFF
-    -DFEATURE_accessibility=OFF
-
-    -DFEATURE_texthtmlparser=ON
-    -DFEATURE_cssparser=ON
-    -DFEATURE_textodfwriter=ON
-    -DFEATURE_textmarkdownreader=ON
-    -DFEATURE_system_libmd4c=OFF
-    -DFEATURE_textmarkdownwriter=ON
-
-    -DFEATURE_opengl=OFF
-    -DFEATURE_opengles2=OFF
-    -DFEATURE_opengles3=OFF
-    -DFEATURE_vulkan=OFF
-    -DFEATURE_egl=OFF
-    -DINPUT_opengl=no
-
-    -DFEATURE_directfb=OFF
-    -DFEATURE_eglfs=OFF
-    -DFEATURE_linuxfb=OFF
-    -DFEATURE_vnc=OFF
-
-    -DFEATURE_evdev=OFF
-    -DFEATURE_libinput=OFF
-    -DFEATURE_mtdev=OFF
-    -DFEATURE_tslib=OFF
-    -DFEATURE_xkbcommon=OFF
-
-    -DFEATURE_gtk3=OFF
-
-    -DFEATURE_sql_sqlite=OFF
-    -DFEATURE_system_sqlite=OFF
-    -DFEATURE_sql_mysql=OFF
-    -DFEATURE_sql_psql=OFF
-    -DFEATURE_sql_odbc=OFF
-    -DFEATURE_sql_db2=OFF
-    -DFEATURE_sql_ibase=OFF
-    -DFEATURE_sql_oci=OFF
-
-    -DFEATURE_neon=ON
-
-    -DFEATURE_system_zlib=ON
-    -DFEATURE_zstd=OFF
+    -DFEATURE_libudev=OFF
+    -DFEATURE_qml_debug=OFF
   "
 
   unset CC CXX LD AR AS RANLIB NM STRIP OBJCOPY CFLAGS CXXFLAGS LDFLAGS CPPFLAGS
 
-  cmake --fresh .. -G Ninja \
+  cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_STAGING_PREFIX="${SYSROOT_PREFIX}/usr" \
-    -DQT_QMAKE_TARGET_MKSPEC=devices/linux-libreelec-g++ \
     -DQT_HOST_PATH="${PKG_BUILD}/.${HOST_NAME}/qtbase/lib/cmake" \
     -DQT_HOST_PATH_CMAKE_DIR="${PKG_BUILD}/.${HOST_NAME}/qtbase/lib/cmake" \
-    -DCMAKE_C_COMPILER_LAUNCHER=${TOOLCHAIN}/bin/ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=${TOOLCHAIN}/bin/ccache \
-    -DFEATURE_ccache=ON \
-    -DQT_USE_CCACHE=ON \
     ${QT_OPTS_TARGET}
 }
 
 make_target() {
   cmake --build . --parallel
-  echo "make target ok"
 }
 
 makeinstall_target() {
-  echo "makeinstall_target"
   cmake --install .
 }
