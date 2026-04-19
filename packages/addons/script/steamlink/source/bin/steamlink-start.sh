de6d1112928175048215a0b6d4c84f2c6b339bbe
Ian Leonard <antonlacon@gmail.com>
2026-04-19 18:27:29 -0400

    steamlink/start-steamlink.sh: refactor
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/addons/script/steamlink/source/bin/steamlink-start.sh
+++ packages/addons/script/steamlink/source/bin/steamlink-start.sh
@@ -3,32 +3,58 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
+set -e
+
 . /etc/profile
 oe_setup_addon script.program.steamlink
 
+UDEV_DIR="/lib/udev/rules.d"
+UPPER="${ADDON_DIR}/steamlink/udev/rules.d"
+WORK="${ADDON_DIR}/steamlink/.overlay"
+
 # Steamlink not ready; abort
-if [ ! -f ${ADDON_DIR}/prep.ok ]; then
+if [ ! -f "${ADDON_DIR}/prep.ok" ]; then
   exit 0
 fi
 
-# Adapt to meet steamlink requirements
-
-# mount an overlay for udev rules and reload udev rules
-if [ $(grep -c "/lib/udev/rules.d" /proc/mounts) -eq 0 ]; then
-  if [ ! -d ${ADDON_DIR}/steamlink/.overlay ]; then
-    mkdir -p ${ADDON_DIR}/steamlink/.overlay
+# Cleanup routine on exit
+cleanup() {
+  # Unmount udev overlay
+  if mountpoint -q "${UDEV_DIR}"; then
+    umount "${UDEV_DIR}"
+    udevadm control --reload-rules
+    udevadm trigger
   fi
-  mount -t overlay overlay -o lowerdir=/lib/udev/rules.d,upperdir=${ADDON_DIR}/steamlink/udev/rules.d/,workdir=${ADDON_DIR}/steamlink/.overlay /lib/udev/rules.d
+
+  # Start Kodi if not running
+  if ! systemctl is-active --quiet kodi; then
+    systemctl start kodi
+  fi
+}
+trap cleanup EXIT INT TERM
+
+# Create overlay directory
+if [ ! -d "${WORK}" ]; then
+  mkdir -p "${WORK}" || {
+    echo "Error: Failed to create directory: ${WORK}" >&2
+    exit 1
+  }
+fi
+
+# Mount udev rules overlay and reload rules
+if ! mountpoint -q "${UDEV_DIR}"; then
+  mount -t overlay overlay \
+    -o "lowerdir=${UDEV_DIR},upperdir=${UPPER},workdir=${WORK}" \
+    "${UDEV_DIR}"
+  udevadm control --reload-rules
   udevadm trigger
 fi
 
-# Launch steamlink
-# controller input goes to kodi if it is running
-systemctl stop kodi
-${ADDON_DIR}/steamlink/steamlink.sh
-# Cleanup
-umount /lib/udev/rules.d
-udevadm trigger
-systemctl start kodi
+# Stop Kodi so controller input only goes to Steam Link
+systemctl stop kodi || echo "Warning: Kodi did not stop cleanly"
 
-exit 0
+# Launch Steam Link
+"${ADDON_DIR}/steamlink/steamlink.sh"
+STATUS=$?
+
+exit $STATUS
