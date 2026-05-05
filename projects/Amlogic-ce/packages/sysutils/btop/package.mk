2e2636acf78ad7cff35ef0f4fa5e3055eb0b4cdb
Peter <peter.vicman@gmail.com>
2026-05-05 08:47:29 +0200

    btop: bump package to 1.4.7

--- projects/Amlogic-ce/packages/sysutils/btop/package.mk
+++ projects/Amlogic-ce/packages/sysutils/btop/package.mk
@@ -2,14 +2,17 @@
 # Copyright (C) 2025-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="btop"
-PKG_VERSION="1.4.0"
-PKG_SHA256="ac0d2371bf69d5136de7e9470c6fb286cbee2e16b4c7a6d2cd48a14796e86650"
+PKG_VERSION="1.4.7"
+PKG_SHA256="933de2e4d1b2211a638be463eb6e8616891bfba73aef5d38060bd8319baeefc6"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/aristocratos/btop"
 PKG_URL="https://github.com/aristocratos/btop/archive/refs/tags/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="btop resource monitor"
-PKG_TOOLCHAIN="auto"
+PKG_TOOLCHAIN="cmake"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release
+                       -DBUILD_TESTING=OFF"
 
 post_makeinstall_target() {
   cat >${INSTALL}/usr/share/btop/btop.conf <<EOF
