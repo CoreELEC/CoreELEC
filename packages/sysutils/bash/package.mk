7c779d32b054450f16f4265afa90af7beea27e2f
Peter <peter.vicman@gmail.com>
2026-05-05 08:50:08 +0200

    bash: bump package to 5.3

--- packages/sysutils/bash/package.mk
+++ packages/sysutils/bash/package.mk
@@ -2,17 +2,13 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="bash"
-PKG_VERSION="5.2.37"
-PKG_SHA256="9599b22ecd1d5787ad7d3b7bf0c59f312b3396d1e281175dd1f8a4014da621ff"
+PKG_VERSION="5.3"
+PKG_SHA256="0d5cd86965f869a26cf64f4b71be7b96f90a3ba8b3d74e27e8e9d9d5550f31ba"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/software/bash/"
 PKG_URL="https://ftp.gnu.org/gnu/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses readline"
 PKG_LONGDESC="Bash is the GNU Project shell - the Bourne Again SHell."
-
-pre_configure_target() {
-  CFLAGS+=" -std=gnu17"
-}
 
 PKG_CONFIGURE_OPTS_TARGET="--with-curses \
                            --without-bash-malloc \
