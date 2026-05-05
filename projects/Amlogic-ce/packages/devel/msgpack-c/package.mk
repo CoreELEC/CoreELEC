9eaf3ced5db6e8857c301507b07ab39c34910478
Peter <peter.vicman@gmail.com>
2026-05-05 11:30:39 +0200

    msgpack-c: bump package to 021da5d4

--- projects/Amlogic-ce/packages/devel/msgpack-c/package.mk
+++ projects/Amlogic-ce/packages/devel/msgpack-c/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="msgpack-c"
-PKG_VERSION="c3df1bb26ebdd01d618ecca7ae2d6b4e37d5abd7"
-PKG_SHA256="581442dd9b94de53b75eb3de3cc0bb96c52e6690125223ecf4fff407772d3949"
+PKG_VERSION="021da5d4b590a8bdf3d720d57b02196566753264"
+PKG_SHA256=""
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/msgpack/msgpack-c"
 PKG_URL="https://github.com/msgpack/msgpack-c/archive/${PKG_VERSION}.tar.gz"
@@ -12,5 +12,3 @@ PKG_LONGDESC="Its like JSON but fast and small."
 
 PKG_CMAKE_OPTS_TARGET="-DMSGPACK_ENABLE_SHARED=OFF \
                        -DMSGPACK_ENABLE_STATIC=ON"
-
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
