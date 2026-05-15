6bb9e789d5e12062a9c0909df68504e0f9200044
Portisch <hugo.portisch@yahoo.de>
2026-05-15 10:06:34 +0200

    common_drivers: bump package to a8878a35

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="c3d3d34ac674d2249d0a972a5e7f298cd8de00c0"
+PKG_VERSION="a8878a3527273385d082d7f5e21605cbd64eb619"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
