0423e4a66f76b51c428f2a70a71342491b1ad43f
Pro-me3us <promoe3us@gmail.com>
2026-05-25 05:21:30 -0400

    common_drivers: bump package to 4e69fc29 & enable gpio_key module
    Enable CONFIG_AMLOGIC_GPIO_KEY=m, and bump common_drivers to add gpio_keypad support.

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="803c1fb29bd1879b95532295e75799585084a731"
+PKG_VERSION="4e69fc291e524e61ed8d917a108c938c827f470b"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
