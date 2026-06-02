35856ec02f8f16d521f343d1568de4d07ec663ad
Portisch <hugo.portisch@yahoo.de>
2026-06-02 16:06:09 +0200

    CoreELEC-settings: bump package to 578fba61

--- projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/CoreELEC-settings/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="CoreELEC-settings"
-PKG_VERSION="671a98580fcc5f048a9490c18a39135b0bff1675"
+PKG_VERSION="578fba610f35376286137908edd8559de84ea538"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://coreelec.org"
