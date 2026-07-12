c12edb4b345ab8f50842f3ef7a7636c74946e7a1
Deé Gábor <dee.gabor@gmail.com>
2026-07-12 12:05:27 +0200

    aml-vnc: bump package to 1.4.0

--- projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
+++ projects/Amlogic-ce/packages/addons/service/aml-vnc/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="aml-vnc"
-PKG_VERSION="1.2.0"
-PKG_SHA256="38009d0ea84868c7e077eb7594f2808bf5acdae3b7e691cb73fb800e9489d064"
-PKG_REV="7"
+PKG_VERSION="1.4.0"
+PKG_SHA256="354ca6cd9bb6910ce20548ab057687f04251ab801b67469102f44cd77f92266d"
+PKG_REV="8"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/dtechsrv/aml-vnc-server/"
@@ -15,19 +15,17 @@ PKG_SHORTDESC="Amlogic VNC server"
 PKG_LONGDESC="Amlogic VNC server is a Virtual Network Computing (VNC) server for Amlogic devices"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="Amlogic VNC"
+PKG_ADDON_NAME="Amlogic VNC Server"
 PKG_ADDON_TYPE="xbmc.service"
-
-pre_configure_target() {
-  export CFLAGS+=" -Wno-stringop-truncation"
-}
 
 makeinstall_target() {
   :
 }
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib}
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
   cp -P ${PKG_BUILD}/aml-vnc ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
   cp $(get_build_dir libvncserver)/.${TARGET_NAME}/libvncserver.so.? ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
 }
