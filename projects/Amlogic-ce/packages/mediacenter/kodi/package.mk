6f8d6388a0be861fea89cc302530aa1895be1238
Portisch <hugo.portisch@yahoo.de>
2026-07-17 09:25:42 +0200

    kodi: bump package to ce2caeff

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="46c546bec3e8fda26a6a3ba348b22aca3681a4b9"
+PKG_VERSION="ce2caeffcdcddb940e2e633637ad08cfc225ef08"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
