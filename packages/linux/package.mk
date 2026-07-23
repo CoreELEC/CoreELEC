59f6d08f7298d019558007101619e4d0212e408e
Matthias Reichl <hias@horus.com>
2026-07-23 17:16:46 +0200

    linux (RPi): update to 6.18.39-820b5b6
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default dvb"
     ;;
   raspberrypi)
-    PKG_VERSION="33cc967e07b0a5439fdcfdf942c904e00f4e794e" # 6.18.38
-    PKG_SHA256="68671cbee99b1271645ff1cecd89c31248def67adb04cbecf0c2ebccabe9aed9"
+    PKG_VERSION="820b5b663c24c6168d8aae4469ab490158a4b709" # 6.18.39
+    PKG_SHA256="95999c1b1240ace11454ce495cf1ec21cf69c9f622ffb0047124b9ddfaf71c63"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi"
