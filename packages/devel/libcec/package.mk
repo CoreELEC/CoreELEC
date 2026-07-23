1e8d2c8d29423b23668c2ad5ca1f119ddfbe1afb
Matthias Reichl <hias@horus.com>
2026-07-23 11:02:03 +0200

    libcec: update to 8.1.1
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libcec"
-PKG_VERSION="62d077c414f415f29a0ea2886ff7c2f79af90f33"
-PKG_SHA256="48c48c38c0aae194a98259bad608f05fb4d857603a95aab3d32da3650e1bcf28"
+PKG_VERSION="8.1.1"
+PKG_SHA256="0247a2e577cefceaa1932d9a2aec2b423e0ce9c77f90c2b8a99197fdd1a0b81d"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="http://libcec.pulse-eight.com/"
-PKG_URL="https://github.com/Pulse-Eight/libcec/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain systemd p8-platform swig:host"
+PKG_URL="https://github.com/Pulse-Eight/libcec/archive/libcec-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain systemd swig:host"
 PKG_LONGDESC="libCEC is an open-source dual licensed library designed for communicating with the Pulse-Eight USB - CEC Adaptor."
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
