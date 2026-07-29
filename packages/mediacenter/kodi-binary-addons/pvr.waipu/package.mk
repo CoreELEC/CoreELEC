379fe2b5e237b560d1c2a5ed1053a7dbad1bdd93
Rudi Heitbaum <rudi@heitbaum.com>
2026-07-29 14:18:33 +0000

    kodi-binary-addons: update to latest versions
    - pvr.waipu: update 22.9.4-Piers to 22.9.5-Piers
    - vfs.rar: update 22.0.3-Piers to 22.0.4-Piers

--- packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.waipu"
-PKG_VERSION="22.9.4-Piers"
-PKG_SHA256="80c5ec2451c45148eaa579f1cc1cc385f76374ddf035c2160aecf43e465553fd"
+PKG_VERSION="22.9.5-Piers"
+PKG_SHA256="d9e7ae09515f1b18caea8b55936ee61af7161dea395956f1d16064fd475b950e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL-2.0-or-later"
--- packages/mediacenter/kodi-binary-addons/vfs.rar/package.mk
+++ packages/mediacenter/kodi-binary-addons/vfs.rar/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vfs.rar"
-PKG_VERSION="22.0.3-Piers"
-PKG_SHA256="9a5144b0ef40cb1c50f4b896e4b4be9f239a350e9fc3bd76d154bea90b97d49b"
-PKG_REV="2"
+PKG_VERSION="22.0.4-Piers"
+PKG_SHA256="dedd32170fae3d9c06e3c1bf2cf4dac6661f550612d9da801220b352b50d82f4"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/xbmc/vfs.rar"
