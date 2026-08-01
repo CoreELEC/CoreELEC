840661d311a2a8cd3bcbc0f88f834aa449a04dce
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-01 13:52:37 +0000

    moby: update to 29.7.1

--- packages/addons/addon-depends/docker/moby/package.mk
+++ packages/addons/addon-depends/docker/moby/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="moby"
-PKG_VERSION="29.7.0"
-PKG_SHA256="0970adb5445056dc763303fea6b38d8f86d06acbabbf6bb1d3bc55c12b57d38e"
+PKG_VERSION="29.7.1"
+PKG_SHA256="e852479cc37cbf151126be1c077bef5e7ceea6c854dd860d46e61004bdf70b76"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://mobyproject.org/"
 PKG_URL="https://github.com/moby/moby/archive/docker-v${PKG_VERSION}.tar.gz"
@@ -13,7 +13,7 @@ PKG_TOOLCHAIN="manual"
 PKG_NO_REFRESH_PATCHES="tools/moby/gen-patches.sh"
 
 # Git commit of the matching release https://github.com/moby/moby
-export PKG_GIT_COMMIT="4b5cb715735a1b7000093bb2ab54295dfedfebfe"
+export PKG_GIT_COMMIT="c5b8ce9274b5c00cb1f8287c8e258edc1f01176d"
 
 PKG_MOBY_BUILDTAGS="daemon \
                     autogen \
