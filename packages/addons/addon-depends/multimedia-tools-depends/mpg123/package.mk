987a64d7077abed4dde6c3682c580708a0d3537b
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-03 13:55:50 +0000

    mpg123: update to 1.33.7

--- packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mpg123"
-PKG_VERSION="1.33.6"
-PKG_SHA256="929a7c18ba662b8927aed4de229ad9ae8ab2b4806dd0f30b90113eb1b4e2195a"
+PKG_VERSION="1.33.7"
+PKG_SHA256="31d0e35a4ca567ec9b5ebda6c3062bb4435d6d3eacd6ef0d95cadd7854dc03ee"
 PKG_LICENSE="LGPL-2.1-only"
 PKG_SITE="https://www.mpg123.org/"
 PKG_URL="https://downloads.sourceforge.net/project/mpg123/mpg123/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
