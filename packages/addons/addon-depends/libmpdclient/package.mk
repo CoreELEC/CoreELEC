2aa9aa5dd3104d7de3bdce8542593cf1ccae3a09
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-09 12:45:41 +0000

    libmpdclient: update to 2.27

--- packages/addons/addon-depends/libmpdclient/package.mk
+++ packages/addons/addon-depends/libmpdclient/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmpdclient"
-PKG_VERSION="2.26"
-PKG_SHA256="35785c6d6318ade47e2c27b00fc6938f5e09d04714080fea3f5e5c522ba3e036"
+PKG_VERSION="2.27"
+PKG_SHA256="88945b5abc11d8f4cea2bb7028e545024a6e060650bd65527a29bc9400daead8"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://www.musicpd.org"
 PKG_URL="https://www.musicpd.org/download/libmpdclient/2/libmpdclient-${PKG_VERSION}.tar.xz"
