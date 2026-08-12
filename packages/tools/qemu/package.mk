8bdce8d0d893d9755c1cb69fed865e01c71eeaa4
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-12 11:43:31 +0000

    qemu: update to 11.1.0
    Changelog:
    - https://wiki.qemu.org/ChangeLog

--- packages/tools/qemu/package.mk
+++ packages/tools/qemu/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="qemu"
-PKG_VERSION="11.0.3"
-PKG_SHA256="da5fcffc32762820568b828ed430a728864d34d50b6d2f30358597760cbb0523"
+PKG_VERSION="11.1.0"
+PKG_SHA256="6ee1d1a61f68212476b27108c26da5f449dc09b626d42f8279ba0dc2e08fa858"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://www.qemu.org"
 PKG_URL="https://download.qemu.org/qemu-${PKG_VERSION}.tar.xz"
