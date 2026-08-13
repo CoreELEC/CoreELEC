3d51bbff01a91a3771ebcefbe0b075ce62630645
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-13 02:13:45 +0000

    zvbi: update to 0.2.45

--- packages/multimedia/zvbi/package.mk
+++ packages/multimedia/zvbi/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="zvbi"
-PKG_VERSION="0.2.44"
-PKG_SHA256="bca620ab670328ad732d161e4ce8d9d9fc832533cb7440e98c50e112b805ac5e"
+PKG_VERSION="0.2.45"
+PKG_SHA256="e6c954fde2a5a635187f19e1ab870a88c1a982012c5f1b33b8f2513e0ab7a50e"
 PKG_LICENSE="LGPL-2.0-or-later"
 PKG_SITE="https://github.com/zapping-vbi/zvbi"
 PKG_URL="https://github.com/zapping-vbi/zvbi/archive/refs/tags/v${PKG_VERSION}.tar.gz"
