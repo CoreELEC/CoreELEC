246153d7976b319d05cf1001da8545b29d5bee96
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-13 02:14:20 +0000

    procps-ng: update to 4.0.7

--- packages/tools/procps-ng/package.mk
+++ packages/tools/procps-ng/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="procps-ng"
-PKG_VERSION="4.0.6"
-PKG_SHA256="144410bd111330b191d4384ba6e8b4861390c644b7188e9487f779116b35a33c"
+PKG_VERSION="4.0.7"
+PKG_SHA256="707d4d43c78b1ff9d0286a4839465e78fa1a82896ec97d3508765b31a808b4b2"
 PKG_LICENSE="GPL-2.0-or-later AND LGPL-2.1-or-later"
 PKG_SITE="https://gitlab.com/procps-ng/procps"
 PKG_URL="https://gitlab.com/procps-ng/procps/-/archive/v${PKG_VERSION}/procps-v${PKG_VERSION}.tar.bz2"
