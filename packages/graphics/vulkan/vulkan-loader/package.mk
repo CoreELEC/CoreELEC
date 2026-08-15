0ab4501b516ca89d610c71b2e2aba26a9c7c35ea
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-15 04:53:30 +0000

    vulkan-loader: update to 1.4.360

--- packages/graphics/vulkan/vulkan-loader/package.mk
+++ packages/graphics/vulkan/vulkan-loader/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.4.359"
-PKG_SHA256="c40f7d49496f1561a9a7147da783cff34bfdc8d242dd4134e05bff59d1b7bf79"
+PKG_VERSION="1.4.360"
+PKG_SHA256="be04a1332e19a1497ccf36073957c3edc220fa141c3fd3d202594218225398b1"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
