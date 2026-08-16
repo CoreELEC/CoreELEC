376a44026806fe9599fc71f1d884c2ef13605237
Peter <peter.vicman@gmail.com>
2026-08-16 20:14:19 +0200

    Revert "libnfs: update to 7.0.0"
    This reverts commit 04cc46752b0f5ce393b5251a352cba38c6c12e6a.
    
    Until someone fix it to work properly with Kodi.

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnfs"
-PKG_VERSION="7.0.0"
-PKG_SHA256="d25c70537d60f1ab307b9cb5e9fb01acff71065fc9547b54dfaec109ba993003"
+PKG_VERSION="6.0.2"
+PKG_SHA256="4e5459cc3e0242447879004e9ad28286d4d27daa42cbdcde423248fad911e747"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="https://github.com/sahlberg/libnfs"
 PKG_URL="https://github.com/sahlberg/libnfs/archive/${PKG_NAME}-${PKG_VERSION}.tar.gz"
