f9f6f9eafdbef0b5ae695d1c2e0027838f9b94dd
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-18 13:56:07 +0000

    tmux: update to 3.7c

--- packages/addons/addon-depends/system-tools-depends/tmux/package.mk
+++ packages/addons/addon-depends/system-tools-depends/tmux/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tmux"
-PKG_VERSION="3.7b"
-PKG_SHA256="87f2e99e3b685973f2ca002ffd6ed7e51a5744f7009daae5a15670b6d532db96"
+PKG_VERSION="3.7c"
+PKG_SHA256="7c60cae9a0e25288e2e24750aafc9e8800fc7fd4555e447e1b29ee4201cfb3bf"
 PKG_LICENSE="ISC"
 PKG_SITE="https://github.com/tmux/tmux/wiki"
 PKG_URL="https://github.com/tmux/tmux/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
