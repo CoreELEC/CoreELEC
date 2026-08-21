0db05471323136270443a933ee3987f1934f33b7
Peter <peter.vicman@gmail.com>
2026-08-21 19:02:33 +0200

    common_drivers: bump package to 9c02323c
    add LEDs on Minix U8K-Ultra

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="32c211ab4c37dbe1171883ebfaaf4a977f2094e2"
+PKG_VERSION="9c02323ccd9901c48a7a6cc700eeb0f9352205fa"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
