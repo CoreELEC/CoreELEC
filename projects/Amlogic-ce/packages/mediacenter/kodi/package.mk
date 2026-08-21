3707bf22c70d8dbae270e08637d329da14f862eb
Portisch <hugo.portisch@yahoo.de>
2026-08-21 12:01:32 +0200

    kodi: bump package to e75e9e22

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="9239a6d04412d709e60eaf503f1198f3cd4d7ee1"
+PKG_VERSION="e75e9e220a23766e6376d7a72b2cc6ce6e602eb1"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
