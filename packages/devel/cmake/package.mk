3941eaa5a43d865a296a6f27d2d6b1240f5a8008
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-26 09:32:19 +0000

    cmake: update to 4.4.3
    Release notes:
    - https://cmake.org/cmake/help/latest/release/index.html

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="4.4.2"
-PKG_SHA256="1db9e61e60b6e0874c86386340b910382f3c5e75b9fbfb44d122063129a2789d"
+PKG_VERSION="4.4.3"
+PKG_SHA256="c46400618b4f1f2b43507f24fb22f3ae830c3416cf23b776e16e1d413aa892f0"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
