0d7813329e52717544d6db80eceedbe9917b513b
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-26 08:14:15 +0000

    ntfsprogs-plus: update to githash 3fe8904
    Log:
    - https://github.com/ntfsprogs-plus/ntfsprogs-plus/compare/53943daef955cc2a4a405c8e37c62478e4272fec...3fe890434082973aa98fd441d65ae84e5d6e5fc8
    - since 0c12ae9ebeedee4b3d8c2f14f198e070f99314d8 libntfs is a static convenience
      library by default - keep it shared so the tools do not each carry a copy

--- packages/sysutils/ntfsprogs-plus/package.mk
+++ packages/sysutils/ntfsprogs-plus/package.mk
@@ -2,14 +2,16 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ntfsprogs-plus"
-PKG_VERSION="53943daef955cc2a4a405c8e37c62478e4272fec"
-PKG_SHA256="44b35d37c74259bd88c94a0cc21fc8b0560bc938eb0f400a9bc74b8e317b17d0"
+PKG_VERSION="3fe890434082973aa98fd441d65ae84e5d6e5fc8"
+PKG_SHA256="05ef1472d8706d3633ce09bb3a10e691f72dc0a1d7d6c4b982ea40d9d296e898"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/ntfsprogs-plus/ntfsprogs-plus"
 PKG_URL="https://github.com/ntfsprogs-plus/ntfsprogs-plus/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libgcrypt"
 PKG_LONGDESC="NTFS filesystem userspace utilities"
 PKG_TOOLCHAIN="autotools"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-library"
 
 post_configure_target() {
   libtool_remove_rpath libtool
