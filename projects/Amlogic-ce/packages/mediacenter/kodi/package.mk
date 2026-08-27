b95c4e1a27912b8d3b6d398cc59a06246e779b53
Portisch <hugo.portisch@yahoo.de>
2026-08-27 18:00:37 +0200

    kodi: bump package to 0f9a78be

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="a24a671d66e961e5023a16cee0e10ce4a7f5dbff"
+PKG_VERSION="0f9a78be129c06a0bd0d083ba1fa6dcdb089261d"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
