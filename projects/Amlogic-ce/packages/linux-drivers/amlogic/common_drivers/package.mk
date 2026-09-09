9d457963070e004c99a9a59a9e6e33da7ba68413
Pro-me3us <promoe3us@gmail.com>
2026-09-09 17:20:08 -0400

    common_drivers: bump package to 168c5fcd
    Fixed typo in dtb Makefile for FireTV 3rd gen Cube dtb

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="225320b0c54f616739a6da4562b0d18e05ebe51b"
+PKG_VERSION="168c5fcd305942cd6b64edbc39267a13263331eb"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
