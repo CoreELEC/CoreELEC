3b43c7ece5e331d76fadb26489589fec0331606b
Garrett Brown <themagnificentmrb@gmail.com>
2026-09-10 16:01:13 -0700

    game.shader.presets: Update to version 22.1.4

--- packages/mediacenter/kodi-binary-addons/game.shader.presets/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.shader.presets/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.shader.presets"
-PKG_VERSION="22.1.1-Piers"
-PKG_SHA256="f3fb9359ddceec7be246dddd0accfd5a5f640ba7876127441a9873e2f4e2fbbf"
+PKG_VERSION="22.1.4-Piers"
+PKG_SHA256="2ed1f319ab7a369e929788c24312d0fa8c9570f75a5752196c318a71a34ec567"
 PKG_REV="4"
 PKG_ARCH="any"
 PKG_LICENSE="GPL-3.0-or-later"
