c20b78f2471dd757824e485a693174b191e4b807
Pro-me3us <promoe3us@gmail.com>
2026-09-10 15:44:47 -0400

    common_drivers: bump package to 08c25691
    FireTV 3rd gen Cube DTB update.  Added cpu freq overclock option,  add use_intf3_tuning to SDIO WIFI.

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="168c5fcd305942cd6b64edbc39267a13263331eb"
+PKG_VERSION="08c256910e0286396e6a410eb9c9b565ab6a4539"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
