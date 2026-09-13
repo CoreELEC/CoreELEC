d9c37aa11432bd670c66ba41e12a3e9f69feb784
Peter <peter.vicman@gmail.com>
2026-09-13 15:02:34 +0200

    common_drivers: bump package to 0a361009
    DT:S7: add s7_s905y5_4g

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="08c256910e0286396e6a410eb9c9b565ab6a4539"
+PKG_VERSION="0a361009a997a298c8797ef3b4b58a5ee965f221"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
