7f02ea36b9922663153a9233c57e502a95d1509f
Matthias Reichl <hias@horus.com>
2026-09-16 21:04:20 +0200

    RPi: update to firmware 6f0881c
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="3d301dd924bcd758a4c8cb19fe8531031f033f43"
-PKG_SHA256="f45ec6025af3fb6449f92eb521869fb4ba481a28d61d6ec55c345de036ac9e88"
+PKG_VERSION="6f0881cba8bea8ec24956a5718714730e9936ec5"
+PKG_SHA256="331cabd23b264cc84ca3b5a6bd6bbac5b5243db7d39c2c06fc23c57b79c4e28a"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="LicenseRef-nonfree"
 PKG_SITE="http://www.broadcom.com"
