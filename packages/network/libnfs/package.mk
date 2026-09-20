1fedeb2e3f87b737aa2c61a3020a7d5c66928062
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-20 12:10:08 +0000

    libnfs: update to 8.0.0

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnfs"
-PKG_VERSION="7.0.2"
-PKG_SHA256="188a8a5de2ad2099a2e97e42f34721bfc4c16c8d33808de0f7484f99d714a3df"
+PKG_VERSION="8.0.0"
+PKG_SHA256="bc91216e927a85142b5de611c7f711558e02119a7daad67620ea631634038350"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="https://github.com/sahlberg/libnfs"
 PKG_URL="https://github.com/sahlberg/libnfs/archive/libnfs-${PKG_VERSION}.tar.gz"
