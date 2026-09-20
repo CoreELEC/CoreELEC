1288a9973fa9bcd379e36f6c32d4a5a540c4f88e
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-20 21:47:52 +0000

    pyparsing: update to 3.3.3

--- packages/addons/addon-depends/flatpak-depends/pyparsing/package.mk
+++ packages/addons/addon-depends/flatpak-depends/pyparsing/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pyparsing"
-PKG_VERSION="3.3.2"
-PKG_SHA256="c777f4d763f140633dcb6d8a3eda953bf7a214dc4eff598413c070bcdc117cbc"
+PKG_VERSION="3.3.3"
+PKG_SHA256="928ae7e20211f3b6f3915a72f06a0cfd29ab9d24279dd6346b6b1a7146397d36"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pypi.org/project/pyparsing/"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME,,}-${PKG_VERSION}.tar.gz"
