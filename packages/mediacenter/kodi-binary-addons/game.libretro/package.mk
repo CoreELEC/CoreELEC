827acd0861b32d29d5aa1e8ce25cd7de0893a35f
Garrett Brown <themagnificentmrb@gmail.com>
2026-09-22 20:35:49 -0700

    game.libretro: Update to v22.9.1

--- packages/mediacenter/kodi-binary-addons/game.libretro/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro"
-PKG_VERSION="22.7.0-Piers"
-PKG_SHA256="f034a5400f5b210abaac3e692741b3c8bd02a57e050dc8f3df846bb4e19c1ec0"
-PKG_REV="2"
+PKG_VERSION="22.9.1-Piers"
+PKG_SHA256="c5fa4bf21efa7599cce35dc6a6f4b3f4c279c7d6edc4d90c6262d9a8f5b2727d"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/kodi-game/game.libretro"
