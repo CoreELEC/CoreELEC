98ab77a6d7dbf2d4b46ee398ad84684d9070b92a
Portisch <hugo.portisch@yahoo.de>
2026-09-24 16:19:03 +0200

    common_drivers: bump package to cc3de300

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="dc8d2939a7b0204e9b54a3234b8edd338b17e1bd"
+PKG_VERSION="cc3de300e3c6c7c4ec626a0d8c9250cabf5cadbf"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
